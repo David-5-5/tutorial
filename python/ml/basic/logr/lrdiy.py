0d68b37d11906fedceb7d058d956729855b283da
luming <luming@chinamobile.com>
2023-01-04 18:22:19 +0800

    Modify logistic regression diy

--- python/ml/basic/logr/lrdiy.py
+++ python/ml/basic/logr/lrdiy.py
@@ -13,3 +13,14 @@ def loadCSV(filename):
 		for i in range(len(dataset)):
 			dataset[i] = [float(x) for x in dataset[i]]	
 	return np.array(dataset)
+
+def normalize(X): 
+    min = X.min
+
+if __name__ == "__main__":
+	# load the dataset
+	dataset = loadCSV('./python/ml/basic/logr/dataset1.csv')
+	X = dataset[:,0:-1]
+	Y = dataset[:, -1:]
+	X = normalize(X)
+    

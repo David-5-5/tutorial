867411cf09bbdf05a9176b5d2da07f4fd2d76398
luming <luming@chinamobile.com>
2023-01-05 19:19:06 +0800

    Modify logistic regression

--- python/ml/basic/logr/lrdiy.py
+++ python/ml/basic/logr/lrdiy.py
@@ -14,13 +14,42 @@ def loadCSV(filename):
 			dataset[i] = [float(x) for x in dataset[i]]	
 	return np.array(dataset)
 
-def normalize(X): 
-    min = X.min
+
+
+def normalize(X):
+	'''
+	function to normalize feature matrix, X
+	'''
+	mins = np.min(X, axis=0)
+	maxs = np.max(X, axis=0)
+	norm_x = 1 - (maxs - X)/(maxs-mins)
+	return norm_x
+
+def logistic_func(beta, X):
+	return 1 / (1 + np.exp(np.dot(X, beta.T)))
+
+
+def grad_desc(beta, X , Y):
+    l = 0.01
+    logic = logistic_func(beta, X)
+    
+
 
 if __name__ == "__main__":
 	# load the dataset
 	dataset = loadCSV('./python/ml/basic/logr/dataset1.csv')
-	X = dataset[:,0:-1]
+
+	# normalizing feature matrix
+	X = normalize(dataset[:,0:-1])
+
+	# response vector	
 	Y = dataset[:, -1:]
-	X = normalize(X)
     
+	# stacking columns with all ones in feature matrix
+	# add coef of x0
+	X = np.hstack((np.matrix(np.ones(X.shape[0])).T, X))
+
+	# The initial of beta for x
+	beta = np.matrix(np.zeros(X.shape[1]))
+ 	
+	grad_desc(beta, X , Y)

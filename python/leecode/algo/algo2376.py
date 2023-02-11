ee4e280e5f8dfee7c6eb401c362906f6cc32d0c8
luming <luming@chinamobile.com>
2023-02-11 23:56:59 +0800

    Add algo 2376

--- python/leecode/algo/algo2376.py
+++ python/leecode/algo/algo2376.py
@@ -1,3 +1,38 @@
 class Solution:
     def countSpecialNumbers(self, n: int) -> int:
+        if n < 10: return n
         
+        sn = str(n)
+        bits = len(sn)
+        full = [0] * (bits-1)
+        ff = [1] * (bits)
+        for i in range(len(full)):
+            full[i] = 9
+            for j in range(i):
+                full[i] = full[i] * (9 - j)
+        
+        for i in range(1, bits):
+            ff[i] = ff[i-1] * (10 - (bits - i))
+
+       
+        ret = 0
+        for i in range(bits-1):
+            ret += full[i]
+        
+        procs = set()
+        for i in range(bits):
+            if len(procs) < i: break
+            digit = int(sn[i]) - 1
+            exclude = 0
+            for proc in procs:
+                if proc <= digit:
+                    exclude += 1
+            digit = (digit - exclude + (1 if i else 0))
+            ret += (digit if digit > -1 else 0) * ff[bits - i - 1]
+            procs.add(int(sn[i]))
+        if len(procs) == bits:ret +=1
+        return ret
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.countSpecialNumbers(1425))

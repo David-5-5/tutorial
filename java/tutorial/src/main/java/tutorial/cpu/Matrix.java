7b279603439c2785281ef63228ff601a61bbd8a6
luming <luming@chinamobile.com>
2023-03-07 19:40:05 +0800

    Add multiple of matrix

---
+++ java/tutorial/src/main/java/tutorial/cpu/Matrix.java
@@ -0,0 +1,50 @@
+package tutorial.cpu;
+
+public class Matrix {
+
+    public static long[][] multipleA(int[][] A, int[][] B) {
+        int rank = A.length;
+        long[][] result = new long[rank][rank];
+
+        for (int i=0; i<rank; i++){
+            for (int j=0; j<rank; j++){
+                long sum = 0;
+                for (int k=0; k<rank; k++){
+                    sum += A[i][k] * B[k][j];
+                }
+                result[i][j] = sum;
+            }
+        }
+        return result;
+    }
+
+    public static long[][] multipleB(int[][] A, int[][] B) {
+        int rank = A.length;
+        long[][] result = new long[rank][rank];
+
+        for (int j=0; j<rank; j++){
+            for (int k=0; k<rank; k++){
+                long rB = B[k][j];
+                for (int i=0; i<rank; i++) {
+                    result[i][j] += A[i][k] * rB;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static long[][] multipleC(int[][] A, int[][] B) {
+        int rank = A.length;
+        long[][] result = new long[rank][rank];
+
+        for (int k=0; k<rank; k++){
+            for (int i=0; i<rank; i++) {
+                long rA = A[i][k];
+                for (int j=0; j<rank; j++) {
+                    result[i][j] += B[k][j] * rA;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ java/tutorial/src/main/java/tutorial/cpu/MatrixBenchmark.java
@@ -0,0 +1,58 @@
+package tutorial.cpu;
+
+import java.io.IOException;
+import java.util.Random;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+@Fork(2)
+@BenchmarkMode(Mode.AverageTime)
+@State(Scope.Benchmark)
+// 预热3次
+@Warmup(iterations = 3, time = 1)
+// 循环 10 次
+@Measurement(iterations = 10, time = 1)
+public class MatrixBenchmark {
+    private static int[][] A = new int[1000][1000];
+    private static int[][] B = new int[1000][1000];
+
+
+    @Setup
+    public void setUp() {
+        Random random = new Random();
+        for (int i=0; i<1000; i++) {
+            for (int j=0; j<1000; j++) {
+                A[i][j] = random.nextInt();
+                B[i][j] = random.nextInt();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        org.openjdk.jmh.Main.main(args);
+    }
+
+    @Benchmark
+    public void testMultipleA() {
+        Matrix.multipleA(A, B);
+    }
+    
+    @Benchmark
+    public void testMultipleB() {
+        Matrix.multipleB(A, B);
+    }
+
+    @Benchmark
+    public void testMultipleC() {
+        Matrix.multipleC(A, B);
+    }
+}
+

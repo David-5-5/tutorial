c73dc591c5d10383acfcfe75a3e1062f0d33f474
luming <luming@192.168.42.132>
2023-03-15 00:15:29 +0800

    Add comment

--- python/leecode/algo/algo1923.py
+++ python/leecode/algo/algo1923.py
@@ -1,3 +1,6 @@
 class Solution:
     def longestCommonSubpath(self, n: int, paths) -> int:
-
+        '''
+        !!!!It's too hard to READ!!!!!!
+        The official solution needs to be further understood
+        '''

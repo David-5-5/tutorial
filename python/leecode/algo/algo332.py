6656d7391d7ceb2ed787a61ba7d69fcff44000c7
luming <luming@192.168.42.132>
2023-04-19 01:45:57 +0800

    Update algo332 wiht Hierholzer

--- python/leecode/algo/algo332.py
+++ python/leecode/algo/algo332.py
@@ -1,8 +1,13 @@
 class Solution:
     def findItinerary(self, tickets):
-        left_edges = len(tickets)
+        '''
+        Hierholzer 算法
+        '''
+        '''
+        Hierholzer 算法
+        '''
         graph = {}
-        
+
         from bisect import bisect_right
         
         for fromi, toi in tickets:
@@ -11,20 +16,19 @@ class Solution:
             else:
                 inx = bisect_right(graph[fromi], toi)
                 graph[fromi].insert(inx, toi)
-        ret = ['JFK']
-        while left_edges:
-            cur = 0
-            while (graph[ret[-1]][cur] not in graph.keys() or len(graph[graph[ret[-1]][cur]])==0) \
-                and cur < len(graph[graph[ret[-1]][cur]])==0 and left_edges>1:
-                cur += 1
-            next = graph[ret[-1]].pop(cur)
-            ret.append(next)
-            
-            left_edges -= 1
-        return ret
+        
+        def dfs(vertex):
+            while vertex in graph.keys() and graph[vertex]:
+                child = graph[vertex].pop(0)
+                dfs(child)
+            ret.append(vertex)
+
+        ret = []
+        dfs('JFK')
+        return ret[::-1]
 
 if __name__ == "__main__":
     sol = Solution()
-    # tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]
-    tickets = [["EZE","TIA"],["EZE","HBA"],["AXA","TIA"],["JFK","AXA"],["ANU","JFK"],["ADL","ANU"],["TIA","AUA"],["ANU","AUA"],["ADL","EZE"],["ADL","EZE"],["EZE","ADL"],["AXA","EZE"],["AUA","AXA"],["JFK","AXA"],["AXA","AUA"],["AUA","ADL"],["ANU","EZE"],["TIA","ADL"],["EZE","ANU"],["AUA","ANU"]]
+    tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+    # tickets = [["EZE","TIA"],["EZE","HBA"],["AXA","TIA"],["JFK","AXA"],["ANU","JFK"],["ADL","ANU"],["TIA","AUA"],["ANU","AUA"],["ADL","EZE"],["ADL","EZE"],["EZE","ADL"],["AXA","EZE"],["AUA","AXA"],["JFK","AXA"],["AXA","AUA"],["AUA","ADL"],["ANU","EZE"],["TIA","ADL"],["EZE","ANU"],["AUA","ANU"]]
     print(sol.findItinerary(tickets))

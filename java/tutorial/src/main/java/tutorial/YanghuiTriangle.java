8cd41f3f89b2e20faf2242fd8456641cc712feab
luming <luming@chinamobile.com>
2023-04-24 16:54:42 +0800

    Add odd count of yanghui triangle

---
+++ java/tutorial/src/main/java/tutorial/YanghuiTriangle.java
@@ -0,0 +1,15 @@
+package tutorial;
+
+import java.util.Scanner;
+
+public class YanghuiTriangle {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt() - 1;
+
+        System.out.println((int)Math.pow(2, Integer.bitCount(n)));
+    }
+
+
+}

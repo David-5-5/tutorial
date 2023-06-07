406a6bad6f33c8f072b415d1d493407312b8c764
luming <luming@chinamobile.com>
2023-06-07 21:33:35 +0800

    Add simple bfs and dfs of graph

---
+++ python/dsaa/graph/traverse.py
@@ -0,0 +1,100 @@
+# Python3 Program to print BFS traversal
+# from a given source vertex. BFS(int s)
+# traverses vertices reachable from s.
+ 
+from collections import defaultdict
+ 
+ 
+# This class represents a directed graph
+# using adjacency list representation
+class Graph:
+ 
+    # Constructor
+    def __init__(self):
+ 
+        # Default dictionary to store graph
+        self.graph = defaultdict(list)
+ 
+    # Function to add an edge to graph
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+ 
+    # Function to print a BFS of graph
+    def BFS(self, s):
+ 
+        # Mark all the vertices as not visited
+        visited = [False] * (max(self.graph) + 1)
+ 
+        # Create a queue for BFS
+        queue = []
+ 
+        # Mark the source node as
+        # visited and enqueue it
+        queue.append(s)
+        visited[s] = True
+ 
+        while queue:
+ 
+            # Dequeue a vertex from
+            # queue and print it
+            s = queue.pop(0)
+            print(s, end=" ")
+ 
+            # Get all adjacent vertices of the
+            # dequeued vertex s. If a adjacent
+            # has not been visited, then mark it
+            # visited and enqueue it
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
+    # prints all not yet visited vertices reachable from s
+    def DFS(self,s):            # prints all vertices in DFS manner from a given source.
+                                # Initially mark all vertices as not visited
+        visited = [False for i in range(len(self.graph))]
+ 
+        # Create a stack for DFS
+        stack = []
+ 
+        # Push the current source node.
+        stack.append(s)
+ 
+        while stack:
+            # Pop a vertex from stack and print it
+            s = stack[-1]
+            stack.pop()
+ 
+            # Stack may contain same vertex twice. So
+            # we need to print the popped item only
+            # if it is not visited.
+            if (not visited[s]):
+                print(s,end=' ')
+                visited[s] = True
+ 
+            # Get all adjacent vertices of the popped vertex s
+            # If a adjacent has not been visited, then push it
+            # to the stack.
+            for node in self.adj[s]:
+                if (not visited[node]):
+                    stack.append(node)
+ 
+# Driver code
+if __name__ == '__main__':
+ 
+    # Create a graph given in
+    # the above diagram
+    g = Graph()
+    g.addEdge(0, 1)
+    g.addEdge(0, 2)
+    g.addEdge(1, 2)
+    g.addEdge(2, 0)
+    g.addEdge(2, 3)
+    g.addEdge(3, 3)
+ 
+    print("Following is Breadth First Traversal"
+          " (starting from vertex 2)")
+    g.BFS(2)
+    g.DFS(2)
+ 
+# This code is contributed by Neelam Yadav

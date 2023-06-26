66a741d6ba54f1b64b6681c23cf29d476ebe4b88
luming <luming@chinamobile.com>
2023-06-26 14:21:13 +0800

    Update algo 1163 with success

--- python/algo/leecode/algo1163.py
+++ python/algo/leecode/algo1163.py
@@ -2,6 +2,7 @@ class Solution:
     def lastSubstring(self, s: str) -> str:
         ret = []
         for let in s:
+            # 找出字典序最大字母开头的子串
             if not ret:
                 ret.append(let)
             else:
@@ -9,13 +10,16 @@ class Solution:
                     ret = [let]
                 else:
                     ret.append(let)
-        result = ''.join(ret)
+        candidate = ''.join(ret)
         
-        inx = ret.index(ret[0], 1) if ret[0] in ret[1:] else -1
-        while inx != -1:
-            result = max(result, ''.join(ret[inx:]))
-            inx = ret.index(ret[0], inx+1) if ret[0] in ret[inx+1:] else -1
-            
+        result = candidate
+        for i in range(0, len(candidate)-1):
+            if candidate[i] < candidate[0] and candidate[i+1] == candidate[0]:
+                # 候选字串中，相同最大字典序字母在中间出现后的子串，要去前一个字符的字典序小于最大序字母
+                # 类如y为最大字典序字母，yuyx，yuyx子串比较yx子串的大小
+                # 若子串为yyyx，yyy开头的子串一定是最大字典序的，则无候选子串可供比较
+                result = max(result, candidate[i+1:])
+
         return result
         
 

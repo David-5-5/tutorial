8191916df4d45f8e55d50d5736a86fa800e9bd9e
luming <luming@chinamobile.com>
2023-07-01 16:05:24 +0800

    Updte with official solution

--- python/algo/leecode/algo1125.py
+++ python/algo/leecode/algo1125.py
@@ -43,10 +43,50 @@ class Solution:
             candidate = addPeople(candidate)
 
 
+    def smallestSufficientTeam2(self, req_skills, people) :
+        n = len(req_skills)
+
+        dp = [None] * (1 << n)
+        dp[0] = []
+        for i, q in enumerate(people):
+            cur_skill = 0
+            for skill in q:
+                cur_skill |= 1 << req_skills.index(skill)
+            
+            for prev in range(1 << n):
+                if dp[prev] is None:
+                    continue
+                comb = prev | cur_skill
+                if dp[comb] is None or len(dp[comb]) > len(dp[prev]) + 1:
+                    dp[comb] = [i] + dp[prev]
+
+        return dp[(1 << n) -1]
+
+    def smallestSufficientTeam3(self, req_skills, people) :
+        n = len(req_skills)
+
+        # 参考官方题解，将1<<n长度list改为dict
+        dp = {}
+        dp[0] = []
+        for i, q in enumerate(people):
+            cur_skill = 0
+            for skill in q:
+                cur_skill |= 1 << req_skills.index(skill)
+            
+            prevs = list(dp.keys())
+            # 避免在稀疏的1<<n长度的list表中遍历
+            for prev in prevs:
+                comb = prev | cur_skill
+                if not (comb in dp.keys()) or len(dp[comb]) > len(dp[prev]) + 1:
+                    dp[comb] = [i] + dp[prev]
+
+        return dp[(1 << n) -1]
+
+
 if __name__ == "__main__":
     sol = Solution()
     req_skills = ["java","nodejs","reactjs"]
     people = [["java"],["nodejs"],["nodejs","reactjs"]]
     req_skills = ["gkgtfxpvxnxlbhxu","baayobms","yomqrposuurmvisx","vsftojpcp","pjovtkytubbouq","hlmvucyi","chpzrslbtd","uighcqpiteg"]
     people = [["pjovtkytubbouq"],["baayobms"],["baayobms","pjovtkytubbouq"],["chpzrslbtd"],[],["yomqrposuurmvisx"],["uighcqpiteg"],[],["gkgtfxpvxnxlbhxu","yomqrposuurmvisx","uighcqpiteg"],[],["pjovtkytubbouq","uighcqpiteg"],["yomqrposuurmvisx"],[],[],[],["pjovtkytubbouq"],["gkgtfxpvxnxlbhxu","yomqrposuurmvisx","pjovtkytubbouq","uighcqpiteg"],["yomqrposuurmvisx","pjovtkytubbouq"],["chpzrslbtd"],["pjovtkytubbouq"],[],[],["baayobms"],["yomqrposuurmvisx","chpzrslbtd"],["yomqrposuurmvisx","pjovtkytubbouq"],["pjovtkytubbouq"],[],["yomqrposuurmvisx"],[],["yomqrposuurmvisx","pjovtkytubbouq"],[],[],[],["yomqrposuurmvisx"],[],["pjovtkytubbouq"],[],[],[],["vsftojpcp"],[],["yomqrposuurmvisx","pjovtkytubbouq"],[],[],["gkgtfxpvxnxlbhxu"],["yomqrposuurmvisx"],["pjovtkytubbouq","chpzrslbtd"],["uighcqpiteg"],["pjovtkytubbouq"],["yomqrposuurmvisx","pjovtkytubbouq","uighcqpiteg"],[],["pjovtkytubbouq"],[],["chpzrslbtd"],["hlmvucyi"],["yomqrposuurmvisx","pjovtkytubbouq"],["pjovtkytubbouq"],[],["yomqrposuurmvisx"],[]]
-    print(sol.smallestSufficientTeam(req_skills, people))
+    print(sol.smallestSufficientTeam3(req_skills, people))

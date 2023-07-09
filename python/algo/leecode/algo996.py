938986685927fdc67a5ed89585cd57fa09647276
luming <luming@chinamobile.com>
2023-07-09 19:43:37 +0800

    Update algo with dfs

--- python/algo/leecode/algo996.py
+++ python/algo/leecode/algo996.py
@@ -1,47 +1,38 @@
+from functools import lru_cache
+import collections
+import math
 class Solution:
     def numSquarefulPerms(self, nums) -> int:
+        n = len(nums)
+
         def isSquare(val : int) -> bool:
             if val == int(val ** .5) ** 2:
                 return True
             else:
                 return False
         
-        nums.sort()
-        if len(nums) == 1 and isSquare(nums):
-            return 1
-        elif len(nums) == 1:
-            return 0
-        
-        if nums[0] == nums[-1] and isSquare(nums[0]+nums[-1]):
-            return 1
-        elif nums[0] == nums[-1]:
-            return 0
-
-        edges = []
-        for i in range(len(nums)-1):
-            for j in range(i, len(nums)):
-                if isSquare(nums[i] + nums[j]):
-                    edges.add((i,j))
-        
-
-        graph = {}
-        def put(u, v):
-            if u not in graph.keys():
-                graph[u] = [v]
-            else:
-                graph[u].append(v)
-            if v not in graph.keys():
-                graph[v] = [u]
-            else:
-                graph[v].append(u)
-
-        for u, v in edges:
-            put(u, v)        
-
-        if len(graph.keys) < len(nums) and len(edges) < len(nums)-1: return 0
-
-        result = set()
+        @lru_cache(maxsize=None)
         def dfs(i, visited):
-            if i not in visited:
-                visited.append(i)
-                if len(visited)
+            if visited == (1 << n) - 1:
+                return 1
+            
+            count = 0
+            for j in range(n):
+                if i != j and isSquare(nums[i]+nums[j]) and visited & 1 << j == 0:
+                    count += dfs(j, visited | 1 << j)
+            return count
+
+        count = 0 
+        for i in range(n):
+            count += dfs(i, 1 << i)
+        
+        distinct = collections.Counter(nums)
+        for v in distinct.values():
+            count //= math.factorial(v)
+
+        return count
+    
+if __name__ == "__main__":
+    sol = Solution()
+    s = [1,17,8]
+    print(sol.numSquarefulPerms(s))

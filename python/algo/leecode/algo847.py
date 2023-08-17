1b8db0113b7f1e73cd6dcc20dcf30dfd27fdbf86
luming <luming@192.168.72.128>
2023-08-18 02:27:22 +0800

    Reference to official solution

--- python/algo/leecode/algo847.py
+++ python/algo/leecode/algo847.py
@@ -53,59 +53,55 @@ class Solution:
 
     def shortestPathLength2(self, graph) -> int:
         n = len(graph)
-        degree = [len(graph[i]) for i in range(n)]
-        def next(u, left, visited):
-            ret = -1
-            degree = 12
-            for v in graph[u]:
-                if v not in visited and left[v] < degree:
-                    ret = v
-                    degree = left[v]
-            return ret
+        dp = [float("inf")] * (1 << n)
         
-        def dfs(u, left, visited, bt, path):
-            visited.append(u)
-            path.append(u)
-            v = next(u, left, visited) 
-            while v != -1:
-                left[u] -= 1
-                left[v] -= 1
-                dfs(v, left, visited, bt, path)
-                if bt[v]:
-                    mid = False
-                    for i in range(bt[v][-1]):
-                        if path[-1-i] in graph[u]:
-                            bt[u].append(1 + i)
-                            mid = True
-                            break
-                    if not mid: bt[u].append(bt[v][-1] + 1)
-                else:
-                    bt[u].append(1)
-                v = next(u, left, visited) 
-        min_dg = min(degree)
-        begins = []
-        for i in range(n):
-            if min_dg == degree[i]:
-                begins.append(i)
-        ans = float("inf")
-        for u in begins:
-            bt = [[] for _ in range(n)]
-            leftd = degree.copy()
-            dfs(u, leftd, [], bt, [])
-            cnt = 0
-            for i in range(n):
-                bt[i].sort()
-                for j in range(len(bt[i])-1):
-                    cnt += bt[i][j]
-            ans = min(ans, cnt + n -1)
+        def bfs(visited):
+            while visited:
+                u, vexs, edges = visited.pop(0)
+                for i in graph[u]:
+                    if (1 << i) & vexs == 0:
+                        dp[vexs|1<<i] = min(dp[vexs|1<<i], dp[vexs] + 1)
+                        visited.append((i,vexs|1<<i, edges+[(u,i)]))
+                    elif vexs < (1 << n)-1 and (u,i) not in edges:
+                        dp[vexs] += 1
+                        visited.append((i,vexs, edges+[(u,i)]))
 
+        for i in range(n):
+            dp[1 << i] = 0
+            bfs([(i,1 << i,[])])
+        
+        return dp[(1 << n) -1]
+
+
+    def shortestPathLength3(self, graph) -> int:
+        n = len(graph)
+
+        q = [(i, 1 << i, 0) for i in range(n)]
+        seen = {(i, 1 << i) for i in range(n)}
+        ans = 0
+        
+        while q:
+            u, mask, dist = q.pop(0)
+            if mask == (1 << n) - 1:
+                ans = dist
+                break
+            # 搜索相邻的节点
+            for v in graph[u]:
+                # 将 mask 的第 v 位置为 1
+                mask_v = mask | (1 << v)
+                if (v, mask_v) not in seen:
+                    q.append((v, mask_v, dist + 1))
+                    seen.add((v, mask_v))
+        
         return ans
+
 
 if __name__ == "__main__":
     sol = Solution()
-    graph = [[1],[0,2,4],[1,3,4],[2],[1,2]]
-    graph = [[2,6],[2,3],[0,1],[1,4,5,6,8],[3,9,7],[3],[3,0],[4],[3],[4]]
-    graph = [[1,2,3,4],[0,2],[0,1],[0,5],[0,6],[3],[4]]
-    graph = [[6,9],[6,8],[6,7],[6,10],[8],[10],[0,1,2,3,8],[2,8,9],[1,4,6,7],[0,7,10],[3,5,9]]
+    graph = [[1,2,3],[0],[0],[0]]
+    # graph = [[1],[0,2,6],[1,3],[2],[5],[4,6],[1,5,7],[6]]
+    # graph = [[2,6],[2,3],[0,1],[1,4,5,6,8],[3,9,7],[3],[3,0],[4],[3],[4]]
+    # graph = [[1,2,3,4],[0,2],[0,1],[0,5],[0,6],[3],[4]]
+    # graph = [[6,9],[6,8],[6,7],[6,10],[8],[10],[0,1,2,3,8],[2,8,9],[1,4,6,7],[0,7,10],[3,5,9]]
     print(sol.shortestPathLength(graph))
-    print(sol.shortestPathLength2(graph))
+    print(sol.shortestPathLength3(graph))

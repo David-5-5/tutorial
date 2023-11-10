854cbe81e5682b7f461fd5fd4dc2e3c3e72bc1cf
luming <luming@192.168.72.128>
2023-11-11 02:01:07 +0800

    Add algo212

--- python/algo/leecode2/algo212.py
+++ python/algo/leecode2/algo212.py
@@ -1,5 +1,63 @@
+from bisect import bisect_right
+
 class Solution:
     def findWords(self, board, words) :
-        words.sort()
-        n, m = len(board), len(board[0])
+        '''
+        总体思路：
+        从board中任一点出发, 遍历board中的节点
+        '''
         
+        '排序, 供后续bisect_right方法快速查找'
+        words.sort()
+        
+        n, m = len(board), len(board[0])
+        ans = set()
+
+        def concate(visited):
+            '''
+            将遍历的节点, 根据坐标, 组合成word
+            '''
+            s = ""
+            for p in visited:
+                x, y = p // m, p % m
+                s += board[x][y]
+            return s
+
+        def find(visited):
+            word = concate(visited)
+            inx = bisect_right(words, word)
+            if inx > 0 and words[inx-1] == word:
+                '一个word仅加入一次, 匹配后即删除, 避免重复和冗余查找'
+                del words[inx-1]
+                if word not in ans : ans.add(word)
+            elif len(word) == 10 or inx == len(words) or words[inx].find(word) != 0:
+                return
+
+            nextInx = len(visited)
+            '下一个填充位，遍历'
+            visited.append(-1)
+            x, y = visited[nextInx-1] // m, visited[nextInx-1] % m
+            for _x, _y in [(0,1),(0,-1),(-1,0),(1,0)]:
+                if 0 <= x+_x < n and 0 <= y+_y < m and (x+_x)*m+y+_y not in visited:
+                    visited[-1] = (x+_x)*m+y+_y
+                    find(visited)
+            '删除剪枝，供后续回溯查找'
+            del visited[nextInx:]
+
+
+        for i in range(n):
+            for j in range(m):
+                find([i*m+j])
+        
+        return list(ans)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]]
+    words = ["oath","pea","eat","rain"]
+    board = [["o","a","b","n"],["o","t","a","e"],["a","h","k","r"],["a","f","l","v"]]
+    words = ["oa","oaa"]
+    board = [["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"],["a","a","a","a","a","a","a","a","a","a","a","a"]]
+    words = ["lllllll","fffffff","ssss","s","rr","xxxx","ttt","eee","ppppppp","iiiiiiiii","xxxxxxxxxx","pppppp","xxxxxx","yy","jj","ccc","zzz","ffffffff","r","mmmmmmmmm","tttttttt","mm","ttttt","qqqqqqqqqq","z","aaaaaaaa","nnnnnnnnn","v","g","ddddddd","eeeeeeeee","aaaaaaa","ee","n","kkkkkkkkk","ff","qq","vvvvv","kkkk","e","nnn","ooo","kkkkk","o","ooooooo","jjj","lll","ssssssss","mmmm","qqqqq","gggggg","rrrrrrrrrr","iiii","bbbbbbbbb","aaaaaa","hhhh","qqq","zzzzzzzzz","xxxxxxxxx","ww","iiiiiii","pp","vvvvvvvvvv","eeeee","nnnnnnn","nnnnnn","nn","nnnnnnnn","wwwwwwww","vvvvvvvv","fffffffff","aaa","p","ddd","ppppppppp","fffff","aaaaaaaaa","oooooooo","jjjj","xxx","zz","hhhhh","uuuuu","f","ddddddddd","zzzzzz","cccccc","kkkkkk","bbbbbbbb","hhhhhhhhhh","uuuuuuu","cccccccccc","jjjjj","gg","ppp","ccccccccc","rrrrrr","c","cccccccc","yyyyy","uuuu","jjjjjjjj","bb","hhh","l","u","yyyyyy","vvv","mmm","ffffff","eeeeeee","qqqqqqq","zzzzzzzzzz","ggg","zzzzzzz","dddddddddd","jjjjjjj","bbbbb","ttttttt","dddddddd","wwwwwww","vvvvvv","iii","ttttttttt","ggggggg","xx","oooooo","cc","rrrr","qqqq","sssssss","oooo","lllllllll","ii","tttttttttt","uuuuuu","kkkkkkkk","wwwwwwwwww","pppppppppp","uuuuuuuu","yyyyyyy","cccc","ggggg","ddddd","llllllllll","tttt","pppppppp","rrrrrrr","nnnn","x","yyy","iiiiiiiiii","iiiiii","llll","nnnnnnnnnn","aaaaaaaaaa","eeeeeeeeee","m","uuu","rrrrrrrr","h","b","vvvvvvv","ll","vv","mmmmmmm","zzzzz","uu","ccccccc","xxxxxxx","ss","eeeeeeee","llllllll","eeee","y","ppppp","qqqqqq","mmmmmm","gggg","yyyyyyyyy","jjjjjj","rrrrr","a","bbbb","ssssss","sss","ooooo","ffffffffff","kkk","xxxxxxxx","wwwwwwwww","w","iiiiiiii","ffff","dddddd","bbbbbb","uuuuuuuuu","kkkkkkk","gggggggggg","qqqqqqqq","vvvvvvvvv","bbbbbbbbbb","nnnnn","tt","wwww","iiiii","hhhhhhh","zzzzzzzz","ssssssssss","j","fff","bbbbbbb","aaaa","mmmmmmmmmm","jjjjjjjjjj","sssss","yyyyyyyy","hh","q","rrrrrrrrr","mmmmmmmm","wwwww","www","rrr","lllll","uuuuuuuuuu","oo","jjjjjjjjj","dddd","pppp","hhhhhhhhh","kk","gggggggg","xxxxx","vvvv","d","qqqqqqqqq","dd","ggggggggg","t","yyyy","bbb","yyyyyyyyyy","tttttt","ccccc","aa","eeeeee","llllll","kkkkkkkkkk","sssssssss","i","hhhhhh","oooooooooo","wwwwww","ooooooooo","zzzz","k","hhhhhhhh","aaaaa","mmmmm"]
+    print(sol.findWords(board, words))

aaf98f9dd79b6ccb1f8a4e9d79b4cd3b0727a986
luming <luming@192.168.72.128>
2023-11-15 22:22:55 +0800

    Compare different implement

--- python/algo/leecode/algo316.py
+++ python/algo/leecode/algo316.py
@@ -5,6 +5,7 @@ class Solution:
         for i in range(n):
             if ret and s[i] in ret:
                 continue
+            # s.find 性能优化
             while ret and ret[-1] > s[i]:
                 if s.find(ret[-1], i) != -1:
                     ret.pop(-1)
@@ -12,6 +13,54 @@ class Solution:
                     break
             ret.append(s[i])
         return ''.join(ret)
+
+    def removeDuplicateLetters2(self, s: str) -> str:
+        """
+        Recommend
+        """
+        ret = []
+        n = len(s)
+        last = [-1] * 26
+
+        for i in range(n):
+            last[ord(s[i])-ord('a')] = i
+    
+        for i in range(n):
+            if ret and s[i] in ret:
+                continue
+            while ret and ret[-1] > s[i]:
+                if last[ord(ret[-1])-ord('a')] > i:
+                    ret.pop(-1)
+                else:
+                    break
+            ret.append(s[i])
+        return ''.join(ret)
+
+
+    def removeDuplicateLetters3(self, s: str) -> str:
+        '''
+        Customized find method
+        '''
+        ret = []
+        n = len(s)
+        def find(ch, i):
+            for inx in range(i+1, n):
+                if s[inx] == ch:
+                    return inx
+            return -1
+        
+        for i in range(n):
+            if ret and s[i] in ret:
+                continue
+            # Use customized find instead of find of str
+            while ret and ret[-1] > s[i]:
+                if find(ret[-1], i) != -1:
+                    ret.pop(-1)
+                else:
+                    break
+            ret.append(s[i])
+        return ''.join(ret)
+
 
 if __name__ == "__main__":
     sol = Solution()

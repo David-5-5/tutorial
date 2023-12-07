44ec7df7956937f460bac129aebcbc00f5ce8846
David-5-5 <13910767907@139.com>
2023-12-07 19:49:12 +0800

    Add algo 1438

---
+++ python/algo/leecode2/algo1438.py
@@ -0,0 +1,22 @@
+from sortedcontainers import SortedDict
+from typing import List
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        begin = 0
+        longest = 1
+        last = SortedDict()
+        for i in range(len(nums)):
+            while last and abs(last.peekitem(-1)[0] - nums[i])>limit:
+                _, inx = last.popitem(-1)
+                begin = max(inx+1, begin)
+            while last and abs(last.peekitem(0)[0] - nums[i])>limit:
+                _, inx = last.popitem(0)
+                begin = max(inx+1, begin)
+            last[nums[i]] = i
+            longest = max(longest, i-begin+1)
+        return longest
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums, limit = [10,1,2,4,7,2], 5
+    print(sol.longestSubarray(nums, limit))

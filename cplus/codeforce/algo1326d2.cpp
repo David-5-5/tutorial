c21d55f6f1ab3c13b78775d97202f825b8a1ece1
David-5-5 <13910767907@139.com>
2023-12-12 20:50:01 +0800

    Add algo 1326d2 in C++

---
+++ cplus/codeforce/algo1326d2.cpp
@@ -0,0 +1,85 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+string solution(string s) {
+    int n = s.length();
+    vector<int> d1(n), d2(n);
+
+    int l = 0, r = -1;
+    for (int i=0; i<n; i++) {
+        if (i <= r)
+            d1[i] = min(d1[l+r-i], r-i);
+        while (0 <= i - d1[i] and i + d1[i] < n and s[i - d1[i]] == s[i + d1[i]])
+            d1[i] += 1;
+        if (i + d1[i] > r)
+            l, r = i - d1[i], i + d1[i];
+    }
+    l = 0;
+    r = -1;
+    for (int i=0; i<n; i++) {
+        if (i <= r)
+            d2[i] = min(d2[l+r-i+1], r-i);
+        while (0 <= i - d2[i] - 1 and i + d2[i] < n and s[i - d2[i] - 1] == s[i + d2[i]])
+            d2[i] += 1;
+        if (i + d2[i] > r)
+            l, r = i - d2[i]-1, i + d2[i];
+    }
+
+    function<string(string, int, int)> find = [&](string s, int l, int r) {
+        if (l == r) return s.substr(l, 1);
+        if (l > r) return s.substr(l, 0);
+
+        int mid = l + (r-l+1) / 2, rem = (r-l+1) % 2;
+        if ((rem == 0 and d2[mid] + mid == r + 1) or (rem==1 and d1[mid]+mid == r+1))
+            return s.substr(l, r+1-l);
+        
+        int lmax = 1;
+        for (int i=l+1; i<=mid; i++) {
+            if (i - d2[i] <= l)
+                lmax = max(lmax, (i-l)*2);
+            if (i + 1 - d1[i] <= l and i<mid)
+                lmax = max(lmax, (i-l+1)*2-1);
+        }
+        int rmax = 1;
+        for (int i=r; i>=mid; i--) {
+            if (i>mid and i - 1 + d2[i] >= r)
+                rmax = max(rmax, (r-i+1)*2);
+            if (i -1 + d1[i] >= r)
+                rmax = max(rmax, (r-i+1)*2-1);
+        }
+        string midstr = "";
+        int count = 0;
+        while (s[l+count] == s[r-count])
+            count += 1;
+        if (count > 0)
+            midstr = s.substr(l, count) + find(s, l+count, r-count) + s.substr(r-count+1, count);
+        
+        int midlen = midstr.length();
+        int maxf = max(max(lmax, rmax), midlen);
+        if (maxf == lmax)
+            return s.substr(l, lmax);
+        else if (maxf == rmax)
+            return s.substr(r-rmax+1, rmax);
+        else
+            return midstr;
+    };
+    return find(s, 0, n-1);
+    
+}
+
+int main() {
+    int total;
+    cin >> total;
+    for (int i = 0; i<total; i++) {
+        string input;
+        cin >> input;
+        cout << solution(input) << endl;
+    }
+    // cout << solution("abcdfdcecba") << endl;
+    // cout << solution("abbaxyzyx") << endl;
+    // cout << solution("codeforces") << endl;
+}

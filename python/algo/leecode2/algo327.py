3b56a383e73c9218ad81c0040bede62ea8d2c3ac
David-5-5 <13910767907@139.com>
2024-02-10 17:17:22 +0800

    Using SortedList replace of list and bisect Remove pre-caculated presum by calcuate step by step

--- python/algo/leecode2/algo327.py
+++ python/algo/leecode2/algo327.py
@@ -1,22 +1,21 @@
 from typing import List
-from itertools import accumulate
-from bisect import bisect_left, bisect_right
+from sortedcontainers import SortedList
 class Solution:
     def countRangeSum(self, nums: List[int], lower: int, upper: int) -> int:
         n = len(nums)
-        pres = list(accumulate(nums, initial=0))
-        sort_pres = [0]
+        sort_pres = SortedList()
+        presum = ans = 0
 
-        ans = 0
-        for i in range(1, n+1):
-            left = bisect_left(sort_pres, pres[i]-upper)
-            right = bisect_right(sort_pres, pres[i]-lower)
+        for i in range(n):
+            sort_pres.add(presum)
+            presum += nums[i]
+
+            left = sort_pres.bisect_left(presum-upper)
+            right = sort_pres.bisect_right(presum-lower)
             ans += right - left
 
-            inx = bisect_right(sort_pres, pres[i])
-            sort_pres.insert(inx, pres[i])
-        
         return ans
+
 
 if __name__ == "__main__":
     sol = Solution()

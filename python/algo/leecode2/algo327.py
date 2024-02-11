fe00e57afae6dc3614f9f5cc106db4a63d7cf805
David-5-5 <13910767907@139.com>
2024-02-11 22:12:30 +0800

    Revise solution of open piont segment tree without finished

--- python/algo/leecode2/algo327.py
+++ python/algo/leecode2/algo327.py
@@ -78,8 +78,46 @@ class Solution:
 
         return ans
 
+    def countRangeSum_dop_segtree(self, nums: List[int], lower: int, upper: int) -> int:
+        '''dynamically open point segment tree
+        动态开点线段树
+        '''
+        from itertools import accumulate
+        psums = list(accumulate(nums, initial=0))
+        
+        # 二维数组，根节点为tree[0], 每个节点包含三个元素len(tree[i]) == 3
+        # tree[i][0] count, tree[i][1] left child, tree[i][2] right child
+        tree = []
+        def update(value:int, s:int, t:int, p:int):
+            tree[p] += 1
+            if s == t:
+                return
+            m = s + ((t - s) >> 1)
+            if value <= m:
+                update(value, s, m, p*2+1)
+            if value > m:
+                update(value, m + 1, t, p*2+2)
+        
+        def query(l, r, s, t, p):
+            if l <= s and t <= r:
+                return tree[p]
+            m = s + ((t - s) >> 1)
+            count = 0
+            if l <= m:
+                count = query(l, r, s, m, p * 2 + 1)
+            if r > m:
+                count = count + query(l, r, m + 1, t, p * 2 + 2)
+            return count
+
+        ans = 0
+        for i in range(len(psums)):
+            left, right = psums[i]-upper, psums[i]-lower
+            ans += query(left, right, 0, i, 0)
+            update(psums[i],0, i, 0)
+
+        return ans
 if __name__ == "__main__":
     sol = Solution()
     nums, lower, upper = [-2,5,-1], -2, 2
     print(sol.countRangeSum(nums, lower, upper))
-    print(sol.countRangeSum_segtree(nums, lower, upper))
+    print(sol.countRangeSum_dop_segtree(nums, lower, upper))

54c6a417ace28c2b7b7e3a21445e16abd77c5413
David-5-5 <13910767907@139.com>
2024-02-11 19:55:34 +0800

    Add segment tree solution without finished

--- python/algo/leecode2/algo327.py
+++ python/algo/leecode2/algo327.py
@@ -29,6 +29,55 @@ class Solution:
         return ans
     
 
+    def countRangeSum_segtree(self, nums: List[int], lower: int, upper: int) -> int:
+        '''segment tree
+        '''
+        from itertools import accumulate
+        psums = list(accumulate(nums, initial=0))
+        allNumber = set()
+        for psum in psums:
+            allNumber.add(psum)
+            allNumber.add(psum-lower)
+            allNumber.add(psum-upper)
+    
+        hvalue, initial  = {}, 0
+        for num in allNumber:
+            hvalue[num] = initial
+            initial += 1
+        
+        import math
+        tree = [-1] * (2 ** (math.ceil(math.log(len(allNumber), 2))+1) - 1)
+        def build(l:int, r:int, p:int):
+            if l == r:
+                tree[p] = 0
+            if r > l:
+                mid = (l + r) // 2
+                build(l, mid, p*2 +1)
+                build(mid+1, r, p*2 +2)
+                tree[p] = tree[p*2 + 1] + tree[p*2 + 2]
+        
+        def update(l:int, r:int, s:int, t:int, p:int):
+            if l <= s and t <= r:
+                tree[p] = 1
+                return
+            m = s + ((t - s) >> 1)
+            if l <= m:
+                update(l, r, s, m, p*2+1)
+            if r > m:
+                update(l, r, m + 1, t, p*2+2)
+            tree[p] = tree[p*2+1] + tree[p*2+2]
+        
+        def query(l, r, s, t, p):
+            if l <= s and t <= r:
+                return tree[p]
+            m = s + ((t - s) >> 1)
+            count = 0
+            if l <= m:
+                count = query(l, r, s, m, p * 2 + 1)
+            if r > m:
+                sum = sum + query(l, r, m + 1, t, p * 2 + 2)
+            return count
+        
 
 
 if __name__ == "__main__":

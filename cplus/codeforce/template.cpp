2bf173887a6ea179ad54d8dcd159d144af8be1e4
David-5-5 <13910767907@139.com>
2024-03-28 10:37:14 +0800

    Add template for codeforce

---
+++ cplus/codeforce/template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+string solution(string s){
+    return "enjoy!";
+}
+
+int main()
+{
+    int total;
+    cin >> total;
+    for (int i = 0; i < total; i++)
+    {
+        string input;
+        cin >> input;
+        cout << solution(input) << endl;
+    }
+}

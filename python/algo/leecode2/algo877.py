a0ba351d4662621e56937bddf1abcbc95ba6855e
David-5-5 <13910767907@139.com>
2024-04-06 17:10:35 +0800

    Update algo 877

--- python/algo/leecode2/algo877.py
+++ python/algo/leecode2/algo877.py
@@ -2,16 +2,15 @@ from typing import List
 from functools import lru_cache
 
 class Solution:
+    # The count of piles is even, the subscript from 0, ... n-1
+    # The plies could be divided into two groups: one group, subscript is even,
+    # the other group, subscript is odd.
+    # So Alice select 0/n-1, so the Bob select odd 1, n-1. or even 0, n-2
+    # So Alice compare the two group, select the large group, She alway win.
     def stoneGame(self, piles: List[int]) -> bool:
-        # return True
+        return True
 
-        @lru_cache(maxsize = None)
-        def stone(l:int, r:int):
-            if (l == r): return piles[l]
-
-            return max(piles[l]+stone(l+1,r), piles[r]+stone(l,r-1))
-
-        return stone(0, len(piles)-1)
+# The dp solution refer to 486
 
 if __name__ == "__main__":
     sol = Solution()

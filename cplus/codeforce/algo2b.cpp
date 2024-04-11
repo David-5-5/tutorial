86cd032592cf8e96ecf79546281e8f88c73d5a9e
David-5-5 <13910767907@139.com>
2024-04-12 00:28:11 +0800

    Calculate each value include count of facotr 2, 5

--- cplus/codeforce/algo2b.cpp
+++ cplus/codeforce/algo2b.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <map>
+
 using namespace std;
 
 string solution(vector<vector<int>> m)
-{
+{   
+    int n = m.size();
+
+    // include 2,5 count
+    map<int, int> inc2;
+    map<int, int> inc5;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (inc2.find(m[i][j])==inc2.end()) {
+                int count = 0;
+                int value = m[i][j];
+                while (value % 2 == 0) {
+                    value /= 2;
+                    count ++;
+                }
+                inc2[m[i][j]] = count;
+                count = 0;
+                while (value % 5 == 0) {
+                    value /= 5;
+                    count ++;
+                }
+                inc5[m[i][j]] = count;
+            }
+        }
+    }
+    
+
     return "ok";
 }
 

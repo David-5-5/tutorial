613cccb4b1ff4f428a333206b1d8e9569c488d0b
David-5-5 <13910767907@139.com>
2024-04-20 16:53:55 +0800

    Add algo 1563

---
+++ cplus/leetcode/algo1563.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int stoneGameV(vector<int>& stoneValue) {
+        int n = stoneValue.size();
+
+    }
+};
+
+
+int main() {
+    Solution solution;
+
+    vector<int> input{6,2,3,4,5,5};
+    solution.stoneGameV(input);
+
+    return 0;
+}

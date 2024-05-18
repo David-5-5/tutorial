90ef1af6cd79ac22760aebbdcc7ea946f9e16517
David-5-5 <13910767907@139.com>
2024-05-18 14:15:52 +0800

    Add pow by mod

--- cplus/dass/math/binpow.cpp
+++ cplus/dass/math/binpow.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+
+using namespace std;
+
 /**
  * Binary exponentiation, is a trick to calculate a^n in the time of O(logN),
  * while the calculation of brute force takes the time of O(N).
@@ -13,4 +17,28 @@ long long binpow(long long a, long long b)
         b >>= 1;
     }
     return res;
+}
+
+/**
+ * According to Fermat's theorem, if m is a prime number, 
+ * we can compute x^{n mod (m-1)} to speed up the algorithm
+ */
+long long binpow(long long a, long long b, long long m)
+{
+    a %= m;
+    long long res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main()
+{
+    cout << " 23 ^ 102222 = " << binpow(23, 1000222) << endl;
+    cout << " 23 ^ 102222 % 2345 = " << binpow(23, 1000222, 2345) << endl;
 }

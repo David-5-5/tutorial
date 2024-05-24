638a3f347e6202b47e7e69b399dec235326448df
David-5-5 <13910767907@139.com>
2024-05-24 10:19:59 +0800

    Revive the wrong spell

--- python/algo/leecode/algo1402.py
+++ python/algo/leecode/algo1402.py
@@ -2,7 +2,7 @@ from itertools import accumulate
 class Solution:
     # 思路:
     # 1 satisfaction[i] 越大，放到最后做，like-time 系数越高
-    # 2 对于负值放到前面做，可以增加后续正值的 ith 值
+    # 2 对于负值放到前面做，可以增加后续正值的 i-th 值，增加 like-time 系数
     # 3 当前面的负值，不能增加总体 like-time 系数时，执行终止，当前值即为最大值
     def maxSatisfaction(self, satisfaction) -> int:
         satisfaction.sort();
@@ -24,7 +24,7 @@ class Solution:
         return satis
 
     def maxSatisfaction2(self, satisfaction) -> int:
-        # 思路与上面一直，使用前缀和，提升执行效率
+        # 思路与上面一致，使用前缀和，提升执行效率
         satisfaction.sort();
         maxSatis = 0;
         lenght = len(satisfaction);

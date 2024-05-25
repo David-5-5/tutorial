3e809a9cd80c2ab7a3e3435c06427e199121311c
David-5-5 <13910767907@139.com>
2024-05-25 13:55:24 +0800

    Remove the wrong solution

--- python/algo/leecode2/algo1675.py
+++ python/algo/leecode2/algo1675.py
@@ -2,41 +2,9 @@ from typing import List
 from bisect import bisect_right
 import heapq
 class Solution:
+
     def minimumDeviation(self, nums: List[int]) -> int:
-        nums.sort()
-        res = nums[-1] + 1
-        inx = 0
-        while nums[inx] % 2 == 1 and inx+1 < len(nums):
-            res = min(res, max(nums[-1], 2*nums[inx]) - nums[inx+1])
-            inx += 1 
-
-
-        loop = []
-        change = True
-        while change and res>0:
-            res = min(res, nums[-1] - nums[0])
-
-            if nums[-1] % 2 == 0 and (nums[0] % 2 == 0 or max(nums[-2]-nums[0], nums[-2]-nums[-1]//2) < max(nums[-1]-nums[1], 2*nums[0]-nums[1])):
-                div = nums.pop(-1) // 2
-                inx = bisect_right(nums, div)
-                nums.insert(inx, div)
-                if inx == 0: loop.append(div)
-            elif nums[0] % 2 == 1:
-                mul = nums.pop(0) * 2
-                inx = bisect_right(nums, mul)
-                nums.insert(inx, mul)
-                if inx+1 == len(nums): loop.append(mul)
-            else:
-                change = False
-
-            if len(loop) == 3 and loop[0] == loop[2]:
-                return res
-            elif len(loop) == 3:
-                loop.pop(0)
-
-        return res
-    
-    def minimumDeviation2(self, nums: List[int]) -> int:
+        # Base on algo 632
         newnums = []
         for num in nums:
             sub = [num]

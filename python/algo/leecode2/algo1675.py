c17bb819317084251a89fcc70048541bf9ab2e2f
David-5-5 <13910767907@139.com>
2024-05-26 21:31:00 +0800

    Addd comment

--- python/algo/leecode2/algo1675.py
+++ python/algo/leecode2/algo1675.py
@@ -2,9 +2,13 @@ from typing import List
 from bisect import bisect_right
 import heapq
 class Solution:
-
     def minimumDeviation(self, nums: List[int]) -> int:
-        # Base on algo 632
+        # 遍历 nums[i],每个数创建一个列表. 如果：
+        # 1) nums[i]为奇数, 列表为[nums[i],nums[i]*2]
+        # 2) nums[i]为偶数, 列表为[nums[i],nums[i]//2,...,nums[i]//2^p],
+        #    其中 nums[i]//2^p 为第一个出现的奇数。
+        #    例如 nums[i]=8, 生成的列表为[8,4,2,1], nums[i]=14, 生成的列表为[14,7]
+        # 题目就转换为最小的区间，包含每个列表的一个值的，等同于algo 632
         newnums = []
         for num in nums:
             sub = [num]
@@ -17,6 +21,7 @@ class Solution:
             sub.sort()
             newnums.append(sub)
 
+        # The method of algo 632
         def smallestRange(nnums):
             rows = len(nnums)
             hq = []

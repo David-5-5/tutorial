37c051646f2918f4b6f897832c85185e2b7cc1af
David-5-5 <13910767907@139.com>
2024-06-03 17:19:03 +0800

    Append new solution by refer to tutorial of back tracing

--- python/algo/leecode2/algo51.py
+++ python/algo/leecode2/algo51.py
@@ -10,8 +10,6 @@ class Solution:
                 row = "".ljust(n, ".")
                 row = row[0:pos%n] + 'Q' + row[pos%n + 1:]
                 solve.append(row)
-
-            solve.append(row)
             ans.append(solve)
 
         def find(q: int, ql: List[int], cover: List[int]):
@@ -51,7 +49,58 @@ class Solution:
             find(i, [], [])
         
         return ans
+    
 
+    # refer to python/dsaa/backtracing.py
+    def solveNQueens2(self, n: int) -> List[List[str]]:
+        ans = []
+        col = [] # col[row] = col imply queen in [row, col]
+
+        # def isValid(r,c)->bool:
+        #     for R in range(r):
+        #         if R+col[R] == r+c or col[R]-R == c-r:
+        #             return False
+        #     return True
+
+        def dfs(r, s):
+            if r == n:
+                ans.append(['.'*c + 'Q' + '.'*(n-c-1) for c in col])
+                return
+
+            for c in s:
+                # if isValid(r, c):
+                # As following statement equivalent to isValid method
+                if all(r+c!=R+col[R] and c-r!=col[R]-R for R in range(r)):
+                    col.append(c)
+                    dfs(r+1, s-{c})
+                    col.pop()
+
+        dfs(0,set(range(n)))
+        return ans
+
+    def solveNQueens3(self, n: int) -> List[List[str]]:
+        ans = []
+        col = [0] * n  # col[row] = col imply queen in [row, col]
+        onPath = [False] * n
+        diag1 = [False] * (2*n-1)
+        diag2 = [False] * (2*n-1)
+
+        def dfs(r):
+            if r == n:
+                ans.append(['.'*c + 'Q' + '.'*(n-c-1) for c in col])
+                return
+
+            for c in range(n):
+                if not onPath[c] and not diag1[r+c] and not diag2[r-c]:
+                    col[r] = c
+                    onPath[c] = diag1[r+c] = diag2[r-c] = True
+                    dfs(r+1)
+                    onPath[c] = diag1[r+c] = diag2[r-c] = False
+
+        dfs(0)
+        return ans    
+    
 if __name__ == "__main__":
     sol = Solution()
     print(sol.solveNQueens(4))
+    print(sol.solveNQueens3(4))

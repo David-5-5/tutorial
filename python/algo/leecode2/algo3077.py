7ac83461607fd2f1f2ccc66fe73757586f9cb045
David-5-5 <13910767907@139.com>
2024-06-06 12:31:23 +0800

    Append the comment

--- python/algo/leecode2/algo3077.py
+++ python/algo/leecode2/algo3077.py
@@ -1,5 +1,25 @@
 from  typing import List
 from itertools import accumulate
+# 划分型 DP
+# 暴力思路，从最后一个子数组思考，按题意 求 dp[i][j] 状态转移的最大值
+# 状态方程应该是二维的 dp[i][j] 表示前 j 个元素划分为 i 个子树组
+# 最后一个元素 ： 选/不选
+# 不选的话，可以转移到前 j-1 个元素划分为 i 个子树组即 dp[i][j] = dp[i][j-1]
+# 选的话，dp[i][j] = dp[i-1][j-1] + sum[j..j] * w_i
+# 从末尾开始，可以选几个 ... p 个
+# dp[i][j] = dp[i-1][j-2] + sum[j-1..j] * w_i
+# ...
+# dp[i][j] = dp[i-1][j-p] + sum[j-p+1..j] * w_i
+# 转移到其中的最大值
+# 暴力三重循环
+# for i in range(1, k+1):
+#   for j in range(i,n+1):
+#       for l in range(i,j):
+#           dp[i][j] = max(dp[i][j-1], dp[i-1][j-l] + sum[j-l+1..j] * w_i)
+# 优化：
+# 记住 j 每次的max最大值
+# max(dp[i][j-1], dp[i-1][j-l] + sum[j-l+1..j] * w_i)
+
 class Solution:
     def maximumStrength(self, nums: List[int], k: int) -> int:
         n = len(nums)

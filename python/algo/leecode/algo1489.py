7192ca2cb7343781b4dffb19705a21b0fa4e278e
David-5-5 <13910767907@139.com>
2024-06-09 13:38:21 +0800

    Correct the solution use kruskal.

--- python/algo/leecode/algo1489.py
+++ python/algo/leecode/algo1489.py
@@ -1,64 +1,62 @@
+from typing import List
 class Solution:
+    # Use Kruskal algorithm of mst
+    # The correct solution commit as 2024.06.09
     def findCriticalAndPseudoCriticalEdges(self, n: int, edges) :
         critical, pseudo = [], []
         ne = len(edges)
-        edgeI = {}
-        for i in range(ne):
-            edgeI[(min(edges[i][0],edges[i][1]), max(edges[i][0],edges[i][1]))] = i
 
-        def find(x:int) -> int:
-            if adt[x] < 0:
-                return x
-            else:
-                adt[x] = find(adt[x])
-                return adt[x]
-        
-        def union(x:int, y:int):
-            root1 = find(x)
-            root2 = find(y)
-            if root1 == root2:
-                cycle[root1] = True
-                return
-            if adt[root2] < adt[root1]:
-                adt[root1] = root2
-            else:
-                if adt[root1] == adt[root2]:
-                    adt[root1] -= 1
-                adt[root2] = root1
-
-        adt = [-1] * n
-        cycle = [False] * n
-        
-        edges.sort(key=lambda p:p[2])
-        candi = []
-        weight = 0
-        def appendCandiate(candi):
-            for u, w in candi:
-                union(u, w)
+        sortedges =  sorted(enumerate(edges), key = lambda p:p[1][2])
+        minw = 0
+        def kruskal(s_edges, exclude:int, include:int):
             
-            for u, w in candi:
-                if not cycle[find(u)]:
-                    critical.append(edgeI[(u,w)])
+            def find(x:int) -> int:
+                if adt[x] < 0:
+                    return x
                 else:
-                    pseudo.append(edgeI[(u,w)])
-        i = 0
-        while i < ne:
-            u, w = edges[i][0], edges[i][1]
-            if find(u) == find(w): 
-                i += 1
-                continue
-            if not candi:
-                candi = [(min(u,w), max(u,w))]
-                weight = edges[i][2]
-                i += 1
-            elif weight == edges[i][2]:
-                candi.append((min(u,w), max(u,w)))
-                i += 1
-            else:
-                appendCandiate(candi)
-                candi = []
-        appendCandiate(candi)
-        candi = []
+                    adt[x] = find(adt[x])
+                    return adt[x]
+            
+            def union(x:int, y:int):
+                root1 = find(x)
+                root2 = find(y)
+                if root1 == root2:
+                    return
+                if adt[root2] < adt[root1]:
+                    adt[root1] = root2
+                else:
+                    if adt[root1] == adt[root2]:
+                        adt[root1] -= 1
+                    adt[root2] = root1
+
+            adt = [-1] * n
+            cnt, weight = 0, 0
+
+            if (include != -1):
+                weight += edges[include][2]
+                union(edges[include][0], edges[include][1])
+
+            for inx, edge in s_edges:
+                if inx == exclude:continue
+
+                if find(edge[0]) != find(edge[1]):
+                    weight += edge[2]
+                    union(edge[0], edge[1])
+                    cnt += 1
+
+            return cnt, weight
+
+        _, minw = kruskal(sortedges, -1, -1)
+        for i in range(ne):
+            cnt, weight = kruskal(sortedges, i, -1)
+            if cnt < n-1 or weight > minw:
+                critical.append(i)
+        
+        for i in range(ne):
+            if i in critical:continue
+            cnt, weight = kruskal(sortedges, -1, i)
+            if cnt == n-1 or weight == minw:
+                pseudo.append(i)
 
         return [critical, pseudo]
 
@@ -111,6 +109,7 @@ class Solution:
                 dfs(i)
 
         return [critical, pseudo]
+
 
     def findCriticalAndPseudoCriticalEdges3(self, n: int, edges) :
         critical, pseudo = [], []
@@ -269,6 +268,7 @@ if __name__ == "__main__":
     # edges, n = [[0,1,1],[1,2,1],[2,3,1],[0,3,1]], 4
     # edges, n = [[0,1,1],[0,2,1],[1,2,1],[2,3,1],[3,4,1],[3,5,1],[4,5,1]], 6
     # edges, n = [[0,1,1],[0,2,2],[1,2,3]], 3
-    # edges, n = [[0,1,2],[0,2,5],[2,3,5],[1,4,4],[2,5,5],[4,5,2]], 6
+    edges, n = [[0,1,2],[0,2,5],[2,3,5],[1,4,4],[2,5,5],[4,5,2]], 6
     edges, n = [[0,1,13],[0,2,6],[2,3,13],[3,4,4],[0,5,11],[4,6,14],[4,7,8],[2,8,6],[4,9,6],[7,10,4],[5,11,3],[6,12,7],[12,13,9],[7,13,2],[5,13,10],[0,6,4],[2,7,3],[0,7,8],[1,12,9],[10,12,11],[1,2,7],[1,3,10],[3,10,6],[6,10,4],[4,8,5],[1,13,4],[11,13,8],[2,12,10],[5,8,1],[3,7,6],[7,12,12],[1,7,9],[5,9,1],[2,13,10],[10,11,4],[3,5,10],[6,11,14],[5,12,3],[0,8,13],[8,9,1],[3,6,8],[0,3,4],[2,9,6],[0,11,4],[2,5,14],[4,11,2],[7,11,11],[1,11,6],[2,10,12],[0,13,4],[3,9,9],[4,12,3],[6,7,10],[6,8,13],[9,11,3],[1,6,2],[2,4,12],[0,10,3],[3,12,1],[3,8,12],[1,8,6],[8,13,2],[10,13,12],[9,13,11],[2,11,14],[5,10,9],[5,6,10],[2,6,9],[4,10,7],[3,13,10],[4,13,3],[3,11,9],[7,9,14],[6,9,5],[1,5,12],[4,5,3],[11,12,3],[0,4,8],[5,7,8],[9,12,13],[8,12,12],[1,10,6],[1,9,9],[7,8,9],[9,10,13],[8,11,3],[6,13,7],[0,12,10],[1,4,8],[8,10,2]], 14
     print(sol.findCriticalAndPseudoCriticalEdges3(n, edges))
+    print(sol.findCriticalAndPseudoCriticalEdges(n, edges))

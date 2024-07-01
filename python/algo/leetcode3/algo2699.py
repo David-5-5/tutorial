012b8623fa4e21ce0c8e1a4c77f9972d4bdaed8b
David-5-5 <13910767907@139.com>
2024-07-01 10:20:56 +0800

    To more graceful python style code

--- python/algo/leetcode3/algo2699.py
+++ python/algo/leetcode3/algo2699.py
@@ -16,12 +16,17 @@ class Solution:
             graph[u].append((v, w))
             graph[v].append((u, w))
 
-        def dijkstra(s:int, graph)->List[int]:
+        def dijkstra(s:int, graph: defaultdict, round=0)->List[int]:
+            """
+            s: source
+            graph: defaultdict(list)
+            round: 0 for first round, 1 for second round
+            """
             dist = [inf] * n
             visited = [False] * n
 
             dist[s] = 0
-            # The element in pr is truple (weight, vector)
+            # The element in pr is truple (weight, vertex)
             pr = []
             heapq.heappush(pr, (0, s))
             while pr:
@@ -30,6 +35,10 @@ class Solution:
                     continue                
                 visited[u] = True
                 for v, w in graph[u]:
+                    if visited[v]:
+                        continue
+                    if w == -1 and round == 1:
+                        spec[(min(u,v),max(u,v))] = max(delta - dist[u] + d0[v],1)                    
                     if not visited[v] and dist[v] > (spec[(min(u,v),max(u,v))] if w==-1 else w) + dist[u]:
                         dist[v] = (spec[(min(u,v),max(u,v))] if w==-1 else w) + dist[u]
                         heapq.heappush(pr, (dist[v], v))
@@ -43,45 +52,23 @@ class Solution:
 
         delta = target - d0[destination]
 
-        def dijkstra1(s:int, graph)->List[int]:
-            dist = [inf] * n
-            visited = [False] * n
-
-            dist[s] = 0
-            # The element in pr is truple (weight, vector)
-            pr = []
-            heapq.heappush(pr, (0, s))
-            while pr:
-                _, u = heapq.heappop(pr)
-                if visited[u]:
-                    continue                
-                visited[u] = True
-                for v, w in graph[u]:
-                    if visited[v]:
-                        continue
-                    if w == -1:
-                        spec[(min(u,v),max(u,v))] = max(delta - dist[u] + d0[v],1)
-                    if dist[v] > (spec[(min(u,v),max(u,v))] if w==-1 else w) + dist[u]:
-
-                        dist[v] = (spec[(min(u,v),max(u,v))] if w==-1 else w) + dist[u]
-                        heapq.heappush(pr, (dist[v], v))
-
-            return dist
-        
-        d1 = dijkstra1(source, graph)
+        d1 = dijkstra(source, graph, 1)
 
         if d1[destination] != target:
             return []
-
-        ans = []
-        for key, w in spec.items():
-            ans.append([key[0],key[1],w])
         
-        for u, v, w in edges:
-            if w != -1:
-                ans.append([u,v,w])
+        # python graceful way to do this, it is equals to the as follow comment code
+        return [[key[0],key[1],w] for key,w in spec.items()] + [[u,v,w] for u, v, w in edges if w != -1]
+        # ans = []
+        # for key, w in spec.items():
+        #     ans.append([key[0],key[1],w])
+        
+        # for u, v, w in edges:
+        #     if w != -1:
+        #         ans.append([u,v,w])
 
-        return ans
+        # return ans
+        
 
 if __name__ == "__main__":
     sol = Solution()

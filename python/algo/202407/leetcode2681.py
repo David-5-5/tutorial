e60471c85f6f54fec076b0acd275c76104413255
David-5-5 <13910767907@139.com>
2024-07-02 03:13:43 +0800

    Finish the solution by myself

--- python/algo/202407/leetcode2681.py
+++ python/algo/202407/leetcode2681.py
@@ -1,20 +1,29 @@
 from itertools import accumulate
 from typing import List
 
-
+# 将 nums 排序后从 1 到 n，增量计算英雄值
+# hero[1] = nums[1]^3
+# hero[2] = nums[1]^3 + nums[2]^3 + nums[1] * nums[2]^2
+#         = here[1] + nums[2]^3 + nums[1] * nums[2]^2
+# hero[3] = here[3] + nums[3]^3 + (2 * nums[1] + nums[2] ) * nums[3]^2
+# hero[4] = here[4] + nums[4]^3 + (4 * nums[1] + 2*nums[2] + nums[3] ) * nums[4]^2
+# hero[5] = here[5] + nums[5]^3 + (8 * nums[1] + 4*nums[2] + 2 * nums[3] + nums[4] ) * nums[5]^2
+#  ...          ...         ...
+# hero[i] = here[i] + nums[i]^3 + (2^(i-2) * nums[1] + 2^(i-3)*nums[2] + ... + 2^0 * nums[i-1] ) * nums[i]^2
+# 
+# 对于 i 计算增量, 括号内的和定义为 eoff_i 为 2^(i-2)*nums[1] + 2^(i-3)*nums[2] + 2^0 * num[i-1]
+# 因此计算 i+1 时，eoff_i+1 = eoff_i * 2 + num[i], O(1) 时间复杂度更新系数
 class Solution:
     def sumOfPower(self, nums: List[int]) -> int:
         MOD = 10 ** 9 + 7
-        
         nums.sort()
-        ss = list(accumulate(accumulate(nums), initial=0))
-
-        hero = 0
+        hero = eoff = 0
         for i, num in enumerate(nums):
-            hero = (hero + (num ** 3) + ss[i] * (num ** 2)) % MOD
+            eoff = (eoff * 2 + nums[i-1] if i else 0) % MOD
+            hero = (hero + (num ** 3) + eoff * (num ** 2)) % MOD
         return hero
 
 if __name__ == "__main__":
     sol = Solution()
-    nums = [2,1,4,3]
+    nums = [2,1,4,3,5]
     print(sol.sumOfPower(nums))

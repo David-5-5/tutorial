e18e9e3255761a3f2cc885c60f9a0452203e14e4
David-5-5 <13910767907@139.com>
2024-07-04 14:55:34 +0800

    Finish the tree array with benchmark case

--- python/dsaa/tree_array.py
+++ python/dsaa/tree_array.py
@@ -1,7 +1,36 @@
 from typing import List
 
-
 class TreeArray:
+    __slots__ =('nums', 'tree')
+
+    def __init__(self, nums:List[int]) -> None:
+        self.nums = [0] * len(nums)
+        self.tree = [0] * (len(nums) + 1)
+        for i, num in enumerate(nums):
+            self.update(i, num)
+
+    def update(self, i:int, num:int):
+        delta = num - self.nums[i]
+        # tree array begin with 1
+        ti = i + 1
+        while ti < len(self.tree):
+            self.tree[ti] += delta
+            # the lowbit of ti is "ti & -ti"
+            ti += ti & -ti # ti &= ti - 1 
+    
+    def _query(self, i:int):
+        res = 0
+        while i:
+            res += self.tree[i]
+            i -= i & -i
+        return res
+
+    def query(self, l:int, r:int) -> int:
+        return self._query(r+1) - self._query(l)
+
+
+# Inefficient implementation    
+class TreeArray_II:
     __slots__ =('nums', 'tree')
 
     def __init__(self, nums:List[int]) -> None:
@@ -37,10 +66,33 @@ class TreeArray:
         return res
 
     def query(self, l:int, r:int) -> int:
-        return self._query(r) - self._query(l-1)
+        return self._query(r+1) - self._query(l)
+
+
 
 
 if __name__ == "__main__":
-    ta = TreeArray([i+1 for i in range(16)])
-    print(ta.query(3,15))
+    from time import perf_counter
+    time_start = perf_counter()
+    ta = TreeArray([i+1 for i in range(10 ** 5)])
+    print(ta.query(5000,10 ** 5-1))
+    print(f'Took {perf_counter() - time_start} seconds')
     
+    time_start = perf_counter() 
+    ta2 = TreeArray_II([i+1 for i in range(10 ** 5)])
+    print(ta2.query(5000,10 ** 5-1))
+    print(f'Took {perf_counter() - time_start} seconds')
+
+
+    from timeit import timeit
+    
+    def useTreeArray():
+        ta = TreeArray([i+1 for i in range(10 ** 5)])
+
+    time_duration = timeit('useTreeArray()',globals=globals(), number=10)
+    print(f'Took {time_duration} seconds')
+
+    def useTreeArrayII():
+        ta = TreeArray_II([i+1 for i in range(10 ** 5)])
+    time_duration = timeit('useTreeArrayII()', globals=globals(), number=10)
+    print(f'Took {time_duration} seconds')

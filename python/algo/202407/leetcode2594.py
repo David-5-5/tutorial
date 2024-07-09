ba5e3638d31c46f249bff39ca8f9ed08d9815716
David-5-5 <13910767907@139.com>
2024-07-09 17:16:33 +0800

    Add leetcode 2594 with bi search

---
+++ python/algo/202407/leetcode2594.py
@@ -0,0 +1,23 @@
+from bisect import bisect_left
+from math import floor, sqrt
+from typing import List
+
+
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        
+        def repair(m:int) -> int:
+            res = 0
+            for v in ranks:
+                res += floor(sqrt(m // v))
+            return res
+    
+        return bisect_left(range(min(ranks)*cars*cars), cars, key=repair)
+
+if __name__ == "__main__":
+    sol = Solution()
+    ranks, cars = [1,2,3,4], 4
+    print(sol.repairCars(ranks, cars))
+    
+
+        

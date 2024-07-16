1a21052d87cbb42a330dbdfd42d403a2d7873c4f
David-5-5 <13910767907@139.com>
2024-07-16 16:48:33 +0800

    Add leetcode 0015 using  dual pointers

---
+++ python/algo/202407/leetcode0015.py
@@ -0,0 +1,47 @@
+from bisect import bisect_left, bisect_right
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        nums.sort()
+        n = len(nums)
+        ans = []
+
+        for i in range(n-2):
+            if 0<i<n and nums[i] == nums[i-1]: # skip the equals
+                continue
+            
+            # two pruning 
+            if nums[i] + nums[i+1] + nums[i+2] > 0:break
+            
+            if nums[i] + nums[-1] + nums[-2] < 0:
+                continue
+
+            j = i + 1
+            k = n -1
+            while j < k:
+                s = nums[i] + nums[j] + nums[k]
+                if s < 0:
+                    j += 1
+                elif s > 0:
+                    k -= 1
+                else:
+                    ans.append([nums[i], nums[j], nums[k]])
+                    j += 1
+                    while j < k and nums[j] == nums[j-1]: # skip the equals
+                        j += 1 
+                    k -= 1
+                    while k > j and nums[k] == nums[k+1]: # skip the equals
+                        k -= 1
+
+        return ans
+    
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [0,0,0]
+    
+    print(sol.threeSum(nums))

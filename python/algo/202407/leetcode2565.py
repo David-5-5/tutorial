15f75e8828336807c989a6da7f1428e7a83b1597
David-5-5 <13910767907@139.com>
2024-07-22 10:25:03 +0800

    Add algo 2565

---
+++ python/algo/202407/leetcode2565.py
@@ -0,0 +1,30 @@
+# 周赛 332, 前后缀分解
+class Solution:
+    def minimumScore(self, s: str, t: str) -> int:
+        n , m = len(s), len(t)
+        # suf[i] 表示匹配了从 i 开始匹配了 t[suf[i]:] 的字符 
+        # 若 suf[i] = m, 匹配 t 中 0 个字符
+        # 若 suf[i] = 0, 匹配 t 中 所有字符
+        suf = [m] * (n + 1)
+        j = m
+        for i in range(n-1, -1, -1):
+            if j and s[i] == t[j-1]:
+                j -= 1
+            suf[i] = j
+
+        
+        ans = suf[0]
+        j = 0
+        for i in range(n):
+            if s[i] == t[j]:
+                j += 1
+                if suf[i] >= j:
+                    ans = min(ans, suf[i+1] - j)
+            if j == m: break
+        return ans
+
+if __name__ == "__main__":
+    
+    solution = Solution()
+    s, t = "abacaba", "bzaa"
+    print(solution.minimumScore(s, t))

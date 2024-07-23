78466292d347dc367a2bf6a4076b8fef98178b2e
David-5-5 <13910767907@139.com>
2024-07-23 14:57:55 +0800

    Add implementation using fast power

--- java/tutorial/src/main/java/tutorial/algo/leetcode/Solution2514.java
+++ java/tutorial/src/main/java/tutorial/algo/leetcode/Solution2514.java
@@ -25,8 +25,51 @@ public class Solution2514 {
 
         return (int)ans;
     }
+    
+    // 快速幂
+    private static long pow(long a, int e) {
+        long res = 1, b = a;
+        for (int i=0; i<31; i++) {
+            if ((e >> i & 1 )== 1) {
+                res = (res * b) % MOD;
+            }
+            b = (b * b) % MOD;
+        }
+        return res;
+    }
+
+    private static long pow2(long x, int n) {
+        var res = 1L;
+        for (; n > 0; n /= 2) {
+            if (n % 2 > 0) res = res * x % MOD;
+            x = x * x % MOD;
+        }
+        return res;
+    } 
+
+    public int countAnagrams2(String s) {
+        long ans = 1;
+        String[] sub = s.split(" ");
+        // 中间变量 a, b 使用 long 类型，避免乘法溢出
+        for (String cs : sub) {
+            // 可以省略 a，直接乘以 ans
+            long a = 1, b = 1;
+            int[] cnt = new int[26];
+            for (int i=0; i<cs.length(); i++) {
+                a = (a * (i+1)) % MOD;
+                int ch = cs.charAt(i) - 'a';
+                cnt[ch]++;
+                b = (b*cnt[ch]) % MOD;
+            }
+            ans = (ans * (a * pow(b, MOD-2)% MOD)) % MOD;
+        }
+
+        return (int)ans;
+    }    
 
     public static void main(String[] args) {
-        System.out.println(new Solution2514().countAnagrams("b okzojaporykbmq tybq zrztwlolvcyumcsq jjuowpp"));
+        System.out.println(new Solution2514().countAnagrams2("b okzojaporykbmq tybq zrztwlolvcyumcsq jjuowpp"));
+        System.out.println(pow(344, MOD-2));
+        System.out.println(pow2(344, MOD-2));
     }
 }

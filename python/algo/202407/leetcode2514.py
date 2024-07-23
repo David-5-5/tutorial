79712df7025eb2dca213680eb389bb8fae0c48d4
David-5-5 <13910767907@139.com>
2024-07-23 10:23:44 +0800

    Add mutliple implmentation

--- python/algo/202407/leetcode2514.py
+++ python/algo/202407/leetcode2514.py
@@ -1,3 +1,4 @@
+import math
 from typing import Counter
 
 
@@ -9,11 +10,25 @@ class Solution:
         for sub in s.split():
             a = b = 1
             cnt = Counter()
-            for i, c in enumerate(sub, 1):
-                a = (a*i) % MOD
+            # 1-th implmenetation
+            a = math.perm(len(sub)) % MOD
+            for c in sub:
                 cnt[c] += 1
                 b = (b*cnt[c]) % MOD
+            
+            # 2-th implmenetation
+            # for i, c in enumerate(sub, 1):
+            #     a = (a*i) % MOD
+            #     cnt[c] += 1
+            #     b = (b*cnt[c]) % MOD                
             ans = (ans * a * pow(b, -1, MOD)) % MOD
+
+            # 3-th implmenetation, it pass without using pow(b, -1, MOD)
+            # for i, c in enumerate(sub, 1):
+            #     a = (a*i)
+            #     cnt[c] += 1
+            #     a //= cnt[c]
+            # ans = (ans * a) % MOD
         return ans
 
 if __name__ == "__main__":

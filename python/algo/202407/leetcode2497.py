54230b9dfa4218febbfb87d8dd203b72a0e0eaa1
David-5-5 <13910767907@139.com>
2024-07-24 15:53:00 +0800

    Add 2497 using heapq.nlargest method

---
+++ python/algo/202407/leetcode2497.py
@@ -0,0 +1,18 @@
+from heapq import nlargest
+from typing import List
+
+
+class Solution:
+    def maxStarSum(self, vals: List[int], edges: List[List[int]], k: int) -> int:
+        g = [[] for _ in range(len(vals))]
+        for e in edges:
+            if vals[e[1]] > 0 : g[e[0]].append(vals[e[1]])
+            if vals[e[0]] > 0 : g[e[1]].append(vals[e[0]])
+        
+        return max(sum(nlargest(k, e)) + vals[v] for v, e in enumerate(g))
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    vals, edges, k = [1,2,3,4,10,-10,-20], [[0,1],[1,2],[1,3],[3,4],[3,5],[3,6]], 2
+    print(sol.maxStarSum(vals, edges, k))

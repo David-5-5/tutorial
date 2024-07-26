f3a0fc74df5f38925e0bece93ef26255ab8f7e0f
David-5-5 <13910767907@139.com>
2024-07-27 02:27:50 +0800

    Append implementation using recurrence

--- python/algo/202407/leetcode2547.py
+++ python/algo/202407/leetcode2547.py
@@ -2,13 +2,12 @@ from functools import cache
 from math import inf
 from typing import List
 
-
 class Solution:
     def minCost(self, nums: List[int], k: int) -> int:
         n = len(nums)
         cost = [[0] * n for  _ in range(n)]
         
-        for i in range(n-1,-1,-1):
+        for i in range(n):
             cnt,unique = [0] * n,  0
             for j in range(i, -1, -1):
                 cnt[nums[j]] += 1
@@ -28,10 +27,31 @@ class Solution:
             return res
         return dfs(0)
 
+
+    def minCost2(self, nums: List[int], k: int) -> int:
+        # 从 minCost 改为递推
+        n = len(nums)
+        dp = [0] * (n+1)
+        for i in range(n):
+            cnt,unique = [0] * n,  0
+            res = inf    
+            for j in range(i, -1, -1):
+                cnt[nums[j]] += 1
+                if cnt[nums[j]] == 1:
+                    unique += 1
+                if cnt[nums[j]] == 2:
+                    unique -= 1
+                if dp[j] + i - j + 1 - unique < res: # 避免用 min 函数
+                    res = dp[j] + i - j + 1 - unique
+            dp[i+1] = res + k
+        return dp[n]
+
+
 if __name__ == "__main__":
     sol = Solution()
     nums, k = [1,2,1,2,1,3,3], 2
     print(sol.minCost(nums, k))
+    print(sol.minCost2(nums, k))
                 
         
 

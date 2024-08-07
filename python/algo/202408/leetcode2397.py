b3375ea834d4b4e1de44b6bce476c6dfad35969c
David-5-5 <13910767907@139.com>
2024-08-07 15:05:37 +0800

    Append new implementation with simplify and fast

--- python/algo/202408/leetcode2397.py
+++ python/algo/202408/leetcode2397.py
@@ -16,8 +16,23 @@ class Solution:
                 if i | v == i: res += 1
             if ans < res: ans = res
         return ans
+    
+    def maximumRows2(self, matrix: List[List[int]], numSelect: int) -> int:
+        # 题解写法, 更简洁，更优雅，更python
+        mask = [sum(v << j for j, v in enumerate(row)) for row in matrix]
+        ans = 0
+        for i in range(1 << len(matrix[0])):
+            if i.bit_count() != numSelect: continue
+            ans = max(ans, sum(i | row == i for row in mask))
+        return ans
+
+    def maximumRows3(self, matrix: List[List[int]], numSelect: int) -> int:
+        # Gosper's hack
+        x = (1 << numSelect) - 1
+        
 
 if __name__ == "__main__":
     sol = Solution()
     matrix, numSelect =[[0,0,0],[1,0,1],[0,1,1],[0,0,1]], 2
     print(sol.maximumRows(matrix, numSelect))
+    print(sol.maximumRows2(matrix, numSelect))

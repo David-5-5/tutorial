e2a6f65f13b4f0878ec5e63304f60c72b8753518
David-5-5 <13910767907@139.com>
2024-08-08 17:07:20 +0800

    Append the execise

--- cplus/learning/languange/showbytes.cpp
+++ cplus/learning/languange/showbytes.cpp
@@ -22,10 +22,17 @@ void show_pointer(void *x) {
 }
 
 int main() {
-    int i = 314156;
-    float f = 232.718;
+    int i = 12345;
+    float f = 12345.0;
     
     show_int(i);
     show_float(f);
     show_pointer(&i);
+
+    int val = 0x87654321;
+    byte_pointer p = (byte_pointer) &val;
+    show_bytes(p, 1);
+    show_bytes(p, 2);
+    show_bytes(p, 3);
+    show_bytes(p, 4);
 }

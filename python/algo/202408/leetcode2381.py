93cb4a0112d3629e36fe3d91f9e17d7bb14cbb9f
David-5-5 <13910767907@139.com>
2024-08-08 17:51:09 +0800

    Append the implementation to optimize the code

--- python/algo/202408/leetcode2381.py
+++ python/algo/202408/leetcode2381.py
@@ -1,22 +1,29 @@
 from itertools import accumulate
+from string import ascii_lowercase
 from typing import List
+
+c2i = {c : i for i, c in enumerate(ascii_lowercase)}
 
 # 双周赛 85 自行解答 差分数组
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        diff = [0] * len(s)
+        diff = [0] * (len(s)+1)
+        # diff = [0] * len(s)
 
         for b, e, d in shifts:
             diff[b] += d if d else -1
-            if e+1 < len(s): diff[e+1] -= 1 if d else -1
+            diff[e+1] -= 1 if d else -1 # diff 长度加一，确保不越界
+            # if e+1 < n: diff[e+1] -= 1 if d else -1
         
-        arr = list(accumulate(diff))
+        # arr = list(accumulate(diff))
+        # ans = [''] * len(s)
+        # for i, c in enumerate(s):
+        #     ans.append(chr((ord(c)-ord('a')+arr[i]+26)%26 + ord('a')))
+        # return ''.join(ans)
 
-        ans = [''] * len(s)
-        for i, c in enumerate(s):
-            ans.append(chr((ord(c)-ord('a')+arr[i]+26)%26 + ord('a')))
+        # return ''.join(chr((ord(c)-ord('a')+d+26)%26 + ord('a')) for c,d in zip(s, accumulate(diff)))
+        return ''.join(ascii_lowercase[(c2i[c]+d)%26] for c,d in zip(s, accumulate(diff)))
 
-        return ''.join(ans)
 
 if __name__ == '__main__':
     solution = Solution()

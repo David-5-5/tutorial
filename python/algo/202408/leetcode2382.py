95103f80716b530842a1e01a3f68a68cfe731e93
David-5-5 <13910767907@139.com>
2024-08-09 17:22:12 +0800

    Append implementation using disjoint sets

--- python/algo/202408/leetcode2382.py
+++ python/algo/202408/leetcode2382.py
@@ -1,15 +1,16 @@
 import heapq
 from itertools import accumulate
+from math import inf
 from typing import List
 
 from sortedcontainers import SortedDict
 
 # 双周赛 85 自行解答 开始未看清题意，使用线段树。由于区间不固定，需要查找区间的最大值
-# 因此改用 SortedDict 存储区间 key = 区间左端点left value = 区间右端点 right
-# 优先队列排序区间的最大值, 优先队列的元素为(-区间和,left,right )，
-# 同时使用set存放已经被分割的失效区间，配合优先队列使用
 class Solution:
     def maximumSegmentSum(self, nums: List[int], removeQueries: List[int]) -> List[int]:
+        # 因此改用 SortedDict 存储区间 key = 区间左端点left value = 区间右端点 right
+        # 优先队列排序区间的最大值, 优先队列的元素为(-区间和,left,right )，
+        # 同时使用set存放已经被分割的失效区间，配合优先队列使用
         n = len(nums)
         presum = list(accumulate(nums, initial=0))
         disable = set()
@@ -37,10 +38,38 @@ class Solution:
             ans.append(-q[0][0]) if q else ans.append(0)
         return ans
 
+    def maximumSegmentSum2(self, nums: List[int], removeQueries: List[int]) -> List[int]:
+        # 参考题解，使用并查集
+        n = len(nums)
 
+        sets = [-inf for _ in range(n)]
+        def find(x:int) -> int:
+            if sets[x] < 0:
+                return x
+            else:
+                sets[x] = find(sets[x])
+                return sets[x]
+
+        def union(u, v):
+            rv = find(v)
+            if sets[rv] != -inf:
+                sets[u] += sets[rv]
+                sets[rv] = u
+        
+        ans = []
+        mx = 0
+        for i in removeQueries[::-1]:
+            ans.append(mx)
+            sets[i] = -nums[i]
+            if i+1 < n:union(i,i+1)
+            if i>0: union(i, i-1)
+
+            if -sets[i] > mx :mx = -sets[i]
+        return ans[::-1]
 
 if __name__ == "__main__":
     sol = Solution()
     nums, removeQueries = [1,2,5,6,1], [0,3,2,4,1]
     nums, removeQueries = [500,822,202,707,298,484,311,680,901,319,343,340], [6,4,0,5,2,3,10,8,7,9,1,11]
     print(sol.maximumSegmentSum(nums, removeQueries))
+    print(sol.maximumSegmentSum2(nums, removeQueries))

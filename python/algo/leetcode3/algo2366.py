d4699a23e81046fcaf585fb3a28c9035a167bd0e
David-5-5 <13910767907@139.com>
2024-08-10 15:05:26 +0800

    Add comment

--- python/algo/leetcode3/algo2366.py
+++ python/algo/leetcode3/algo2366.py
@@ -1,16 +1,16 @@
 import math
 from typing import List
 
-
+# 双周赛 84 贪婪算法
 class Solution:
     def minimumReplacement(self, nums: List[int]) -> int:
         ans = 0
 
         limit = nums[-1]
         for v in reversed(nums):
-            x = math.ceil(v/limit)
+            x = math.ceil(v/limit) # 拆分的最小数
             ans += x - 1
-            limit = math.floor(v/x)
+            limit = math.floor(v/x) # 更新上限
         return ans
     
 if __name__ == "__main__":

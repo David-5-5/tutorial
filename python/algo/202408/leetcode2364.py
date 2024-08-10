fa8e424de0410a0b0106a380068dd1a626550ac4
David-5-5 <13910767907@139.com>
2024-08-10 15:05:46 +0800

    Add comment

--- python/algo/202408/leetcode2364.py
+++ python/algo/202408/leetcode2364.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from typing import List
 
-
+# 双周赛 84 自行解答
 class Solution:
     def countBadPairs(self, nums: List[int]) -> int:
         cnt, n = defaultdict(int), len(nums)
@@ -11,6 +11,10 @@ class Solution:
             cnt[v-i] += 1
         return n * (n-1)//2 - res
     
+        # nums = [v - i for i, v in enumerate(nums)]
+        # cnt = Counter(nums)
+        # return n * (n-1) // 2 - sum(v*(v-1)//2 for v in cnt.values())
+
         # res = n * (n-1)//2
         # for i, v in enumerate(nums):
         #     res -= cnt[v-i]

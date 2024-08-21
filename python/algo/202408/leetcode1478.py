2330706657ca49a02bde28545e6f2bb997a5401b
David-5-5 <13910767907@139.com>
2024-08-21 14:09:33 +0800

    Append main

--- python/algo/202408/leetcode1478.py
+++ python/algo/202408/leetcode1478.py
@@ -3,17 +3,17 @@ from math import inf
 from typing import List
 
 # similiar to 2463
-# 自行解答
+# 自行解答 动态 + 贪婪
 class Solution:
     def minDistance(self, houses: List[int], k: int) -> int:
         houses.sort()
         n = len(houses)
 
-        def distance(l:int, r:int) -> int:
+        def distance(l:int, r:int) -> int: # 贪婪
             res = 0
             mid = (l + r) // 2
             for i in range(l, r+1):
-                res += abs(houses[i]-houses[mid])
+                res += abs(houses[i]-houses[mid]) 
             return res
 
         @cache
@@ -28,3 +28,8 @@ class Solution:
             return res
 
         return dfs(0, k)
+
+if __name__ == "__main__":
+    sol = Solution()
+    houses, k = [1,4,8,10,20], 3
+    print(sol.minDistance(houses, k))

3ab46b0adc7f755b6e0228ff69da9198d236587d
David-5-5 <13910767907@139.com>
2024-08-23 02:05:11 +0800

    Append implement using self method reverse int

--- python/algo/202408/leetcode2442.py
+++ python/algo/202408/leetcode2442.py
@@ -3,4 +3,13 @@ from typing import List
 
 class Solution:
     def countDistinctIntegers(self, nums: List[int]) -> int:
-        return len(set(nums) | set([int(str(c)[::-1])for c in nums]))
+        # return len(set(nums) | set([int(str(c)[::-1])for c in nums]))
+        s = set()
+        for c in nums:
+            s.add(c)
+            x = 0
+            while c:
+                x = x*10 + c % 10
+                c // 10
+            s.add(x)
+        return len(s)

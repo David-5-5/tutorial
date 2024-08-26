98f354f04b97f56f1569216dc1915d6b2c575057
David-5-5 <13910767907@139.com>
2024-08-27 00:20:50 +0800

    Add new implementation

--- python/algo/202408/leetcode2433.py
+++ python/algo/202408/leetcode2433.py
@@ -10,8 +10,17 @@ class Solution:
             ans.append(p ^ prev)
             prev = p
         return ans
+    
+    def findArray2(self, pref: List[int]) -> List[int]:
+        ans = []
 
+        pref = [0] + pref
+        for i in range(1, len(pref)):
+            ans.append(pref[i]^pref[i-1])
+        return ans
+    
 if __name__ == "__main__":
     sol = Solution()
     pref = [5,2,0,3,1]
     print(sol.findArray(pref))
+    print(sol.findArray2(pref))

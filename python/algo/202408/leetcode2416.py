f54c1a6f65fd22934d66e43095c9406fe19ff210
David-5-5 <13910767907@139.com>
2024-08-31 20:01:14 +0800

    Add algo using trie

---
+++ python/algo/202408/leetcode2416.py
@@ -0,0 +1,46 @@
+from collections import defaultdict
+from typing import List
+
+# The Trie class
+class Trie:
+    
+    def __init__(self):
+        self.children = defaultdict(Trie)
+        self.score = 0
+        self.ids = []
+    
+    def insert(self, i, word):
+        cur = self
+        for c in word:
+            cur = cur.children[c]
+            cur.score += 1
+        cur.ids.append(i)
+    
+# 周赛 311
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+
+        root = Trie()
+        for i, word in enumerate(words):
+            root.insert(i, word)
+        
+        ans = [0] * len(words)
+
+        def dfs(node:Trie, sum):
+            if node is None: return
+            sum += node.score
+
+            for i in node.ids:
+                ans[i] = sum
+            for child in node.children.values():
+                dfs(child, sum)
+
+        dfs(root, 0)
+        return ans
+
+
+    
+if __name__ == "__main__":
+    sol = Solution()
+    words = ["abc","ab","bc","b"]
+    print(sol.sumPrefixScores(words))

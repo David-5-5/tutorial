1eae230561fed2f9b39c800be357f749c5b66bd4
David-5-5 <13910767907@139.com>
2024-09-02 01:54:30 +0800

    Append implementation using bitwise of number replace of set

--- python/algo/202409/leetcode2401.py
+++ python/algo/202409/leetcode2401.py
@@ -3,6 +3,7 @@ from typing import Counter, List
 # 周赛 309
 class Solution:
     def longestNiceSubarray(self, nums: List[int]) -> int:
+        # 自行解答 执行时间 4119 ms
         cnt = Counter()
         l = -1
         def contain(s:set) -> bool:
@@ -24,8 +25,22 @@ class Solution:
             ans = max(ans, r-l)
         return ans
 
+    def longestNiceSubarray2(self, nums: List[int]) -> int:
+        # 自行解答，使用 +, -, | 运算代替集合运算
+        # 优化后执行时间 108 ms
+        l = -1
+        sum = ans = 0
+        for r, v in enumerate(nums):
+            while sum | v != sum + v:
+                l += 1
+                sum -= nums[l]
+            sum += v
+            if ans < r-l: ans = r-l
+        return ans
+
 if __name__ == "__main__":
     sol = Solution()
     nums = [1,3,8,48,10]
     nums = [135745088,609245787,16,2048,2097152]
     print(sol.longestNiceSubarray(nums))
+    print(sol.longestNiceSubarray2(nums))

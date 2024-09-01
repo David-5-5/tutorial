0d17864cf8885f0ac2f81e0392cb65684f322f8e
David-5-5 <13910767907@139.com>
2024-09-01 22:03:42 +0800

    Add new implementation using heap

--- python/algo/202409/leetcode2406.py
+++ python/algo/202409/leetcode2406.py
@@ -1,10 +1,12 @@
 from bisect import bisect_right
 from cmath import inf
+import heapq
 from typing import List
 
 # 周赛 310
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
+        # 自行解答
         intervals.sort()
         
         ans = right = 0
@@ -17,8 +19,20 @@ class Solution:
                 _, right = intervals.pop(inx)
         return ans + 1
 
+    def minGroups2(self, intervals: List[List[int]]) -> int:
+        # 参考题解，使用堆
+        intervals.sort()
+
+        q = []
+        for l, r in intervals:
+            if q and l > q[0] :
+                heapq.heapreplace(q, r) # 更改组
+            else:
+                heapq.heappush(q, r) # 增加新的组
+        return len(q)
 
 if __name__ == "__main__":
     sol = Solution()
     intervals = [[5,10],[6,8],[1,5],[2,3],[1,10]]
-    print(sol.minGroups(intervals))
+    # print(sol.minGroups(intervals))
+    print(sol.minGroups2(intervals))

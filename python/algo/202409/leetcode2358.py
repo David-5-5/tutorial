9bf00f7346ee8540c6a73e6514f6126852e55886
David-5-5 <13910767907@139.com>
2024-09-09 23:44:56 +0800

    Add algo using binary search

---
+++ python/algo/202409/leetcode2358.py
@@ -0,0 +1,22 @@
+
+from bisect import bisect_right
+from typing import List
+
+MAX = 10 ** 5
+
+q = []
+i = 1
+while i*(i+1) // 2 <= MAX:
+    q.append(i*(i+1) // 2)
+    i += 1
+
+class Solution:
+    def maximumGroups(self, grades: List[int]) -> int:
+        n = len(grades)
+        inx = bisect_right(q, n)
+        return inx
+
+if __name__ == "__main__":
+    sol = Solution()
+    grades = [10,6,12,7,3,5]
+    print(sol.maximumGroups(grades))

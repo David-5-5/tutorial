868956c66f246cd2256be758c27668bce291b2a8
David-5-5 <13910767907@139.com>
2024-09-09 16:45:42 +0800

    Add algo using floyd and bitwise

---
+++ python/algo/202409/leetcode2959.py
@@ -0,0 +1,32 @@
+from math import inf
+from typing import List
+
+# 双周赛 119
+class Solution:
+    def numberOfSets(self, n: int, maxDistance: int, roads: List[List[int]]) -> int:
+        # 参考题解 Floyd 最短路径算法 结合 位集合运算
+        def check(bs:int) -> int:
+            # 初始化 i == j 表示 节点相同，即节点到自身的距离， bs>>i==0 表示节点 i 不在集合中
+            f = [[0 if i==j or bs>>j&1==0 or bs>>i&1==0 else inf for i in range(n)] for j in range(n)]
+
+            for u, v, w in roads:
+                if f[u][v] > w:
+                    f[u][v] = w
+                    f[v][u] = w
+
+            for k in range(n):
+                if bs>>k&1==0 : continue
+                for x in range(n):
+                    if bs>>x&1==0 : continue
+                    for y in range(n):
+                        if bs>>y&1==0:continue
+                        f[x][y] = min(f[x][y], f[x][k] + f[k][y])
+
+            return 1 if max(max(row) for row in f) <= maxDistance else 0
+
+        return sum(check(s) for s in range(1<<n))
+    
+if __name__ == "__main__":
+    sol = Solution()
+    n, maxDistance, roads = 3, 5, [[0,1,2],[1,2,10],[0,2,10]]
+    print(sol.numberOfSets(n, maxDistance, roads))

2893cf9375e24cfb62c60179713a0909fc045f21
David-5-5 <13910767907@139.com>
2024-09-14 01:19:38 +0800

    Add algo using double pointer

---
+++ python/algo/202409/leetcode2337.py
@@ -0,0 +1,28 @@
+class Solution:
+    def canChange(self, start: str, target: str) -> bool:
+        # 自行解答
+        n = len(start)
+        i = j = 0
+
+        while i < n or j < n: # 从这到 break,消除末尾的 _
+            while i < n and start[i] == '_':
+                i += 1
+            while j < n and target[j] == '_':
+                j += 1
+            
+            if j == n or i == n: break
+
+            if (start[i] == target[j] == 'L' and j <= i ) or start[i] == target[j] == 'R' and j >= i:
+                i += 1
+                j += 1
+            else:
+                return False
+
+        return i == j
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    start, target = "_L__R__R_", "L______RR"
+    # start, target = "R_L_", "__LR"
+    print(sol.canChange(start, target))

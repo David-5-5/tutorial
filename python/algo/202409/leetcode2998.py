d66eb389c7a451ed7737663d02f7ba797ca88e29
David-5-5 <13910767907@139.com>
2024-09-15 13:23:56 +0800

    Add algo using dynamic programming

---
+++ python/algo/202409/leetcode2998.py
@@ -0,0 +1,25 @@
+from functools import cache
+
+# 双周赛 121
+class Solution:
+    def minimumOperationsToMakeEqual(self, x: int, y: int) -> int:
+        # 自行解答 DP 动态规划
+        @cache
+        def check(cur:int):
+            if cur == y: return 0
+            if cur < y: return y - cur # 加 1
+
+            res = cur - y # 减 1
+            res = min(res, cur % 5 + 1 + check(cur//5)) # - (cur % 11) 除 11
+            res = min(res, (5 - cur % 5) + 1 +  check(cur//5 + 1)) # + 11 - (cur % 11) 除 11
+            res = min(res, cur % 11 + 1 + check(cur//11)) # - (cur % 11) 除 11
+            res = min(res, (11 - cur % 11) + 1 + check(cur//11 + 1)) # + 11 - (cur % 11) 除 11
+
+            return res
+
+        return check(x)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.minimumOperationsToMakeEqual(25, 30))

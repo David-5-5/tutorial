b27a1c143909fc4157669a20db35bbb61916e4a4
David-5-5 <13910767907@139.com>
2024-09-16 17:49:53 +0800

    Append with simpify

--- python/algo/leecode/algo_offerII039.py
+++ python/algo/leecode/algo_offerII039.py
@@ -1,22 +1,21 @@
+# 主站 84
 class Solution:
     # 题目变更为 LCR 039
+    # 简化
     def largestRectangleArea(self, heights) -> int:
         n  = len(heights)
         if n == 1: return heights[0]
         maxArea = 0
         stack = []
         for i in range(n):
-            if len(stack) == 0 or stack[-1][0] <= heights[i]:
-                if heights[i] > 0:
-                    stack.append((heights[i], 1))
-            else:
-                lens = 0
-                while stack and stack[-1][0] > heights[i]:
-                    height, broad = stack.pop(-1)
-                    lens += broad
-                    maxArea = max(maxArea, height * lens)
-                if heights[i] > 0:
-                    stack.append((heights[i], lens + 1))
+            # 删除不必要的逻辑
+            lens = 0
+            while stack and stack[-1][0] > heights[i]:
+                height, broad = stack.pop(-1)
+                lens += broad
+                maxArea = max(maxArea, height * lens)
+            if heights[i] > 0:
+                stack.append((heights[i], lens + 1))
 
         lens = 0
         while stack:

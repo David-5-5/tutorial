a52f0aa255233c207bf790b158a515b7e2eac947
David-5-5 <13910767907@139.com>
2024-09-19 17:47:06 +0800

    Append new solution

--- python/algo/202409/leetcode2304.py
+++ python/algo/202409/leetcode2304.py
@@ -7,6 +7,19 @@ class Solution:
     def minPathCost(self, grid: List[List[int]], moveCost: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
 
+        # @cache
+        # def dfs(r:int, c:int) -> int:
+        #     if r == m-1: return grid[r][c]
+
+        #     res = inf
+            
+        #     for c_n in range(n):
+        #         res = min(res, grid[r][c] + moveCost[grid[r][c]][c_n] + dfs(r+1, c_n))
+            
+        #     return res
+
+        # return min(dfs(0, c) for c in range(n))
+
         @cache
         def dfs(r:int, c:int) -> int:
             if r == m-1: return grid[r][c]
@@ -14,12 +27,13 @@ class Solution:
             res = inf
             
             for c_n in range(n):
-                res = min(res, grid[r][c] + moveCost[grid[r][c]][c_n] + dfs(r+1, c_n))
+                res = min(res, (grid[r][c] + moveCost[grid[r][c]][c_n] if r > -1 else 0) + dfs(r+1, c_n))
             
             return res
 
-        return min(dfs(0, c) for c in range(n))
-    
+        return dfs(-1, -1)
+
+
 if __name__ == "__main__":
     sol = Solution()
     grid, moveCost = [[5,3],[4,0],[2,1]], [[9,8],[1,5],[10,12],[18,6],[2,4],[14,3]]

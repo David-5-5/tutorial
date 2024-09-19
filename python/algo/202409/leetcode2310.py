d8a055a1af751ccf662c60c5c195f95277d43e3c
David-5-5 <13910767907@139.com>
2024-09-19 10:48:17 +0800

    Add algo using math

---
+++ python/algo/202409/leetcode2310.py
@@ -0,0 +1,11 @@
+# 周赛 298
+class Solution:
+    def minimumNumbers(self, num: int, k: int) -> int:
+        if num == 0: return 0
+        # 求最小值，最多为 10
+        for i in range(1, 11):
+            if num < i * k :break
+            if (num - i * k) % 10 == 0:
+                return i
+
+        return -1

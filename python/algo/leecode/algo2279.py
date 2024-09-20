433187c8d72f2d0ef8d136b5f378b72c73a44b6d
David-5-5 <13910767907@139.com>
2024-09-21 01:23:43 +0800

    Append  simpified implementation

--- python/algo/leecode/algo2279.py
+++ python/algo/leecode/algo2279.py
@@ -1,5 +1,7 @@
+# 周赛 294
 class Solution:
     def maximumBags(self, capacity, rocks, additionalRocks: int) -> int:
+        # 2022 11 851 ms
         if len(capacity) == 0:
             return 0
         
@@ -11,7 +13,7 @@ class Solution:
                 fullCount += 1
             else:
                 attr.append(capacity[i] - rocks[i])
-
+        
         attr.sort()
         additional = additionalRocks
         while len(attr) > 0 and attr[0] <= additional:
@@ -19,7 +21,21 @@ class Solution:
             additional -= attr.pop(0)
 
         return fullCount
+    
+    def maximumBags2(self, capacity, rocks, additionalRocks: int) -> int:
+        # 2024.9 重写 思路一致，贪心 + 排序 106 ms
+        fullCount = 0
+        to_fill  = sorted([x-y for x,y in zip(capacity, rocks)])
 
+        for f in to_fill:
+            if additionalRocks == 0:break
+            if additionalRocks < f:
+                additionalRocks = 0
+            else:
+                additionalRocks -= f
+                fullCount += 1
+
+        return fullCount    
 if __name__ == "__main__":
     sol = Solution()
     capacity, rocks, additionalRocks = [2,3,4,5], [1,2,4,4], 2

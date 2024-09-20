040d58268b64e96c802e61e9c199403626de126d
David-5-5 <13910767907@139.com>
2024-09-20 16:01:24 +0800

    Append implementation using bitmask dp

--- python/algo/202409/leetcode2305.py
+++ python/algo/202409/leetcode2305.py
@@ -19,6 +19,9 @@ class Solution:
                     if backtrack(workloads, i + 1, limit):
                         return True
                     workloads[j] -= cur
+                # 如果当前工人未被分配工作，那么下一个工人也必然未被分配工作
+                # 或者当前工作恰能使该工人的工作量达到了上限
+                # 这两种情况下我们无需尝试继续分配工作                    
                 if workloads[j] == 0 or workloads[j] + cur == limit:
                     break
             return False
@@ -34,7 +37,31 @@ class Solution:
         # return l 
         return bisect_left(range(l, r), True, key=check) + l
 
+    def distributeCookies2(self, cookies: List[int], k: int) -> int:
+        # 状态压缩
+        n = len(cookies)
+        sum = [0] * (1 << n)
+        for i in range(1, 1 << n):
+            x = bin(i)[::-1].index('1')
+            y = i - (1 << x)
+            sum[i] = sum[y] + cookies[x]
+
+        dp = [[0] * (1 << n) for _ in range(k)]
+        dp[0] = sum
+
+        for i in range(1, k):
+            for j in range(1 << n):
+                minn = float('inf')
+                x = j # 式 1 
+                while x:
+                    minn = min(minn, max(dp[i - 1][j - x], sum[x]))
+                    x = (x - 1) & j # 式 2 结合式 1, 遍历集合中
+                dp[i][j] = minn
+
+        return dp[k - 1][(1 << n) - 1]
+
 if __name__ == "__main__":
     sol = Solution()
     cookies, k = [8,15,10,20,8], 2
     print(sol.distributeCookies(cookies, k)) 
+    print(sol.distributeCookies2(cookies, k)) 

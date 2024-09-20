34796190ad05d4c8ce533d641cbab2cd026268b8
David-5-5 <13910767907@139.com>
2024-09-20 21:39:37 +0800

    Append implementation using prefix sum and hash

--- python/algo/202409/leetcode3026.py
+++ python/algo/202409/leetcode3026.py
@@ -1,39 +1,29 @@
 from cmath import inf
+from collections import defaultdict
 from itertools import accumulate
 from typing import List
 
 # 双周赛 123
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        # 提示 前缀和 + 哈希，自行解答 
         presum = list(accumulate(nums, initial=0))
-
-        ans, n = -inf, len(nums)
         
-        s_nums = sorted(enumerate(nums), key=lambda p:p[1])
-        l = r = 0
-        while l < n and r < n:
-            if s_nums[r][1] - s_nums[l][1] == k:
-                while l+1 < n and s_nums[l][1] == s_nums[l+1][1] and (s_nums[l][1] <= 0 and s_nums[l][0]<s_nums[r][0] or s_nums[l][1] > 0 and s_nums[l][0]>s_nums[r][0] ):
-                    l += 1
-                while r+1 < n and s_nums[r][1] == s_nums[r+1][1] and (s_nums[r][1] >= 0 and s_nums[l][0]<s_nums[r][0] or s_nums[r][1] < 0 and s_nums[l][0]>s_nums[r][0] ):
-                    r += 1
-                end = max(s_nums[r][0], s_nums[l][0]) + 1
-                begin = min(s_nums[r][0], s_nums[l][0])                
-                if presum[end] - presum[begin] > ans:
-                    ans = presum[end] - presum[begin]
-                
-                while l+1 < n and s_nums[l][0] == s_nums[l+1][0]: l += 1
-                while r+1 < n and s_nums[r][0] == s_nums[r+1][0]: r += 1
-                l += 1
-                r += 1
-            
-            if r < n and s_nums[r][1] - s_nums[l][1] > k:
-                l += 1
-            elif r < n and s_nums[r][1] - s_nums[l][1] < k:
-                r += 1
-
+        # key: v, value: min(presum[i]), where i is index of v in nums
+        # 求区间和的最大值:
+        #   result = max(presum[j+1] - presum[i]),where abs(nums[j] - nums[i]) == k
+        #   result = presum[j+1] - min(presum[i]), 因此相同的key保留最小的 presum[i]
+        vsum = defaultdict(lambda:inf)
+        
+        ans = -inf
+        for j, v in enumerate(nums):
+            if ans < presum[j+1] - vsum[v-k]:
+                ans = presum[j+1] - vsum[v-k]
+            if ans < presum[j+1] - vsum[v+k]:
+                ans = presum[j+1] - vsum[v+k]
+            # 相同 
+            if vsum[v] >  presum[j]: vsum[v] = presum[j]
         return 0 if ans == -inf else ans 
-
 
 if __name__ == "__main__":
     sol = Solution()

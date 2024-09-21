1326693fc97aa4f314d1d3b754032d1ed2b3662d
David-5-5 <13910767907@139.com>
2024-09-22 00:26:16 +0800

    Add algo 3029 using  hash

---
+++ python/algo/202409/leetcode3029.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lastNonEmptyString(self, s: str) -> str:
+        lett = [[-1,0] for _ in range(26)]
+        mx = 0
+        for i, ch in enumerate(s):
+            lett[ord(ch)-ord('a')][0] = i
+            lett[ord(ch)-ord('a')][1] += 1
+            if lett[ord(ch)-ord('a')][1] > mx:
+                mx = lett[ord(ch)-ord('a')][1]
+        
+        ans = ""
+        for i, (_, cnt) in sorted(enumerate(lett), key=lambda p:p[1]):
+            if mx == cnt:
+                ans += chr(ord('a')+i)
+        return ans
+if __name__ == "__main__":
+    sol = Solution()
+    s = "aabcbbca"
+    print(sol.lastNonEmptyString(s))

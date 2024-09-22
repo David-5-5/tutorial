335cc7dbcb3c933a4195ccc0c8810e10251922dc
David-5-5 <13910767907@139.com>
2024-09-22 15:52:29 +0800

    Append simpified implmenentation

--- python/algo/202409/leetcode2295.py
+++ python/algo/202409/leetcode2295.py
@@ -5,18 +5,23 @@ class Solution:
     def arrayChange(self, nums: List[int], operations: List[List[int]]) -> List[int]:
         # 自行解答
         transfer = {}
-        for f, t in operations[::-1]: # 倒序
-            if t in transfer.keys():
-                transfer[f] = transfer[t]
-                del transfer[f]
-            else: transfer[f] = t
+        # for f, t in operations[::-1]: # 倒序
+        #     if t in transfer.keys():
+        #         transfer[f] = transfer[t]
+        #         del transfer[f]  # 删除可忽略
+        #     else: transfer[f] = t
 
-        for i, num in enumerate(nums):
-            if num not in transfer:continue
-            nums[i] = transfer[num]
+        # for i, num in enumerate(nums):
+        #     if num not in transfer:continue
+        #     nums[i] = transfer[num]
         
-        return nums
+        # return nums
 
+        # 参考题解，更 python 的写法
+        for f, t in operations[::-1]: # 倒序
+            transfer[f] = transfer.get(t, t)
+        
+        return [transfer.get(x, x) for x in nums]
 
 if __name__ == "__main__":
     sol = Solution()

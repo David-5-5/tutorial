54d7cd5c7bd33add8455a2832ba2b39b23c1e3db
David-5-5 <13910767907@139.com>
2024-09-24 10:20:49 +0800

    Append new implmenentation with bitwise dp

--- python/algo/202409/leetcode2572.py
+++ python/algo/202409/leetcode2572.py
@@ -1,5 +1,5 @@
 from functools import cache
-from typing import List
+from typing import Counter, List
 
 PRIMES = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 NSQ_MASK = [0] * 31
@@ -16,14 +16,34 @@ MOD = 10 ** 9 + 7
 class Solution:
     def squareFreeSubsets(self, nums: List[int]) -> int:
         # 参考题解  背包
-        MASK = 2 ** 9
-        f = [0] * (MASK)
+        MASK = 1 << (len(PRIMES))
+        f = [0] * MASK
         f[0] = 1
         for v in nums:
             mask = NSQ_MASK[v]
-            if mask > 0:
+            if mask >= 0:
                 for j in range(MASK-1, mask-1, -1):
                     if (mask | j) == j:
-                        f[j] = (f[j] + f[j^mask]) % MOD
+                        f[j] = (f[j] + f[j-mask]) % MOD
         return (sum(f) - 1) % MOD
 
+
+    def squareFreeSubsets2(self, nums: List[int]) -> int:
+        # 参考题解  状压DP
+        MASK = 1 << (len(PRIMES))
+        f = [0] * (MASK)
+
+        cnt = Counter(nums)
+        f[0] = pow(2, cnt[1], MOD)
+        for v, c in cnt.items():
+            mask = NSQ_MASK[v]
+            if mask > 0:
+                for j in range(MASK-1, -1, -1):
+                    if (mask & j) == 0:
+                        f[mask | j] = (f[mask | j] + f[j] * c)% MOD
+        return (sum(f) - 1) % MOD
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [3,4,4,5]
+    print(sol.squareFreeSubsets2(nums))

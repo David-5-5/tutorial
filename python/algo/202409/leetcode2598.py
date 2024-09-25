81657e39c2d5195088fb20e7bff1be4c05f57bb6
David-5-5 <13910767907@139.com>
2024-09-25 23:57:16 +0800

    Add algo using hash and modular

---
+++ python/algo/202409/leetcode2598.py
@@ -0,0 +1,20 @@
+from collections import Counter, defaultdict
+from typing import List
+
+# 周赛 337
+class Solution:
+    def findSmallestInteger(self, nums: List[int], value: int) -> int:
+        cnt = defaultdict(int)
+        for v in nums:
+            cnt[v%value] += 1
+        ans = 0
+        while True:
+            if cnt[ans%value] == 0: break
+            cnt[ans%value] -= 1
+            ans += 1
+        return ans
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums, value = [1,-10,7,13,6,8], 5
+    print(sol.findSmallestInteger(nums, value))

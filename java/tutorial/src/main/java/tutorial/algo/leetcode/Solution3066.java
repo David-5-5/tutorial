ad2ac459b7b65522bfae4ec8c7ab86f4b59914c2
David-5-5 <13910767907@139.com>
2024-09-26 23:53:00 +0800

    Add algo using java with PriorityQueue

---
+++ java/tutorial/src/main/java/tutorial/algo/leetcode/Solution3066.java
@@ -0,0 +1,27 @@
+package tutorial.algo.leetcode;
+
+import java.util.PriorityQueue;
+
+public class Solution3066 {
+
+    public int minOperations(int[] nums, int k) {
+        PriorityQueue<Long> numbers = new PriorityQueue<>();
+        for (int x :nums)
+            numbers.add((long)x);
+        
+        int ans = 0;
+        while (numbers.size() > 1 && numbers.peek() < k)  {
+            long a = numbers.poll();
+            long b = numbers.poll();
+            numbers.add((a*2 + b));
+            ans ++;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args ) {
+        int[] nums = new int[]{1000000000,999999999,1000000000,999999999,1000000000,999999999};
+        System.out.println(new Solution3066().minOperations(nums, 1000000000));
+
+    }
+}

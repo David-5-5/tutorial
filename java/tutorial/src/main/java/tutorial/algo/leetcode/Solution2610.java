c1209071b469d5927502a3121219b654f978a527
David-5-5 <13910767907@139.com>
2024-09-29 17:56:54 +0800

    Add algo using hash

---
+++ java/tutorial/src/main/java/tutorial/algo/leetcode/Solution2610.java
@@ -0,0 +1,30 @@
+package tutorial.algo.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+
+public class Solution2610 {
+    public List<List<Integer>> findMatrix(int[] nums) {
+        HashMap<Integer, Integer> cnt = new HashMap<>();
+        for (int x:nums) cnt.merge(x, 1, Integer::sum);
+
+        List<List<Integer>> ans = new ArrayList<>();
+        while (!cnt.isEmpty()) {
+            List<Integer> row = new ArrayList<>();
+            for (Iterator<Entry<Integer, Integer>> it = cnt.entrySet().iterator(); it.hasNext(); ) {
+                Entry<Integer, Integer> item = it.next();
+                row.add(item.getKey());
+                if (item.getValue()>1)
+                    item.setValue(item.getValue()-1);
+                else
+                    it.remove();
+            }
+            ans.add(row);
+        }
+
+        return ans;
+    }
+}

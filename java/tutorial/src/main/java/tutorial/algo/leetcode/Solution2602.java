32a2e96fb6c1b265131a56e7c0a4fb7cf0db59b8
David-5-5 <13910767907@139.com>
2024-09-29 11:27:14 +0800

    Append implementation using customized binary search method

--- java/tutorial/src/main/java/tutorial/algo/leetcode/Solution2602.java
+++ java/tutorial/src/main/java/tutorial/algo/leetcode/Solution2602.java
@@ -19,14 +19,34 @@ public class Solution2602 {
         List<Long> ans = new ArrayList<>();
         for (int q:queries) {
             Long res = 0l;
-            int inx = Arrays.binarySearch(nums, q);
-            if (inx < 0) inx = -inx - 1;
+            // int inx = Arrays.binarySearch(nums, q);
+            // if (inx < 0) inx = -inx - 1;
+
+            // using customer method instead of Array.binarySearch
+            int inx = lowerBound(nums, q);
             res += ((long)q * inx) - (preSum[inx] - preSum[0]);
             res += (preSum[n]- preSum[inx]) - ((long)q * (n-inx));
             ans.add(res);
         }
 
         return ans;
+    }
+
+    private int lowerBound(int[] nums, int target) {
+        int left = -1;
+        int right = nums.length; // 开区间 (left, right)
+        while (left + 1 < right) { // 区间不为空
+            // 循环不变量：
+            // nums[left] < target
+            // nums[right] >= target
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid; // 范围缩小到 (mid, right)
+            } else {
+                right = mid; // 范围缩小到 (left, mid)
+            }
+        }
+        return right;
     }
 
     public static void main(String[] args) {

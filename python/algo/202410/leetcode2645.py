d6542cd2880d6ac65e726e8795a37423a0f5c4e9
David-5-5 <13910767907@139.com>
2024-10-01 23:59:53 +0800

    Add algo using enumerator

---
+++ python/algo/202410/leetcode2645.py
@@ -0,0 +1,18 @@
+class Solution:
+    def addMinimum(self, word: str) -> int:
+        exp, exi = "abc", 0
+
+        ans = 0
+        for ch in word:
+            while ch != exp[exi]:
+                ans += 1
+                exi = (exi + 1) % 3
+            exi = (exi + 1) % 3
+        
+        if word[-1] == 'a': ans += 2
+        elif word[-1] == 'b': ans += 1
+        return ans
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.addMinimum("b"))

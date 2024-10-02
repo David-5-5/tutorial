fa6e42eaf380cd174dcf10d70c283bf3d8616759
David-5-5 <13910767907@139.com>
2024-10-02 17:58:00 +0800

    Append implementation using hash

--- python/algo/202410/leetcode3097.py
+++ python/algo/202410/leetcode3097.py
@@ -4,6 +4,7 @@ from typing import List
 # 双周赛 127
 class Solution:
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
+        # 自行解答 使用双指针
         cnt = [0] * 31
         l = or_sum = 0
         ans = inf
@@ -29,7 +30,21 @@ class Solution:
                     out >>= 1
                 l += 1
         return -1 if ans == inf else ans
-    
+
+    def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
+        # 参考题解
+        d = dict() # key or的和, value是得到or值的最大左端点
+        ans = inf
+        for r, num in enumerate(nums):
+            d = {or_|num: left for or_, left in d.items()}
+            d[num] = r
+            for or_, l in d.items():
+                if or_ >= k and ans > r-l+1: ans = r-l+1
+
+        return -1 if ans == inf else ans
+
+
+
 if __name__ == "__main__":
     sol = Solution()
     nums, k = [1,2,3], 2

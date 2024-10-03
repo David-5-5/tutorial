bebf03090a7a797a13e0daa97376a170c60a7420
David-5-5 <13910767907@139.com>
2024-10-03 21:36:07 +0800

    Append the solution

--- python/algo/202410/leetcode2662.py
+++ python/algo/202410/leetcode2662.py
@@ -6,11 +6,13 @@ from typing import List
 # 周赛 343
 class Solution:
     def minimumCost(self, start: List[int], target: List[int], specialRoads: List[List[int]]) -> int:
+        # 自行解答，牢牢记清楚公式
         def dis(x1:int, y1:int, x2:int, y2:int):
             return abs(x2-x1) + abs(y2-y1)
         
         sx, sy = start
         tx, ty = target
+        if (sx, sy) == (tx,ty): return 0
         g = defaultdict(list)
         g[(sx, sy)].append((tx, ty, dis(sx, sy, tx, ty)))
 
@@ -27,26 +29,23 @@ class Solution:
                 g[(sr[j][2],sr[j][3])].append((sr[i][0],sr[i][1], dis(sr[j][2],sr[j][3], sr[i][0],sr[i][1])))
 
         pg = [(0, sx, sy)]
-        vis = {key:False for key in g.keys()}
+        vis = []
         path = {key:inf for key in g.keys()}
         path[(sx,sy)] = 0
-        vis[(tx, ty)] = False
         path[(tx, ty)] = inf
-        # vis[(sx,sy)] = True
-        # for x, y, c in g[(sx, sy)]:
-        #     pg.append(c, x, y)
-        # heapq.heapify(pg)
+
         while pg:
             _, x, y = heapq.heappop(pg)
-            if vis[(x,y)]:continue
-            if (x,y) == (tx,ty): return path[(tx,ty)]
-            vis[(x,y)] = True
+            if (x, y) in vis: continue
+            vis.append((x,y))
             for u, v, c in g[(x,y)]:
-                heapq.heappush(pg, (c, u, v))
-                path[(u,v)] = min(path[(u,v)], path[(x,y)]+c)
+                if path[(u,v)] > path[(x,y)]+c:
+                    path[(u,v)] = path[(x,y)]+c
+                    heapq.heappush(pg, (path[(u,v)], u, v))
+        return  path[(tx,ty)]
     
 if __name__ == "__main__":
     sol = Solution()
     start, target, specialRoads = [1,1], [4,5], [[1,2,3,3,2],[3,4,4,5,1]]
-    start, target, specialRoads = [1,1], [10,4], [[4,2,1,1,3],[1,2,7,4,4],[10,3,6,1,2],[6,1,1,2,3]]
+    start, target, specialRoads = [1,1], [4,9], [[3,2,4,5,2],[4,5,4,1,1],[1,9,2,9,2],[2,2,2,5,3]]
     print(sol.minimumCost(start, target, specialRoads))

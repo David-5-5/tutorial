f59e905b62350d1bdbf0ed5f4a84d0110f432b10
David-5-5 <13910767907@139.com>
2024-10-10 16:45:46 +0800

    Append simpified implementation

--- python/algo/leecode/algo_offerII039.py
+++ python/algo/leecode/algo_offerII039.py
@@ -1,8 +1,11 @@
 # 主站 84
+from typing import List
+
+
 class Solution:
     # 题目变更为 LCR 039
     # 简化
-    def largestRectangleArea(self, heights) -> int:
+    def largestRectangleArea(self, heights: List[int]) -> int:
         heights.append(0) # 哨兵
 
         n  = len(heights)
@@ -27,7 +30,23 @@ class Solution:
 
         return maxArea
 
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        heights.append(0) # 哨兵
+
+        n  = len(heights)
+        if n == 1: return heights[0]
+        maxArea = 0
+        stack = []
+        for i in range(n):
+            while stack and heights[stack[-1]] >= heights[i]:
+                inx = stack.pop(-1)
+                maxArea = max(maxArea, heights[inx] * (i-1-(stack[-1] if stack else -1)))
+            stack.append(i)
+
+        return maxArea
+
 if __name__ == "__main__":
     sol = Solution()
     heights = [2,1,5,6,2,3]
     print(sol.largestRectangleArea(heights))
+    print(sol.largestRectangleArea2(heights))

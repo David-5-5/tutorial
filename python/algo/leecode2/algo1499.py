4125f2067b5f42567c1c610a8e4bc2239ab63a49
David-5-5 <13910767907@139.com>
2024-10-12 02:03:41 +0800

    Append implementation

--- python/algo/leecode2/algo1499.py
+++ python/algo/leecode2/algo1499.py
@@ -1,3 +1,4 @@
+from cmath import inf
 from collections import deque
 from typing import List
 class Solution:
@@ -28,9 +29,25 @@ class Solution:
                 ans = max(ans, q[-1][1] + q[-1][0] + q[0][1] - q[0][0])
         return ans
 
+    def findMaxValueOfEquation2(self, points: List[List[int]], k: int) -> int:
+        # 2024.10 再次练习，自行解答
+        q = deque() # (x, y-x) x 递增， y-x递减
+
+        ans = -inf
+        for x, y in points:
+            while q and q[0][0] + k < x:
+                q.popleft()
+            if q: ans = max(ans, q[0][1]+y+x) # 当前记录和头部记录相加即为最大值
+            while q and q[-1][1] < y-x:
+                q.pop()
+            q.append((x,y-x))
+        return ans
+
+
 if __name__ == "__main__":
     sol = Solution()
     points, k = [[-19,-12],[-13,-18],[-12,18],[-11,-8],[-8,2],[-7,12],[-5,16],[-3,9],[1,-7],[5,-4],[6,-20],[10,4],[16,4],[19,-9],[20,19]], 6
     print(sol.findMaxValueOfEquation(points, k))      
+    print(sol.findMaxValueOfEquation2(points, k))   
 
         

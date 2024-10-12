78470e89ac431378947670c52232f67ea75441ec
David-5-5 <13910767907@139.com>
2024-10-12 18:05:41 +0800

    Append comments

--- python/algo/leecode2/algo2945.py
+++ python/algo/leecode2/algo2945.py
@@ -1,3 +1,5 @@
+from collections import deque
+from itertools import accumulate
 from typing import List
 class Solution:
     def findMaximumLength(self, nums: List[int]) -> int:
@@ -29,8 +31,34 @@ class Solution:
             mo_que.append(i)
         
         return dp[n]
+    
+    def findMaximumLength2(self, nums: List[int]) -> int:
+        # 2024.10 使用 accumulate deque 等内置方法
+        n = len(nums)
+        # dp 表示 dp[i] = dp[j] + 1 
+        #        last[j] <= last[i] = psum[i+1] - psum[j]
+        # 限制条件 j < i, last[i] 尽量小 last[j]+psum[j] 递增
+        dp, last= [0] * (n + 1), [0] * (n + 1)
+        psum = list(accumulate(nums, initial=0))
 
+        q = deque()
+
+        q.append(0)
+        for i in range(1, n+1):
+            while len(q) > 1 and last[q[1]] + psum[q[1]] <= psum[i]:
+                q.popleft()
+            
+            dp[i] = dp[q[0]] + 1
+            last[i] = psum[i] - psum[q[0]]
+
+            while q and last[q[-1]] + psum[q[-1]] >= psum[i] + last[i]:
+                q.pop()
+            
+            q.append(i)
+
+        return dp[n]
 if __name__ == "__main__":
     sol = Solution()
-    nums = [418,421,309,442,80,305,166,884,791,353]
-    print(sol.findMaximumLength(nums))
+    nums = [279,641,864,511,317,169,848,277]
+    
+    print(sol.findMaximumLength2(nums))

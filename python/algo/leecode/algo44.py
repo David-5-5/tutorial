27ace22b989f7b348eec8af116e7a729d88dcecb
David-5-5 <13910767907@139.com>
2024-10-17 17:19:24 +0800

    Append comments

--- python/algo/leecode/algo44.py
+++ python/algo/leecode/algo44.py
@@ -3,7 +3,7 @@ class Solution:
         m, n = len(s), len(p)
         dp = [[False] * (n + 1) for _ in range(m + 1)]
         dp[0][0] = True
-        for i in range(1, n + 1):
+        for i in range(1, n + 1): # 匹配空字符串
             if p[i - 1] == '*':
                 dp[0][i] = True
             else:

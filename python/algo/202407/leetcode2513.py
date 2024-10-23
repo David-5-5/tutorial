cf07fff01fc2d6786b7285b76c1be29d276557ea
luming <luming@chinamobile.com>
2024-10-23 10:58:30 +0800

    Append comments

--- python/algo/202407/leetcode2513.py
+++ python/algo/202407/leetcode2513.py
@@ -1,14 +1,19 @@
 from bisect import bisect_left
 import math
 
-
+# 双周赛 94
 class Solution:
     def minimizeSet(self, divisor1: int, divisor2: int, uniqueCnt1: int, uniqueCnt2: int) -> int:
+        # 参考题解
         lcm = math.lcm(divisor1, divisor2)
 
         def check(mx:int) -> bool:
-            left1 = max(0, uniqueCnt1 - (mx // divisor2 - mx // lcm))
+            # mx // divisor2 - mx // lcm 表明仅能被 arr1 选择的数字
+            # left1 表明是剩下  uniqueCnt1 - (mx // divisor2 - mx // lcm)，
+            #       后续在两组数字共享的数字中选择 (arr2 同样处理)
+            left1 = max(0, uniqueCnt1 - (mx // divisor2 - mx // lcm)) 
             left2 = max(0, uniqueCnt2- (mx // divisor1 - mx // lcm))
+            # common 表示是arr1 和 arr2 共享的数据
             common = mx - (mx // divisor1 + mx // divisor2 - mx // lcm)
             return  common >= left1 + left2
             

b468e7e57ca43a9e697e5dd668221dfe7fadfb9e
luming <luming@chinamobile.com>
2024-10-23 11:15:44 +0800

    Append comments

--- python/algo/202407/leetcode2514.py
+++ python/algo/202407/leetcode2514.py
@@ -20,10 +20,14 @@ class Solution:
             # for i, c in enumerate(sub, 1):
             #     a = (a*i) % MOD
             #     cnt[c] += 1
-            #     b = (b*cnt[c]) % MOD                
+            #     b = (b*cnt[c]) % MOD
+            # 求逆元           
             ans = (ans * a * pow(b, -1, MOD)) % MOD
 
             # 3-th implmenetation, it pass without using pow(b, -1, MOD)
+            # 同位排列组合数相乘 每个单词的同位异构的数量为
+            #   n!/k1!k2!..km!
+            #   其中 n 为每个单词的长度, k1,.. km 为单词中 m 个字母计数
             # for i, c in enumerate(sub, 1):
             #     a = (a*i)
             #     cnt[c] += 1

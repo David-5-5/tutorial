b3b4c2fdc9141afe33fe45247807f048a1465498
David-5-5 <13910767907@139.com>
2024-10-26 21:02:37 +0800

    Add using algo using list reverse and list add which refer to leetcode 2

--- python/algo/202410/leetcode0445.py
+++ python/algo/202410/leetcode0445.py
@@ -19,3 +19,37 @@ class Solution:
                 cur = next
 
             return prev
+        
+        # equals to # leetcode 2 #
+        def add(l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:  
+            cur1, cur2, prev1, bit = l1, l2, None, 0
+            while cur1 and cur2:
+                val = cur1.val + cur2.val + bit
+                bit = val // 10
+                cur1.val = val % 10
+                prev1 = cur1
+                cur1 = cur1.next
+                cur2 = cur2.next
+            
+            if cur2: tail = cur2
+            else: tail = cur1
+
+            if tail:
+                prev1.next = tail
+                while bit:
+                    val = tail.val + bit
+                    bit = val // 10
+                    tail.val = val % 10
+                    if bit and tail.next:
+                        tail = tail.next
+                    elif bit:
+                        node = ListNode(bit)
+                        tail.next = node
+                        break
+            elif bit:
+                node = ListNode(bit)
+                prev1.next = node
+
+            return l1
+
+        return reverse(add(reverse(l1), reverse(l2)))

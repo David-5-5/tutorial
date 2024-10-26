e75acb451e6f6b58902bf24cac4079a13b907776
David-5-5 <13910767907@139.com>
2024-10-26 20:41:12 +0800

    Append the correct implementation

--- python/algo/202410/leetcode0002.py
+++ python/algo/202410/leetcode0002.py
@@ -8,22 +8,33 @@ from typing import Optional
 # 专题 链表练习
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-
-
+        # 自行解答
         cur1, cur2, prev1, bit = l1, l2, None, 0
         while cur1 and cur2:
-            cur1.val = (cur1.val + cur2.val + bit) % 10
-            if cur1.val + cur2.val + bit > 9: bit = 1
-            else:bit = 0
+            val = cur1.val + cur2.val + bit
+            bit = val // 10
+            cur1.val = val % 10
             prev1 = cur1
             cur1 = cur1.next
             cur2 = cur2.next
         
-        if cur2:
-            cur2.val += bit
-            prev1.next = cur2
+        if cur2: tail = cur2
+        else: tail = cur1
 
-        if cur1:
-            cur1.val += bit
-    
-        return reverse(l1)
+        if tail:
+            prev1.next = tail
+            while bit:
+                val = tail.val + bit
+                bit = val // 10
+                tail.val = val % 10
+                if bit and tail.next:
+                    tail = tail.next
+                elif bit:
+                    node = ListNode(bit)
+                    tail.next = node
+                    break
+        elif bit:
+            node = ListNode(bit)
+            prev1.next = node
+
+        return l1

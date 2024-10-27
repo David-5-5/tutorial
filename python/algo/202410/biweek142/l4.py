e2b7c55b83604374416ceda03fb72cf11a421885
David-5-5 <13910767907@139.com>
2024-10-27 15:29:29 +0800

    Add algo using optimized dp

--- python/algo/202410/biweek142/l4.py
+++ python/algo/202410/biweek142/l4.py
@@ -1,28 +1,47 @@
+from itertools import accumulate
+
 class Solution:
     def possibleStringCount(self, word: str, k: int) -> int:
+        # 参考题解
         if len(word) == k: return 1
 
-        ans = 0
         MOD = 10 ** 9 + 7
 
         word = word + '0'
         count = 1
-        nums = []
+        nums, res = [], 1
         for i in range(1, len(word)):
             if word[i-1] == word[i]:
                 count += 1
             else:
                 nums.append(count)
+                res = (res * count) % MOD
                 count = 1
-
-        f = [0] * k
-        f[0] = 1
-        for i in range(len(nums)):
-            for j in range(1, min(k, nums[i])+1):
-                f[k-1] += f[k-j]
+        m = len(nums)
+        if m >= k: return res
         
-        ans = pow(2, len(nums), MOD)
-        return (ans - 2 * sum(f)) % MOD
+        # f = [[0]*(k+1) for _ in range(m+1)]
+        # f[0][0] = 1
+        # for i, c in enumerate(nums):
+        #     presum = list(accumulate(f[i], initial=0))
+        #     # j <= i 的 f[i][j] 都是 0, 至少选一个
+        #     for j in range(i+1, k):
+        #         f[i+1][j] = (presum[j] - presum[max(j-c, 0)]) % MOD
+        
+        # return (res - sum(f[m][m:])) % MOD
+
+        f = [0]*(k+1)
+        f[0] = 1
+        for i, c in enumerate(nums):
+            presum = list(accumulate(f, initial=0))
+            # j <= i 的 f[j] 都是 0, 至少选一个
+            f = [0] * (k+1)
+            for j in range(i+1, k):
+                f[j] = (presum[j] - presum[max(j-c, 0)]) % MOD
+        
+        return (res - sum(f[m:])) % MOD
 
 if __name__ == "__main__":
     sol = Solution()
+    word, k = "aabbccdd", 7
+    print(sol.possibleStringCount(word, k))

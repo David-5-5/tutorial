764aa3d6115769f415952d7668d97e64cb3c1689
David-5-5 <13910767907@139.com>
2024-10-27 13:01:00 +0800

    Add algo without finish

--- python/algo/202410/biweek142/l4.py
+++ python/algo/202410/biweek142/l4.py
@@ -3,8 +3,26 @@ class Solution:
         if len(word) == k: return 1
 
         ans = 0
-       
-        return ans + 1        
+        MOD = 10 ** 9 + 7
+
+        word = word + '0'
+        count = 1
+        nums = []
+        for i in range(1, len(word)):
+            if word[i-1] == word[i]:
+                count += 1
+            else:
+                nums.append(count)
+                count = 1
+
+        f = [0] * k
+        f[0] = 1
+        for i in range(len(nums)):
+            for j in range(1, min(k, nums[i])+1):
+                f[k-1] += f[k-j]
+        
+        ans = pow(2, len(nums), MOD)
+        return (ans - 2 * sum(f)) % MOD
 
 if __name__ == "__main__":
     sol = Solution()

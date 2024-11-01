e893a393e5535e4f8247f3238847692a0ebe13fc
David-5-5 <13910767907@139.com>
2024-11-01 18:08:00 +0800

    Add algo using system permutation

---
+++ python/algo/202411/leetcode0047.py
@@ -0,0 +1,13 @@
+from collections import Counter
+from itertools import permutations
+from typing import List
+
+# 专题: 全排列
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        return list(set(permutations(nums)))
+    
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [1,2,1]
+    print(sol.permuteUnique(nums))    

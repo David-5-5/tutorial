030561cf46415580b704fd8210f67f27d76da6a9
David-5-5 <13910767907@139.com>
2024-11-01 14:50:19 +0800

    Add algo using enumerate method

---
+++ python/algo/202411/leetcode2849.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isReachableAtTime(self, sx: int, sy: int, fx: int, fy: int, t: int) -> bool:
+        if t == 1:
+            return (fx, fy) in [(sx-1,sy-1), (sx-1, sy), (sx-1, sy+1),(sx,sy-1), (sx, sy+1), (sx+1,sy-1), (sx+1, sy), (sx+1, sy+1)]
+
+        return abs(fy-sy) <= t and abs(fx-sx) <= t

023bd7eb63611c9846026f0b62ac98aa6fd40c54
David-5-5 <13910767907@139.com>
2024-11-02 16:14:43 +0800

    Append the __main__ method

--- python/algo/202411/leetcode2849.py
+++ python/algo/202411/leetcode2849.py
@@ -5,3 +5,9 @@ class Solution:
             return (fx, fy) in [(sx-1,sy-1), (sx-1, sy), (sx-1, sy+1),(sx,sy-1), (sx, sy+1), (sx+1,sy-1), (sx+1, sy), (sx+1, sy+1)]
 
         return abs(fy-sy) <= t and abs(fx-sx) <= t
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.isReachableAtTime(2, 4, 7, 7, 6))
+    print(sol.isReachableAtTime(1, 1, 4, 2, 1))
+    print(sol.isReachableAtTime(1, 2, 1, 2, 1))

7cead943a97dc6278172a5e2f453638d66ebe850
David-5-5 <13910767907@139.com>
2024-11-03 12:04:43 +0800

    Without Finish

---
+++ python/algo/202411/week422/l4.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def countBalancedPermutations(self, num: str) -> int:
+        pass
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    print(sol)

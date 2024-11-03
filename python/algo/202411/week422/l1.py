0a4e5846dae4504bdaeef89b91c869ca58a35e85
David-5-5 <13910767907@139.com>
2024-11-03 12:04:08 +0800

    Add algo

---
+++ python/algo/202411/week422/l1.py
@@ -0,0 +1,31 @@
+from bisect import bisect_left
+from functools import cache
+from typing import List
+
+
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        n , m = len(moveTime), len(moveTime[0])
+        vis = [[0] * m for _ in range(n)]
+        clock = 0
+
+        def check(mx:int) -> bool:
+            nonlocal clock
+            clock += 1
+            dis = [[0] * m for _ in range(n)]
+            @cache
+            def dfs(u:int, v:int, limit:int) :
+                
+                if u == 0 and v == 0: 
+                    return True
+
+                dis[u][v] = limit
+                if limit <= moveTime[u][v]: 
+                    return False
+                for x, y in [(u+1,v), (u-1,v), (u,v-1), (u,v+1)]:
+                    if 0<=x<n and 0<=y<m and limit > dis[x][y] and dfs(x, y, limit-1):
+                        return True
+                return False
+            return dfs(n-1,m-1,mx)
+        r = max(max(row) for row in moveTime) + n + m + 1
+        return bisect_left(range(r), True, key=check) 

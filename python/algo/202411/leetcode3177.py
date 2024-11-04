d0bd6b7334376b515515c8c150ecf59ead2de66f
David-5-5 <13910767907@139.com>
2024-11-05 00:58:39 +0800

    Append the implementation using optimizied dp

--- python/algo/202411/leetcode3177.py
+++ python/algo/202411/leetcode3177.py
@@ -5,32 +5,32 @@ from typing import List
 
 # 双周赛 132
 class Solution:
+
     def maximumLength(self, nums: List[int], k: int) -> int:
+        # DP 优化
+        # 递归 -> 递推 -> DP 优化
+        # 更改为递推的 DP 状态转移方称为
+        # dp[x][j] = max(dp[x][j] + 1, dp[y][j-1] + 1 for y in set) 
+        #   其中 x ! = y x = nums[i]
+        # 优化后的 DP 状态转移方程为：
+        # dp[x][j] = max(dp[x][j] + 1, mx[j-1]) 
+        # 维护 mx[j-1] 的数组 记录 dp[y][j-1] 的最大值
         
-        if len(nums) == 1 : return 1
-        v_pos = defaultdict(list)
-        for i, v in enumerate(nums):
-            v_pos[v].append(i)
-        
-        max_prev = [0] * (k+1)
-        ans = 0
-        @cache
-        def dfs(i:int, c:int)-> int:
-            if i < 0: return 0
-            if i == 0: return 1
+        # ！！！代码能力仍需要提升，看着转移方程，都写不出正确有效代码！！！
+        dp = defaultdict(lambda: [0] * (k + 1)) # 用 x = nums[i] 代替 i 作为状态转移方程的维度
+        mx = [0] * (k+1)
 
-            if nums[i-1] == nums[i]: return 1 + dfs(i-1, c)
+        for x in nums:
 
-            inx = bisect_left(v_pos[nums[i]], i)
-            if c == 0:
-                return 1 + (dfs(v_pos[nums[i]][inx-1], c)if inx else 0)
+            f = dp[x]
+            # 倒序遍历 dp[x][j] = 1 +  mx[j-1] 是上一个元素的 j-1
+            for j in range(k, -1, -1):
+                f[j] += 1
+                if j>0:
+                    f[j] = max(f[j], mx[j-1] + 1)
+                mx[j] = max(mx[j], f[j])
+        return mx[k]
 
-
-            res = max(1+(dfs(v_pos[nums[i]][inx-1], c) if inx else 0), 1 + max_prev[c-1])
-            max_prev[c] = max(max_prev[c], res)
-            return res
-
-        return max(dfs(i, k) for i in range(1, range(len(nums))))
 
 if __name__ == "__main__":
     sol = Solution()

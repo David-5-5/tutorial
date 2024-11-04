930f87bb87c5c1e245502ee25f7a88b38c997dbe
David-5-5 <13910767907@139.com>
2024-11-05 01:22:05 +0800

    Append the implementation using Dijkstra and parity

--- python/algo/202411/week422/l3.py
+++ python/algo/202411/week422/l3.py
@@ -1,28 +1,34 @@
 from bisect import bisect_left
+from cmath import inf
 from functools import cache
+import heapq
 from typing import List
 
 
 class Solution:
     def minTimeToReach(self, moveTime: List[List[int]]) -> int:
-        n , m = len(moveTime), len(moveTime[0])
+        # 参考题解，使用 Dijkstra 最短距离
+        n, m = len(moveTime), len(moveTime[0])
+        dist = [inf] * (n*m)
+        visited = [False] * (n*m)
 
-        def check(mx:int) -> bool:
-            dis = [[0] * m for _ in range(n)]
-            @cache
-            def dfs(u:int, v:int, limit:int, step:int, time:int) :
-                
-                if u == n-1 and v == m-1: 
-                    if moveTime[u][v] + step < limit: return True
-                    else: return False
+        dist[0] = 0
+        # The element in pr is truple (weight, vertex)
+        pr = []
+        heapq.heappush(pr, (0, 0,0)) 
+        while pr:
+            _, r, c = heapq.heappop(pr)
+            if visited[r*m+c]:
+                continue
+            visited[r*m+c] = True
+            for nr, nc in [(r-1, c),(r+1, c),(r, c-1),(r, c+1)]:
+                if 0<=nr<n and 0<=nc<m and not visited[nr*m+nc] and dist[nr*m+nc] > max(dist[r*m+c],moveTime[nr][nc])+1+(r+c)%2:
+                    dist[nr*m+nc] = max(dist[r*m+c],moveTime[nr][nc])+1+(r+c)%2
+                    heapq.heappush(pr, (dist[nr*m+nc], nr, nc))
 
-                dis[u][v] = moveTime[u][v] + step
-                if dis[u][v] >= limit: 
-                    return False
-                for x, y in [(u+1,v), (u-1,v), (u,v-1), (u,v+1)]:
-                    if 0<=x<n and 0<=y<m and moveTime[x][y]+step+time >dis[u][v] and dfs(x, y, limit, step + time , time+1):
-                        return True
-                return False
-            return dfs(0,0,mx,0, 1)
-        r = max(max(row) for row in moveTime) + n + m + 1
-        return bisect_left(range(r), True, key=check)
+        return dist[-1]
+
+if __name__ == "__main__":
+    sol = Solution()
+    moveTime = [[0,4],[4,4]]
+    print(sol.minTimeToReach(moveTime))

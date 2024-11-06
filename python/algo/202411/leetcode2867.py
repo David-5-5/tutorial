4b38f93d05cc6c6182754fa1a6092ebea402e515
David-5-5 <111078666+David-5-5@users.noreply.github.com>
2024-11-06 22:46:19 +0800

    Update leetcode2867.py
    Finish algo

--- python/algo/202411/leetcode2867.py
+++ python/algo/202411/leetcode2867.py
@@ -13,7 +13,7 @@ for i in range(2, isqrt(MX) + 1):
 # 周赛 364
 class Solution:
     def countPaths(self, n: int, edges: List[List[int]]) -> int:
-        g = [[] for _ in range(n)]
+        g = [[] for _ in range(n+1)]
         for u, v in edges:
             g[u].append(v)
             g[v].append(u)
@@ -23,12 +23,29 @@ class Solution:
         def dfs(u:int, fa:int) :
             nodes.append(u)
             for v in g[u]:
-                if v == fa: continue
-                if not is_prime[v]: dfs(v, u)
+                if v != fa and not is_prime[v]: dfs(v, u)
         
-        count = [-1] * n
+        count = [-1] * (n+1)
+        ans = 0
         for x in range(1, n+1):
-            if not is_prime: continue # 非质数，忽略
-            
+            if not is_prime[x]: continue # 非质数，忽略
 
+            left = 0
+            for y in g[x]:
+                if is_prime[y]: continue
+                if count[y] == -1:
+                    nodes.clear()
+                    dfs(y, -1)
+                    for v in nodes:
+                        count[v] = len(nodes)
+                ans += left * count[y]
+                left += count[y]
+            ans += left
+        return ans
+
+if __name__ == "__main__":
+    sol = Solution()
+    n, edges = 5, [[1,2],[1,3],[2,4],[2,5]]
+    print(sol.countPaths(n, edges))
+                    
 

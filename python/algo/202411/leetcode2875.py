534b3276fb5155af00028a9ca76117c671bcdb74
luming <luming@chinamobile.com>
2024-11-07 00:34:45 +0800

    Add definition of method

---
+++ python/algo/202411/leetcode2875.py
@@ -0,0 +1,8 @@
+from itertools import accumulate
+from math import inf
+from typing import List
+from sortedcontainers import SortedList
+
+# 周赛 365
+class Solution:
+    def minSizeSubarray(self, nums: List[int], target: int) -> int:

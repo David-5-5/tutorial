42d8a61b7afce47b5758f55689d461221d0cdd1e
David-5-5 <111078666+David-5-5@users.noreply.github.com>
2024-11-07 19:51:39 +0800

    Update leetcode2876.py
    Append the code construct the graph

--- python/algo/202411/leetcode2876.py
+++ python/algo/202411/leetcode2876.py
@@ -3,4 +3,10 @@ from typing import List
 # 周赛 365
 class Solution:
     def countVisitedNodes(self, edges: List[int]) -> List[int]:
-        
+        n = len(edges)
+        g = [[] for _ in range(n)]
+        deg = [0] * n
+
+        for u, v in enumerate(edges):
+            g[u].append[v]
+            deg[v] += 1

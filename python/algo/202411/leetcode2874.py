e17e080cef80b6c299569c1b4204864f15998d3c
David-5-5 <111078666+David-5-5@users.noreply.github.com>
2024-11-08 23:51:44 +0800

    Update leetcode2874.py
    Append implementation using iterate with j

--- python/algo/202411/leetcode2874.py
+++ python/algo/202411/leetcode2874.py
@@ -1,6 +1,23 @@
 from typing import List
 
-
+# 周赛 365
 class Solution:
     def maximumTripletValue(self, nums: List[int]) -> int:
-        pass
+        # 参考题解 前后缀分解 前缀最大值 + 后缀最大值 遍历 j
+        n = len(nums)
+        suf_max, per_max = [0] * n, [0] * n
+
+        suf_max[-1] = nums[-1]
+        for i in range(n-2, -1, -1):
+            suf_max[i] = max(suf_max[i+1], nums[i])
+        
+        per_max[0] = nums[0]
+        for i in range(1, n):
+            per_max[i] = max(per_max[i-1], nums[i])
+        
+        ans = 0
+        # 遍历 j
+        for i in range(1, n-1):
+            ans = max(ans, (per_max[i-1]-nums[i])*suf_max[i+1])
+        
+        return ans

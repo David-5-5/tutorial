9719b23e0e25b2ac5dae8b9dcdaea3da65e6cbc5
David-5-5 <13910767907@139.com>
2024-11-11 18:03:55 +0800

    Append the optimize

--- python/algo/202411/week423/l3.py
+++ python/algo/202411/week423/l3.py
@@ -4,24 +4,21 @@ from typing import List
 
 class Solution:
     def sumOfGoodSubsequences(self, nums: List[int]) -> int:
+        # 自行解答 
         MOD = 10 ** 9 + 7
-        s = defaultdict(int)
-        cnt = defaultdict(int)
+        dp = defaultdict(lambda: [0,0])
         ans = 0
 
         for v in nums:
-            c = 1
-            ss = 0
-            if v-1 in s.keys():
-                ss += s[v-1]
-                c += cnt[v-1]
-            if v+1 in s.keys():
-                ss += s[v+1]
-                c += cnt[v+1]
-            g = ss + c * v
-            ans = (ans + g) % MOD
-            cnt[v] += c
-            s[v] += g
+            ss, cnt = 0, 1 # 本身
+            for k in [v-1, v+1]:
+                if k in dp.keys():
+                    ss += dp[k][0]
+                    cnt += dp[k][1]
+            cur_sum = ss + cnt * v
+            ans = (ans + cur_sum) % MOD
+            dp[v][1] += cnt
+            dp[v][0] += cur_sum
 
         return ans % MOD
 

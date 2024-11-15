58405c19b9f6b528269318a492b14fcf53ce36bc
David-5-5 <13910767907@139.com>
2024-11-16 02:13:56 +0800

    Append implementation using logTrick and three pointer

--- python/algo/202411/leetcode3209.py
+++ python/algo/202411/leetcode3209.py
@@ -18,7 +18,22 @@ class Solution:
             ans += r - l 
         return ans
 
+    def countSubarrays2(self, nums: List[int], k: int) -> int:
+        # 参考题解 logTrick + 三指针
+        ans = l = r = 0
+        for i, v in enumerate(nums):
 
+            for j in range(i-1, -1, -1):
+                if nums[j] == nums[j] & v:
+                    break
+                nums[j] &= v
+            
+            while l <= i and nums[l] < k:
+                l += 1
+            while r <= i and nums[r] <= k:
+                r += 1
+            ans += r - l 
+        return ans
 
 if __name__ == "__main__":
     sol = Solution()

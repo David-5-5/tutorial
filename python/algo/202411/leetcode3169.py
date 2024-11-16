a1afa55aa937797958455e40e1a10d9b41a4e3fd
David-5-5 <13910767907@139.com>
2024-11-16 14:14:46 +0800

    Append new implementation

--- python/algo/202411/leetcode3169.py
+++ python/algo/202411/leetcode3169.py
@@ -6,7 +6,7 @@ class Solution:
         # 类似于 leetcode 56, 排序后 O(N) 的时间复杂度进行区间合并，并计算区间外的天数
         # 不需要 SortDict，这样做比较复杂
         meetings.sort() # meetings.sort(key=lambda p:p[0])
-        start, end = 1, 0
+        start, end = 1, 0 # 边界设置，有技巧 end - start + 1 = 0
         for s, e in meetings:
             if s > end:
                 days -= end - start + 1
@@ -16,7 +16,20 @@ class Solution:
         days -= end - start + 1
         return days
 
-    
+    def countDays2(self, days: int, meetings: List[List[int]]) -> int:
+        # 另一种边界设置
+        meetings.sort() # meetings.sort(key=lambda p:p[0])
+        start, end = meetings[0]
+        for s, e in meetings[1:]:
+            if s > end:
+                days -= end - start + 1
+                start = s # 下一个区间
+            end = max(end, e) # 存在包含关系
+        
+        days -= end - start + 1
+        return days
+
+
 if __name__ == "__main__":
     sol = Solution()
     days, meetings = 10, [[5,7],[1,3],[9,10]]

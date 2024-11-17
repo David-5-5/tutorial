151e157257bb9b03472fc49547f5256ceac8c6d9
David-5-5 <13910767907@139.com>
2024-11-17 16:09:23 +0800

    Add algo using perfix and suffix of miminze value

---
+++ python/algo/202411/leetcode2909.py
@@ -0,0 +1,21 @@
+from math import inf
+from typing import List
+
+# 周赛 368
+class Solution:
+    def minimumSum(self, nums: List[int]) -> int:
+        n = len(nums)
+        suf_mx = [0] * n
+        suf_mx[-1] = nums[-1]
+
+        # 计算后缀最小值
+        for i in range(n-2, -1, -1):
+            suf_mx[i] = min(nums[i], suf_mx[i+1])
+        
+        ans, pre_mx = inf, nums[0]
+        for i in range(1,n-1):
+            if nums[i] > pre_mx and nums[i] > suf_mx[i+1]:
+                ans = min(ans, nums[i] + pre_mx + suf_mx[i+1])
+            # 更新前缀最小值
+            if pre_mx > nums[i]: pre_mx = nums[i]
+        return ans if ans < inf else -1

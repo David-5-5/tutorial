8dea197262cf01bc81380b1e269443b8399e7cc4
David-5-5 <13910767907@139.com>
2024-11-17 12:07:52 +0800

    Add algo using differency array and binary search

---
+++ python/algo/202411/week424/l3.py
@@ -0,0 +1,23 @@
+from bisect import bisect_left
+from itertools import accumulate
+from typing import List
+
+
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        n = len(queries)
+        def check(k:int):
+            diff = [0] * len(nums)
+            for l, r, v in queries[0:k]:
+                diff[l] += v
+                if r + 1 < len(nums): diff[r+1] -= v
+            pres = list(accumulate(diff))
+            return all(ori<=qu for ori, qu in zip(nums, pres))
+
+        ans = bisect_left(range(n+1), True, key=check)        
+        return ans if ans < n+1 else -1
+    
+if __name__ == "__main__":
+    sol = Solution()
+    nums, queries = [10], [[0,0,5],[0,0,3],[0,0,2],[0,0,1],[0,0,4],[0,0,1],[0,0,4],[0,0,5],[0,0,3],[0,0,4],[0,0,1]]
+    print(sol.isZeroArray(nums,queries))

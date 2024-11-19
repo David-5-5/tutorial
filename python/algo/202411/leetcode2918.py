ae8b427ca00ab96dd5328a96b6197727d303ca78
David-5-5 <13910767907@139.com>
2024-11-19 11:17:38 +0800

    Append new implementation refer to solution

--- python/algo/202411/leetcode2918.py
+++ python/algo/202411/leetcode2918.py
@@ -4,21 +4,7 @@ from typing import List
 # 周赛 369
 class Solution:
     def minSum(self, nums1: List[int], nums2: List[int]) -> int:
-        # mx1, mn1, mx2, mn2 = 0, 0, 0, 0
-        # for v in nums1:
-        #     if v == 0:
-        #         mx1 = inf
-        #         mn1 += 1
-        #     else:
-        #         mx1 += v
-        #         mn1 += v
-        # for v in nums2:
-        #     if v == 0:
-        #         mx2 = inf
-        #         mn2 += 1
-        #     else:
-        #         mx2 += v
-        #         mn2 += v
+        # 自行解答 分类讨论
         mn1 = mx1 = sum(nums1)
         mn2 = mx2 = sum(nums2)
         cnt1, cnt2 = nums1.count(0), nums2.count(0)
@@ -28,11 +14,20 @@ class Solution:
         if cnt2:
             mn2 += cnt2
             mx2 = inf
-        if mx1 == inf and mx2 == inf: return max(mn1, mn2)
+        # if mx1 == inf and mx2 == inf: return max(mn1, mn2)
         if mn1 > mx2 or mn2 > mx1: return -1
         else: 
             return min(max(mn1, mn2), min(mx1, mx2))
+        
+    def minSum(self, nums1: List[int], nums2: List[int]) -> int:
+        # 参考题解
+        mn1 = sum(max(v, 1) for v in nums1)
+        mn2 = sum(max(v, 1) for v in nums2)
 
+        if( mn1 < mn2 and nums1.count(0) == 0) or (mn1 > mn2 and nums2.count(0) == 0):
+            return -1
+        else: return max(mn1, mn2)
+        
 if __name__ == "__main__":
     sol = Solution()
     nums1, nums2 = [0,17,20,17,5,0,14,19,7,8,16,18,6], [21,1,27,19,2,2,24,21,16,1,13,27,8,5,3,11,13,7,29,7]

4d534791013e927b9c5def8c342a028c83f190da
David-5-5 <13910767907@139.com>
2024-11-22 22:38:42 +0800

    Append comments

--- python/algo/202411/leetcode2554.py
+++ python/algo/202411/leetcode2554.py
@@ -3,10 +3,11 @@ from typing import List
 # 贪心专题
 class Solution:
     def maxCount(self, banned: List[int], n: int, maxSum: int) -> int:
+        # 编程题，必须用 set 替换 list
         ban = set(banned)
         ans = tot = 0
         for i in range(1, n+1):
-            if i in ban:continue
+            if i in ban:continue # O(logN), 如果是 list O(N)
             if tot + i > maxSum:break
             tot += i
             ans += 1

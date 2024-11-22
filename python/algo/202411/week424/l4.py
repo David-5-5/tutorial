d4d06006cc08790f7525d18a32335df1e131e793
David-5-5 <13910767907@139.com>
2024-11-22 19:07:56 +0800

    Append partial code

--- python/algo/202411/week424/l4.py
+++ python/algo/202411/week424/l4.py
@@ -4,14 +4,26 @@ from typing import List
 
 class Solution:
     def minDifference(self, nums: List[int]) -> int:
-        mn, mx = inf, -inf
+        # 参考题解，区间贪心
+        minL, maxR, n = inf, 0, len(nums)
         for v in nums:
-            if v == -1:continue
-            if v < mn: mn = v
-            if v > mx: mx = v
-        if mx <= mn: return 0
+            if minL > v: minL = v
+            if maxR < v: maxR = v
+        
+        ans = 0
+        if minL >= maxR: return 0
 
-        # def check()
+        for i, v in enumerate(nums):
+            if nums[i] == -1:
+                l, r = minL, maxR
+                if i > 0: l = nums[i-1]
+                if i + 1 < n: r = nums[i+1]
+                if l == -1 or r == -1:
+                    ans = max(ans, (maxR - minL + 2)//3)
+                else:
+                    ans = max(ans, min(r-minL, maxR-l))
+                    
+
         
     
 if __name__ == "__main__":

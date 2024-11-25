d8c5e64ff47f940b7faf076b563aa0dd0b25917d
David-5-5 <13910767907@139.com>
2024-11-25 20:04:40 +0800

    Append comments

--- python/algo/202411/leetcode1775.py
+++ python/algo/202411/leetcode1775.py
@@ -3,6 +3,7 @@ from typing import List
 # 贪心专题
 class Solution:
     def minOperations(self, nums1: List[int], nums2: List[int]) -> int:
+        # 自行解答
         nums1.sort()
         nums2.sort()
         sum1, sum2 = sum(nums1), sum(nums2)
@@ -13,7 +14,9 @@ class Solution:
         
         ans, diff, i, j, n = 0, abs(sum2 - sum1), 0, len(nums2) - 1, len(nums1)
         
-        while diff and (i < n or j >=0):
+        # 每次将差距减少一个最大值，从和小的数组从最小值开始增加数
+        # 从和大的数组从最大值开始
+        while diff and ((i < n and 6 - nums1[i]) or (j >=0 and nums2[j] - 1)) :
             if i < n and j >= 0:
                 if 6 - nums1[i] > nums2[j] - 1:
                     diff -= min(diff, 6 - nums1[i])

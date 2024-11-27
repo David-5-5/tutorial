cc2d84115e3bfb42a873a6130ac82e603b914ba3
David-5-5 <13910767907@139.com>
2024-11-28 00:43:23 +0800

    Add algo using greedy

---
+++ python/algo/202411/leetcode1007.py
@@ -0,0 +1,60 @@
+from math import inf
+from typing import List
+
+# 贪心专题
+class Solution:
+    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        # 自行解答，第一个元素分别和后面元素比较，四种情况：
+        # top[0] 在 top          和后续的tops, bottoms比较需要旋转次数
+        #        旋转到bottom(+1) 和后续的tops, bottoms比较需要旋转次数
+        # botton[0] 在 bottom    和后续的tops, bottoms比较需要旋转次数
+        #        旋转到 top(+1)   和后续的tops, bottoms比较需要旋转次数
+        # 比较罗嗦
+        ans = inf
+        
+        res, fst = 0, tops[0]
+        for t, b in zip(tops[1:], bottoms[1:]):
+            if fst == t:
+                continue
+            elif fst == b: res += 1
+            else:
+                res = -1
+                break
+        if res != -1: ans = min(ans, res)
+        
+        res, fst= 1, tops[0]
+        for t, b in zip(tops[1:], bottoms[1:]):
+            if fst == b:
+                continue
+            elif fst == t: res += 1
+            else:
+                res = -1
+                break
+        if res != -1: ans = min(ans, res)
+
+        res, fst = 0, bottoms[0]
+        for t, b in zip(tops[1:], bottoms[1:]):
+            if fst == b: 
+                double += 1
+                continue
+            elif fst == t: res += 1
+            else:
+                res = -1
+                break
+        if res != -1: ans = min(ans, res)
+        
+        res, fst = 1, bottoms[0]
+        for t, b in zip(tops[1:], bottoms[1:]):
+            if fst == t: 
+                continue
+            elif fst == b: res += 1
+            else:
+                res = -1
+                break
+        if res != -1: ans = min(ans, res)
+        return -1 if ans == inf else ans
+
+if __name__ == "__main__":
+    sol = Solution()
+    tops, bottoms =[2,1,1,3,2,1,2,2,1], [3,2,3,1,3,2,3,3,2]
+    print(sol.minDominoRotations(tops, bottoms))

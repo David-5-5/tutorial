14cd93295723f8b84ca6f5a54028ebf2ce50a506
David-5-5 <13910767907@139.com>
2024-11-27 15:52:06 +0800

    Add algo using greedy

---
+++ python/algo/202411/leetcode1144.py
@@ -0,0 +1,17 @@
+from math import inf
+from typing import List
+
+# 贪心专题 从最左/最右开始贪心
+class Solution:
+    def movesToMakeZigzag(self, nums: List[int]) -> int:
+        # 自行解答
+        n = len(nums)
+        res1 = res2 = 0
+
+        for i in range(1, n, 2):
+            res1 += nums[i] - min(nums[i], min(nums[i-1], nums[i+1] if i+1<n else inf)-1)
+        
+        for i in range(0, n, 2):
+            res2 += nums[i] - min(nums[i], min(nums[i-1] if i>0 else inf, nums[i+1] if i+1<n else inf)-1)
+        
+        return min(res1, res2)

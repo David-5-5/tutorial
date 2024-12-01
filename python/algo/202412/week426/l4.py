6af08f9bced16019b0afacd2a475de5a32cf8731
David-5-5 <13910767907@139.com>
2024-12-01 14:45:57 +0800

    Append new implementation extract common methods

--- python/algo/202412/week426/l4.py
+++ python/algo/202412/week426/l4.py
@@ -25,7 +25,8 @@ class Solution:
         #   2, 节点 u 加上以 fa 为子节点后的距离为奇数、偶数的节点数
         #           ans[u][0] = ans[u][0] + new_fa_even
         #           ans[u][1] = ans[u][1] + new_fa_odd
-        #  注，需要用临时变量，ans[u] 变化
+        #  注，1、需要用临时变量，ans[u] 变化
+        #     2、父子节点的解耦性互反
         ans1 = [[0,0] for _ in range(n)]
         ans2 = [[0,0] for _ in range(m)]
 
@@ -51,6 +52,7 @@ class Solution:
         dfs2(0, -1)
 
         mx_odd, _ = ans2[0]
+        # 换根 DP
         def reroot2(u:int, fa:int):
             nonlocal mx_odd
             if fa != -1:
@@ -82,9 +84,57 @@ class Solution:
         reroot1(0, -1)
         return [even + mx_odd for _,even in ans1]
 
+    def maxTargetNodes2(self, edges1: List[List[int]], edges2: List[List[int]]) -> List[int]:
+        # 基于 maxTargetNodes 抽象公共方法，简化代码
+        n, m = len(edges1) + 1, len(edges2) + 1
+        
+        g1, g2 = [[] for _ in range(n)], [[] for _ in range(m)]
+        for u, v in edges1:
+            g1[u].append(v)
+            g1[v].append(u)
+
+        for u, v in edges2:
+            g2[u].append(v)
+            g2[v].append(u)
+
+        ans1 = [[0,0] for _ in range(n)]
+        ans2 = [[0,0] for _ in range(m)]
+
+        def dfs(g:List[List[int]], u:int, fa:int, ans: List[List[int]]) :
+            ans[u] = [0, 1]
+
+            for v in g[u]:
+                if v == fa:continue
+                dfs(g, v, u, ans)
+                ans[u][0] += ans[v][1]
+                ans[u][1] += ans[v][0]
+        
+        dfs(g1, 0, -1, ans1)
+        dfs(g2, 0, -1, ans2)
+
+        mx_odd, _ = ans2[0]
+        def reroot(g:List[List[int]], u:int, fa:int, need_mx:bool, ans: List[List[int]]):
+            nonlocal mx_odd
+            if fa != -1:
+                fa_odd = ans[fa][0] - ans[u][1]
+                fa_even = ans[fa][1] - ans[u][0]
+
+                ans[u][0] = ans[u][0] + fa_even
+                ans[u][1] = ans[u][1] + fa_odd
+                if need_mx and ans[u][1] > mx_odd : mx_odd =  ans[u][1]
+
+            for v in g[u]:
+                if v == fa: continue
+                reroot(g, v, u, need_mx, ans)
+
+        reroot(g2, 0, -1, True, ans2)
+        reroot(g1, 0, -1, False, ans1)
+        return [even + mx_odd for _,even in ans1]
+
 if __name__ == "__main__":
     sol = Solution()
     edges1, edges2 = [[0,1],[0,2],[2,3],[2,4]], [[0,1],[0,2],[0,3],[2,7],[1,4],[4,5],[4,6]]
     edges1, edges2 = [[0,1],[0,2],[0,3],[0,4]], [[0,1],[1,2],[2,3]]
 
     print(sol.maxTargetNodes(edges1, edges2))
+    print(sol.maxTargetNodes2(edges1, edges2))

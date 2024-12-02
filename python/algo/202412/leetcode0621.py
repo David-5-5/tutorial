b2723f79b7dc0e3bbdd5499a3605c997a14ccd9f
David-5-5 <13910767907@139.com>
2024-12-02 16:01:23 +0800

    Append new implementation using enumerate method

--- python/algo/202412/leetcode0621.py
+++ python/algo/202412/leetcode0621.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import heapq
 from typing import List
 
 # 贪心专题 相邻不同 证明/构造方案
@@ -38,9 +39,34 @@ class Solution:
 
         # return max((maxExec - 1) * (n + 1) + maxCount, len(tasks))
 
-    
+    def leastInterval2(self, tasks: List[str], n: int) -> int:
+        # 参考题解 模拟方法 设置二元组，每个任务类型 (valid, rest)
+        # 设置 time，可以执行的任务类型的 valid <= time
+        # 遍历任务类型列表，找到满足 valid <= time 最大的rest
+        # 并将任务类型的二元组更新为 (time+n+1, rest-1)
+        cnt = Counter(tasks)
+        t = []
+        for _, c in cnt.items():
+            t.append((0, c))    # 初始化任务类型二元组
+        
+        time = 0
+        while t:
+            next = -1
+            for i in range(len(t)):
+                if t[i][0]<=time and (next == -1 or t[i][1] > t[next][1]):
+                    next = i # 查找满足条件的最大的 rest 下标
+            if next != -1:
+                _, c = t.pop(next)
+                if c - 1 > 0:   # 剩余次数大于 0
+                    t.append((time+n+1, c-1))
+            time += 1
+
+        return time
+
 if __name__ == "__main__":
     sol = Solution()
     tasks, n = ["A","A","A","B","B","B"], 2
-    tasks, n = ["A","B","C","D","A","B","V"], 3
+    # tasks, n = ["A","B","C","D","A","B","V"], 3
+    # tasks, n = ["B","C","D","A","A","A","A","G"], 1
     print(sol.leastInterval(tasks, n))
+    print(sol.leastInterval2(tasks, n))    

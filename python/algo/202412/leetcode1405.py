94e8742887c6e5bd41c2aa63860ca113399ae182
David-5-5 <13910767907@139.com>
2024-12-04 23:39:31 +0800

    Append implementation using priority queue

--- python/algo/202412/leetcode1405.py
+++ python/algo/202412/leetcode1405.py
@@ -1,31 +1,33 @@
+import heapq
+
+# 贪心专题 相邻不同 证明/构造方案
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        cnt = [[a, 'a'], [b, 'b'], [c, 'c']]
-
-        cnt.sort(reverse=True)
-        odd_t = False
-        if cnt[0][0] > cnt[1][0] + cnt[2][0] + 1:
-            m = cnt[0][0] // 2 + cnt[0][0] % 2
-            if m > cnt[1][0] + cnt[2][0] + 1:
-                m = cnt[1][0] + cnt[2][0] + 1
+        # 自行解答，使用堆，优先使用数量最多的字符
+        cnt = []
+        if a : cnt.append((-a, 'a'))
+        if b : cnt.append((-b, 'b'))
+        if c : cnt.append((-c, 'c'))
+        heapq.heapify(cnt)
+        ans = []
+        while cnt:
+            count, ch = heapq.heappop(cnt)
+            if len(ans) >= 2 and ans[-1] == ans[-2] == ch:
+                if not cnt:
+                    break
+                else:
+                    nxt_c, nxt_ch = heapq.heappop(cnt)
+                    ans.append(nxt_ch)
+                    if nxt_c + 1:
+                        heapq.heappush(cnt, (nxt_c+1, nxt_ch))
+                    ans.append(ch)
             else:
-                if cnt[0][0] % 2: odd_t = True
-            cnt[0][0] = m
-            cnt[0][1] += cnt[0][1]
-        
-        ans = [''] * (sum(l for l, _ in cnt))
-        begin = 0
-        for ch_c, ch in cnt:
-            for i in range(ch_c):
-                if odd_t and i + 1 == ch_c:
-                    ans[begin] = ch[0]
-                else: ans[begin] = ch
-                begin += 2
-                if begin >= len(ans):
-                    begin = 1
-                
+                ans.append(ch)
+            if count + 1:
+                heapq.heappush(cnt, (count+1, ch))
         return "".join(ans)
-            
+        
+
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.longestDiverseString(1,1,7))
+    print(sol.longestDiverseString(7,1,0))

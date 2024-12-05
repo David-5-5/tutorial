202a14189ee6ccd2d5a31ac2b9231c5cb2e19c8d
David-5-5 <13910767907@139.com>
2024-12-06 00:58:11 +0800

    Add algo using hash based on 421

---
+++ python/algo/202412/leetcode2935.py
@@ -0,0 +1,23 @@
+from typing import List
+
+# 周赛 371
+class Solution:
+    def maximumStrongPairXor(self, nums: List[int]) -> int:
+        # 参考题解，从 421 复制代码并修改
+        nums.sort() # 排序，前面遍历的小于后面的
+
+        target = 0
+        high = max(nums).bit_length() - 1
+
+        for i in range(high, -1, -1):
+            target <<= 1
+            newval = target + 1 # 尽量找当前位为 1
+            h = dict() # key 为 高位的mask，value 为原值
+            for v in nums:
+                v_mask = v >> i
+                # !!!太巧妙了!!!
+                if v_mask ^ newval in h and h[v_mask ^ newval]*2>=v:
+                    target = newval # 找到当前位为1的XOR
+                    break
+                h[v_mask] = v
+        return target

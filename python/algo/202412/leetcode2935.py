ce68f46428eb5cee8191054a9600df54fda65de7
David-5-5 <13910767907@139.com>
2024-12-06 15:01:51 +0800

    Append implementation using 0-1 Trie

--- python/algo/202412/leetcode2935.py
+++ python/algo/202412/leetcode2935.py
@@ -1,22 +1,51 @@
 from typing import List
 
-
 # The Trie class
-class Trie:
-    
+class Node:
+    __slots__ = 'children', 'cnt'
+
     def __init__(self):
         self.children = [None, None]
-        self.cnt = 0
+        self.cnt = 0  # 子树大小
+
+class Trie:
+    # 0-1 字典树
+    def __init__(self, h:int):
+        self.root = Node()
+        self.HIGH_BIT = h
     
-    def insert(self, word):
-        cur = self
-        for c in word:
-            cur = cur.children[c]
+    def insert(self, v:int):
+        cur = self.root
+        cur.cnt += 1
+        for i in range(self.HIGH_BIT, -1, -1):
+            if cur.children[v>>i&1] is None:
+                cur.children[v>>i&1] = Node()
+            cur = cur.children[v>>i&1]
             cur.cnt += 1
+    
+    def remove(self, v:int):
+        cur = self.root
+        cur.cnt -= 1
+        for i in range(self.HIGH_BIT, -1, -1):
+            cur = cur.children[v>>i&1]
+            cur.cnt -= 1
+    
+    # 求异或最大值
+    def max_xor(self, y:int) -> int:
+        ans = 0
+        cur = self.root
+        for i in range(self.HIGH_BIT, -1, -1):
+            mask = y>>i & 1
+            if cur.children[mask^1] is not None and cur.children[mask^1].cnt != 0:
+                ans |= 1 << i
+                cur = cur.children[mask^1]
+            else: cur = cur.children[mask]
+        return ans
+
 # 周赛 371
 class Solution:
     def maximumStrongPairXor(self, nums: List[int]) -> int:
-        # 参考题解，从 421 复制代码并修改
+        # 参考题解，哈希 从 421 复制代码并修改
         nums.sort() # 排序，前面遍历的小于后面的
 
         target = 0
@@ -34,3 +63,20 @@ class Solution:
                     break
                 h[v_mask] = v # 如果有v_mask一样的，可以直接覆盖, 为什么？
         return target
+
+
+    def maximumStrongPairXor2(self, nums: List[int]) -> int:
+        # 参考题解，0-1 Trie
+        nums.sort() # 排序，前面遍历的小于后面的
+        
+        trie = Trie(nums[-1].bit_length() - 1)
+
+        ans = l = 0
+        for v in nums:
+            while nums[l] * 2 < v:
+                trie.remove(nums[l])
+                l += 1
+            trie.insert(v)
+            ans = max(ans, trie.max_xor(v))
+        
+        return ans

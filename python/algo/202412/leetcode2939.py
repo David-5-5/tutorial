908f894f771aa367440c72b0145e3ab571f23974
David-5-5 <13910767907@139.com>
2024-12-12 11:24:55 +0800

    Add algo using bit operation

---
+++ python/algo/202412/leetcode2939.py
@@ -0,0 +1,33 @@
+
+class Solution:
+    def maximumXorProduct(self, a: int, b: int, n: int) -> int:
+        # 自行解答，位运算
+        MOD = 10 ** 9 + 7
+        if a > b: a, b = b, a # a < b
+
+        mx = (2 ** 50 - 1)
+        for i in range(n):
+            mx -= 1 << i
+        
+        to_avg = False
+        if a & mx == b & mx:
+            to_avg = True
+        
+        first = True
+        for i in range(n-1, -1, -1):
+            if a >> i & 1 == b >> i & 1: # 相等
+                a |= 1 << i
+                b |= 1 << i
+            else: # 不等
+                if not to_avg or first: 
+                    a |= 1 << i
+                    if b >> i & 1: b ^= 1 << i
+                    if to_avg: first = False
+                else:
+                    b |= 1 << i
+                    if a >> i & 1: a ^= 1 << i
+        return (a * b) % MOD
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.maximumXorProduct(12, 5, 4))

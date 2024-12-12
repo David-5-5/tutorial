791fbae0f2f89636186451684279d483bbec2899
David-5-5 <13910767907@139.com>
2024-12-12 15:51:20 +0800

    Append comments

--- python/algo/202412/leetcode2939.py
+++ python/algo/202412/leetcode2939.py
@@ -2,6 +2,13 @@
 class Solution:
     def maximumXorProduct(self, a: int, b: int, n: int) -> int:
         # 自行解答，位运算
+        # 最初的思路，求 (a XOR X) * (b XOR X) 的最大值 按位比较 a b，
+        #   相同，则 a，b 同时设置为 1
+        #   不同，则 将第一个不同位 1 设置到 a 上，其余不同位 1 设置到 b 上，确保 a b 的差尽量小
+        #        不同位上的数字分配到 a b 上，这样乘积是最大的
+        #        分两种情况判断：
+        #           第一种情况，a b 的最高位一样或在n的范围内，将不同的数字平均分配到 a b上
+        #           第二种情况，a b 的最高位不一样且在n范围外，将所用数字分配到小的数字上
         MOD = 10 ** 9 + 7
         if a > b: a, b = b, a # a < b
 
@@ -29,7 +36,7 @@ class Solution:
         return (a * b) % MOD
 
     def maximumXorProduct2(self, a: int, b: int, n: int) -> int:
-        # 位运算优化
+        # 位运算优化，思路同 maximumXorProduct 优化了位运算的添加元素/删除元素操作
         MOD = 10 ** 9 + 7
         if a > b: a, b = b, a # a < b
 
@@ -44,8 +51,8 @@ class Solution:
                 b |= 1 << i
             else: # 不等
                 if not to_avg or first: 
-                    a |= 1 << i
-                    b &= ~(1 << i)
+                    a |= 1 << i     # 将元素 i 加入集合
+                    b &= ~(1 << i)  # 将元素 i 移出集合
                     if to_avg: first = False
                 else:
                     b |= 1 << i
@@ -54,7 +61,8 @@ class Solution:
 
 
     def maximumXorProduct3(self, a: int, b: int, n: int) -> int:
-        # 位运算优化
+        # 去掉不必要的情况判断，在位不同的时候，把元素添加到小的数字中即可
+        # 优化后代码如下
         MOD = 10 ** 9 + 7
 
         for i in range(n-1, -1, -1):
@@ -62,9 +70,9 @@ class Solution:
                 a |= 1 << i
                 b |= 1 << i
             else: # 不等
-                if a < b:
-                    a |= 1 << i
-                    b &= ~(1 << i)
+                if a < b:   # 尽量向小的数中增加值
+                    a |= 1 << i     # 将元素 i 加入集合
+                    b &= ~(1 << i)  # 将元素 i 移出集合
                 else:
                     b |= 1 << i
                     a &= ~(1 << i)

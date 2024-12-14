8266a7651f85086c98af14b04e1ae50bdc53d332
David-5-5 <13910767907@139.com>
2024-12-14 15:40:25 +0800

    Add algo using greedy by sorting right endpoint

---
+++ python/algo/202412/leetcode0646.py
@@ -0,0 +1,17 @@
+from typing import List
+
+# 贪心专题 - 不相交区间
+class Solution:
+    def findLongestChain(self, pairs: List[List[int]]) -> int:
+        pairs.sort(key=lambda p:p[1]) # 按右端点排序
+        # 因为右端点尽量小，才能有 后续的pair 的左端点 > 当前的右端点
+
+        right = pairs[0][1]
+        ans = 1
+        for l, r in pairs[1:]:
+            if l > right:
+                ans += 1
+                right = r
+        return ans
+
+

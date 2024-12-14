46eab8ef41a59ddbbc7994569ec1c4ff6f46b90e
David-5-5 <13910767907@139.com>
2024-12-14 15:52:37 +0800

    Append comments

--- python/algo/202412/leetcode0646.py
+++ python/algo/202412/leetcode0646.py
@@ -3,6 +3,7 @@ from typing import List
 # 贪心专题 - 不相交区间
 class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
+        # 自行解答
         pairs.sort(key=lambda p:p[1]) # 按右端点排序
         # 因为右端点尽量小，才能有 后续的pair 的左端点 > 当前的右端点
 

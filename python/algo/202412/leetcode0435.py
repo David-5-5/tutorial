784fb1ea06564e5f6e7604c2a077c58926e11097
David-5-5 <13910767907@139.com>
2024-12-14 21:28:17 +0800

    Append implementation using 646

--- python/algo/202412/leetcode0435.py
+++ python/algo/202412/leetcode0435.py
@@ -14,3 +14,16 @@ class Solution:
             if r > left: ans += 1 # 删除区间
             else: left = l        # 选择区间，并更新左端点的值
         return ans
+
+        intervals.sort(key=lambda p:p[1]) # 按右端点排序
+        # 因为右端点尽量小，才能有 后续的pair 的左端点 > 当前的右端点
+
+    def eraseOverlapIntervals2(self, intervals: List[List[int]]) -> int:
+        # 同 646
+        right = intervals[0][1]
+        ans = 1
+        for l, r in intervals[1:]:
+            if l >= right: # > 改为 >=
+                ans += 1   # 保留的数
+                right = r
+        return len(intervals) - ans # 删除的数

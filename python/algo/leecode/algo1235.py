f3847c177c7aee430bdd428c0271bba5ffbfeb91
David-5-5 <13910767907@139.com>
2024-12-15 14:40:50 +0800

    Append comments

--- python/algo/leecode/algo1235.py
+++ python/algo/leecode/algo1235.py
@@ -1,19 +1,27 @@
+from typing import List
+
+# DP专题 - 不相交区间； 2024.12 复习
 class Solution:
-    def jobScheduling(self, startTime, endTime, profit) -> int:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        # 由于 startTime和endTime取值范围为 10 ^ 9, 不用分组(2830,2008)
+        # 按照右节点排序
         n = len(startTime)
         dp = [0] * (n + 1)
         from bisect import bisect_right
         tasks = sorted(zip(startTime, endTime, profit), key= lambda p:p[1])
         endTime.sort()
         for i in range(n):
-            k = bisect_right(endTime, tasks[i][0])
+            # 确定状态转移的上一个状态值(下标)
+            k = bisect_right(endTime, tasks[i][0]) # 当前起始时间的在endtime数组中下标，
+            
             # key paramater is supported since python3.10
             # The code is concise but the performance is bad.
             # k = bisect_right(tasks, tasks[i][0], hi=i, key=lambda p: p[1])
             dp[i+1] = max(dp[i], dp[k]+tasks[i][2])
 
         return dp[n]
-
+    
+    
 if __name__ == "__main__":
     sol = Solution()
     startTime, endTime, profit = [1,2,3,3], [3,4,5,6], [50,10,40,70]

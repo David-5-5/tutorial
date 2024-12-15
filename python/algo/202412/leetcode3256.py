af78d6dd487e8dff07d98e9dba5108ad91d04efb
David-5-5 <13910767907@139.com>
2024-12-15 23:45:27 +0800

    Append implementation which pass

--- python/algo/202412/leetcode3256.py
+++ python/algo/202412/leetcode3256.py
@@ -5,36 +5,39 @@ from typing import List
 # 双周赛 137
 class Solution:
     def maximumValueSum(self, board: List[List[int]]) -> int:
+        # 自行解答，8486ms ，刚刚通过
         rows, cols = len(board), len(board[0])
 
+        # 后缀最大值, 按列(最大的三行)
         suf_mn = [[(-inf,-1),(-inf,-1),(-inf, -1)] for _ in range(cols)]
         for c in range(cols-1, 1, -1):
-            if c + 1 < cols: suf_mn[c] = suf_mn[c+1]
+            if c + 1 < cols: suf_mn[c] = suf_mn[c+1].copy()
             for r in range(rows):
                 if r in [r for _, r in suf_mn[c]]:
                     for i in range(3):
                         if suf_mn[c][i][1] == r and suf_mn[c][i][0] < board[r][c]:
                             suf_mn[c][i] = (board[r][c], r)
-                            suf_mn[c].sort()
+                            suf_mn[c].sort(reverse=True)
                 else:
-                    if board[r][c] > suf_mn[c][0]:
+                    if board[r][c] > suf_mn[c][0][0]:
                         suf_mn[c].insert(0, (board[r][c], r))
-                    elif board[r][c] > suf_mn[c][1]:
+                    elif board[r][c] > suf_mn[c][1][0]:
                         suf_mn[c].insert(1, (board[r][c], r))
-                    elif board[r][c] > suf_mn[c][1]:
+                    elif board[r][c] > suf_mn[c][2][0]:
                         suf_mn[c].insert(2, (board[r][c], r))
-                    del suf_mn[3:]
-                    
+                    del suf_mn[c][3:]
+
         @cache
         def dfs(c:int, r1:int, r2:int):
             if c == cols: return -inf
 
+            
             if r1 != -1 and r2 != -1:
+
                 for i in range(3):
                     if suf_mn[c][i][1] not in (r1,r2):
                         return suf_mn[c][i][0]
-                
-
+            
             res = -inf
             res = max(res, dfs(c+1, r1, r2)) # 不选
             if r1 == -1:

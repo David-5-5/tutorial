3216e198e966a15974036d76799d8aadf4aaebd6
David-5-5 <13910767907@139.com>
2024-12-17 21:17:06 +0800

    Append finished implementation

--- python/algo/202412/leetcode2954.py
+++ python/algo/202412/leetcode2954.py
@@ -1,5 +1,23 @@
-from math import comb
 from typing import List
+
+MOD = 1_000_000_007
+MX = 100_000
+
+# 组合数模板 组合数 求 逆元 
+# replace math.comb
+fac = [0] * MX
+fac[0] = 1
+for i in range(1, MX):
+    fac[i] = fac[i - 1] * i % MOD
+
+inv_fac = [0] * MX
+inv_fac[MX - 1] = pow(fac[MX - 1], -1, MOD)
+for i in range(MX - 1, 0, -1):
+    inv_fac[i - 1] = inv_fac[i] * i % MOD
+
+def comb(n: int, k: int) -> int:
+    return fac[n] * inv_fac[k] % MOD * inv_fac[n - k] % MOD
+
 
 # 周赛 374 
 class Solution:
@@ -9,25 +27,23 @@ class Solution:
         # 对于两个只有一个感染的区间排序数为 1
         # 对于多个区间 m = m_1 m_2 ... m_k-1, m_k
         #   排序数为C(m,m1)*C(m2,m-m1)*...*C(mk,m-m_1-...-m_k-1) * 2^(m-k)
-        MOD = 10 ** 9 + 7
-
+    
         sub = 0
         if sick[0] != 0:
-            sub += sick[0]
+            sub += sick[0] - 1
             sick.insert(0,-1)
         if sick[-1] != n-1:
-            sub += n - sick[-1] - 1
+            sub += n - sick[-1] - 2
             sick.append(n)
 
         seg = []
         for l, r in zip(sick,sick[1:]):
-            seg.append(r-l-1)
+            if r-l-1:seg.append(r-l-1)
 
         m = sum(seg)
-        ans = pow(2, m-len(seg), MOD) if sub == 0 else pow(2, m-len(seg)-sub+2, MOD)
+        ans = pow(2, m-len(seg), MOD) if sub == 0 else pow(2, m-len(seg)-sub, MOD)
 
         for i in seg:
             ans = ans * comb(m, i) % MOD
             m -= i
         return ans
-

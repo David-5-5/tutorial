83bbee51209c8f97ff7fceca0954080dcbc0ee81
David-5-5 <13910767907@139.com>
2024-12-18 23:10:25 +0800

    Append comments

--- python/algo/202412/leetcode0045.py
+++ python/algo/202412/leetcode0045.py
@@ -7,18 +7,20 @@ class Solution:
         n = len(nums)
         right_most = [0] * (n)
         for i, r in enumerate(nums):
-            right_most[i] = max(right_most[i], i + r) # max(n-1, i+r)
+            right_most[i] = max(right_most[i], i+r) # min(n-1, i+r)
 
         ans = 0
-        cur_right = 0  # 已建造的桥的右端点
-        next_right = 0  # 下一座桥的右端点的最大值
-        for i in range(n):  # 如果走到 n-1 时没有返回 -1，那么必然可以到达 n
-            if cur_right >= n-1 : break
+        cur_right = 0   # 当前右端点
+        next_right = 0  # 当前右端点覆盖的后续区间的右端点最大值
+        for i in range(n-1):  
+            # if cur_right >= n-1 : break # 可以不用
             next_right = max(next_right, right_most[i])
             if i == cur_right:  # 到达已建造的桥的右端点
                 cur_right = next_right  # 造一座桥
                 ans += 1
 
         return ans
-
     
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.jump([2,3,1,1,4]))

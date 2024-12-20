9fe03ac03c3c308c60da6a08eb7b37dd2548e594
David-5-5 <13910767907@139.com>
2024-12-20 18:04:57 +0800

    Append the optimized operation of heap

--- python/algo/202412/leetcode1705.py
+++ python/algo/202412/leetcode1705.py
@@ -10,14 +10,17 @@ class Solution:
         while h or i < n:
             if i < n:
                 c, d = apples[i], days[i]
-                if c:heapq.heappush(h, (d+i-1, c))
+                if c:heapq.heappush(h, [d+i-1, c])
             
             while h and h[0][0] < i: # 过期的苹果
                 heapq.heappop(h)
             
             if h:
-                d, c = heapq.heappop(h)
-                if c > 1: heapq.heappush(h, (d, c-1))
+                # d, c = heapq.heappop(h)
+                # if c > 1: heapq.heappush(h, (d, c-1))
+                # 原地更新优于 pop / push
+                h[0][1] -= 1
+                if h[0][1] == 0: heapq.heappop(h)
                 ans += 1 # 吃到苹果
             i += 1
         return ans

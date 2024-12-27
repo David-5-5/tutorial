dbee940104c8526a75e5dbde496f852e64862799
David-5-5 <13910767907@139.com>
2024-12-27 10:44:38 +0800

    Append new implementation with elegant python code

--- python/algo/202412/leetcode2975.py
+++ python/algo/202412/leetcode2975.py
@@ -1,8 +1,10 @@
+from itertools import combinations
 from typing import List
 
 # 周赛 376 
 class Solution:
     def maximizeSquareArea(self, m: int, n: int, hFences: List[int], vFences: List[int]) -> int:
+        # 自行解答
         fences = [[1] + hFences + [m], [1] + vFences + [n]]
         for i in range(2): fences[i].sort()
 
@@ -12,7 +14,7 @@ class Solution:
             for i in range(length-1):
                 for j in range(i+1,length):
                     sides[k].add(fences[k][j] - fences[k][i])
-        
+
         h_side = sorted(list(sides[0]))
         v_side = sorted(list(sides[1]))
 
@@ -27,5 +29,17 @@ class Solution:
                 j += 1
             else:
                 i += 1
-
         return -1 if ans == -1 else ans % (10 ** 9 + 7)
+
+    def maximizeSquareArea2(self, m: int, n: int, hFences: List[int], vFences: List[int]) -> int:
+        # 参考题解，太清晰了
+        def f(a: List[int], mx:int) -> set[int]:
+            a.extend([1, mx])
+            a.sort()
+            return set(y-x for x, y in combinations(a, 2))
+        h_set = f(hFences, m)
+        v_set = f(vFences, n)
+
+        mx = max(h_set.intersection(v_set), default=-1)
+
+        return -1 if mx < 0 else pow(mx, 2, 10 ** 9 + 7)

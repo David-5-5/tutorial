c7a5c472049431af618bf23734a15521c76e3a49
David-5-5 <13910767907@139.com>
2024-12-28 17:55:02 +0800

    Append new implementation using dp and decreasing dimension

--- python/algo/202412/leetcode1334.py
+++ python/algo/202412/leetcode1334.py
@@ -8,13 +8,9 @@ class Solution:
         # 参考题解 - 记忆化搜索
         
         # Begin Floyd 模板
-        # g = [[] for _ in range(n)]
         w = [[0 if i == j else inf for j in range(n)] for i in range(n)]
-        for u, v, weight in edges:
-            # g[u].append(v)
-            # g[v].append(u)
-            w[u][v] = weight
-            w[v][u] = weight
+        for u, v, wt in edges:
+            w[u][v] = w[v][u] = wt
         
         @cache
         def dfs(k:int, i:int, j:int) -> int:
@@ -35,3 +31,60 @@ class Solution:
         
         return ans
 
+    def findTheCity2(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        # 参考题解 - 递推
+        
+        # Begin Floyd 模板
+        w = [[0 if i == j else inf for j in range(n)] for i in range(n)]
+        for u, v, wt in edges:
+            w[u][v] = w[v][u] = wt
+        # k 范围[0,n]
+        f = [[[0] * n for _ in range(n)] for _ in range(n+1)]
+        f[0] = w
+
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    f[k+1][i][j] = min(f[k][i][j], f[k][i][k]+f[k][k][j])
+        
+        # End Floyd 模板
+        
+        ans, mn_cnt = -1, inf
+        for i in range(n):
+            cnt = 0
+            for j in range(n):
+                if i != j and f[n][i][j] <= distanceThreshold:
+                    cnt += 1
+            if cnt <= mn_cnt:
+                ans = i
+                mn_cnt = cnt
+        
+        return ans
+
+    def findTheCity3(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+        # 参考题解 - 递推 + 降维
+        
+        # Begin Floyd 模板
+        w = [[0 if i == j else inf for j in range(n)] for i in range(n)]
+        for u, v, wt in edges:
+            w[u][v] = w[v][u] = wt
+        # k 范围[0,n]
+
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    w[i][j] = min(w[i][j], w[i][k]+w[k][j])
+        
+        # End Floyd 模板
+        
+        ans, mn_cnt = -1, inf
+        for i in range(n):
+            cnt = 0
+            for j in range(n):
+                if i != j and w[i][j] <= distanceThreshold:
+                    cnt += 1
+            if cnt <= mn_cnt:
+                ans = i
+                mn_cnt = cnt
+        
+        return ans

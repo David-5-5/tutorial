ba95cbb08052e9da57c8da4303ceb8fe5f27d152
David-5-5 <13910767907@139.com>
2024-12-28 21:55:11 +0800

    Append new implementation using floyd

--- python/algo/leecode2/algo2976.py
+++ python/algo/leecode2/algo2976.py
@@ -1,8 +1,10 @@
+from math import inf
 from typing import List
 import bisect
 import heapq
 class Solution:
     def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
+        # 自行解答 使用 Dijkstra，但未使用标准模板，可读性差
         shortest = [[0 if i == j else float("inf") for i in range(26)] for j in range(26)]
 
         sorted_ori = [q for _, q in sorted(enumerate(original), key=lambda p:p[1])]
@@ -42,7 +44,31 @@ class Solution:
         return ans
 
 
+    def minimumCost2(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
+        # 参考题解 Floyd 最短路径 
+        # 1334 模板
+        # Begin Floyd 递推 + 降维 模板 
+        w = [[0 if i == j else inf for j in range(26)] for i in range(26)] # 26 个字母
+        for u, v, wt in zip(original, changed, cost):
+            u, v = ord(u)-ord('a'), ord(v)-ord('a')
+            # w[u][v] = w[v][u] = wt
+            if wt < w[u][v] : w[u][v] = wt # 有向图
+        # k 范围[0,n]
+
+        for k in range(26):
+            for i in range(26):
+                for j in range(26):
+                    w[i][j] = min(w[i][j], w[i][k]+w[k][j])
+        # End Floyd 模板
+
+        ans = sum(w[ord(u)-ord('a')][ord(v)-ord('a')] for u, v in zip(source, target))
+        return -1 if ans == inf else ans
+        
+
+
+
 if __name__ == "__main__":
     sol = Solution()
     source, target, original, changed, cost = "abcd", "acbe", ["a","b","c","c","e","d"], ["b","c","b","e","b","e"], [2,5,5,1,2,20]
     print(sol.minimumCost(source, target, original, changed, cost))
+    print(sol.minimumCost2(source, target, original, changed, cost))

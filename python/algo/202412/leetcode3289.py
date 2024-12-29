8e8b8a30a96b4d1ffe3b01bff67e2c0839d5f301
David-5-5 <13910767907@139.com>
2024-12-29 14:41:41 +0800

    Append implementation using 2-d array

--- python/algo/202412/leetcode3289.py
+++ python/algo/202412/leetcode3289.py
@@ -5,6 +5,7 @@ from typing import List
 # 双周赛 139 Dijkstra 最短距离
 class Solution:
     def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
+        # 自行解答
         m, n = len(grid), len(grid[0])
 
         pq = [(0, 0)]
@@ -24,5 +25,27 @@ class Solution:
                     if not vis[v] and dis[v] > dis[u]+grid[r][c]:
                         dis[v] = dis[u]+grid[r][c]
                         heapq.heappush(pq, (dis[v], v))
-        print(dis)
+
         return dis[m*n-1] < health
+
+    def findSafeWalk2(self, grid: List[List[int]], health: int) -> bool:
+        # 自行解答，vis, dis 使用二位数组
+        m, n = len(grid), len(grid[0])
+
+        pq = [[0, (0,0)]]
+        vis = [[False] * n for _ in range(m)]
+        dis = [[inf] * n for _ in range(m)]
+        dis[0][0] = grid[0][0]
+
+        while pq:
+            _, (x,y) = heapq.heappop(pq)
+            if vis[x][y]: continue
+
+            vis[x][y] = True
+            for r, c in [(x+1,y),(x-1,y),(x,y+1),(x,y-1)]:
+                if 0<=r<m and 0<=c<n:
+                    if not vis[r][c] and dis[r][c]  > dis[x][y]+grid[r][c]:
+                        dis[r][c] = dis[x][y]+grid[r][c]
+                        heapq.heappush(pq, (dis[r][c], (r,c)))
+
+        return dis[m-1][n-1] < health

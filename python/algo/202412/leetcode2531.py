e99e0d6251c364f657e61d88840fe2330029615f
David-5-5 <13910767907@139.com>
2024-12-31 14:31:57 +0800

    Append comments

--- python/algo/202412/leetcode2531.py
+++ python/algo/202412/leetcode2531.py
@@ -1,9 +1,10 @@
 from typing import Counter
 
+# 周赛 327
 class Solution:
     def isItPossible(self, word1: str, word2: str) -> bool:
-        # 参考题解，优雅的写出比较算法
-        # 起始思考的分类方法，不容易把各种情况思考全面
+        # 参考题解，优雅的写出结构清晰的代码
+        
         cnt1, cnt2 = Counter(word1), Counter(word2)
 
         for x, c in cnt1.items():    # 时间复杂度 26 * 26 遍历交互任两种字符的情况
@@ -13,4 +14,5 @@ class Solution:
                    len(cnt2) - (d==1) + (x not in cnt2):
                     return True
         return False
-                    
+    
+        # 备注：起始思考的分类方法，不容易把各种情况思考全面            

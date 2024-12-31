aff73f54a9f615bb9d7be04a58338c7a55ccddb4
David-5-5 <13910767907@139.com>
2024-12-31 14:30:23 +0800

    Add algo using enumerate method

---
+++ python/algo/202412/leetcode2531.py
@@ -0,0 +1,16 @@
+from typing import Counter
+
+class Solution:
+    def isItPossible(self, word1: str, word2: str) -> bool:
+        # 参考题解，优雅的写出比较算法
+        # 起始思考的分类方法，不容易把各种情况思考全面
+        cnt1, cnt2 = Counter(word1), Counter(word2)
+
+        for x, c in cnt1.items():    # 时间复杂度 26 * 26 遍历交互任两种字符的情况
+            for y, d in cnt2.items():
+                if x == y and len(cnt1) == len(cnt2): return True
+                if x != y and len(cnt1) - (c==1) + (y not in cnt1) == \
+                   len(cnt2) - (d==1) + (x not in cnt2):
+                    return True
+        return False
+                    

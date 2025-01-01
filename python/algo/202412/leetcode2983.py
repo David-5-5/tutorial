2de146ee1c7445ebeca7b454f099ec80551040d2
David-5-5 <13910767907@139.com>
2025-01-01 19:08:03 +0800

    Add algo using enumerate and prefix sum

--- python/algo/202412/leetcode2983.py
+++ python/algo/202412/leetcode2983.py
@@ -1,36 +1,55 @@
-from bisect import bisect_left, bisect_right
 from typing import List
 
-
+# 周赛 378
 class Solution:
     def canMakePalindromeQueries(self, s: str, queries: List[List[int]]) -> List[bool]:
-        n, m = len(s), len(queries)
-        if sorted(s[0:n//2]) != sorted(s[n//2:]):
-            return [False] * m
+        # 参考题解 大模拟   
+        # 前缀和 + 区间分类讨论
+        n = len(s)
+        left, right = s[0:n//2], s[n//2:][::-1]
+        pres_l = [[0] * 26 for _ in range((n//2) + 1)]
+        pres_r = [[0] * 26 for _ in range((n//2) + 1)]
+        pres_d = [0] * ((n//2) + 1)
 
-        sl = list(s[0:n//2])
-        intervals = []
-        for i in range(n//2):
-            if sl[i] != s[n-i-1]:
-                j = sl.index(s[n-i-1],i)
-                sl[j] = sl[i]
-                sl[i] = s[n-i-1]
-                intervals.append([i,j])
+        def cal_prefix_sum(t:str, pre:List[int]):
+            for i in range(n//2):
+                for ch in range(26):
+                    pre[i+1][ch] = pre[i][ch] + (ch == ord(t[i])-ord('a'))
         
-        merge = []
-        left, right = intervals[0]
-        for l, r in intervals[1:]:
-            if l > right:
-                merge.append([left, right])
-                left, right = l, r
-            else:
-                if r > right : right = r
-        merge.append([left, right])
+        cal_prefix_sum(left, pres_l)
+        cal_prefix_sum(right, pres_r)
 
-        ans = []
-        for a, b, c, d in queries:
-            c, d = n - 1 - d, n - 1 - c
+        for i in range(n//2):
+            pres_d[i+1] = pres_d[i] +  (left[i] != right[i])
+        
+        def count(l:int, r:int, pres:List[List[int]]) -> List[int]:
+            return [x-y for x, y in zip(pres[r+1], pres[l])]
+        
+        def substract(s1:List[int], s2:List[int]):
+            for i, v in enumerate(s2):
+                if s1[i] < v: return False
+                s1[i] -= v
+            return s1
 
-            ans.append(all((a<=l and r<=b) or (c<=l and r<=d)for l, r in merge))
-            
+        def check(l1:int, r1:int, l2:int, r2:int, pre1:List[List[int]], pre2:List[List[int]]) -> bool:
+            if pres_d[l1] > 0 or pres_d[n//2] - pres_d[max(r1,r2)+1] > 0: # 检查区间的两边是否全等
+                return False
+            if r2 <= r1: # 区间包含
+                return count(l1,r1,pre1) == count(l1,r1,pre2)
+            elif r1 < l2: # 两个不相交区间
+                return pres_d[l2] - pres_d[r1+1] == 0 and \
+                        count(l1,r1,pre1) == count(l1,r1,pre2) and \
+                        count(l2,r2,pre1) == count(l2,r2,pre2)
+            else: # l2 < r1
+                s1 = substract(count(l1,r1,pre1), count(l1,l2-1,pre2))
+                s2 = substract(count(l2,r2, pre2), count(r1+1,r2,pre1))
+                return s1 and s2 and s1 == s2
+    
+        ans = [False] * len(queries)
+        for i, (l1, r1, c, d) in enumerate(queries):
+            l2, r2 = n - d - 1, n - c - 1
+
+            ans[i] = check(l1, r1, l2, r2, pres_l, pres_r) if l1 <= l2 \
+                        else check(l2, r2, l1, r1, pres_r, pres_l)
+
         return ans

93f4c707584b3c92938877a87b58de0911d128db
David-5-5 <13910767907@139.com>
2025-01-03 02:41:19 +0800

    Add template of algo

---
+++ cplus/leetcode/202501/algo0564.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 贪心专题 - 回文串贪心
+
+class Solution {
+public:
+    string nearestPalindromic(string n) {
+        
+    }
+};
---
+++ cplus/leetcode/202501/algo1147.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 贪心专题 - 回文串贪心
+class Solution {
+public:
+    int longestDecomposition(string text) {
+        
+    }
+};

7e1a4e667af078007f6a8831295a749213bb5330
David-5-5 <13910767907@139.com>
2025-01-04 22:12:47 +0800

    Add algo using greedy

---
+++ cplus/leetcode/202501/algo0397.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int integerReplacement(int n) {
+        // 自行解答
+        if (n == 1) return 0;
+        if (n == 2) return 1;
+        if (n == 3 || n == 4) return 2;
+
+        if (n%2) {
+            if ((n-1) % 4) return 2 + integerReplacement(n/2+1); // n = 2^31 -1, n+1溢出
+            else  return 2 + integerReplacement(n/2);
+        } return 1 + integerReplacement(n/2);   
+        
+    }
+
+    int integerReplacement(int n) {
+        // 参考题解
+        if (n == 1) return 0;
+
+        if (n%2) {
+            return 2 + min(integerReplacement(n/2+1),integerReplacement(n/2));
+        } return 1 + integerReplacement(n/2);
+        
+    }
+};

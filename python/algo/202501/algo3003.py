bf712a2fff92977d3106a268243f9f7858a0e111
David-5-5 <13910767907@139.com>
2025-01-05 23:05:47 +0800

    Add algo using memory search

---
+++ python/algo/202501/algo3003.py
@@ -0,0 +1,26 @@
+class Solution:
+    def maxPartitionsAfterOperations(self, s: str, k: int) -> int:
+        @cache
+        def dfs(i:int, mask:int, changed:bool) -> int:
+            if i == len(s): return 1
+
+            bit = ord(s[i]) - ord('a')
+
+            # not change
+            res = 0
+            if (mask | 1 << bit).bit_count() > k:
+                res = dfs(i+1, 1<<bit, changed) + 1
+            else: res = dfs(i+1, mask | 1 << bit, changed)
+
+            # change it
+            if changed: return res
+            for j in range(26):
+                if j == bit: continue
+                if (mask | 1 << j).bit_count() > k:
+                    res = max(res, dfs(i+1, 1 << j, True) + 1)
+                else: res = max(res, dfs(i+1, mask | 1 << j, True))
+            return res
+        
+        return dfs(0, 0, False)
+                
+

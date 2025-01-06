2eb694e6f1ed6c3ebd1bc621c24ec36125362060
David-5-5 <13910767907@139.com>
2025-01-06 18:12:01 +0800

    Add algo using counter and sort

---
+++ cplus/leetcode/202501/algo3016.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        // 自行解答
+        int cnt[26] = {0};
+        for (char ch : word)
+            cnt[ch-'a'] += 1;
+
+        sort(cnt, cnt+26);
+        reverse(cnt, cnt+26);
+
+        int ans = 0;
+        for (int i=0; i<26; i++) {
+            ans += cnt[i] * ((i+8) / 8); // 上取整
+        }
+        return ans;    
+    }   
+};

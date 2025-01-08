14d53734d1130237503140896c574347769996fa
David-5-5 <13910767907@139.com>
2025-01-08 23:20:30 +0800

    Append implementation which pass through

--- cplus/leetcode/202501/algo3017.cpp
+++ cplus/leetcode/202501/algo3017.cpp
@@ -5,36 +5,51 @@ class Solution {
 public:
     vector<long long> countOfPairs(int n, int x, int y) {
         vector<long long> ans(n);
-
         if (x > y) {
             swap(x, y);
         }
 
-        for (int i=1; i<=n; i++) {
-            if (i < (x + y) / 2) {
+        // 情况 1：x == y， 情况 2：x+1 == y 直接处理
+        if (x + 1 >= y) {
+            for (int i = 1; i < n; i++) {
+                ans[i - 1] = (n - i) * 2;
+            }
+            return ans;
+        }
+
+        vector<long long> diff(n+1);
+
+        auto add = [&](int l, int r) {
+            diff[l] ++;
+            diff[r+1] --;
+        };
+
+        for (int i=1; i<n; i++) {
+            if (i <= x) {
                 int j = (x + y + 1 ) / 2;
-                // j < (x + y + 1)/2 距离 [1, j-1] 区间都加 1
-                ans[0] += 1;
-                ans[j-i] -= 1;
+                // j < (x + y + 1)/2 距离 [1, j-i] 区间都加 1
+                add(1, j-i);
                 // (x + y + 1)/2 < j <= y 距离 [x-i+1, x-i+1+y-(j+1)] 区间+1
-                ans[abs(x-i)] += 1;
-                if (abs(x-i)+y-j < n) ans[abs(x-i)+y-j] -= 1;
-
-                // j > y 时，距离[x-i+2, x-i+n-(y+1)] 区间 +1
-                ans[abs(x-i)+1] += 1;
-                if (abs(x-i)+n-y < n) ans[abs(x-i)+n-y] -= 1;
-
+                add(x-i+2, x-i+y-j);
+                // j > y 时，距离[x-i+2, x-i+1+n-y] 区间 +1
+                add(x-i+1, x-i+n+1-y);
+            }else if (i < (x + y) / 2) {
+                int j = i + (y - x + 1) / 2;
+                add(1, j-i);
+                add(i-x+2, i-x+y-j);
+                add(i-x+1, i-x+n+1-y);
+                // 等价于上面两行代码，差别在于，y 在那个区间
+                // add(i-x+1, i-x+y-j); 
+                // add(i-x+2, i-x+n+1-y;         
             } else{ 
-                ans[0] += 1;
-                if (n-i+1 < n) ans[n-i+1] -= 1;
+                add(1, n-i);
             }
         }
 
-        ans[0] *= 2;
+        ans[0] = diff[1] * 2;
         for (int i=1; i<n; i++) {
-            ans[i] = ans[i] * 2 + ans[i-1];
+            ans[i] = diff[i+1] * 2 + ans[i-1];
         }
-
-        return ans;       
+        return ans;
     }
 };

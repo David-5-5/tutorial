bb620afd354d55880823fc4ea94a0d7489fb54f1
David-5-5 <13910767907@139.com>
2025-01-10 00:55:35 +0800

    Append comments

--- python/algo/202409/leetcode2611.py
+++ python/algo/202409/leetcode2611.py
@@ -3,7 +3,18 @@ from typing import List
 # 周赛 339
 class Solution:
     def miceAndCheese(self, reward1: List[int], reward2: List[int], k: int) -> int:
-        # 自行解答 reward1 - reward2 的差 倒序排序
+        # 自行解答 reward1 - reward2 的差倒序排序
+        
+        # 2025.1. 复习，补充思路
+        # reward1[i1] + reward1[i2] + ...+ reward[ik]  + reward2[j1] + ... + reward2[jm] 为最大值
+        # 假设 ik 与 jm 互换，则有:
+        # reward1[i1] + reward1[i2] + ...+ reward1[ik]  + reward2[j1] + ... + reward2[jm]
+        #  > reward1[i1] + reward1[i2] + ...+ reward1[jm]  + reward2[j1] + ... + reward2[ik]
+        # 消除相同项:
+        #   reward1[ik] + reward2[jm] > reward1[jm] + reward2[ik]
+        #   reward1[ik] - reword2[ik] > reward1[jm] - reward2[jm]
+        # 即按照 reward1[x] - reword2[x] 倒序排序，第一只老鼠优选选择 reward1[x] - reword2[x] 大的项
+        # 通过结论反推 选择项目与 reward1, reward2 数组的关系
         n = len(reward1)
         if n == k: return sum(reward1)
 

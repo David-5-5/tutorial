86a307fb9b5ecf475e23ab3fe44b3e58cadf840b
David-5-5 <13910767907@139.com>
2025-01-11 12:59:09 +0800

    Append comments

--- python/algo/202407/leetcode2565.py
+++ python/algo/202407/leetcode2565.py
@@ -5,6 +5,9 @@ class Solution:
         # suf[i] 表示匹配了从 i 开始匹配了 t[suf[i]:] 的字符 
         # 若 suf[i] = m, 匹配 t 中 0 个字符
         # 若 suf[i] = 0, 匹配 t 中 所有字符
+        
+        # 前后缀 字符串匹配 模板
+        # 模板 开始 后缀匹配
         suf = [m] * (n + 1)
         j = m
         for i in range(n-1, -1, -1):
@@ -17,7 +20,9 @@ class Solution:
         j = 0
         for i in range(n):
             if s[i] == t[j]:
+                # 前缀匹配
                 j += 1
+                # 以上是模板
                 if suf[i] >= j:
                     ans = min(ans, suf[i+1] - j)
             if j == m: break

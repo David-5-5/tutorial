ad4b6b1eb0981891fdcccfe4d7e9f4b739c36b26
David-5-5 <13910767907@139.com>
2025-01-12 19:25:36 +0800

    Append comments

--- python/algo/leecode2/algo1526.py
+++ python/algo/leecode2/algo1526.py
@@ -1,7 +1,7 @@
 from typing import List
 class Solution:
     def minNumberOperations(self, target: List[int]) -> int:
-        # 思路 从 target -> initial [0, 0, 0, ..., 0]
+        # 自行解答 思路 从 target -> initial [0, 0, 0, ..., 0]
         # 遍历每个元素，初始prev为0，
         # a） 当前 val > prev 时， val 变为 0 的在prev次数外，需要增加的操作次数 val - prev
         # b)  当前 val <= prev 时, 表明 val 可以和同一子数组中已经可以变更为 0， 不需要额外的操作

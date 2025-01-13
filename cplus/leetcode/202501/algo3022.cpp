4ab4ceca6d7eb0046a6ec0bd1c1ed61f97e573e4
David-5-5 <13910767907@139.com>
2025-01-14 04:29:28 +0800

    Append implementation which pass through

--- cplus/leetcode/202501/algo3022.cpp
+++ cplus/leetcode/202501/algo3022.cpp
@@ -5,9 +5,37 @@ using namespace std;
 class Solution {
 public:
     int minOrAfterOperations(vector<int>& nums, int k) {
+        // 参考题解，按从高位计算
+        // 相邻项合并 -> 子数组 -> 每段是合并为 0 的长度 - 1
+        // 计算低位时，需要同时带上高位进行计算，忽略掉无法消除的位
+        int ans = 0, mask = 0; 
+        for (int b=29; b>=0; b--){
+            int cnt = 0, and_res = -1; // 各位全为 1
+            for (auto x : nums) {
+                mask |= 1 << b;
+                and_res &= x & mask;
+                if (and_res) 
+                    cnt += 1; // 合并
+                else and_res = -1; // and_res, 开始新的一段子数组
+                // 由于 k < nums.size() 无需判断最后是否 and_res 不等于 0
+                // 当nums所有项都无法AND为0时，cnt > k 必定成立
+            }
+            if (cnt > k) {
+                ans |= 1 << b;    // 该位无法消除，加入ans
+                mask ^= 1 << b;   // the bit of mask set to 0, ignore it
+            }
+        }
+        return ans;
+
+    }
+
+    int minOrAfterOperations_org(vector<int>& nums, int k) {
         // int mx = ranges::max(nums);
+        // 总结：遇到不好写的代码，一定是知识点没有掌握
+        // 相邻项与运算并删除
         int bits = 5;
 
+        // 按位合并项，并删除 --> 死胡同
         auto to_and = [&](int b) -> int {
             int n = nums.size();
             if (k == 0) return 0;
@@ -47,6 +75,8 @@ public:
         for (auto v : nums) ans |= v;
         return ans;
     }
+
+
 };
 
 int main() {

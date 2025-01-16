02e261ed6945c7cf0450b1b383811dc2fcb03a54
David-5-5 <13910767907@139.com>
2025-01-17 03:02:59 +0800

    Append implementation using bfs

--- cplus/leetcode/202501/algo0417.cpp
+++ cplus/leetcode/202501/algo0417.cpp
@@ -7,7 +7,7 @@ class Solution {
     
 public:
     const int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    vector<vector<int>>& heights;
+    vector<vector<int>> heights;
     void dfs(int row, int col, vector<vector<bool>>& ocean) {
         if (ocean[row][col]) return;
         int m = heights.size(), n = heights[0].size();
@@ -17,6 +17,29 @@ public:
             if (nr>=0 && nr <m && nc>=0 && nc<n &&
              !ocean[nr][nc] && heights[nr][nc] > heights[row][col])
                 dfs(nr, nc, ocean);
+        }
+
+    }
+
+    void bfs(int row, int col, vector<vector<bool>>& ocean) {
+        int m = heights.size(), n = heights[0].size();
+        
+        ocean[row][col] = true;
+        queue<pair<int, int>> qu;
+        qu.emplace(row, col);
+
+        while (!qu.empty()){
+            auto [row, col] = qu.front();
+            qu.pop();
+
+            for (int i=0; i<4; i++) {
+                int nr = row + dirs[i][0], nc = col + dirs[i][1];
+                if (nr>=0 && nr <m && nc>=0 && nc<n &&
+                    !ocean[nr][nc] && heights[nr][nc] > heights[row][col]){
+                    ocean[nr][nc] = true;
+                    qu.emplace(nr, nc);
+                }
+            }
         }
 
     }
@@ -32,11 +55,15 @@ public:
         for (int i=0; i<n; i++) {
             dfs(0, i, pacific);
             dfs(m-1, i, altantic);
+            // bfs(0, i, pacific);
+            // bfs(m-1, i, altantic);
         }
 
         for (int i=0; i<m; i++) {
             dfs(i, 0, pacific);
             dfs(i, n-1, altantic);
+            // bfs(i, 0, pacific);
+            // bfs(i, n-1, altantic);
         }
         vector<vector<int>> ans;
         for (int i=0; i<m; i++) {
@@ -48,3 +75,9 @@ public:
         return ans;
     }
 };
+
+int main() {
+    vector<vector<int>> heights = {{1,2,2,3,5},{3,2,3,4,4},{2,4,5,3,1},{6,7,1,4,5},{5,1,1,2,4}};
+    Solution().pacificAtlantic(heights);
+    return 0;
+}

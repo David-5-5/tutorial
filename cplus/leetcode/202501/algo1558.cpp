8b220085f113ecc4888abfb3312ca8ba95770d37
David-5-5 <13910767907@139.com>
2025-01-16 20:01:49 +0800

    Add algo using greedy

---
+++ cplus/leetcode/202501/algo1558.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 贪心专题 - 逆向思维
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        // 自行解答 
+        int mx = ranges::max(nums), ans = 0;
+        for (auto v:nums)
+            ans += __builtin_popcount(v); // + 操作是每个数的 二进制 1 的数量
+        
+        int bit = 0;
+        for (int i=31;i>0;i--) // 乘 2 操作是最大数最高位移位到 1 次数
+            if (mx >> i & 1) {
+                bit = i;
+                break;
+            }
+        ans += bit;
+        return ans;
+        
+    }
+};

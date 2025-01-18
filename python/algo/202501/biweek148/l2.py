f3a8fb75cd152bf7a07fd92499f03f3b383f64a1
David-5-5 <13910767907@139.com>
2025-01-18 23:59:30 +0800

    Add algo using greedy

---
+++ python/algo/202501/biweek148/l2.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def minCost(self, arr: List[int], brr: List[int], k: int) -> int:
+        ans1 = ans2 = 0
+        for a, b, in zip(arr, brr):
+            ans1 += abs(a-b)
+
+        arr.sort()
+        brr.sort()
+        for a, b, in zip(arr, brr):
+            ans2 += abs(a-b)
+
+        return min(ans1, ans2+k)
+
+        
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol)

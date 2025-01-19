b71e2c0ee81d48db014789dce37956ac081c6657
David-5-5 <13910767907@139.com>
2025-01-19 16:07:24 +0800

    Add alao using prefix sum

---
+++ python/algo/202501/week433/l1.py
@@ -0,0 +1,16 @@
+from itertools import accumulate
+from typing import List
+
+class Solution:
+    def subarraySum(self, nums: List[int]) -> int:
+        pres = list(accumulate(nums, initial=0))
+
+        ans = 0
+        for i, v in enumerate(nums):
+            ans += pres[i+1] - pres[max(0, i-v)]
+        return ans
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol)

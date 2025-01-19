7af2a249230410187945592a296a850970b3c4c6
David-5-5 <13910767907@139.com>
2025-01-19 20:34:05 +0800

    Append correct implementation using cutomized comb replace of  math.comb

--- python/algo/202501/week433/l2.py
+++ python/algo/202501/week433/l2.py
@@ -1,32 +1,36 @@
 from math import factorial
 from typing import List
 
+# copy from 2954
+# 组合数模板 组合数 求 逆元 
+# replace math.comb 
+MOD = 10 ** 9 + 7
+MX = 100_000
+
+fac = [0] * MX
+fac[0] = 1
+for i in range(1, MX):
+    fac[i] = fac[i - 1] * i % MOD
+
+inv_fac = [0] * MX
+inv_fac[MX - 1] = pow(fac[MX - 1], -1, MOD)
+for i in range(MX - 1, 0, -1):
+    inv_fac[i - 1] = inv_fac[i] * i % MOD
+
+def comb(n: int, k: int) -> int:
+    return fac[n] * inv_fac[k] % MOD * inv_fac[n - k] % MOD
+
 
 class Solution:
     def minMaxSums(self, nums: List[int], k: int) -> int:
-        MOD = 10 ** 9 + 7
-        nums.sort()
-        n = len(nums)
-        ans = 0
         
-        def count(i:int):
-            res = 0
-            c = n-i-1
-            for j in range(1, k+1):
-                if j > c:break
-                res += factorial(c, j)
+        nums.sort()
+        n, ans = len(nums), 0
         
         for i, v in enumerate(nums):
-            cnt = count(i)
-            ans = (ans + (cnt * v)) % MOD
-
-        
-        for i, v in  enumerate(nums[::-1]):
-            cnt = count(i)
-            ans = (ans + (cnt * v)) % MOD
-
+            cnt = sum(comb(n-i-1, j) for j in range(k) if n-i-1 >= j) % MOD
+            ans = (ans + (cnt * (v+nums[n-i-1]))) % MOD
         return ans % MOD
-
 
 if __name__ == "__main__":
     sol = Solution()

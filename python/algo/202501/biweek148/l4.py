72f80d6078307643c11d7f072233df26a6f28c47
David-5-5 <13910767907@139.com>
2025-01-21 00:00:35 +0800

    Append the implementation using comb

--- python/algo/202501/biweek148/l4.py
+++ python/algo/202501/biweek148/l4.py
@@ -1,11 +1,14 @@
+from math import comb
 from typing import List
-
 
 class Solution:
     def distanceSum(self, m: int, n: int, k: int) -> int:
         MOD = 10 ** 9 + 7
-
-        
+        # 固定两个点的距离
+        # |xi - xj| 的组合数是 comb(m*n-2, k-2)
+        # |xi - xj| 的情况是 m*m*sum(d*(n-d))=m*m*comb(n,3)  1<=d<n-1
+        # y 同理
+        return comb(n*m-2) % MOD (n * n * comb(n+1, 3) + m * m * comb(m+1, 3)) % MOD
         
 if __name__ == "__main__":
     sol = Solution()

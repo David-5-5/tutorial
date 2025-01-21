ea36f8e3d305926293562c60eaf491cbd03b2ac9
David-5-5 <13910767907@139.com>
2025-01-21 19:58:44 +0800

    Add algo without finish

---
+++ cplus/leetcode/202501/algo1657.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 贪心专题 - 等价转换
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        if (word1.size() != word2.size()) return false;
+        
+        for (int i=0;)
+    }
+};

380387d3cc6a7e6f7c9a711c2d4552beba0d67e9
David-5-5 <13910767907@139.com>
2025-01-23 02:47:23 +0800

    Add algo using greedy and constructive

---
+++ cplus/leetcode/202501/algo1253.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 贪心专题 - 构造题
+class Solution {
+public:
+    vector<vector<int>> reconstructMatrix(int upper, int lower, vector<int>& colsum) {
+        // 自行解答
+        int n = colsum.size();
+        vector<vector<int>> ans(2, vector<int>(n));
+        
+        for (int i=0; i<n; i++) {
+            if (colsum[i] == 2)  {
+                ans[0][i] = 1, upper --;
+                ans[1][i] = 1, lower --;
+            } else if (colsum[i] == 1) {
+                if (upper > lower) 
+                    ans[0][i] = 1, upper --;
+                else ans[1][i] = 1, lower --;
+            }
+            if (upper < 0 || lower < 0) return {};
+        }
+        if(upper!=0 || lower!= 0) return {};
+        return ans;
+    }
+};

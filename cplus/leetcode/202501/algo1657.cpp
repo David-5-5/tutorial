38bf0ae5625a627f458c9eedc0e71fa0f5a58b00
David-5-5 <13910767907@139.com>
2025-01-22 14:41:19 +0800

    Append implmentation

--- cplus/leetcode/202501/algo1657.cpp
+++ cplus/leetcode/202501/algo1657.cpp
@@ -5,8 +5,39 @@ using namespace std;
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
+        // 自行解答，c++ 编程训练
         if (word1.size() != word2.size()) return false;
         
-        for (int i=0;)
+        vector<int> cnt1(26), cnt2(26);
+        set<char> chset1, chset2;   // 不需要排序
+        for (int i=0; i < word1.size(); i++) {
+            char ch1 = word1[i], ch2 = word2[i];
+            cnt1[ch1-'a'] ++, cnt2[ch2-'a'] ++;
+            chset1.emplace(ch1), chset2.emplace(ch2);
+        }
+        sort(cnt1.begin(), cnt1.end()), sort(cnt2.begin(), cnt2.end());
+        
+        return cnt1 == cnt2 && chset1 == chset2;    
     }
+
+    bool closeStrings(string word1, string word2) {
+        // 参考题解，c++ 编程训练
+        if (word1.size() != word2.size()) return false;
+        
+        vector<int> cnt1(26), cnt2(26);
+        for (int i=0; i < word1.size(); i++) {
+            char ch1 = word1[i], ch2 = word2[i];
+            cnt1[ch1-'a'] ++, cnt2[ch2-'a'] ++;
+        }
+        sort(cnt1.begin(), cnt1.end()), sort(cnt2.begin(), cnt2.end());
+        
+        // 判断包含的字符是否相同，性能好
+        for (int i = 0; i < 26; i++) {          
+            if (cnt1[i] > 0 && cnt2[i] == 0 || cnt1[i] == 0 && cnt2[i] > 0) {
+                return false;
+            }
+        }
+
+        return cnt1 == cnt2;    
+    }    
 };

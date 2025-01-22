cc78fba3be14a348e06ad7b448b1abaa58d511aa
David-5-5 <13910767907@139.com>
2025-01-23 02:31:08 +0800

    Append new implemenation

--- cplus/leetcode/202501/algo1968.cpp
+++ cplus/leetcode/202501/algo1968.cpp
@@ -14,4 +14,16 @@ public:
             else ans.push_back(nums[--mx]), even ^= 1;
         return ans;
     }
+
+    vector<int> rearrangeArray(vector<int>& nums) {
+        // 参考题解，写法不同，思路相同
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), m = (n+1) / 2;
+        vector<int> ans;
+        for (int i=0; i<m; i++) {
+            ans.push_back(nums[i]);
+            if (i+m<n) ans.push_back(nums[i+m]);
+        }
+        return ans;
+    }
 };

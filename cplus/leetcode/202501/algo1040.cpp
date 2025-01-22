e84d72f95fd4899727baf855999c128f4dca91e4
David-5-5 <13910767907@139.com>
2025-01-23 02:23:25 +0800

    Add algo using greedy

---
+++ cplus/leetcode/202501/algo1040.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 贪心专题 - 等价转换
+class Solution {
+public:
+    vector<int> numMovesStonesII(vector<int>& stones) {
+        // 自行解答 使用 942 ID...ID 排列
+
+    }
+};

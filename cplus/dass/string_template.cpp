747e4911a17064b82d79ed2005ea464d20f4fca0
David-5-5 <13910767907@139.com>
2025-01-24 00:36:45 +0800

    Append kmp template

--- cplus/dass/string_template.cpp
+++ cplus/dass/string_template.cpp
@@ -2,11 +2,35 @@
 using namespace std;
 
 // string 算法相关模板
-// kmp 
+// kmp
+// 参数 str 字符串 pat 模式串
+// 返回 vector<int> str 中匹配 pat 的第一个字符索引
+auto kmp = [](string str, string pat) -> vector<int> {
+    int n = str.size(), m = pat.size();
+    vector<int> p_pi(m);
+    for (int i=1; i < m; i++) {
+        int j = p_pi[i-1];
+        while (j > 0 && pat[j]!=pat[i])
+            j = p_pi[j-1];
+        if (pat[j] == pat[i])
+            p_pi[i] = j + 1;
+        else p_pi[i] = j;
+    }
+
+    vector<int> ans;
+    int j = 0;
+    for (int i=0; i<n; i++) {
+        while (j > 0 && str[i] != pat[j])
+            j = p_pi[j-1];
+        if (str[i] == pat[j]) j++;
+        if (j == m) ans.push_back(i-j+1), j = p_pi[j-1];
+    }
+
+    return ans;
+};
+
+
 // z function 扩展 kmp
-
-
-
 // z 函数 z函数 zfunction z-function z function 模板
 // 按思路
 auto zFunc_ori = [](string text) -> vector<int> {
@@ -53,7 +77,8 @@ auto zFunc = [](string text) -> vector<int> {
 };
 
 int main() {
-    vector<int> ans = zFunc("bacabaababbababa");
+    // vector<int> ans = zFunc("bacabaababbababa");
+    vector<int> ans = kmp("1021102","102");
     for (auto v:ans) cout << v;
     cout << endl;
 }

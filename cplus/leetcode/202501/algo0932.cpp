5c756540cc14ecb0d935e5515bf7b03c160c1a9a
David-5-5 <13910767907@139.com>
2025-01-28 01:36:18 +0800

    Append comments

--- cplus/leetcode/202501/algo0932.cpp
+++ cplus/leetcode/202501/algo0932.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Solution {
 public:
     vector<int> beautifulArray(int n) {
+        // 参考题解 分治，思维难度较高
         unordered_map<int, vector<int>> memo;
         memo[1] = {1}; // 分治的递归边界
 
@@ -12,8 +13,12 @@ public:
             if (memo.count(N)) return memo[N];
 
             vector<int> res;
-            for (auto x: f((N+1)/2)) res.push_back(x*2-1);
-            for (auto x: f(N/2)) res.push_back(x*2);
+            // 题目的要求，隐含在分治的策略中，分成奇数和偶数两部分
+            // a[k] * 2 = a[i] + a[j] 其中 a[i] 为奇数 a[j] 为偶数
+            // 确保上式不成立；
+            // 同时两个满足该条件数组合并后，仍满足条件
+            for (auto x: f((N+1)/2)) res.push_back(x*2-1); // 奇数
+            for (auto x: f(N/2)) res.push_back(x*2);       // 偶数
             
             return memo[N] = res;
         };

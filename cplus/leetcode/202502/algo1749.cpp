4fea196b932f27b586b8b75269a7a68680838aa5
David-5-5 <13910767907@139.com>
2025-01-31 22:51:47 +0800

    Append new implementation

--- cplus/leetcode/202502/algo1749.cpp
+++ cplus/leetcode/202502/algo1749.cpp
@@ -20,4 +20,24 @@ public:
         for (auto v : nums) inv.push_back(-v);
         return max(max_sum(nums), max_sum(inv));
     }
+
+    int maxAbsoluteSum(vector<int>& nums) {
+        // 自行解答，生成 nums 的负数，再求最大值
+        auto cal_sum = [](vector<int>& nums, bool postive) -> int {
+            int sum = 0, ans = 0;
+            for (auto& v :nums) {
+                sum += v;
+                if (postive){
+                    if (sum < 0) sum = 0;
+                    ans = max(ans, sum);
+                } else {
+                    if (sum > 0) sum = 0;
+                    ans = min(ans, sum);                    
+                }
+            }
+            return ans;
+        };
+
+        return max(cal_sum(nums, true), -cal_sum(nums, false));
+    }    
 };

734017276da4fa3db80c775b5c3ad3685b359306
David-5-5 <13910767907@139.com>
2025-02-01 00:27:12 +0800

    Add algo using dp

---
+++ cplus/leetcode/202502/algo0918.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 动态规划 - 最大子数组和
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int n = nums.size();
+        int tot = accumulate(nums.begin(), nums.end(), 0);
+
+        int cur_max = INT_MIN, cur_min = 0;
+        int ans_max = INT_MIN, ans_min = 0;
+
+
+        for (int i=0; i < n; i++) {
+            cur_max = max(cur_max, 0) + nums[i]; // 至少包含一个元素
+            ans_max = max(ans_max, cur_max);
+            
+            cur_min = min(cur_min, 0) + nums[i];
+            ans_min = min(cur_min, ans_min);
+        }
+        return tot==ans_min?ans_max: max(ans_max, tot-ans_min);
+    }
+};

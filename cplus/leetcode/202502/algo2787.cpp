f7b76378d2ce434f4166d0c2d20dba077c0ddde9
David-5-5 <13910767907@139.com>
2025-02-06 00:57:51 +0800

    Add algo using 0-1 knapsack

---
+++ cplus/leetcode/202502/algo2787.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 动态规划 - 0-1 背包
+class Solution {
+public:
+    int numberOfWays(int n, int x) {
+        // 自行解答 标准 0-1 背包
+        int mod = 1e9+7;
+        vector<int> dp(n+1);
+        dp[0] = 1;
+
+        for (int i=1; i<=300; i++) {
+            int y = int(pow(i, x));
+            if (y>n) break;
+            for(int j=n; j>=y; j--){
+                dp[j] = (dp[j] + dp[j-y]) % mod;
+            }
+        }
+
+        return dp.back();        
+    }
+};

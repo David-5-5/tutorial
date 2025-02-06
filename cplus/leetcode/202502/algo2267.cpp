fa61d3170eb7afac480f1495021d6a5c4aa1548b
David-5-5 <13910767907@139.com>
2025-02-07 00:23:13 +0800

    Append comments

--- cplus/leetcode/202502/algo2267.cpp
+++ cplus/leetcode/202502/algo2267.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Solution {
 public:
     bool hasValidPath(vector<vector<char>>& grid) {
+        // 自行解答
         int m = grid.size(), n = grid[0].size();
 
         if ((m + n - 1)%2 || grid[0][0] == ')' || grid[m-1][n-1] == '(') return false;
@@ -14,19 +15,18 @@ public:
         function<bool(int,int,int)> dfs = [&](int i, int j, int p) -> bool {
             
             if (i>=m || j>=n || p > (m+n-1)/2 || p < 0) {
-                cout << "i=" << i << ",j=" << j << ",p=" << p << endl; 
                 return false;
             }            
             if (i == m-1 && j == n-1) if (p == 1) return true; else return false;
 
             if (memo[i][j][p] != -1) return memo[i][j][p];
 
-            cout << "i=" << i << ",j=" << j << ",p=" << p << endl; 
-            if (dfs(i+1, j, p + (grid[i][j] == '(' ? 1 : -1))) return memo[i][j][p] = 1;
-            if (dfs(i, j+1, p + (grid[i][j] == '(' ? 1 : -1))) return memo[i][j][p] = 1;
+            // p 的下一个值放到递归函数内部
+            int change = grid[i][j] == '(' ? 1 : -1;
+            if (dfs(i+1, j, p + change)) return memo[i][j][p] = 1;
+            if (dfs(i, j+1, p + change)) return memo[i][j][p] = 1;
 
             return memo[i][j][p] = 0;
-
         };
 
         return dfs(0,0,0);

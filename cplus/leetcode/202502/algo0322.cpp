aef2b627ffb0176906852ad37239863f0d2f3b95
David-5-5 <13910767907@139.com>
2025-02-12 02:31:19 +0800

    Append algo using original full knapsack

--- cplus/leetcode/202502/algo0322.cpp
+++ cplus/leetcode/202502/algo0322.cpp
@@ -20,4 +20,24 @@ public:
         return dp[amount];
         
     }
+
+    int coinChange2(vector<int>& coins, int amount) {
+        // 自行解答 完全背包 非降维写法
+        int n = coins.size();
+        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
+        dp[0][0] = 0;
+        for (int i=0; i<n; i++) {
+            int x = coins[i];
+            for (int k=0; k<=amount; k++) {     // k begin from 0
+                if (dp[i][k]!=-1) dp[i+1][k] = dp[i][k];  // initial dp[i+1][k]
+                if (k<x || dp[i+1][k-x] == -1) continue; // dp[k-x] is invalid
+
+                if (dp[i][k] == -1) dp[i+1][k] = dp[i+1][k-x]+1;
+                else dp[i+1][k] = min(dp[i][k], dp[i+1][k-x]+1);   // set the minimize
+            }
+        }
+
+        return dp[n][amount];
+        
+    }
 };

5538c61a09cdf875c50da9351e9eff69c1236829
David-5-5 <13910767907@139.com>
2025-02-15 03:21:24 +0800

    Append implementation refer to solution

--- python/algo/202502/algo1449.py
+++ python/algo/202502/algo1449.py
@@ -1,3 +1,4 @@
+from math import inf
 from typing import List
 
 # 动态规划 -  完全背包
@@ -21,8 +22,28 @@ class Solution:
 
         return "".join([str(i) for i in reversed(dp[target])])
 
+    def largestNumber(self, cost: List[int], target: int) -> str:
+        # 参考题解 首先求解出最大次数，然后根据最大次数计算使用的数字
+        dp = [-inf] * (target + 1) # 列表存储最大长度的方案选择的字符
+        dp[0] = 0
+        for k in cost:
+            for j in range(k, target+1):
+                dp[j] = max(dp[j], dp[j-k]+1)
+        
+        if dp[target] == -inf: return "0"
+        
+        ans = ""
+        p = target
+        for i in range(9, 0, -1):
+            while p>=cost[i-1] and dp[p] == dp[p-cost[i-1]] + 1:
+                ans += str(i)
+                p -= cost[i-1]
+        return ans
+
 
 if __name__ == "__main__":
     sol = Solution()
     cost, target = [4,3,2,5,6,7,2,5,5], 9
     print(sol.largestNumber(cost, target))
+
+

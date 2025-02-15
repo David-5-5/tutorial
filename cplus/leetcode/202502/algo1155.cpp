cac5ca20e6c08be744afcafc68f73e573700710f
David-5-5 <13910767907@139.com>
2025-02-16 01:28:59 +0800

    Append comments

--- cplus/leetcode/202502/algo1155.cpp
+++ cplus/leetcode/202502/algo1155.cpp
@@ -5,14 +5,14 @@ using namespace std;
 class Solution {
 public:
     int numRollsToTarget(int n, int k, int target) {
-        // 学习 oi-wiki 分组背包 模板
+        // 学习 oi-wiki 分组背包 模板 求方案数
         int mod = 1e9 + 7;
         vector<int> dp(target+1, 0);
         dp[0] = 1;
-        for (int g=0; g<n; g++)             // 循环每一组, n个硬币
+        for (int g=0; g<n; g++)              // 循环每一组, n个硬币
             for (int x=target; x>=0; x--) {  // 循环背包容量
                 dp[x] = 0;                   // oi-wiki 模板求最大值，计数需要设置为 0
-                for (int j=1; j<=k; j++)    // 每个硬币 k 个面
+                for (int j=1; j<=k; j++)     // 每个硬币 k 个面
                     if (x>=j)
                         dp[x] = (dp[x] + dp[x-j]) % mod;
             }

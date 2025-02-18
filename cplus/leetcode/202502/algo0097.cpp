2262ea2edf83162b447ec4c95964f1f2ad332fad
David-5-5 <13910767907@139.com>
2025-02-19 01:37:49 +0800

    Append simpified implementation

--- cplus/leetcode/202502/algo0097.cpp
+++ cplus/leetcode/202502/algo0097.cpp
@@ -38,4 +38,35 @@ public:
         };
         return dfs(0, 0);     
     }
+
+    bool isInterleave(string s1, string s2, string s3) {
+        // 自行解答 - 简化
+        if (s1.size() + s2.size() != s3.size()) return false;
+
+        int m = s1.size(), n = s2.size();
+        vector<vector<int>> memo(m, vector<int>(n, -1));
+
+        function<bool(int, int)> dfs = [&](int i, int j) ->bool {
+        // auto dfs = [&](this auto&& dfs, int i, int j) -> bool {
+            // cout << i << ", " << j << endl;
+            if (i == m && j == n) return true;
+            if (i == m) return s2.substr(j) == s3.substr(m+j);
+            if (j == n) return s1.substr(i) == s3.substr(n+i);
+            
+            auto& res = memo[i][j];
+            if (res != -1) return res;
+            
+            if (s1[i] == s3[i+j])  { // 深刻理解递归，不需要 for 语句
+                res = dfs(i+1, j); 
+                if (res) return true;
+            } 
+            if (s2[j] == s3[i+j])  { 
+                res = dfs(i, j+1);
+                if (res) return true;
+            } 
+            
+            return false;
+        };
+        return dfs(0, 0);     
+    }    
 };

fa770a4b61fd3886461f184c6c75b77bfc668f8c
David-5-5 <13910767907@139.com>
2025-02-19 17:13:37 +0800

    Append comments

--- cplus/leetcode/202502/algo1035.cpp
+++ cplus/leetcode/202502/algo1035.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// 动态规划 - 最长公共子序列（LCS）
+// 动态规划 - 最长公共子序列 (LCS)
 class Solution {
 public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {

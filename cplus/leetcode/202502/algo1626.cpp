bebff9b2984a3191158e715f8b3e83186a225a00
David-5-5 <13910767907@139.com>
2025-02-23 00:53:07 +0800

    Append implementation by alternative sorting

--- cplus/leetcode/202502/algo1626.cpp
+++ cplus/leetcode/202502/algo1626.cpp
@@ -26,5 +26,25 @@ public:
         return ans;
     }
 
+    int bestTeamScore2(vector<int>& scores, vector<int>& ages) {
+        // 参考题解 O(n^2) 解法 按照(年龄,分数)排序
+        int n = scores.size();
+        vector<vector<int>> zip;
+        for (int i=0; i<n; i++)
+            zip.push_back({ages[i], scores[i]});
+        
+        sort(zip.begin(), zip.end()); // 按年龄排序
 
+        int ans = 0;
+        vector<int> dp(n, 0);
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<i; j++) {
+                if (zip[i][1] >= zip[j][1]) dp[i] = max(dp[i], dp[j]);
+            }
+            dp[i] += zip[i][1];
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
 };

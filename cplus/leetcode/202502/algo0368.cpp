d59ddc3e8984a49ff1141a9bc6ae1e176a055bcf
David-5-5 <13910767907@139.com>
2025-02-24 01:02:57 +0800

    Append comments

--- cplus/leetcode/202502/algo0368.cpp
+++ cplus/leetcode/202502/algo0368.cpp
@@ -5,9 +5,17 @@ using namespace std;
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
+        // 自行解答
         sort(nums.begin(), nums.end()); // 排序
         int cnt = 1, inx = 0, n= nums.size();
-        vector<pair<int,int>> dp(n, {-1,0});
+        // dp[i].first = j, 上一个能被nums[i]整除的的元素下标
+        //        which max(dp[j].second) 0<=j<i 
+        // 记录上一个元素的索引是为了输出结果数组
+        // 也可以找到最大的cnt 后。从最大 cnt 的元素倒序
+        // 找 cnt -1 同时能够整除下一个元素，直至cnt 为 0 
+        
+        // dp[i].second = dp[j].second + 1  子序列的长度
+        vector<pair<int,int>> dp(n, {-1,0}); 
         dp[0] = {-1, 1};
         // 逻辑同 300
         for (int i=1; i<n; i++) {

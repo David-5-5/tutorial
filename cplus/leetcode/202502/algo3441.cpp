b4ac903cf8acdc9bbc9afc2cb4997ce8ac43c1ca
David-5-5 <13910767907@139.com>
2025-02-26 00:09:07 +0800

    Append comments

---
+++ cplus/leetcode/202502/algo3441.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 动态规划 - 最优划分
+class Solution {
+public:
+    string minCostGoodCaption(string caption) {
+        int l = 0, n = caption.size();
+        
+        string ans = "";
+        
+
+        while l < n:
+
+        
+    }
+};

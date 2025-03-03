910380b408aeff319dc9d677f68b99f45dfae6f8
David-5-5 <13910767907@139.com>
2025-03-04 00:48:22 +0800

    Append optimized implementation

--- cplus/leetcode/202503/algo2708.cpp
+++ cplus/leetcode/202503/algo2708.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Solution {
 public:
     long long maxStrength(vector<int>& nums) {
+        // 自行解答 动态规划
         int n = nums.size();
         long mx = nums[0], mn = nums[0];
         for (int i=1; i<n; i++) {
@@ -16,5 +17,16 @@ public:
         return mx;        
     }
 
+    long long maxStrength2(vector<int>& nums) {
+        // 参考题解，简化版本
+        int n = nums.size();
+        long mx = nums[0], mn = nums[0];
+        for (int i=1; i<n; i++) {
+            long x = nums[i], tmp = mn;
+            mn = min({x, x*mn, x*mx, mn});
+            mx = max({x, x*tmp, x*mx, mx});
+        }
 
+        return mx;        
+    }
 };

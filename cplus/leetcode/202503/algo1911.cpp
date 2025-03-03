56513ee147cb0961f719221fad8b0048d33d5b58
David-5-5 <13910767907@139.com>
2025-03-04 01:00:24 +0800

    Add algo using state dp

---
+++ cplus/leetcode/202503/algo1911.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 动态规划 -  状态机 DP - 基础
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        // 自行解答
+        int n = nums.size();
+        vector<vector<long>> memo(n, vector<long>(2, -1));
+        auto dfs = [&](this auto&& dfs, int i, int odd) -> long {
+            if (i == n) return 0;
+
+            auto& res = memo[i][odd];
+            if (res != -1) return res;
+            res = dfs(i+1, odd);
+            res = max(res, (odd==1?nums[i]:-nums[i]) + dfs(i+1, odd^1));
+            return res;
+        };
+
+        return dfs(0, 1);        
+    }
+};

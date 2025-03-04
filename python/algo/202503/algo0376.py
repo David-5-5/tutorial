e12a87eb9427c7e8a065703238df59c8a5b1a3de
David-5-5 <13910767907@139.com>
2025-03-05 00:23:16 +0800

    Add algo using memory search

---
+++ python/algo/202503/algo0376.py
@@ -0,0 +1,23 @@
+from functools import cache
+from typing import List
+
+
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        @cache
+        def dfs(i:int, p:int, inc:int):
+            if (i == n): return 0
+
+            res = dfs(i+1, p, inc)
+
+            if p == -1:  # 未选择
+                res = max(res, 1 + dfs(i+1, i, inc))
+            elif inc == 0 and nums[p]!=nums[i]: # 只选了一个
+                res = max(res, 1 + dfs(i+1, i, 1 if nums[i]>nums[p] else -1))
+            elif inc > 0 and nums[i]<nums[p] or inc < 0 and nums[i]>nums[p]:
+                res = max(res, 1 + dfs(i+1, i,  -inc))
+                
+            return res
+        return dfs(0, -1, 0)

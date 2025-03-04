bfce150a791735ceddb25c0979c4f20038c5c56c
David-5-5 <13910767907@139.com>
2025-03-05 00:51:20 +0800

    Append comments

--- cplus/leetcode/202503/algo0376.cpp
+++ cplus/leetcode/202503/algo0376.cpp
@@ -11,12 +11,12 @@ public:
 
         if (n<2) return n;
 
-        int down = 1, up = 1;
+        int down = 1, up = 1;   // 初始化 nums[0] 为结尾的上升或下降序列的长度
         for (int i=1; i<n; i++) {
             if (nums[i] > nums[i-1]) {
-                up = max(up, down+1);
+                up = max(up, down+1);           // 转移方程 i-1为结尾 的上升序列和 i-1 为结尾的下降序列 
             } else if (nums[i] < nums[i-1]) {
-                down = max(down, up+1);
+                down = max(down, up+1);         // 转移方程
             }
         }
 

cc4a60f8e42660fe344b69931177ed9521bf9e93
David-5-5 <13910767907@139.com>
2025-03-07 01:24:11 +0800

    Append comments

--- cplus/leetcode/202503/algo3434.cpp
+++ cplus/leetcode/202503/algo3434.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
+        // 参考题解 状态 dp
         int n = nums.size();
 
         unordered_set<int> st(nums.begin(), nums.end());
@@ -12,9 +13,9 @@ public:
         for (auto& target:st) {
             int f0 = 0, f1 = 0, f2 = 0;
             for (auto& x : nums)  {
-                f2 = max(f2, f1) + (x==k);
-                f1 = max(f1, f0) + (x == target);
-                f0 += (x == k);
+                f2 = max(f2, f1) + (x==k);          // 在子数组右边，做中右
+                f1 = max(f1, f0) + (x == target);   // 在子数组中
+                f0 += (x == k);                     // 在子数组做
             }
             ans = max({ans, f1, f2});
         }

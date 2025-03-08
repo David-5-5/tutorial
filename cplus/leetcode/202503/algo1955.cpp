3353589620d45cac71dae654290ce3deab19cf8b
David-5-5 <13910767907@139.com>
2025-03-08 17:29:56 +0800

    Append commnets

--- cplus/leetcode/202503/algo1955.cpp
+++ cplus/leetcode/202503/algo1955.cpp
@@ -5,9 +5,14 @@ using namespace std;
 class Solution {
 public:
     int countSpecialSubsequences(vector<int>& nums) {
-        
+        // 参考题解
         const int mod = 1e9 + 7;
 
+        // f0 表示正整数个 0 组成的子序列
+        // f1 表示正整数个 0 紧接着正整数个 1 组成的子序列
+        // f2 表示正整数个 0 紧接着正整数个 1，最后正整数个 2 组成的子序列
+        // 状态转移时
+        // 当 nums[i] == 0 时，
         int f0 = 0, f1 = 0, f2 = 0;
         for (auto x:nums) {
             if (x == 0) f0 = ((long)f0 * 2 + 1) % mod;

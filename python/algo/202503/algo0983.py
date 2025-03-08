4e064fc220983b27cfba1e491aa56e1db044b0ec
David-5-5 <13910767907@139.com>
2025-03-09 01:25:18 +0800

    Append new implementation

--- python/algo/202503/algo0983.py
+++ python/algo/202503/algo0983.py
@@ -17,3 +17,15 @@ class Solution:
             res = min(res, costs[2] + dfs(i+1, days[i]+29))
             return res
         return dfs(0, 0)
+
+    def mincostTickets2(self, days: List[int], costs: List[int]) -> int:
+        # 参考题解 降维
+        days = set(days)
+        
+        @cache
+        def dfs(i:int) -> int:
+            if i > 365: return 0
+
+            if i not in days: return dfs(i+1)
+            return min(costs[0] + dfs(i+1), costs[1] + dfs(i+7), costs[2] + dfs(i+30))
+        return dfs(1)

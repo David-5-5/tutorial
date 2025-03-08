e6dd1a395e3ccf361abe957b66cd5b86cb29b8bd
David-5-5 <13910767907@139.com>
2025-03-09 01:36:52 +0800

    Append new implementation

--- cplus/leetcode/202503/algo0983.cpp
+++ cplus/leetcode/202503/algo0983.cpp
@@ -5,13 +5,13 @@ using namespace std;
 class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        // 自行解答，记忆化搜索超时 python 记忆化搜索可以通过
-        int n = days.size(); 
+        // 自行解答
+        int n = days.size();
         vector<vector<int>> memo(n, vector<int>(366, -1));
 
         auto dfs = [&](this auto&& dfs, int i, int v) -> int {
-            if (i == n) return 0;
-            auto res = memo[i][v];
+            if (i >= n || v > 365) return 0;
+            auto& res = memo[i][v];
             if (res != -1) return res;
 
             if (days[i] <= v) return res = dfs(i+1, v);
@@ -23,4 +23,25 @@ public:
         };
         return dfs(0,0);
     }
+
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        // 参考题解 降维
+        unordered_set dayset(days.begin(), days.end());     // set
+        vector<int> memo(366, -1);
+
+        auto dfs = [&](this auto&& dfs, int i) -> int {
+            if (i > 365) return 0;
+            auto& res = memo[i];
+            if (res != -1) return res;
+            
+            if (!dayset.count(i)) return res = dfs(i+1);
+            res = costs[0] + dfs(i+1);
+            res = min(res, costs[1] + dfs(i+7));
+            res = min(res, costs[2] + dfs(i+30));
+            
+            return res;
+        };
+        return dfs(1);
+    }
+
 };

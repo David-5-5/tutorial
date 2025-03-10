90543d916a6a0926fcc158ffc68e03d432b296a9
David-5-5 <13910767907@139.com>
2025-03-11 02:01:53 +0800

    Append comments

--- cplus/leetcode/202503/algo2167.cpp
+++ cplus/leetcode/202503/algo2167.cpp
@@ -5,6 +5,10 @@ using namespace std;
 class Solution {
 public:
     int minimumTime(string s) {
+        // 参考题解
+        // 启示：
+        // 前后端操作 - 前后缀分解
+        // 根据状态转移方程递推 代替 记忆化搜索，这样易于进行优化和组合
         int n = s.size();
         
         vector<int> suf(n+1);
@@ -14,7 +18,6 @@ public:
             else suf[i] = min(suf[i+1] + 2, n-i);
         }
     
-
         int ans = suf[0], pre = 0;
         for (int i=0; i<n-1; i++) {
             if (s[i] == '1')

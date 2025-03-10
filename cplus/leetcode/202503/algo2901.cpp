e76ee61bcd729938618110c132cf1307e443c6f8
David-5-5 <13910767907@139.com>
2025-03-10 16:02:49 +0800

    Append comments

--- cplus/leetcode/202503/algo2901.cpp
+++ cplus/leetcode/202503/algo2901.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Solution {
 public:
     vector<string> getWordsInLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        // 参考题解
         auto check = [&](int i, int j) -> bool {
             if (words[i].size() != words[j].size() || groups[i] == groups[j]) return false;
             int cnt = 0;
@@ -17,7 +18,7 @@ public:
         int n = words.size(), begin = n - 1;
         vector<int> f(n), from(n);
         for (int i = n-1; i>=0; i--) {
-            for (int j=i+1; j<n; j++) {
+            for (int j=i+1; j<n; j++) { // i != j
                 if (f[j] > f[i] && check(i, j)){
                     f[i] = f[j];
                     from[i] = j;
@@ -25,10 +26,10 @@ public:
             }
             f[i] ++;    // 计数自身
             if (f[i] > f[begin]) {
-                begin = i;
+                begin = i;          // 记录最大序列长度及起点
             }
         }
-        int m = f[begin];   // 最大路径，begin为起点
+        int m = f[begin];           // 最大路径，begin为起点
         vector<string> ans(m);
         for (int i = 0; i < m; i++) {
             ans[i] = words[begin];

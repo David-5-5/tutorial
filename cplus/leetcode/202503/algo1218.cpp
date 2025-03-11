f87d9cbe04c950acf984c337084faa30136f445a
David-5-5 <13910767907@139.com>
2025-03-11 22:55:24 +0800

    Append comments

--- cplus/leetcode/202503/algo1218.cpp
+++ cplus/leetcode/202503/algo1218.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Solution {
 public:
     int longestSubsequence(vector<int>& arr, int difference) {
+        // 自行解答
         int n = arr.size();
         unordered_map<int, int> pos;
         vector<int> dp(n, 1);
@@ -13,10 +14,10 @@ public:
         for (int i = 0; i<n; i++) {
             int prev = arr[i] - difference;
             if (pos.contains(prev)) {
-                dp[i] = dp[pos[prev]] + 1;
+                dp[i] = dp[pos[prev]] + 1; // 前序的值 + 1
             }
             pos[arr[i]] = i;
-            ans = max(ans, dp[i]);
+            ans = max(ans, dp[i]);        // 更新当前值的最后下标
         }
 
         return ans;

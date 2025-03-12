811c8a1b19eca823dd8fc6abd8506cee30c0a198
David-5-5 <13910767907@139.com>
2025-03-13 01:11:02 +0800

    Append comments

--- cplus/leetcode/202503/aglo0873.cpp
+++ cplus/leetcode/202503/aglo0873.cpp
@@ -5,15 +5,19 @@ using namespace std;
 class Solution {
 public:
     int lenLongestFibSubseq(vector<int>& arr) {
+        // 参考题解
         int n = arr.size();
         unordered_map<int, int> indices;
         vector<vector<int>> dp(n, vector<int>(n));
         int ans = 0;
      
         for (int i=0; i<n; i++) {
-            for (int j = i - 1; j >= 0 && arr[j] * 2 > arr[i]; j--) {
+            // 下面的边界条件很关键 arr[j] * 2 > arr[i] 保证 k < j
+            // for (int j = i - 1; j >= 0 && arr[j] * 2 > arr[i]; j--) {
+            for (int j=0; j<n; j++)  {
                 if (indices.contains(arr[i]-arr[j])){
                     int k = indices[arr[i]-arr[j]];
+                    if (k > j) break;
                     dp[j][i] = max(dp[k][j] + 1, 3);                    
                 }
                 ans = max(ans, dp[j][i]);

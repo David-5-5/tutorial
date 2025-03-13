6b60d548c32bcedf9cd7a3cb0057b45d87515dbf
David-5-5 <13910767907@139.com>
2025-03-13 15:11:11 +0800

    Append comments

--- cplus/leetcode/202503/algo2188.cpp
+++ cplus/leetcode/202503/algo2188.cpp
@@ -5,10 +5,12 @@ using namespace std;
 class Solution {
 public:
     int minimumFinishTime(vector<vector<int>>& tires, int changeTime, int numLaps) {
+        // 参考题解
         vector<int> minSec(17, INT_MAX / 2);
 
         for (auto& x : tires) {
             int sec = x[0];
+            // 第 i 圈的用时不应超过 sec<=changeTime+x[0]，否则更换轮胎即可
             for (int i=1, sum=0; sec<=changeTime+tire[0]; i++) {
                 sum += sec;
                 minSec[i] = min(sec, minSec[i]);
@@ -20,8 +22,8 @@ public:
         dp[0] = -changeTime;
         for (int i=1; i<=numLaps; i++) {
             for (int j=1; j<min(17, i); j++)
-                dp[i] = min(dp[i], dp[i-j] + minSec[j]);
-            dp[i] += changeTime;
+                dp[i] = min(dp[i], dp[i-j] + minSec[j]);  // 连续跑 j 圈转移方程
+            dp[i] += changeTime;    // 更换轮胎时间
         }
 
         return dp[numLaps];

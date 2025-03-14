d13dec084d17a0ea7559097a239503ffa68c2a85
David-5-5 <13910767907@139.com>
2025-03-15 00:19:37 +0800

    Append comments

--- cplus/leetcode/202502/algo3441.cpp
+++ cplus/leetcode/202502/algo3441.cpp
@@ -5,19 +5,22 @@ using namespace std;
 class Solution {
 public:
     string minCostGoodCaption(string caption) {
+        // 参考题解
         int n = caption.size();
         if (n<3) return "";
 
         vector<array<int, 26>> dp(n+1);     // 记录最小修改次数
         vector<array<int, 26>> nxt(n+1);    //记录转移来源
-        vector<int> minj(n+1);
+        vector<int> minj(n+1);              // f[i][minj[i]] = min(f[i][k])  0 <= k < 26
         
         for (int i=n-1; i>=0; i--) {
             int mn = INT_MAX;   
             for (int j=0; j<26; j++) {  // 枚举字母
+                // 状态转移方程
                 int res = dp[i + 1][j] + abs(caption[i] - 'a' - j);
                 int res2 = i <= n - 6 ? dp[i + 3][minj[i + 3]] + abs(caption[i] - 'a' - j) + abs(caption[i + 1] - 'a' - j) + abs(caption[i + 2] - 'a' - j) : INT_MAX;
-
+                
+                // 为输出最小字典序结果
                 if (res2 < res || res2 == res && minj[i + 3] < j) {
                     res = res2;
                     nxt[i][j] = minj[i + 3]; // 记录转移来源

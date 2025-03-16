75aa5e5e4f827635ad583680b3dbf53756ada42f
David-5-5 <13910767907@139.com>
2025-03-16 12:48:32 +0800

    Append comments

--- cplus/leetcode/202503/algo1220.cpp
+++ cplus/leetcode/202503/algo1220.cpp
@@ -5,34 +5,44 @@ using namespace std;
 class Solution {
 public:
     int countVowelPermutation(int n) {
+        // 自行解答
         const int MOD = 1e9 + 7;
 
-        // 矩阵快速幂 模板
+        // 矩阵转移方程 根据 a e i o u 后面的字符确定
+        vector<vector<int>> trans = {{0, 1, 0, 0, 0}, {1, 0 , 1, 0, 0}, {1, 1 , 0, 1, 1}, 
+                                     {0, 0, 1, 0, 1}, {1, 0, 0, 0, 0}};
+        // vector<vector<int>> result = {{1}, {1}, {1}, {1}, {1}};
+
+        // I 单位矩阵
+        vector<vector<int>> ans = {{1, 0, 0, 0, 0}, {0, 1, 0, 0, 0}, {0, 0, 1, 0, 0},
+                                   {0, 0, 0, 1, 0}, {0, 0, 0, 0, 1}};   
+
+        // BEGIN 矩阵快速幂 c++ 模板
         auto pow = [&](vector<vector<int>> a, vector<vector<int>> b) -> vector<vector<int>> {
             int r = a.size(), m = a[0].size(), c = b[0].size();
             vector<vector<int>> res(r, vector<int>(c));
             for (int i=0; i<r; i++) {
                 for (int j=0; j<c; j++)
                     for (int k=0; k<m; k++)
+                        // 乘法溢出
                         res[i][j] = (res[i][j] + (long)a[i][k] * b[k][j]) % MOD;
             }
             return res;
         };
-        vector<vector<int>> trans = {{0, 1, 0, 0, 0}, {1, 0 , 1, 0, 0}, {1, 1 , 0, 1, 1}, {0, 0, 1, 0, 1}, {1, 0, 0, 0, 0}};
-        vector<vector<int>> result = {{1}, {1}, {1}, {1}, {1}};
-
-        vector<vector<int>> ans = {{1, 0, 0, 0, 0}, {0, 1, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 1, 0}, {0, 0, 0, 0, 1}};   
         int k = n - 1;
-        while (k) {
-            if ((k & 1)== 1)
+        while (k) {     // 矩阵快速幂
+            if ((k & 1)== 1)    
                 ans = pow(trans, ans);
             trans = pow(trans, trans);
             k = k >> 1;
         }
-
-        auto res = pow(ans, result);
+       // END 矩阵快速幂 c++ 模板
+ 
+        // auto res = pow(ans, result);
         int sum = 0;
-        for (auto& r : res) sum = (sum + r[0])% MOD;
+        for (auto& r : ans) 
+            for (auto& v: r) 
+                sum = (sum + v)% MOD;
         return sum;
 
     }

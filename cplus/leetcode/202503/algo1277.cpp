a70bdf41774b74fb22268fa3344c51613ee57d2a
David-5-5 <13910767907@139.com>
2025-03-17 18:40:01 +0800

    Add algo using matrix dp

---
+++ cplus/leetcode/202503/algo1277.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 动态规划 -  子矩形 DP
+class Solution {
+public:
+    int countSquares(vector<vector<char>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        int ans = 0;
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        for (int i = 0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j== 0) dp[i][j] = 1;
+                    else dp[i][j] =  min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1])) + 1;
+                }
+                ans += dp[i][j];
+            }
+
+        }
+        return ans;
+    }
+};

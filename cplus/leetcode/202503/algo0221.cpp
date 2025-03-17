9fec3f694e3c53c574347ec2ce49c54f93999df5
David-5-5 <13910767907@139.com>
2025-03-17 18:24:55 +0800

    Append comments

--- cplus/leetcode/202503/algo0221.cpp
+++ cplus/leetcode/202503/algo0221.cpp
@@ -5,6 +5,10 @@ using namespace std;
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
+        // 参考题解，状态转移方程
+        // dp[i][j] = min(dp[i-1][j], dp[i-1][j-1]. dp[i][j-1]) + 1, matrix[i][j] = 1
+        //            1, i == 0 or j == 0
+        // dp[i][j] = 0 , matrix[i][j] = 0
         int n = matrix.size(), m = matrix[0].size();
         int ans = 0;
         vector<vector<int>> dp(n, vector<int>(m, 0));

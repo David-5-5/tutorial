1eba809310501f7bd01cda61ad95995d98c85d48
David-5-5 <13910767907@139.com>
2025-03-19 00:15:36 +0800

    Append comments

--- cplus/leetcode/202503/algo3218.cpp
+++ cplus/leetcode/202503/algo3218.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Solution {
 public:
     int minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
-        
+        // 自行解答
         unordered_map<int, int> memo;
         auto dfs = [&](this auto&& dfs, int x0, int x1, int y0, int y1) ->int {
             
@@ -15,16 +15,16 @@ public:
             if (memo.count(mask)) return memo[mask];
 
             int res = INT_MAX/2;
-
+            
+            // cut in horizontal
             for (int x=x0+1; x<x1; x++) {
-                // cout << "x=" << x << endl;
                 res = min(res, horizontalCut[x-1] + dfs(x0, x, y0, y1) + dfs(x, x1, y0, y1));
             }
-
+            // cut in vertical
             for (int y=y0+1; y<y1; y++) {
                 res = min(res, verticalCut[y-1] + dfs(x0, x1, y0, y) + dfs(x0, x1, y, y1));
             }
-
+            // calculate the minimize value
             return memo[mask] = res;
         };
         

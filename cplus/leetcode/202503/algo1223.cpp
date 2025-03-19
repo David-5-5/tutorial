62d67b19fbdb1b61d25494871b0a415101c99818
David-5-5 <13910767907@139.com>
2025-03-20 00:07:24 +0800

    Add algo using multi-dimension dp

---
+++ cplus/leetcode/202503/algo1223.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 动态规划 -  多维 DP
+class Solution {
+public:
+    int dieSimulator(int n, vector<int>& rollMax) {
+
+        const int mod = 1e9 + 7;
+        vector<vector<vector<int>>> memo(n, vector<vector<int>>(6, vector<int>(16, -1)));
+        auto dfs = [&](this auto&& dfs, int i, int p, int c) {
+            if (i == n) return  1;
+
+            auto& res = memo[i][p][c];
+            if (res != -1) return res;
+            
+            res = 0;
+            for (int j = 0; j<6; j++) {
+                if (j == p && c == rollMax[j]) continue;
+                res = ((long)res + dfs(i+1, j, j==p?c+1:1))%mod;
+            }
+            return res;        
+        };
+
+        return dfs(0, 0, 0); 
+    }
+};

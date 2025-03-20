9613cd4302c885c177f731fdf50a044117bff0d5
David-5-5 <13910767907@139.com>
2025-03-21 01:05:10 +0800

    Append comments

--- python/algo/202503/algo3154.py
+++ python/algo/202503/algo3154.py
@@ -3,12 +3,14 @@ from functools import cache
 # 动态规划 -  多维 DP
 class Solution:
     def waysToReachStair(self, k: int) -> int:
-
+        # 自行解答
         @cache
         def dfs(i:int, j:int, non:bool) -> int:
-            if i > k + 1: return 0
+            # 递归终止：向下一步不能连续使用，且jump是指数增长。
+            if i > k + 1: return 0  
 
             res = (i == k)
+            # 不能连续使用
             if (i>0 and non): res += dfs(i-1, j, False)
             res += dfs(i+(1<<j), j+1, True)
             return res

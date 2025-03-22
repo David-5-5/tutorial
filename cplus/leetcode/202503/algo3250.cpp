ba8729757db81b4c9abbe09558242049eba6751f
David-5-5 <13910767907@139.com>
2025-03-22 20:39:43 +0800

    Append optimized implementation using dp and prefix sum

--- cplus/leetcode/202503/algo3250.cpp
+++ cplus/leetcode/202503/algo3250.cpp
@@ -40,8 +40,8 @@ public:
     int countOfPairs2(vector<int>& nums) {
         // 自行解答  降低一维，但时间复杂度未变，优化存储空间
         const int mod = 1e9 + 7;
-        int n = nums.size(), mx = ranges::max(nums);
-
+        int n = nums.size(), mx = ranges::max(nums)i
+        
         vector<vector<int>> memo(n+1, vector<int>(mx+1, -1));
         // i index of nums
         // s previous value of arr1
@@ -91,5 +91,29 @@ public:
     }
 
 
+    int countOfPairs4(vector<int>& nums) {
+        // 自行解答，一步步优化过来，脑子都转不过来了！！
+        const int mod = 1e9 + 7;
+        int n = nums.size(), mx = ranges::max(nums);
 
+        vector<vector<int>> dp(n, vector<int>(mx+1));
+        
+        for (int j=0; j<=nums[0]; j++)  dp[0][j] = 1; // 初始值
+
+        for (int i=1; i<n; i++) {
+            for (int j=0; j<=nums[i]; j++) {
+                int d = max(0, nums[i] - nums[i-1]);
+                if (j+d > nums[i]) break;
+                // 不需要显式计算前缀和
+                // 完成方法或技巧，基于 countOfPairs3 方法 
+                // 根据例子，单步跟踪，找出 j 和 n_s 的关系，总结出规律!!!
+                dp[i][j+d] = ((j+d>0?dp[i][j+d-1]: 0)  + dp[i-1][j]) % mod;
+            }
+
+        };
+
+        int ans = 0;
+        for (auto v:dp[n-1]) ans = (ans + v) % mod;
+        return ans;    
+    }
 };

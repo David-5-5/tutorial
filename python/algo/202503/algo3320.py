b469cc1244623a950acfef2659d68393a6e48c40
David-5-5 <13910767907@139.com>
2025-03-24 01:15:03 +0800

    Add algo using memory search

---
+++ python/algo/202503/algo3320.py
@@ -0,0 +1,27 @@
+from functools import cache
+
+# 动态规划 -  多维 DP
+class Solution:
+    def countWinningSequences(self, s: str) -> int:
+        mod = 10 ** 9 + 7
+        n = len(s)
+        game = "FWE"
+
+        def score(a, b) -> int: 
+            if a == b: return 0
+            if a == 'F' and b == 'E' or a =='W' and b == 'F' or a=='E' and b == 'W':
+                return 1
+            else: return -1
+                
+        @cache
+        def dfs(i: int, t:int, p:int) -> int:
+            if i<0: return 1 if t > 0 else 0
+            
+            if (t+i) < 0: return 0  # 剪枝
+
+            res = 0
+            for k in range(3):
+                if k == p: continue
+                res = (res + dfs(i-1, t + score(game[k], s[i]), k)) % mod
+            return res
+        return dfs(n-1, 0, -1)

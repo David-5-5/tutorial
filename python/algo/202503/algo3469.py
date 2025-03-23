8906248d7011b32b1b01dbcb0b288074692a7b0a
David-5-5 <13910767907@139.com>
2025-03-23 14:42:43 +0800

    Append implementation using index replace of value

--- python/algo/202503/algo3469.py
+++ python/algo/202503/algo3469.py
@@ -3,8 +3,8 @@ from typing import List
 
 # # 动态规划 - 多维 DP
 class Solution:
-    # 自行解答 
     def minCost(self, nums: List[int]) -> int:
+        # 自行解答 
         n = len(nums)
         if n < 3: return max(nums)
 
@@ -19,6 +19,21 @@ class Solution:
         
         return dfs(1, nums[0])
 
+    def minCost2(self, nums: List[int]) -> int:
+        # 参考题解, 使用下标代替值域，便于改为递推
+        n = len(nums)
+        if n < 3: return max(nums)
+
+        @cache
+        def dfs(i: int, j:int) -> int:
+            if i == n: return nums[j]
+            if i+1 == n: return max(nums[j], nums[i])
+
+            sub = [(nums[j],j), (nums[i],i), (nums[i+1],i+1)]
+            sub.sort()
+            return min(sub[2][0] + dfs(i+2, sub[0][1]), sub[1][0] + dfs(i+2, sub[2][1]))
+        
+        return dfs(1, 0)
 
 if __name__ == "__main__":
     sol = Solution()

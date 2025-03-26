1cfbcae813d3934f1aa13515d317f5d6a90d1756
David-5-5 <13910767907@139.com>
2025-03-27 00:48:25 +0800

    Add algo using memory search

---
+++ python/algo/202503/algo1884.py
@@ -0,0 +1,20 @@
+from functools import cache
+from math import inf
+
+# 动态规划 - 多维 DP
+class Solution:
+    def twoEggDrop(self, n: int) -> int:
+        # 自行解答 - 性能需要优化
+        @cache
+        def dfs(i: int, j:int, k:int) -> int:
+            if k == 0: return inf
+            if k == 1: return j - i + 1
+            if i > j: return 0
+
+            res = inf
+            for t in range(i, j+1):
+                res = min(res, 1 + max(dfs(t+1, j, k),dfs(i, t-1, k-1)))
+            return res
+        ans = dfs(1, n, 2)
+        dfs.cache_clear()
+        return ans

1d93dd794537cf71d3508ca48510ce03da5ba81b
David-5-5 <13910767907@139.com>
2025-03-28 09:48:12 +0800

    Append implementation using hash

--- cplus/leetcode/202503/algo0219.cpp
+++ cplus/leetcode/202503/algo0219.cpp
@@ -5,9 +5,10 @@ using namespace std;
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        // 自行阶段，排序
         int n = nums.size(), id[n];
         iota(id, id+n, 0);
-        sort(id, id+n, [&](int i, int j){
+        sort(id, id+n, [&](int i, int j){   // 稳定排序
             return nums[i] < nums[j];
         });
 
@@ -16,6 +17,19 @@ public:
                 return true;
             }
         }
+        return false;  
+    }
+
+    bool containsNearbyDuplicate2(vector<int>& nums, int k) {
+        // hash表
+        int n = nums.size();
+        unordered_map<int, int> pos;
+
+        for (int i=0; i<n; ++i) {
+            if (pos.count(nums[i]) && i - pos[nums[i]] <= k) return true;
+            pos[nums[i]] = i;
+        }
         return false;
+
     }
 };

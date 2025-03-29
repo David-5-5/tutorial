601e55b5f9c0bf016b5bc8cd8e2ac530b2a9d656
David-5-5 <13910767907@139.com>
2025-03-30 02:18:59 +0800

    Append implementation using optimized enumerate method

--- cplus/leetcode/202503/algo3404.cpp
+++ cplus/leetcode/202503/algo3404.cpp
@@ -26,24 +26,29 @@ public:
 
     }
 
-    
+    // 四个数 枚举
     long long numberOfSubsequences2(vector<int>& nums) {
         // 参考题解，nums[p] * nums[r] == nums[q] * nums[s]
         // nums[p]/nums[q] == nums[s] / nums[r]
         // 考虑到这个思路，但是实现的具体步骤没有想清楚
-        int n = nums.size(), ans = 0;
-        unordered_map<int, int> pval;
+        int n = nums.size();
+        long ans = 0;
+        unordered_map<float, int> cnt;
+        // a b c d 分别代表 nums[p] nums[r] nums[q] nums[s]
         
-        for (int q=2; q<n; ++q) {
-            pval[nums[q-2]] ++;
-            for (int r=4; q+1<r && r<n; ++r) {
-                for (int s=6; r+1<s && s<n; ++s) {
-                    if (nums[q] * nums[s] % nums[r] == 0 ){
-                        int v = nums[q] * nums[s] / nums[r];
-                        ans += pval[v];
-                    }
-                }
+        for (int i=4; i<n-2; ++i) {     // 这个枚举的代码技巧太厉害了
+            float b = nums[i-2];
+            // 枚举 a
+            for (int j=0; j<i-3; j++) {
+                cnt[nums[j]/b] ++;      // a / b nums[p] / nums[q]
             }
+
+            float c = nums[i];
+            // 枚举 d
+            for (int j=i+2; j<n; j++) {
+                ans += cnt[nums[j]/c];
+            }
+
         }
 
         return ans;

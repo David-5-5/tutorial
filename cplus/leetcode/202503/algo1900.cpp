4587bf0f88cb0c425af29dc23a119a69c63d279f
David-5-5 <13910767907@139.com>
2025-03-31 02:12:19 +0800

    Append new implementation which still need to optimize

--- cplus/leetcode/202503/algo1900.cpp
+++ cplus/leetcode/202503/algo1900.cpp
@@ -75,5 +75,59 @@ public:
         int val = dfs((1<<n) - 1);
         return {val/100, val%100}; 
     }    
-  
+
+    vector<int> earliestAndLatest(int n, int firstPlayer, int secondPlayer) {
+        // 进一步优化，但目前效果不理想
+        vector<int> memo(1<<n, -1);
+
+        auto dfs = [&](this auto&& dfs, int mask) -> int {
+            
+            auto& res = memo[mask];
+            if (res != -1) return res;
+            vector<int> arr;
+            for (int i=0; i<28; i++) {
+                if ((mask>>i) & 1) arr.push_back(i+1);
+            }
+            
+            int m = arr.size();
+            if (m == 2) return res = 101;
+            int f = -1, s = -1;
+            for (int i=0; i<m/2; i++) {
+                if (arr[i] == firstPlayer && arr[m-i-1] == secondPlayer) return res = 101; 
+                if (arr[i] == firstPlayer || arr[m-i-1] == firstPlayer) {
+                    f = i;
+                } 
+                if (arr[i] == secondPlayer || arr[m-i-1] == secondPlayer) {
+                    s = i;
+                }
+            }
+            
+            int mx = 0, mn = INT_MAX;
+            
+            vector<int> select; 
+            for (int i=0; i<m; i++) {
+                if (i!=f && i!=s && f!=m-i-1 && s!=m-i-1)
+                    select.push_back(arr[i]);
+            }
+            m = select.size();
+            int cnt = 1 << (m/2);
+            // cout << m << "," << f << ", " << s << endl;
+            for (int i=0; i<cnt; i++) {
+                int nxt_mask = 1 << (firstPlayer-1) | 1 << (secondPlayer-1);
+                for (int j=0; j<m/2; j++) {
+                    if (i>>j & 1) nxt_mask |= 1 << (select[m-j-1]-1);
+                    else nxt_mask |= 1 << (select[j]-1);
+                }
+                if (m%2) nxt_mask |= 1 <<(select[m/2]-1);
+                
+                int val = memo[nxt_mask]!=-1?memo[nxt_mask]:dfs(nxt_mask);
+                mx = max(mx, 1 + val % 100);
+                mn = min(mn, 1 + val / 100);
+            }
+            return res = mn * 100 + mx;
+        };
+
+        int val = dfs((1<<n) - 1);
+        return {val/100, val%100};    
+    }       
 };

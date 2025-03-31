cef2feff5ba4ef50803630fda8d950182650ee91
David-5-5 <13910767907@139.com>
2025-03-31 11:28:34 +0800

    Append optimized implementation with time complexity O(n^2)

--- cplus/leetcode/202503/algo1995.cpp
+++ cplus/leetcode/202503/algo1995.cpp
@@ -23,5 +23,24 @@ public:
     }
 
 
+    int countQuadruplets2(vector<int>& nums) {
+        // 参考题解 nums[a] + nums[b] + nums[c] == nums[d] 
+        // nums[a] + nums[b] == nums[d] - nums[c]
+        // 遍历 nums[b] nums[c]
+        unordered_map<int, int> cnt;
+        int n = nums.size(), ans = 0;
 
+        for (int c=2; c<n-1; c++) {
+            int b = c-1;
+            for (int a=0; a<b; a++) {           // 遍历 a
+                cnt[nums[a]+nums[b]] ++;
+            }
+            
+            for (int d=c+1; d<n; d++) {         // 遍历 d
+                ans += cnt[nums[d]-nums[c]];
+            }
+
+        }
+        return ans;  
+    }
 };

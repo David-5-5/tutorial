0c31cf4d935a228cada352dac41de136eba2b7e5
David-5-5 <13910767907@139.com>
2025-03-31 09:28:22 +0800

    Append test case for ftt

--- cplus/dass/math/polynomial/ftt.cpp
+++ cplus/dass/math/polynomial/ftt.cpp
@@ -64,9 +64,40 @@ void workFFT(int n, int m)
         res[i]  = (int)(a[i].x / tot + 0.5); //向上去整
 }
 
+void print(int len, int itemsPerLn) {
+
+  for (int i=0; i<len; i++) {
+    printf("%d, ", res[i]);      // _zeats by generating
+    if ((i+1)%itemsPerLn==0) printf("\n");
+  }
+  printf("\n");
+}
+
+
+void selfFFT(int n)
+{// a[0, n], b[0, m]
+    while ((1 << bit) < n + 1)
+        bit++;
+    tot = 1 << bit;
+    for (int i = 0; i < tot; i++)
+        rev[i] = (rev[i >> 1] >> 1) | ((i & 1) << (bit - 1));
+    //递推(bit<<1)在bit之前，就已经被算出rev,最后一位是否为1
+    fft(a, 1);
+    for (int i = 0; i <= n; i++)
+        res[i]  = (int)(a[i].x / tot + 0.5); //向上去整    
+
+    print(256, 16);
+    fft(a, -1);//逆变换，点表示法转换为多项式表示法
+    
+    for (int i = 0; i <= n; i++)
+        res[i]  = (int)(a[i].x / tot + 0.5); //向上去整
+    print(256, 16);
+}
+
+
+
 int main()
 {   
-
     int coff_a[] = {3, 6, 1, 4, 2};
     int coff_b[] = {3, 3, 4, 2, 3};
     for (int i=0; i < 5; i++) {
@@ -77,4 +108,15 @@ int main()
     workFFT(5, 5);
     for (int i = 0; i <= 10; i++)
         cout << res[i] << ' ';
+    cout << endl;
+
+    // cout << "Calcuate 256 for ring" << endl;
+    
+    const int KYBER_Q = 3329;
+    for (int i = 0; i<256; i++) {
+        a[i].x = i * i % KYBER_Q;
+    }
+
+    selfFFT(256);
+
 }

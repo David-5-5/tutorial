c4f5f8dbcc0b1412742bc92f96508085606f6ae5
David-5-5 <13910767907@139.com>
2025-04-01 18:25:57 +0800

    Append optimized implementation

--- cplus/leetcode/202503/algo3265.cpp
+++ cplus/leetcode/202503/algo3265.cpp
@@ -27,4 +27,30 @@ public:
         }
         return ans;    
     }
+
+
+    int countPairs2(vector<int>& nums) {
+        // 自行解答, 优化后的代码，觉得漂亮多了
+        // 时间复杂度分析 O(n*m^2)  n 数组长度 m 为数组中值的数位长度
+        unordered_map<int, int> cnt;
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        for (auto v:nums) {
+            ans += cnt[v];
+            // string sv = format("{:0>7}", to_string(v)); // 补齐到固定长度
+            string sv = to_string(v);                   // 排序后不需要补齐
+            for (int i=1; i<sv.length(); i++) {         // 7 ** 2
+                for (int j=0; j<i; j++) {
+                    if (sv[i] != sv[j]) {
+                        swap(sv[i], sv[j]);             // 使用内置函数完成交换
+                        ans += cnt[stoi(sv)];           // 使用更安全的 stoi
+                        swap(sv[i], sv[j]);             // 恢复现场！！！
+                    }
+                }
+            }
+
+            cnt[v] ++;
+        }
+        return ans; 
+    }
 };

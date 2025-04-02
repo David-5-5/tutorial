7a7c11cfbed93e911a738cc5c0ae2e77e84a64af
David-5-5 <13910767907@139.com>
2025-04-02 14:07:11 +0800

    Add new implementation using stack and enumerate  2

--- cplus/leetcode/202504/algo0456.cpp
+++ cplus/leetcode/202504/algo0456.cpp
@@ -6,6 +6,7 @@ class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
         // 自行解答，有这个思路都不敢写，题解的第一种方法和思路的一致。
+        // 枚举 3
         int n = nums.size();
         vector<int> suf;
         for (int i=2; i<n; i++) suf.push_back(nums[i]);
@@ -23,5 +24,27 @@ public:
         }
 
         return false;       
+    }
+
+    bool find132pattern2(vector<int>& nums) {
+        // 参考题解 枚举 2
+        int n = nums.size();
+        stack<int> can_k;
+        can_k.push(nums[n-1]);
+        int max_k = INT_MIN;
+
+        for (int i=n-2; i>=0; i--) {
+            if (nums[i] < max_k) return true;
+
+            while (!can_k.empty() && nums[i] > can_k.top()) {
+                max_k = max(max_k, can_k.top());
+                // max_k = can_k.top();     // it is also ok ?
+                can_k.pop();
+            }
+
+            if (nums[i] > max_k) can_k.push(nums[i]);
+        }
+
+        return false;
     }
 };

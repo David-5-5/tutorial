6abde3d04fe71011002e5be85fbcd6998131a8d5
David-5-5 <13910767907@139.com>
2025-04-02 22:48:36 +0800

    Append new implementation refer to solution

--- cplus/leetcode/202504/algo2055.cpp
+++ cplus/leetcode/202504/algo2055.cpp
@@ -28,7 +28,7 @@ public:
     }
 
     
-    vector<int> platesBetweenCandles(string s, vector<vector<int>>& queries) {
+    vector<int> platesBetweenCandles2(string s, vector<vector<int>>& queries) {
         // 自行解答 使用 set
         // 学习 set lower_bound upper_bound usage
         int n = s.length();
@@ -51,5 +51,34 @@ public:
             else ans.push_back(0);
         }
         return ans;     
+    }
+
+    vector<int> platesBetweenCandles3(string s, vector<vector<int>>& queries) {
+        // 参考题解，预处理每个下标最近的 left 或 right 的candle
+        int n = s.length();
+        vector<int> pres(n+1);
+        for (int i=0; i<n; i++) {
+            pres[i+1] = pres[i] + (s[i] == '*' ? 1 : 0);
+        }
+
+        vector<int> left(n);
+        for (int i=0, l=-1; i<n; i++) {
+            if (s[i] == '|') l = i;
+            left[i] = l;
+        }
+
+        vector<int> right(n);
+        for (int i=n-1, r=n; i>=0; i--) {
+            if (s[i] == '|') r = i;
+            right[i] = r;
+        }
+
+
+        vector<int> ans;
+        for (auto & q : queries) {
+            if (right[q[0]] > left[q[1]]) ans.push_back(0);
+            else ans.push_back(pres[left[q[1]]]-pres[right[q[0]]]);
+        }
+        return ans;
     }
 };

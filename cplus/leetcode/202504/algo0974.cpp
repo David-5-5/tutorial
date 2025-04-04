5194b34d1e04cf0a4a570db6c7152e86ad76ee1b
David-5-5 <13910767907@139.com>
2025-04-04 11:54:09 +0800

    Append comments

--- cplus/leetcode/202504/algo0974.cpp
+++ cplus/leetcode/202504/algo0974.cpp
@@ -5,13 +5,14 @@ using namespace std;
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        // 自行解答
+        // 自行解答 
+        // TEMPLATE - Begin 前缀和计算 
         int n = nums.size();
         vector<int> pres(n+1);
-
         for (int i=0; i<n; i++) pres[i+1] = pres[i] + nums[i];
+        // TEMPLATE - End   前缀和计算 
 
-        int cnt[k], ans = 0;
+        int cnt[k], ans = 0;            // 模 k，有限值域，数组代替哈希
         memset(cnt, 0, sizeof(cnt));
         
         for (auto& v: pres) {

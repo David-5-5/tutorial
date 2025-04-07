2b31555375da66a7ae729ac2a07a39be4b9e879e
David-5-5 <13910767907@139.com>
2025-04-08 00:41:50 +0800

    Append optimized implementation

--- cplus/leetcode/202504/algo1547.cpp
+++ cplus/leetcode/202504/algo1547.cpp
@@ -6,7 +6,7 @@ class Solution {
 public:
     int minCost(int n, vector<int>& cuts) {
         // 自行解答，记忆化搜索 O(N^3)
-        
+
         unordered_map<long, int> memo;
         auto dfs = [&](this auto&& dfs, int i, int j) {
             long mask = (long) i << 32 | j;
@@ -25,6 +25,30 @@ public:
         };
 
         return dfs(0, n);
+
+    }
+    
+    int minCost2(int n, vector<int>& cuts) {
+        // 参考解答，优化实现，记忆化搜索 O(N^3)
+        
+        cuts.push_back(0), cuts.push_back(n);
+        int m = cuts.size();
+        sort(cuts.begin(), cuts.end());
+        
+        vector<vector<int>> memo(m, vector<int>(m, -1));
+        auto dfs = [&](this auto&& dfs, int i, int j) {
+            if (i+1 == j) return 0;
+
+            auto& res = memo[i][j];
+            if (res != - 1) return res;
+            res = INT_MAX;
+            for (int k=i+1; k < j; ++k) {
+                res = min(res, dfs(i, k) + dfs(k, j));
+            }
+            return res += cuts[j] - cuts[i];
+        };
+
+        return dfs(0, m-1);   
 
     }
 };

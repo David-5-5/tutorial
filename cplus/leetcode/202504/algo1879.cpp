17559235d0e0062f3bd0d9879f5288f7282b4433
David-5-5 <13910767907@139.com>
2025-04-09 02:07:04 +0800

    Append comments

--- cplus/leetcode/202504/algo1879.cpp
+++ cplus/leetcode/202504/algo1879.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Solution {
 public:
     int minimumXORSum(vector<int>& nums1, vector<int>& nums2) {
+        // 自行解答
         int n = nums2.size();
 
         vector<int> memo(1<<n, INT_MAX);
@@ -13,14 +14,12 @@ public:
             
             auto& res = memo[state];
             if (res != INT_MAX) return res;
-
-            int i = n - __builtin_popcount(state);            
-            for (int j=0; j<n; j++) {
-                // 位运算 和 比较符 !!! 多加括号
-                if ((state >> j) & 1) {
+            
+            // 计算包含一个的个数，减少一个递归参数
+            int i = n - __builtin_popcount(state);  
+            for (int j=0; j<n; j++) if ((state >> j) & 1) 
+                    // 按题解计算数组异或
                     res = min(res, (nums1[i] ^ nums2[j]) + dfs(state - (1 << j))) ;
-                }
-            }
 
             return res;
         };

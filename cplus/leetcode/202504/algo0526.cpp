e8aa3623f039aae4d01435fb8e044252962bca9f
David-5-5 <13910767907@139.com>
2025-04-09 02:03:43 +0800

    Add algo using state dp

---
+++ cplus/leetcode/202504/algo0526.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 动态规划 - 9.1 排列型 - 相邻无关
+class Solution {
+public:
+    int countArrangement(int n) {
+
+        vector<int> memo(1<<n, -1);
+        auto dfs = [&] (this auto&& dfs, int state) -> int {
+            if (state == 0) return 1;
+            
+            auto& res = memo[state];
+            if (res != -1) return res;
+
+            int i = n - __builtin_popcount(state);            
+            res = 0;
+            for (int j=0; j<n; j++) {
+                // 位运算 和 比较符 !!! 多加括号
+                if (((state >> j) & 1) && ((i+1) % (j+1)==0 || (j+1) % (i+1)==0)) {
+                    res += dfs(state - (1 << j)) ;
+                }
+            }
+
+            return res;
+        };
+
+        return dfs((1<<n)-1);
+    }
+};
+
+int main() {
+    cout << Solution().countArrangement(2);
+}

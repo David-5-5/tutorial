79f36884088b9cf11700bc4226e9193dbc333517
David-5-5 <13910767907@139.com>
2025-04-09 02:04:33 +0800

    Append comments

--- cplus/leetcode/202504/algo0526.cpp
+++ cplus/leetcode/202504/algo0526.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Solution {
 public:
     int countArrangement(int n) {
-
+        // 自行解答
         vector<int> memo(1<<n, -1);
         auto dfs = [&] (this auto&& dfs, int state) -> int {
             if (state == 0) return 1;
@@ -13,7 +13,7 @@ public:
             auto& res = memo[state];
             if (res != -1) return res;
 
-            int i = n - __builtin_popcount(state);            
+            int i = n - __builtin_popcount(state); // 计算包含一个个数，减少一个递归参数
             res = 0;
             for (int j=0; j<n; j++) {
                 // 位运算 和 比较符 !!! 多加括号

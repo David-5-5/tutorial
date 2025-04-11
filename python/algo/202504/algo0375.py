3afb3cb34baeabc947dd311f4dc432bbf5ec61a8
David-5-5 <13910767907@139.com>
2025-04-11 14:57:12 +0800

    Add algo using distinct dp

---
+++ python/algo/202504/algo0375.py
@@ -0,0 +1,17 @@
+from functools import cache
+
+# 动态规划 -  其他区间 DP
+class Solution:
+    def getMoneyAmount(self, n: int) -> int:
+        
+        @cache
+        def dfs(i:int, j:int) -> int:
+            if i >= j: return 0     # 必须 >=
+
+            res = n*n
+            for k in range(i, j):   # 如果剩两个数，猜小的
+                res = min(res, k + max(dfs(i,k-1), dfs(k+1, j)))
+            
+            return res
+        
+        return dfs(1, n)

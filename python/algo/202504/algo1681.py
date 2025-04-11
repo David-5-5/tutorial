3975b4d98bd41f83abdc21e8ea43a06411083a11
David-5-5 <13910767907@139.com>
2025-04-12 02:24:52 +0800

    Append new implementation refer to solution

--- python/algo/202504/algo1681.py
+++ python/algo/202504/algo1681.py
@@ -55,3 +55,93 @@ class Solution:
             return res
         ans = dfs((1<<n) - 1, k)
         return ans if ans < inf else -1
+
+
+    def minimumIncompatibility2(self, nums: List[int], k: int) -> int:
+        # 超时 递归出现大量冗余计算
+        n = len(nums)
+        cnt = n // k
+
+        def traverse_submasks(mask:int, i:int) -> List[int]:
+            arr = []
+            submask = mask
+            while True:
+                if submask.bit_count() == i:
+                    arr.append(submask)
+                submask = (submask - 1) & mask
+                if submask == mask:
+                    break
+            return arr
+
+        @cache
+        def dfs(state:int) -> int:
+            if state.bit_count() == 0: return 0
+            
+            res = inf
+            next = traverse_submasks(state, state.bit_count()-cnt)
+
+            for nt in next:
+                cur = state - nt
+                if values[cur] < inf:
+                    res = min(res, values[cur] + dfs(nt))
+            return res
+        
+        values = defaultdict(int)
+        masks = traverse_submasks((1<<n) - 1, cnt)
+        for v in masks:
+            ntset, mx, mn = set(), 0, inf
+
+            for i in range(n):
+                if v >> i & 1:
+                    ntset.add(nums[i])
+                    mx = max(mx, nums[i])
+                    mn = min(mn, nums[i])
+            if len(ntset) == v.bit_count():
+                values[v] = mx - mn
+            else:  values[v] = inf
+
+        ans = dfs((1<<n) - 1)
+        return ans if ans < inf else -1 
+
+    def minimumIncompatibility3(self, nums: List[int], k: int) -> int:
+        # 参考题解，基于 minimumIncompatibility2 二进制子集遍历技巧
+        # 从小到大遍历，避免重复
+        n = len(nums)
+        cnt = n // k
+        # 遍历所有子集
+        values = {}
+        for mask in range(1<<n):
+            if mask.bit_count() != cnt: continue
+            distinct = set()
+            mx, mn = 0, 18  # nums[i] <= 16
+            for i in range(n):
+                if mask >> i & 1:
+                    distinct.add(nums[i])
+                    if mx < nums[i]:mx = nums[i]
+                    if mn > nums[i]:mn = nums[i]
+            if len(distinct) == mask.bit_count():
+                values[mask] = mx-mn
+        
+
+        dp = [inf] * (1 << n)
+        dp[0] = 0
+        for mask in range(1<<n):
+            if dp[mask] == inf: # 剪枝
+                continue    
+            # 状态转移
+            # k nums[i], v: i 相同的值只取一个
+            exclusive = {}  # 和 mask 不相交的位
+            for i in range(n):
+                if mask >> i & 1 == 0:
+                    exclusive[nums[i]] = i
+            ex_mask = 0
+            if len(exclusive) < cnt: # 剪枝
+                continue
+            for v in exclusive.values():
+                ex_mask |= 1 << v
+            nxt = ex_mask
+            while nxt:
+                if nxt in values.keys():
+                    dp[mask | nxt] = min(dp[mask|nxt], dp[mask]+ values[nxt])
+                nxt = (nxt - 1) & ex_mask
+        return dp[-1] if dp[-1]<inf else -1

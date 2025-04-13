8ef5813334d153ebdec02c52a1d9ef482cc17053
David-5-5 <13910767907@139.com>
2025-04-13 22:06:23 +0800

    Add algo using dp and subset traversal

--- cplus/leetcode/202504/algo1655.cpp
+++ cplus/leetcode/202504/algo1655.cpp
@@ -5,20 +5,35 @@ using namespace std;
 class Solution {
 public:
     bool canDistribute(vector<int>& nums, vector<int>& quantity) {
-        unordered_map<int, int> cnt;
-        for (auto& v : nums) cnt[v] ++;
-        vector<int> ini;
-        for (auto& [_, v]: cnt) ini.push_back(v);        
+        unordered_map<int, int> cnt_map;
+        for (auto& v : nums) cnt_map[v] ++;
+        vector<int> cnt;
+        for (auto& [_, v]: cnt_map) cnt.push_back(v);        
 
-        int  n = cnt.size(), m = 1 << quantity.size();
-        for (int mask=0; mask<m; ++mask) {
-            
+        int  n = cnt_map.size(), m = 1 << quantity.size();
+        
+        vector<int> comb(m);
+        for (int i=0; i<quantity.size(); ++i) {
+            int low = 1 << i;
+            for (int j=0; j<low; ++j) 
+                comb[j|1<<i] = comb[j] + quantity[i];
         }
 
+        vector<vector<int>> dp(n+1, vector<int>(m, false));
+        for (int i=0; i<=n; i++) dp[i][0] = true;
 
+        for (int i=0; i<n; ++i) for (int mask=0; mask<m; ++mask) {
+            if (dp[i][mask]==false) continue;
+            dp[i+1][mask] = true;  
+            int exclusive = m - 1 - mask;
+            int sub = exclusive;
+            while (sub) {
+                if (cnt[i] >= comb[sub]) dp[i+1][mask|sub] = true;
+                sub = (sub-1) & exclusive;
+            }
+        }
 
-        for (auto& v: dp[(1<<n)-1]) if (v>=0) return true;
-        return false;
+        return dp[n][m-1];
 
     }
 };

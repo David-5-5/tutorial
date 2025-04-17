3b70db31e1d52239544ef426c84afd086f7690f6
David-5-5 <13910767907@139.com>
2025-04-18 02:23:04 +0800

    Add algo using state compress dp

---
+++ python/algo/202504/algo1349.py
@@ -0,0 +1,31 @@
+from functools import cache
+from typing import List
+
+
+class Solution:
+    def maxStudents(self, seats: List[List[str]]) -> int:
+        m, n = len(seats), len(seats[0])
+
+        def check(top:int, left, pos:int) -> bool:
+            x, y = pos // n, pos % n
+            if seats[x][y] == '#': return False
+            
+            if y and (top>>(y-1)&1) : return False # left of top
+            if y<n-1 and (top>>(y+1)&1): return False # right of top
+            if y and (left>>(y-1)&1): return False # left
+            
+            return True
+
+        @cache
+        def dfs(top:int, left:int, pos:int) ->int:
+            if pos == m * n: return 0
+            
+            # 不坐
+            res = dfs(top, left, pos + 1) if (pos+1) % n else dfs(left, 0, pos + 1)
+            # 坐
+            if check(top, left, pos):
+                left |= 1<<(pos%n)
+                res = max(res, (dfs(top, left, pos + 1) if (pos+1) % n else dfs(left, 0, pos + 1))+1)
+            return res
+
+        return dfs(0, 0, 0)        

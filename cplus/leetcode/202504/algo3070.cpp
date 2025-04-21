0edfe61613ef77269020b8fa4283a349c2c37f21
David-5-5 <13910767907@139.com>
2025-04-21 20:25:30 +0800

    Add algo without finish

---
+++ cplus/leetcode/202504/algo3070.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 常用数据结构 -  1.6 二维前缀和
+class Solution {
+public:
+    int countSubmatrices(vector<vector<int>>& grid, int k) {
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> pres(n+1, vector<int>(m+1));
+
+        for (int i=0; i<n; ++i) for (int j=0; j<m; ++j) {
+            pres[i+1][j+1] = grid[i][j] + pres[i][j+1] + pres[i+1][j] - pres[i][j];
+        }
+
+        
+    }
+};

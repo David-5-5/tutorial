531f79dfac71a0d74e02c2d6dd545a2274fb8870
David-5-5 <13910767907@139.com>
2025-04-21 22:43:35 +0800

    Append finished implementation

--- cplus/leetcode/202504/algo3070.cpp
+++ cplus/leetcode/202504/algo3070.cpp
@@ -12,6 +12,12 @@ public:
             pres[i+1][j+1] = grid[i][j] + pres[i][j+1] + pres[i+1][j] - pres[i][j];
         }
 
+        int ans = 0;
+        for (int i=0; i<n; ++i) for(int j=0; j<m; ++j) {           
+            if (pres[i+1][j+1] - pres[i+1][0] - pres[0][j+1] <= k)
+                ans += 1;
+        }        
         
+        return ans;
     }
 };

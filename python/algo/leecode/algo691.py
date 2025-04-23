24362d5b1ca634781de5bab5b31e5f3081a6954c
David-5-5 <13910767907@139.com>
2025-04-24 00:23:23 +0800

    Append the code from leetcode

--- python/algo/leecode/algo691.py
+++ python/algo/leecode/algo691.py
@@ -1,18 +1,27 @@
+from functools import cache
+from typing import Counter
+
+
 class Solution:
     def minStickers(self, stickers, target: str) -> int:
-        if len(stickers) == 0: return -1
-        n = len(stickers)
-
-        dp = [0 for _ in range(2 ^ n)]
-        
-
-
-        return dp[2^n -1]        
-            
-        
-        
-        
-
+        m = len(target)
+        @cache
+        def dp(mask: int) -> int:
+            if mask == 0:
+                return 0
+            res = m + 1
+            for sticker in stickers:
+                left = mask
+                cnt = Counter(sticker)
+                for i, c in enumerate(target):
+                    if mask >> i & 1 and cnt[c]:
+                        cnt[c] -= 1
+                        left ^= 1 << i
+                if left < mask:
+                    res = min(res, dp(left) + 1)
+            return res
+        res = dp((1 << m) - 1)
+        return res if res <= m else -1
 
 if __name__ == "__main__":
     sol = Solution()

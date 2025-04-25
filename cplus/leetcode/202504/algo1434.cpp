13a0bea99339432b54e6d79cce1469d57d5d0e6c
David-5-5 <13910767907@139.com>
2025-04-26 01:46:13 +0800

    Append optimized implementation

--- cplus/leetcode/202504/algo1434.cpp
+++ cplus/leetcode/202504/algo1434.cpp
@@ -28,5 +28,38 @@ public:
         return dfs(0);
     }
 
+    int numberWays2(vector<vector<int>>& hats) {
+        // 参考题意，自行解答
+        // 帽子多，人少 变换为人戴帽子
+        const int mod = 1e9 + 7;
+        int n = hats.size(), mx = 0;
+        vector<vector<int>> persons(40, vector<int>());
+        for (int i=0; i<n; i++) {
+            for (auto& v : hats[i]) {
+                persons[v-1].push_back(i);
+                mx = max(v, mx);
+            }
+        }
+        
+        unordered_map<int, int> memo;
+        // 将 帽子戴 n 个人头上，s 第 i 个人是否戴帽子
+        auto dfs = [&](this auto&& dfs, int i, int s) -> int {
+            int j = __builtin_popcount(s);
+            if (j == n) return 1;
+            if (n-j > mx-i) return 0; // 剩下的帽子不够了
+            
+            int mask = i << 10 | s;
+            if (memo.count(mask)) return memo[mask];
 
+            long res = dfs(i+1, s);     // 不选
+            for (auto& v : persons[i]) {
+                if ((s>>v & 1) == 0) {
+                    res += dfs(i+1, s|1<<v);
+                }
+            }
+            return memo[mask] = (res % mod);
+        };
+
+        return dfs(0, 0);
+    }    
 };

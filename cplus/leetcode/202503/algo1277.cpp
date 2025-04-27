a9e5fe0f6c449dbe63da6203588367b901c8f9f4
David-5-5 <13910767907@139.com>
2025-04-27 15:00:01 +0800

    Append implementation using prefxi sum

--- cplus/leetcode/202503/algo1277.cpp
+++ cplus/leetcode/202503/algo1277.cpp
@@ -24,4 +24,22 @@ public:
         }
         return ans;
     }
+
+    int countSquares2(vector<vector<char>>& matrix) {
+        // 2025.04 前缀和 时间复杂度 O(min(m,n)nm)
+        int m = matrix.size(), n = matrix[0].size();
+
+        vector<vector<int>> pres(m+1, vector<int>(n+1));
+        int ans = 0;
+        for (int i=0; i<m; ++i) for (int j=0; j<n; ++j) {
+            pres[i+1][j+1] = matrix[i][j] + pres[i][j+1] + pres[i+1][j] - pres[i][j];
+
+            int k = 0;
+            while (k<=i && k<=j && pres[i+1][j+1] + pres[i-k][j-k] - pres[i+1][j-k] - pres[i-k][j+1] == (k+1)*(k+1))
+                k ++;
+            ans += k;
+        }
+        return ans;
+    }
+
 };

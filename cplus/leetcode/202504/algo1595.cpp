38ad6a05bad28da468d072ba6ef57ffed1e4fe80
David-5-5 <13910767907@139.com>
2025-04-30 23:27:43 +0800

    Append comments

--- cplus/leetcode/202504/algo1595.cpp
+++ cplus/leetcode/202504/algo1595.cpp
@@ -8,6 +8,15 @@ public:
         // 参考题解
         int n = cost.size(), m = cost[0].size(), st = 1 << m;
         
+        // i 表示cost1 的序号 s 表示 cost2 的状态压缩
+        // dfs(i, s) 表示 cost1 的前 i 个元素和 cost2 的状态压缩 s
+        // 其状态转移方程为
+        // dfs(i, s) = min (dfs(i, s^1<<k), dfs(i-1, s), dfs(i-1, s^1<<k)) + cost[i-1][k] k in s
+        // 当第 1 组的 i-1 个点与第 2 组的 k 点相连
+        // 其中 dfs(i, s^1<<k) 表示 s 中的第 k 个点不与其他节点相连
+        //      dfs(i-1, s) 表示 i-1 个节点不与第 2 组中的其他节点相连
+        //      dfs(i-1, s^1<<k) 表示 i-1 个节点不与第 2 组中的其他节点相连
+        //                       且 s 中的第 k 个点不与其他节点相连
         int n = cost.size(), m = cost[0].size(), st = 1 << m;
         
         vector<vector<int>> dp(n+1,vector<int>(st, INT_MAX/2));

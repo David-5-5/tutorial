0d5641075fdceee421cc720ff10d7955a0f77e6a
David-5-5 <13910767907@139.com>
2025-05-01 12:40:49 +0800

    Append new implementation reduce 1-d of dfs

--- cplus/leetcode/202504/algo1815.cpp
+++ cplus/leetcode/202504/algo1815.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Solution {
 public:
     int maxHappyGroups(int batchSize, vector<int>& groups) {
-        // 自行解答 
+        // 自行解答
         int cnt[batchSize], n =  groups.size();
         memset(cnt, 0, sizeof(cnt));
         for (auto& v : groups) cnt[v%batchSize] ++;
@@ -37,5 +37,37 @@ public:
 
     }
 
+    int maxHappyGroups2(int batchSize, vector<int>& groups) {
+        // 自行解答 
+        int cnt[batchSize], n =  groups.size();
+        memset(cnt, 0, sizeof(cnt));
+        for (auto& v : groups) cnt[v%batchSize] ++;
+    
+        unordered_map<long, int> memo;
+        // 减少一维参数
+        auto dfs = [&](this auto&& dfs, int l, long s) -> int {
+            if (s == 0) return 0;
 
+            long mask = (long)l << 56 | s;
+            if (memo.count(mask)) return memo[mask];
+
+            int res = 0;
+            for (int k=0; k<batchSize; ++k) {
+                int c = (s >> (5*k)) & 31;
+                if (c) {
+                    // cout << (s&(tot1^(c-1)<<(5*k)))) << endl;
+                    res = max(res, dfs((l+k)%batchSize, s-((long)1<<(5*k))));
+                }
+            }
+            return memo[mask] = res + (l==0);
+        };
+
+        long s = 0;
+        for (int i=0; i<batchSize; ++i) {
+            s |= (long) cnt[i] << (5*i);    // 强制转化为 long 
+        }
+        
+        return dfs(0, s);  
+
+    }    
 };

4751af33b3afd6f5be3585ada1e2b4022896476b
David-5-5 <13910767907@139.com>
2025-05-04 02:55:44 +0800

    Append comments

--- cplus/leetcode/202505/algo3347.cpp
+++ cplus/leetcode/202505/algo3347.cpp
@@ -5,19 +5,22 @@ using namespace std;
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
+        // 参考题解，技巧很强
+        // 差分可以想到，但是结合 numOperations 就没有思路了
         unordered_map<int, int> cnt;
-        map<int, int> diff;
+        map<int, int> diff;     // 数据域大的情况下使用 map 代替 vector
         
         for (auto& x : nums) {
             cnt[x] ++;
             diff[x-k] ++;
-            diff[x];        // !!确保能遍历到 v!!
+            diff[x];        // !!确保能遍历到 v!!，点睛之笔
             diff[x+k+1] --;
         }
 
         int ans = 0, sum = 0;
         for (auto& [x, c]:diff) {
             sum += c;
+            // 结合 numOperations，计算 nums 中 x 的次数
             ans = max(ans, min(sum, cnt[x] + numOperations));
         }
 

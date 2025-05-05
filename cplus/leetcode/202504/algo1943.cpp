93f6939b976779c87e65d2def5a77d66b92a3843
David-5-5 <13910767907@139.com>
2025-05-05 14:23:23 +0800

    Append optimized implementation use unordered_map instead of map

--- cplus/leetcode/202504/algo1943.cpp
+++ cplus/leetcode/202504/algo1943.cpp
@@ -25,4 +25,28 @@ public:
         return ans;    
     }
 
+    vector<vector<long long>> splitPainting2(vector<vector<int>>& segments) {
+        // 使用 unordered_map + vector + sort 性能优于 map
+        unordered_map<int, long long> color;
+
+        for (auto& seg : segments) {
+            int l = seg[0], r = seg[1], c = seg[2];
+            color.count(l)? color[l]+=c: color[l]=c;
+            color.count(r)? color[r]-=c: color[r]=-c;
+        }
+        
+        vector<pair<int, long long>> v_col;
+        for (auto& [i, c]: color) v_col.push_back({i, c});
+        sort(v_col.begin(), v_col.end());
+
+        vector<vector<long long>> ans;
+        long prev = 0, col = 0, start = 0;
+        for (auto& [i, c]: v_col) {
+            if (start && col) ans.push_back({start, i, col});
+            start = i;
+            col += c;
+        }
+
+        return ans;    
+    }
 };

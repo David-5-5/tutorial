010639fb9943d0ab2978b6714e1e101cbc35da66
David-5-5 <13910767907@139.com>
2025-05-05 14:22:27 +0800

    Append comments

--- cplus/leetcode/202505/algo1674.cpp
+++ cplus/leetcode/202505/algo1674.cpp
@@ -12,14 +12,12 @@ public:
         // 更改 1 次区间 l1 = 1 + min(a,b), r1= limit+max(a,b)
         // 更改 2 次区间 l2 = 2, r2=  2 * limit
         // 区间的包含关系如下：
-        // l2 <= l1  <= l0 <= r0 <= r1 <= r2        
+        // l2 <= l1  <= l0 <= r0 <= r1 <= r2
         vector<int> diff(limit*2 + 2, 0);
 
         int n = nums.size(), m = n/2;
 
         for (int i=0; i<m; ++i) {
-
-
             // 默认所有区间使用差分修改 2 次
             diff[2] += 2;
             diff[2*limit + 1] -2;

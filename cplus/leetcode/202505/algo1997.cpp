00385a8ce6812f15bc59ab6c996ee00897959e8c
David-5-5 <13910767907@139.com>
2025-05-07 23:15:53 +0800

    Append implementation using dp and optimization with prefix sum

--- cplus/leetcode/202505/algo1997.cpp
+++ cplus/leetcode/202505/algo1997.cpp
@@ -19,4 +19,25 @@ public:
         }
         return ans % mod;
     }
+
+    int firstDayBeenInAllRooms2(vector<int>& nextVisit) {
+        // 自行解答，前缀和
+        const int mod = 1e9 + 7;
+        int ans = 0, cur=0, n = nextVisit.size(), left = n;
+        vector<int> cnt(n), pres(n+1);
+        for (int i=0; i<n; ++i) {
+            if (i == n-1) break;    // 到达最后一个房间
+            cnt[i] += 1;            // 达到新房间，数量+1
+            pres[i+1] = ((long)pres[i] + cnt[i]) % mod; // 计算前缀和
+            if (cnt[i] % 2) {
+                // nextVisit[i] 回到 i 需要的天数
+                cnt[i] += ((long)pres[i+1] - pres[nextVisit[i]] + mod) % mod; 
+                pres[i+1] = ((long)pres[i] + cnt[i]) % mod; // 更新前缀和
+            } 
+            
+        }
+        return pres[n-1] % mod;
+    } 
+
+ 
 };

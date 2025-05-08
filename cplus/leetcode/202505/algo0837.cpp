3f4eb678d045affac4b23b95dca4c2b8711394c6
David-5-5 <13910767907@139.com>
2025-05-09 03:17:52 +0800

    Add algo without finish

---
+++ cplus/leetcode/202505/algo0837.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 动态规划 - 11.1 前缀和优化 DP
+class Solution {
+public:
+    double new21Game(int n, int k, int maxPts) {
+        int begin = 0;
+        double ans;
+
+        
+        auto dfs = [&](int i, double p) -> double {
+
+        };
+
+        return dfs(0, 1);
+    }
+};

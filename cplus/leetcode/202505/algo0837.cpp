a0de651f79c7ca12f5d6592850131b5c038cc7c0
David-5-5 <13910767907@139.com>
2025-05-09 11:13:50 +0800

    Append implementation using dp with optimization using prefix sum

--- cplus/leetcode/202505/algo0837.cpp
+++ cplus/leetcode/202505/algo0837.cpp
@@ -20,5 +20,19 @@ public:
         return dp[0];  
     }
 
+    double new21Game(int n, int k, int maxPts) {
+        // 参考题意解答，前缀和优化
+        vector<double> dp(k+maxPts);   // k-1 + maxPts 为取的最大值
+        
+        double sum = 0;     // sum(dp[(k-1)+1],..., dp[(k-1) + maxPts] 初始
+        for (int i = min(n,k+maxPts-1); i>=k; --i) 
+            sum += 1.0, dp[i] = 1.0;
 
+        for (int i=k-1; i>=0; --i) {            
+            dp[i] = sum / maxPts;
+            sum += dp[i] - dp[i+maxPts];    // 前缀和优化
+        }
+
+        return dp[0];        
+    }    
 };

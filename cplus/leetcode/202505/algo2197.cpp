1988961f342c57e822f6d96a5d28666c20c80c6a
David-5-5 <13910767907@139.com>
2025-05-11 00:10:25 +0800

    Add algo using stack

---
+++ cplus/leetcode/202505/algo2197.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 常用数据结构 - 栈 3.3 邻项消除
+class Solution {
+public:
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        // 自行解答 WA 一次
+        int n = nums.size();
+        vector<int> ans;
+
+        auto push = [&] (int v) {
+            while (ans.size() && gcd(ans.back(), v)>1) {
+                v = lcm(v, ans.back());
+                ans.pop_back();
+            }
+            ans.push_back(v);
+        };
+
+        int val = nums[0], i=1;
+        while (i < n) {
+            if (gcd(val, nums[i])>1) {
+                val = lcm(val, nums[i++]);
+            } else {
+                push(val);
+                val = nums[i++];
+            }
+        }
+        push(val);
+        return ans;
+            
+    }
+
+
+};

56bc4023193f8d8f6d48b70dc5554c116262c9bc
David-5-5 <13910767907@139.com>
2025-05-13 02:11:17 +0800

    Append implementation with space optimazation

--- cplus/leetcode/202505/algo3494.cpp
+++ cplus/leetcode/202505/algo3494.cpp
@@ -21,4 +21,24 @@ public:
         return dp[m][n];
 
     }
+
+
+    long long minTime(vector<int>& skill, vector<int>& mana) {
+        // 参考题解, 空间复杂度降低一维
+        int n = skill.size(), m = mana.size();
+
+        // 先不减少纬度
+        vector<long long> dp(n+1);
+        for (int i=1; i<=m; ++i) {
+            for (int j=1; j<=n; ++j) {
+                dp[j] = max(dp[j], dp[j-1]) + skill[j-1] * mana[i-1];
+            }
+            for (int j=n; j; --j) {
+                dp[j-1] = dp[j] - skill[j-1] * mana[i-1];
+            }
+        }
+        return dp[n];
+
+    }
+    
 };

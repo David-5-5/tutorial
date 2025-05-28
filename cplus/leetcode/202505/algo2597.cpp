8f20abcc0a52a76d71627c9b89b611cd601f8dd8
David-5-5 <13910767907@139.com>
2025-05-28 10:59:37 +0800

    Add algo using recursive dp

---
+++ cplus/leetcode/202505/algo2597.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 动态规划 - 一维 DP
+class Solution {
+public:
+    int beautifulSubsets(vector<int>& nums, int k) {
+        // 复习，动态规划 + 乘法原理 + 加法原理
+
+        // 按 v%k 分组，不同组之间的差肯定不等于 k
+        // 因此不同组之间子集的数目使用乘法原理，同一组内的子集的数目使用加法原理
+        unordered_map<int, map<int, int>> group;
+        for (auto& v: nums) {
+            group[v%k][v] ++;
+        }
+
+        int ans = 1;
+        for (auto& [_, cnt] : group) {
+            int m = cnt.size();
+            vector<pair<int, int>> g;
+            transform(cnt.begin(), cnt.end(), back_inserter(g), [](const auto& val) {
+                return pair<int, int>(val.first, val.second);
+            });
+            // vector<int> memo(m+1, -1);                  // using memo 12ms, without memo 3ms
+            auto dfs = [&](this auto&& dfs, int i) {
+                if (i == 0) return 1;
+                // auto & res = memo[i];
+                // if (res != -1) return res;
+                int res = dfs(i-1);
+                res += ((int)pow(2, g[i-1].second)-1) * ((i == 1 || g[i-1].first-g[i-2].first!=k) ? dfs(i-1) : dfs(i-2));
+                return res;
+            };
+            
+            ans *= dfs(m);
+        }
+        return ans-1;
+    }
+};

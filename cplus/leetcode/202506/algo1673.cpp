57a730252009fb500b2252d7add0842f3836deda
David-5-5 <13910767907@139.com>
2025-05-31 13:19:23 +0800

    Add algo using two stack

---
+++ cplus/leetcode/202506/algo1673.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 常用数据结构 - 3.7 单调栈 - 最小字典序
+class Solution {
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k) {
+        // 自行解答 - 性能不好
+        vector<int> ans; // 遍历 nums 过程中保持单调递增
+        // 记录从 ans 出栈后ans的高度，后续需要插入到ans中
+        stack<pair<int,int>> st;
+
+        for (auto v : nums) {
+            while (ans.size() && ans.back() > v) {
+                int val = ans.back();
+                ans.pop_back();
+                st.push({ans.size(), val});
+            }
+            ans.push_back(v);
+        }
+
+        int sup = k - ans.size();
+        for (int i=0; i<sup; ++i) {
+            auto [inx, val] = st.top();
+            st.pop();
+            ans.insert(ans.begin()+inx, val);
+        }
+
+        ans.resize(k);
+
+        return ans;
+    }
+};

08d45f6b33e35c8e4ae923046da5bf79a81cf53d
David-5-5 <13910767907@139.com>
2025-06-02 03:15:19 +0800

    Append implementation using bitset replace of unordered_set

--- cplus/leetcode/202506/algo3543.cpp
+++ cplus/leetcode/202506/algo3543.cpp
@@ -32,6 +32,7 @@ public:
     }
 
     int maxWeight2(int n, vector<vector<int>>& edges, int k, int t) {
+        // 拓扑序 DP 针对 DAG
         if (n <= k) return -1;
         vector<vector<pair<int, int>>> g(n);
         vector<int> deg(n);
@@ -69,5 +70,44 @@ public:
         return ans;
     }
 
-    
+    int maxWeight3(int n, vector<vector<int>>& edges, int k, int t) {
+        // 拓扑序 DP 针对 DAG
+        if (n <= k) return -1;
+        vector<vector<pair<int, int>>> g(n);
+        vector<int> deg(n);
+        for (auto e : edges) {
+            g[e[0]].emplace_back(e[1], e[2]);   // 有向图
+            deg[e[1]] ++;
+        }
+
+        // 使用 bitset 代替 unordered_set
+        vector f(n, vector<bitset<600>>(k+1));
+        queue<int> q;
+        for (int i=0; i<n; ++i) if (deg[i] == 0) q.push(i);
+
+        int ans = -1;
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            f[u][0].set(0);     // x
+            for (int s = t-1; s>=0; --s) {
+                if (f[u][k].test(s)) {
+                    ans = max(ans, s);
+                    break;
+                }
+            }
+
+            for (auto& [v, w]: g[u]) {
+                for (int i=0; i<k && !f[u][i].none(); ++i) 
+                    f[v][i+1] |= f[u][i] << w;
+
+                if (--deg[v] == 0) {
+                    q.push(v);
+                }
+            }
+        }
+        return ans;
+    }
+
+
 };

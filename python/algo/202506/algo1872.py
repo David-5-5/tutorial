333fddda423f8850b71e12027b44b62f92c4adeb
David-5-5 <13910767907@139.com>
2025-06-04 00:34:06 +0800

    Append optimized implementation not use max function

--- python/algo/202506/algo1872.py
+++ python/algo/202506/algo1872.py
@@ -28,12 +28,20 @@ class Solution:
         pres = list(accumulate(stones, initial=0))
 
         f = [-inf] * (n)
-        f[n-1] = pres[n]
+        f[n-1] = pres[n]        # 初始化
         for i in range(n-2,0,-1):            
+            # 二次循环
             # for j in range(i, n):     # 迭代（递推）
             #     f[i] = max(f[i], pres[j+1] - f[j+1])
-            # 写法一
+            
+            # 优化后 写法一
             f[i] = max(f[i+1], pres[i+1] - f[i+1])
             
-            
+            # 写法二，不用 max 函数，性能更优
+            # val = pres[i+1] - f[i+1]
+            # if f[i+1] > val:
+            #     f[i] = f[i+1]
+            # else: f[i] = val
+                        
+
         return f[1]

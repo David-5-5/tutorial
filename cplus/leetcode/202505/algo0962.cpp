c5ac5331a02df5afae211d62e59b2262bda9e98f
David-5-5 <13910767907@139.com>
2025-06-08 13:34:44 +0800

    Append implementation using stack and binary search

--- cplus/leetcode/202505/algo0962.cpp
+++ cplus/leetcode/202505/algo0962.cpp
@@ -25,4 +25,38 @@ public:
         return ans;
         
     }
+
+    int maxWidthRamp2(vector<int>& nums) {
+        // 递减队列，二分查找
+        vector<pair<int, int>> dec;
+
+        auto binary_search_le = [&](int target) {
+            int left = 0, right = dec.size() - 1;
+            int result = -1;  // 初始化为未找到
+            
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                
+                if (dec[mid].first <= target) {
+                    result = mid;        // 记录当前可能的答案
+                    right = mid - 1;     // 继续向左查找更小的索引
+                } else {
+                    left = mid + 1;      // 向右查找
+                }
+            }
+            
+            return result;
+        };
+        dec.push_back({nums[0], 0});
+        
+        int ans = 0;
+        for (int i=1; i<nums.size(); ++i) {
+            int j = binary_search_le(nums[i]);
+            if (j != -1)  ans = max(ans, i- dec[j].second);
+            if (nums[i] < dec.back().first)  dec.push_back({nums[i], i});
+        }
+
+        return ans;
+
+    }
 };

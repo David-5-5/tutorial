8dc3d6598b8d6e2758abaa406bbd666354da54a1
David-5-5 <13910767907@139.com>
2025-06-11 10:37:00 +0800

    Append implementation using customized compare of priority queue

--- cplus/leetcode/202506/algo1792.cpp
+++ cplus/leetcode/202506/algo1792.cpp
@@ -30,4 +30,34 @@ public:
         return sum / classes.size(); 
     }
 
+
+    double maxAverageRatio2(vector<vector<int>>& classes, int extraStudents) {
+        // 自行解答 - 自定义 cmp 函数
+        auto cmp = [](pair<int,int> a, pair<int, int> b) -> bool {
+            return 1.0 * (a.second+1)/(a.first+1) - 1.0*a.second/a.first < 
+                1.0 * (b.second+1)/(b.first+1) - 1.0*b.second/b.first;
+        };
+        priority_queue<pair<int, int>, vector<pair<int,int>>, decltype(cmp)> pq(cmp); //  默认是大顶堆
+        
+        double sum = 0.0;
+
+        for (auto c: classes) {
+            int p = c[0], t = c[1];
+            if (p == t) sum += 1;
+            else {
+                sum += 1.0 * p / t;
+                pq.emplace(t, p);
+            }
+        }
+
+        if (pq.size()) {
+            for (int _=0; _<extraStudents; ++_) {
+                auto [t, p] = pq.top(); pq.pop();
+                sum += 1.0*(p+1)/(t+1) - 1.0*p/t;
+                pq.emplace(t+1, p+1);
+            }
+        }
+
+        return sum / classes.size(); 
+    }
 };

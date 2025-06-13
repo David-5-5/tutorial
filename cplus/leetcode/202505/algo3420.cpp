f90c7bcadf28b2c9043288e2635af726abeb032f
David-5-5 <13910767907@139.com>
2025-06-13 19:49:01 +0800

    Add algo with blank method

---
+++ cplus/leetcode/202505/algo3420.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 常用数据结构 - 3.7 单调栈 - 进阶
+class Solution {
+public:
+    long long countNonDecreasingSubarrays(vector<int>& nums, int k) {
+        
+    }
+};

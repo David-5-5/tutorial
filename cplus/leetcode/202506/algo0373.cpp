fdae66c0a90dbbed52d17517921b306538fb15be
David-5-5 <13910767907@139.com>
2025-06-19 19:14:48 +0800

    Add algo using priority queue

---
+++ cplus/leetcode/202506/algo0373.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 常用数据结构 - 5.4 堆 - 第 K 小/大
+class Solution {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        // 自行解答
+        vector<vector<int>> ans;
+        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<>> pq;
+        set<pair<int, int>> vis;
+
+        pq.emplace(nums1[0]+nums2[0], 0, 0);
+        vis.insert({0,0});
+        while (ans.size() < k) {
+            auto [_, u, v] = pq.top(); pq.pop();
+            ans.push_back({nums1[u], nums2[v]});
+
+            if (u+1<nums1.size() && !vis.count({u+1,v})) {
+                pq.emplace(nums1[u+1]+nums2[v], u+1, v);
+                vis.insert({u+1, v});
+            }
+            if (v+1<nums2.size() && !vis.count({u,v+1})) {
+                pq.emplace(nums1[u]+nums2[v+1], u, v+1);
+                vis.insert({u, v+1});
+            }
+        }
+        return ans; 
+    }
+
+};

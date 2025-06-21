45c9a89037480cf09c81a5cf017bb05567d7df3e
David-5-5 <13910767907@139.com>
2025-06-21 13:18:25 +0800

    Add algo using priority queue

--- cplus/leetcode/202506/algo1792.cpp
+++ cplus/leetcode/202506/algo1792.cpp
@@ -60,4 +60,38 @@ public:
 
         return sum / classes.size(); 
     }
+
+
+    double maxAverageRatio3(vector<vector<int>>& classes, int extraStudents) {
+        // 自行解答 - 自定义优先队列的结构体 
+        struct Ratio {
+            int total, pass;
+            bool operator < (const Ratio& oth) const {
+                return 1.0 * (pass+1)/(total+1) - 1.0*pass/total <
+                1.0 * (oth.pass+1)/(oth.total+1) - 1.0*oth.pass/oth.total;
+            };
+        };
+        priority_queue<Ratio> pq; //  默认是大顶堆
+        
+        double sum = 0.0;
+
+        for (auto c: classes) {
+            int p = c[0], t = c[1];
+            if (p == t) sum += 1;
+            else {
+                sum += 1.0 * p / t;
+                pq.emplace(t, p);
+            }
+        }
+
+        if (pq.size()) {
+            for (int _=0; _<extraStudents; ++_) {
+                auto [t, p] = pq.top(); pq.pop();
+                sum += 1.0*(p+1)/(t+1) - 1.0*p/t;
+                pq.emplace(t+1, p+1);
+            }
+        }
+
+        return sum / classes.size(); 
+    }
 };

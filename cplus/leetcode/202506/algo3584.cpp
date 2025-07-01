ea9eb3819647acf891f774685faeddf30c2ca003
David-5-5 <13910767907@139.com>
2025-07-01 10:42:04 +0800

    Append implementation refer to solution

--- cplus/leetcode/202506/algo3584.cpp
+++ cplus/leetcode/202506/algo3584.cpp
@@ -5,6 +5,7 @@ using namespace std;
 class Solution {
 public:
     long long maximumProduct(vector<int>& nums, int m) {
+        // 自行解答
         long long ans = LLONG_MIN;
         if (m == 1) {
             for (auto& v:nums) {
@@ -19,6 +20,21 @@ public:
             ans = max(ans, (long long) nums[r] * mn);
             l ++;
             mx = max(mx, nums[l]); mn = min(mn, nums[l]);
+        }
+        return ans;
+    }
+
+
+    long long maximumProduct2(vector<int>& nums, int m) {
+        // 参考题解 不需要特判 m = 1 时 最大值 或 最小值相乘 一定是结果
+        long long ans = LLONG_MIN;
+        int mx=INT_MIN, mn = INT_MAX;
+        for (int r=m-1; r<nums.size(); ++r) {
+            mx = max(mx, nums[r-m+1]), mn = min(mn, nums[r-m+1]);
+            long long x = nums[r];
+            // 看上去好看，但是性能差，特别是数据量较多的情况，出现过超时
+            // ans = max({ans, x * mx, x * mn});
+            ans = max(ans, x * mx); ans = max(ans, x * mn);
         }
         return ans;
     }

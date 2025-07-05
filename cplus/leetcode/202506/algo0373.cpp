bd7207abb9afaaee03b5afb4dc51114881b38fc3
David-5-5 <13910767907@139.com>
2025-07-05 23:43:45 +0800

    Append optimized implementation

--- cplus/leetcode/202506/algo0373.cpp
+++ cplus/leetcode/202506/algo0373.cpp
@@ -28,4 +28,21 @@ public:
         return ans; 
     }
 
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        // 参考题解 - 避免 vis 数组 - 相关技巧关注注释语句
+        vector<vector<int>> ans;
+        priority_queue<tuple<int,int,int>, vector<tuple<int,int,int>>, greater<>> pq;
+        int n = nums1.size(), m = nums2.size();
+
+        for (int i=0; i<min(k, n); i++)
+            pq.emplace(nums1[i]+nums2[0], i, 0);    // 将 nums1 所有的元素 + nums2[0]
+
+        while (ans.size() < k) {
+            auto [_, u, v] = pq.top(); pq.pop();
+            ans.push_back({nums1[u], nums2[v]});
+
+            if (v+1<m) pq.emplace(nums1[u]+nums2[v+1], u, v+1); // 增加 nums2的下一个元素
+        }
+        return ans; 
+    }
 };

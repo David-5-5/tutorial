7ce68875328d7a7d0c28e7ca6313603f1433ad8e
David-5-5 <13910767907@139.com>
2025-07-06 13:29:44 +0800

    Add algo using union find

---
+++ cplus/leetcode/202507/algo3532.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 常用数据结构 - 7.1 并查集 - 基础
+class UnionFind {                   // 并查集模板
+    vector<int> fa;
+public:
+    UnionFind(int n): fa(n) {
+        ranges::iota(fa, 0);
+    }
+
+    int find(int x) {
+        if (fa[x] != x) {
+            fa[x] = find(fa[x]);
+        }
+        return fa[x];
+    }
+
+    void merge(int from, int to) {
+        int x = find(from), y = find(to);
+        if (x != y) {
+            fa[x] = y;
+        }
+    }
+
+    bool is_same(int x, int y) {
+        return find(x) == find(y);
+    }
+};
+
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        auto uf = UnionFind(n);
+
+        for (int i=1; i<n; ++i) if (nums[i]-nums[i-1]<=maxDiff) uf.merge(i-1, i);
+
+        vector<bool> ans;
+        for (auto& q: queries) {
+            int x = q[0], y = q[1]; ans.push_back(uf.is_same(x, y));
+        }
+        return ans;
+    }
+};

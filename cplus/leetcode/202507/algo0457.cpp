f5ef961c8381365ef88a7b3e9367f7ec53ae094c
David-5-5 <13910767907@139.com>
2025-07-08 00:34:47 +0800

    Add algo using slow and fast pointer

---
+++ cplus/leetcode/202507/algo0457.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 链表 - 1.6 快慢指针
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    bool circularArrayLoop(vector<int>& nums) {
+        // 自行解答，快慢指针
+        int n = nums.size();
+        auto next = [&](int cur) {
+            return ((cur + nums[cur]) % n + n) % n; // 保证返回值在 [0,n) 中
+        };
+
+        for (int i = 0; i < n; i++) {
+            if (!nums[i]) {
+                continue;
+            }
+            int slow = i, fast = next(i);
+            // 判断非零且方向相同
+            while (nums[slow] * nums[fast] > 0 && nums[slow] * nums[next(fast)] > 0) {
+                if (slow == fast) {
+                    if (slow != next(slow)) {
+                        return true;
+                    } else {
+                        break;
+                    }
+                }
+                slow = next(slow);
+                fast = next(next(fast));
+            }
+            int add = i;
+            while (nums[add] * nums[next(add)] > 0) {
+                int tmp = add;
+                add = next(add);
+                nums[tmp] = 0;
+            }
+        }
+        return false;
+    }
+};

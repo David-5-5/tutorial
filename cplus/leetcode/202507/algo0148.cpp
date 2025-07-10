27dd7df504f29a3d17c7e52dab5d06defa9d0a4a
David-5-5 <13910767907@139.com>
2025-07-11 04:15:17 +0800

    Add algo using linked list

---
+++ cplus/leetcode/202507/algo0148.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 链表 - 1.9 分治
+/**
+ * Definition for singly-linked list.
+ */
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        // 最简单方法
+        vector<int> arr;
+        auto cur = head;
+        while (cur) {
+            arr.push_back(cur->val); cur = cur->next;
+        }
+        sort(arr.begin(), arr.end());
+        auto cur = head;
+        for (auto v: arr) {cur->val = v; cur= cur->next;}
+        return head;
+    }
+};

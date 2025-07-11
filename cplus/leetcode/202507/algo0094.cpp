95964f589ffc827dd333abda53d2ba5d6d80526e
David-5-5 <13910767907@139.com>
2025-07-12 02:14:40 +0800

    Add algo using binary tree

---
+++ cplus/leetcode/202507/algo0094.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// 二叉树 - 2.1 遍历二叉树 
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        // 自行解答
+        vector<int> ans;
+        auto dfs = [&](this auto&& dfs, TreeNode* node) -> void {
+            if (node == nullptr) return;
+            dfs(node->left); ans.push_back(node->val); dfs(node->right);
+        };
+
+        dfs(root);
+        return ans;
+    }
+};

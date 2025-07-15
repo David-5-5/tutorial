189fb784b89572abfa542444b7d74e4c430f10f5
David-5-5 <13910767907@139.com>
2025-07-15 10:17:58 +0800

    Add algo using trie

--- cplus/leetcode/202507/algo3093.cpp
+++ cplus/leetcode/202507/algo3093.cpp
@@ -61,5 +61,38 @@ public:
         return ans;
     }
 
+    vector<int> stringIndices2(vector<string>& wordsContainer, vector<string>& wordsQuery) {
 
+        for (int i=0; i<wordsContainer.size(); ++i) {
+            auto node = root;
+            if (wordsContainer[i].length() < wordsContainer[node->result].length()){
+                node->result = i;
+            }
+            
+            for (int j=wordsContainer[i].length()-1; j>=0; --j) {
+                auto & c = wordsContainer[i][j];
+                if (node->son[c-'a'] == nullptr) {
+                    node->son[c-'a'] = new Node(); node->son[c-'a']->result = i;
+                }
+                node = node->son[c-'a'];
+                if (wordsContainer[i].length() < wordsContainer[node->result].length()){
+                    node->result = i;
+                }
+            }
+        }
+
+        vector<int> ans;
+        for (auto& q: wordsQuery) {
+            auto node = root;
+            
+            for (int i=q.length()-1; i>=0; --i) {
+                auto& c= q[i];
+                if (node->son[c-'a'] == nullptr) break;
+                node = node->son[c-'a'];
+            }
+            ans.push_back(node->result);
+        }
+        dfs_free(root);
+        return ans;
+    }
 };

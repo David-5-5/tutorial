e5cecb7eb901ae9d300c9b355fb9f7f282cc43f3
David-5-5 <13910767907@139.com>
2025-07-17 23:56:49 +0800

    Add aglo using dfs of binary tree

---
+++ cplus/leetcode/202507/algo1339.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// 二叉树 - 2.3 自底向上 DFS
+class Solution {
+public:
+    int maxProduct(TreeNode* root) {
+        // 自行解答
+        const int mod = 1e9 + 7;
+
+        int sum = 0;
+        auto dfs = [&](this auto&& dfs, TreeNode* node) -> void {
+            sum += node->val;
+            if (node->left) dfs(node->left);
+            if (node->right) dfs(node->right);
+        };
+        dfs(root);
+
+        long long ans = 0;
+        auto cal = [&](this auto&& cal, TreeNode* node) -> int {
+            int sub_sum = node->val;
+            if (node->left)  sub_sum += cal(node->left);
+            if (node->right) sub_sum += cal(node->right);
+            ans = max(ans, (long long)sub_sum * (sum-sub_sum));
+            return sub_sum;
+        };
+
+        cal(root);
+        return ans % mod;
+    }
+
+ 
+};

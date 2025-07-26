2f668a55f81e81c5c57f1aa481cfada902877f52
David-5-5 <13910767907@139.com>
2025-07-26 23:49:51 +0800

    Append implementation of refert to solution

--- cplus/leetcode/202507/algo1339.cpp
+++ cplus/leetcode/202507/algo1339.cpp
@@ -38,5 +38,28 @@ public:
         return ans % mod;
     }
 
- 
+    int maxProduct2(TreeNode* root) {
+        // 参考题解
+        const int mod = 1e9 + 7;
+
+        int sum = 0; 
+        auto dfs = [&](this auto&& dfs, TreeNode* node) -> void {
+            sum += node->val;
+            if (node->left) dfs(node->left);
+            if (node->right) dfs(node->right);
+        };
+        dfs(root);
+
+        int best = sum; // best == sum/2
+        auto dfs2 = [&](this auto&& dfs2, TreeNode* node) -> int {
+            int res = node->val;
+            if (node->left)  res += dfs2(node->left);
+            if (node->right) res += dfs2(node->right);
+            if (abs(res * 2 - sum) < abs(best*2-sum)) best = res;
+            return res;
+        };
+
+        dfs2(root);
+        return ((long long)best * (sum-best)) % mod;
+    }    
 };

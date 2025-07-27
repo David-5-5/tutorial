4a37503553b0f36fdf1b35747160e8132d45609a
David-5-5 <13910767907@139.com>
2025-07-27 19:52:51 +0800

    Append new implementation with refer to solution

--- cplus/leetcode/202507/algo0865.cpp
+++ cplus/leetcode/202507/algo0865.cpp
@@ -39,5 +39,25 @@ public:
         return ans;
     }
 
-    
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        // 参考题解 这个递归写法很漂亮
+        auto dfs = [&](this auto&& dfs, TreeNode* node) -> pair<TreeNode*, int> {
+            if (!node) return {node, 0};
+            
+            auto left  = dfs(node->left);
+            auto right = dfs(node->right);
+            
+            if (left.second == right.second)
+                return {node, left.second+1};
+            else if (left.second > right.second)
+                return {left.first, left.second+1};
+            else return {right.first, right.second+1};
+
+        };
+
+        return dfs(root).first;
+        
+    }
+
+
 };

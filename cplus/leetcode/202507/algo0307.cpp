70cbbd9136686791ed5c3c0fe42328566eedaa6d
David-5-5 <13910767907@139.com>
2025-08-01 10:09:20 +0800

    Add algo using fenwick tree

---
+++ cplus/leetcode/202507/algo0307.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 常用数据结构 - 8.1 树状数组和线段树 - 树状数组
+template<typename T>
+class FenwickTree {
+    vector<T> tree;
+
+public:
+    // 使用下标 1 到 n
+    FenwickTree(int n) : tree(n + 1) {}
+
+    // a[i] 增加 val, 1 <= i <= n
+    void update(int i, T val) {
+        for (; i < tree.size(); i += i & -i) {
+            tree[i] += val;
+        }
+    }
+
+    // 求前缀和 a[1] + ... + a[i] 1 <= i <= n
+    T pre(int i) const {
+        T res = 0;
+        for (; i > 0; i &= i - 1) { // i-= i&-i
+            res += tree[i];
+        }
+        return res;
+    }
+
+    // 求区间和 a[l] + ... + a[r], 1 <= l <= r <= n
+    T query(int l, int r) const {
+        if (r < l) {
+            return 0;
+        }
+        return pre(r) - pre(l - 1);
+    }
+};
+
+class NumArray {
+private:
+    FenwickTree<int> ft;
+    vector<int> nums;       // 保留 nums，记录 delta
+public:
+    NumArray(vector<int>& nums): ft(nums.size()), nums(nums.size()) {
+        for (int i=0; i<nums.size(); i++) update(i, nums[i]);
+    }
+    
+    void update(int index, int val) {
+        int delta = val - nums[index]; nums[index] = val;
+        ft.update(index+1, delta);
+    }
+    
+    int sumRange(int left, int right) {
+        return ft.query(left+1, right+1);
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * obj->update(index,val);
+ * int param_2 = obj->sumRange(left,right);
+ */

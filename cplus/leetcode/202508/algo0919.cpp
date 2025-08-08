954ef2b8d5c09652c17d79988e9d365c9c60e17b
David-5-5 <13910767907@139.com>
2025-08-09 00:37:00 +0800

    Add algo using bfs of binary tree

---
+++ cplus/leetcode/202508/algo0919.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// 二叉树 - 2.13 二叉树 BFS
+class CBTInserter {
+private:
+    vector<TreeNode*> tree;
+public:
+    CBTInserter(TreeNode* root) {
+        vector<TreeNode*> cur; cur.push_back(root);
+        while (!cur.empty()) {
+            vector<TreeNode*> nxt;
+            for (auto& node:cur) {
+                tree.push_back(node);
+                if (node->left) nxt.push_back(node->left);
+                if (node->right) nxt.push_back(node->right);
+            }
+            cur = move(nxt);
+        }
+
+    }
+    
+    int insert(int val) {
+        int fa = (tree.size()-1)/2;
+        cout << fa << endl;
+        tree.push_back(new TreeNode(val));
+        if (!tree[fa]->left) tree[fa]->left = tree.back();
+        else tree[fa]->right = tree.back();
+        return tree[fa]->val;
+    }
+    
+    TreeNode* get_root() {
+        return tree[0];
+    }
+};

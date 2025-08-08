75b252b64f571da54858df5f0a6e707b9218c0f6
David-5-5 <13910767907@139.com>
2025-08-09 00:37:10 +0800

    Add algo using bfs of binary tree

---
+++ cplus/leetcode/202508/algo1302.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// 二叉树 - 2.13 二叉树 BFS
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        // 自行解答
+        int sum = 0;
+
+        vector<TreeNode*> cur; cur.push_back(root);
+        while (!cur.empty()) {
+            sum = 0;
+            vector<TreeNode*> nxt;
+            for (auto& node:cur) {
+                sum += node->val;
+                if (node->left) nxt.push_back(node->left);
+                if (node->right) nxt.push_back(node->right);
+            }
+            cur = move(nxt);
+        }        
+        return sum;    
+    }
+};

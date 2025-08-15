dbe9f2a46bd1ac3a577450841e4c4462c7ad9b85
David-5-5 <13910767907@139.com>
2025-08-16 00:35:32 +0800

    Add algo using dfs of tree

---
+++ cplus/leetcode/202508/algo2368.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 一般树
+class Solution {
+public:
+    int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
+        // 自行解答
+        vector<vector<int>> g(n, vector<int>());
+        set<int> rest(restricted.begin(), restricted.end());
+
+        for (auto & e : edges) {
+            auto & u = e[0], v = e[1];
+            g[u].push_back(v); g[v].push_back(u);
+        }
+        vector<bool> vis(n);
+        int ans = 0;
+        auto dfs = [&](this auto&& dfs, int u) -> void {
+            ans ++;
+            for (auto& v: g[u] ) {
+                if (vis[v] || rest.count(v)) continue;
+                vis[v] = true; dfs(v);
+            }
+        };
+        vis[0]=true; dfs(0); return ans;       
+    }
+};

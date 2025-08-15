03c125d13be1009a8cb551c47200d46d83358f04
David-5-5 <13910767907@139.com>
2025-08-16 01:03:10 +0800

    Add algo using dfs of tree and judge the direction of edge

---
+++ cplus/leetcode/202508/algo1466.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 一般树
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        // 自行解答
+        vector<vector<pair<int, int>>> g(n, vector<pair<int, int>>());
+
+        for (int i=0; i<n-1; ++i) {
+            auto e = connections[i];
+            auto & u = e[0], v = e[1];
+            g[u].emplace_back(v, i); g[v].emplace_back(u, i);
+        }
+        int ans = 0;
+        auto dfs = [&](this auto&& dfs, int u, int fa) -> void {
+            for (auto& [v, i]: g[u] ) {
+                if (v == fa) continue;
+                if (connections[i][1] == v) ans ++;
+                dfs(v, u);
+            }
+        };
+        dfs(0, -1); return ans;       
+    }
+};

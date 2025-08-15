534d2d7768d6ae7248b362422b76e3849a83bac4
David-5-5 <13910767907@139.com>
2025-08-16 00:53:09 +0800

    Append implementation using fa

--- cplus/leetcode/202508/algo2368.cpp
+++ cplus/leetcode/202508/algo2368.cpp
@@ -24,4 +24,24 @@ public:
         };
         vis[0]=true; dfs(0); return ans;       
     }
+
+    int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
+        // 自行解答
+        vector<vector<int>> g(n, vector<int>());
+        set<int> rest(restricted.begin(), restricted.end());
+
+        for (auto & e : edges) {
+            auto & u = e[0], v = e[1];
+            g[u].push_back(v); g[v].push_back(u);
+        }
+        int ans = 0;
+        auto dfs = [&](this auto&& dfs, int u, int fa) -> void {
+            ans ++;
+            for (auto& v: g[u] ) {
+                if (v == fa || rest.count(v)) continue;
+                dfs(v, u);
+            }
+        };
+        dfs(0, -1); return ans;         
+    }
 };

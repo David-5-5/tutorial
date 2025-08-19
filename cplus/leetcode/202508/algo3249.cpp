83ff6539df1802f4b6859c16b75b8482a8367d3e
David-5-5 <13910767907@139.com>
2025-08-20 02:04:44 +0800

    Append opitimized implementation

--- cplus/leetcode/202508/algo3249.cpp
+++ cplus/leetcode/202508/algo3249.cpp
@@ -28,5 +28,30 @@ public:
         
     }
 
-      
+    
+    int countGoodNodes2(vector<vector<int>>& edges) {
+        // 自行解答
+        int ans = 0, n = edges.size() + 1;
+        vector<vector<int>> g(n, vector<int>());
+        for (const auto& edge : edges) {
+            int u = edge[0], v = edge[1];
+            g[u].emplace_back(v);
+            g[v].emplace_back(u);
+        }
+        auto dfs = [&] (this auto&& dfs, int u, int fa) -> int {
+            int cnt = 1, prev = 0; bool equals = true;
+            for (auto& v: g[u]) {
+                if (v == fa) continue;
+                auto child = dfs(v, u);
+                cnt += child; 
+                if (equals) {   // 这样写快多了
+                    equals = (prev == 0 || prev == child); prev = child;
+                }
+            }
+            if (equals) ans += 1;
+            return cnt;
+        };
+
+        dfs(0, -1); return ans;
+    }    
 };

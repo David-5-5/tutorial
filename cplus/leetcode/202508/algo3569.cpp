df0c6b726e65eacb4de061888ddaaf8b91a54253
David-5-5 <13910767907@139.com>
2025-08-19 19:11:34 +0800

    Append implementation using new template

--- cplus/leetcode/202508/algo3569.cpp
+++ cplus/leetcode/202508/algo3569.cpp
@@ -20,17 +20,16 @@ int init = [] {
 class Solution {
 public:
     vector<int> maximumCount(vector<int>& nums, vector<vector<int>>& queries) {
-        // 参考题解 - 
+        // 参考题解 - 这题的思维难点在于如何统计最大值
+        // 根据题解代码实现，是当前区间 + 1, 外围区间取不同区间的最大值
         int n = nums.size();
         // 参考 oi-wiki 线段树模板 - 区间更新 最大值
         vector<int> tree(4 * n);
         vector<int> tp(4 * n);     // 支持懒标记
 
         // 下推懒标记
-        auto pushdown = [&] (this auto&& pushdown, int s, int t, int p) -> void{
+        auto pushdown = [&] (int s, int t, int p) -> void{
             if (s == t) return;  // 叶子节点无需下推
-            int m = s + ((t - s) >> 1);
-
             // 更新左子树
             tree[p * 2 + 1] += tp[p]; tp[p * 2 + 1] += tp[p];
             // 更新右子树
@@ -106,7 +105,77 @@ public:
         return ans;
     }
 
+    vector<int> maximumCount2(vector<int>& nums, vector<vector<int>>& queries) {
+        // 参考题解 - 代码优化
+        int n = nums.size();
+        // 参考 oi-wiki 线段树模板 - 区间更新 最大值
+        vector<int> tree(4 * n);
+        vector<int> tp(4 * n);     // 支持懒标记
 
+        // 下推懒标记
+        auto pushdown = [&] (int s, int t, int p) -> void{
+            if (s == t) return;  // 叶子节点无需下推
+            // 更新左子树
+            tree[p * 2 + 1] += tp[p]; tp[p * 2 + 1] += tp[p];
+            // 更新右子树
+            tree[p * 2 + 2] += tp[p]; tp[p * 2 + 2] += tp[p];
+            // 清空当前节点懒标记
+            tp[p] = 0;
+        };
+
+        // 区间更新：[l, r] 区间增加 value
+        auto update = [&](this auto&& update, int l, int r, int value, int s, int t, int p) -> void{
+            if (l <= s && t <= r) {
+                // 当前区间完全在更新范围内
+                tree[p] += value;            // 区间 + val
+                if (s != t)  tp[p] += value; // 非叶子节点才需要标记
+                return;
+            }
+            // 下推懒标记
+            if (tp[p] != 0) pushdown(s, t, p);
+
+            int m = s + ((t - s) >> 1);
+            // 递归更新左子树
+            if (l <= m)  update(l, r, value, s, m, p * 2 + 1);
+            // 递归更新右子树
+            if (r > m) update(l, r, value, m + 1, t, p * 2 + 2);
+            // 更新当前节点值
+            tree[p] = max(tree[p * 2 + 1], tree[p * 2 + 2]);
+        };
+        // 参考 oi-wiki 线段树模板 - 区间更新 最大值
+        unordered_map<int, set<int>> mp;
+        // 区间 收益
+        auto apply = [&](int key, int val) -> void {
+            if (mp.count(key) && mp[key].size()) {
+                int l = *mp[key].begin(), r = *mp[key].rbegin();
+                if (l!=r) update(l, r, val, 0, n-1, 0);
+            }
+        };
+        for (int i = 0; i < n; ++i) if (is_prime[nums[i]]) mp[nums[i]].insert(i);
+        for (auto& [x, _]: mp) apply(x, 1);
+
+        vector<int> ans;
+        for (auto& q : queries) {
+            auto inx = q[0], val = q[1];
+            if (nums[inx] != val) {
+                if (is_prime[nums[inx]]) {
+                    apply(nums[inx], -1);             // 撤销原有区间收益
+                    mp[nums[inx]].erase(inx); 
+                    if (mp[nums[inx]].size()==0) mp.erase(nums[inx]);
+                    apply(nums[inx], 1);              // 增加变更后区间收益
+                }
+                nums[inx] = val;
+                if (is_prime[val]) {
+                    apply(val, -1);             // 撤销原有区间收益
+                    mp[nums[inx]].insert(inx);
+                    apply(val, 1);              // 增加变更后区间收益
+                }
+            }
+            ans.push_back(tree[0]+mp.size());
+        }
+        
+        return ans;
+    }
 };
 
 int main() {

95d689de0bce3b2d51356e732e5301f6a21b0b7c
David-5-5 <13910767907@139.com>
2025-08-24 15:17:41 +0800

    Append implementation using hash

--- cplus/leetcode/202508/algo2154.cpp
+++ cplus/leetcode/202508/algo2154.cpp
@@ -14,5 +14,19 @@ public:
         }
         return original;
     }
-    
+
+
+    int findFinalValue2(vector<int>& nums, int original) {
+        // 哈希表，时间复杂度O(N)
+        int mask = 0;
+        for(auto & num : nums){
+            if(num % original == 0){
+                int k = num / original;
+                if ((k & (k-1)) == 0) mask |= k;
+            }
+        }
+        mask = ~ mask;
+        return original * (mask & (-mask)); // lowbit
+    }
+        
 };

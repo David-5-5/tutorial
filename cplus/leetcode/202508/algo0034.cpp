74d07508439e8724c41bc9deba9b05b4bf3cba56
David-5-5 <13910767907@139.com>
2025-08-26 16:53:25 +0800

    Add algo using lower_bound

---
+++ cplus/leetcode/202508/algo0034.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    // vector<int> searchRange(vector<int>& nums, int target) {
+    //     auto left = ranges::lower_bound(nums, target);
+    //     if (left == nums.end() || *left != target) return {-1,-1};
+    //     int l = left - nums.begin();
+    //     int right = ranges::lower_bound(nums, target+1)-nums.begin()-1;
+    //     return {l, right};
+    // }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        auto left = ranges::lower_bound(nums, target);
+        if (left == nums.end() || *left != target) return {-1,-1};
+        // left - nums.begin() 返回的是 difference_type 类型（通常是 long）
+        // 直接放入 int 类型的向量中，这在列表初始化时是不允许的。
+        // 添加 static_cast<int>() 消除窄化转换的错误提示
+        return {static_cast<int>(left - nums.begin()), 
+           static_cast<int>(ranges::lower_bound(nums, target+1)-nums.begin()-1)};
+    }
+};

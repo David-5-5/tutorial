e36e9351afacdb3668ea5c5f9e662f845997f473
David-5-5 <13910767907@139.com>
2025-09-07 14:48:34 +0800

    Append comments

--- python/algo/202411/leetcode0047.py
+++ python/algo/202411/leetcode0047.py
@@ -2,7 +2,7 @@ from collections import Counter
 from itertools import permutations
 from typing import List
 
-# 专题: 全排列
+# 专题: 重复数据 全排列 
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         return list(set(permutations(nums)))

6697b22daecb7d92ce89b956cf4cebd1dce99f9b
David-5-5 <13910767907@139.com>
2025-09-10 22:24:37 +0800

    Append implementation refer to 491

--- cplus/leetcode/202509/algo0040.cpp
+++ cplus/leetcode/202509/algo0040.cpp
@@ -30,5 +30,32 @@ public:
         return ans;
     }
 
+    vector<vector<int>> combinationSum2_2(vector<int>& candidates, int target) {
+        // 参考 491 选择重复元素的做法，由于未剪枝，超时
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans; int n = candidates.size();
 
+        auto dfs = [&] (this auto&& dfs, int i, int sum, vector<int>& path) -> void  {
+            
+            if (i == n) {
+                if (sum == target) ans.push_back(path);
+                return;
+            } 
+            
+            // sum += candidates[i];
+            if (path.empty() || candidates[i] != path.back()) {
+                dfs(i+1, sum, path);
+            }
+
+            path.push_back(candidates[i]);        // 选
+            dfs(i+1, sum+candidates[i], path);
+            path.pop_back();  // 恢复现场
+
+
+        };
+
+        vector<int> path;
+        dfs(0, 0, path);
+        return ans;
+    }
 };

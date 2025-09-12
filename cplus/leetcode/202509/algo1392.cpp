3a2e38712bca64be31b3464457e5cf655ecf5f24
David-5-5 <13910767907@139.com>
2025-09-13 01:43:32 +0800

    Add algo using rabin-karp with monte carlo

---
+++ cplus/leetcode/202509/algo1392.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Rabin-Karp 字符串编码
+class Solution {
+public:
+    string longestPrefix(string s) {
+        // Rabin karp 变型 - 蒙特卡洛方法 双 hash
+        int mod1 = 1e9 + 7, mod2 = 1e9 + 9;
+        int base1 = 401, base2 = 701;
+        long long bm1 = 1, bm2 = 1;        
+        int n = s.length();
+
+        auto prefixHash = [&](pair<long long, long long>& hash, int i) -> void {
+            hash.first = (hash.first * base1 + s[i]) % mod1;
+            hash.second = (hash.second * base2 + s[i]) % mod2;
+        };
+
+        auto suffixHash = [&](pair<long long, long long>& hash, int i) -> void {
+
+            hash.first = (bm1 * s[i]  + hash.first) % mod1;
+            hash.second = (bm2 * s[i] + hash.second) % mod2;
+            bm1 = (bm1 * base1) % mod1; bm2 = (bm2 * base2) % mod2; 
+        };
+
+        pair<long long, long long> pHash = {0, 0}, sHash = {0, 0}; int len = -1;
+        for (int i=0; i<n-1; ++i) {
+            prefixHash(pHash, i); suffixHash(sHash, n - i - 1);
+            if (pHash == sHash) len = i;
+        }
+
+        return s.substr(0, len+1);
+
+    }
+
+
+};

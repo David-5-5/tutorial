1b072da1f9a1cf0b1bb2fdf1ebcc42450c4d1762
David-5-5 <13910767907@139.com>
2025-09-17 10:34:05 +0800

    Add algo using prime sieve

---
+++ cplus/leetcode/202509/algo0204.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e7 + 1;
+bool is_prime[N];
+vector<int> primes;
+int init = [] {
+    memset(is_prime, true, sizeof(is_prime));
+    is_prime[0] = false; is_prime[1] = false;
+    for (int i = 2; i < N; ++i) {       // 需要统计质数数量，此处不能筛至平方根
+        if (is_prime[i]) {
+            primes.push_back(i);
+            for (long long j = 1LL * i * i; j < N; j += i) {
+                is_prime[j] = false;
+            }
+        }
+    }
+    return 0;
+}();
+
+
+// 数学算法 - 数论 - 1.2 预处理质数（筛质数）
+class Solution {
+public:
+    int countPrimes(int n) {
+        return ranges::lower_bound(primes, n) - primes.begin();
+    }
+};

330fd29f6ed901c09dc29577196cb2e14b7962d3
David-5-5 <13910767907@139.com>
2025-09-27 01:12:56 +0800

    Add algo using sliding window

---
+++ cplus/leetcode/202509/algo2841.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 滑动窗口与双指针 - 定长滑动窗口 1.1 基础
+class Solution {
+public:
+    long long maxSum(vector<int>& nums, int m, int k) {
+        unordered_map<int, int> cnt; int n = nums.size();
+        long long sum = 0, ans = 0; 
+        for (int i=0; i<k-1;++i) {
+            cnt[nums[i]] ++; sum += nums[i];
+        }
+
+        for (int i=k-1; i<n; ++i) {
+            cnt[nums[i]] ++; sum += nums[i];
+            if (cnt.size()>=m) ans = max(ans, sum);
+            sum -= nums[i-k+1]; cnt[nums[i-k+1]] --;
+            if (cnt[nums[i-k+1]] == 0) cnt.erase(nums[i-k+1]);
+        }
+        return ans;
+    }
+};

13c514e81e5d2efee1a5557d425a484c650f9f6b
David-5-5 <13910767907@139.com>
2025-09-29 01:03:40 +0800

    Append revised implementation

--- cplus/leetcode/202509/algo0030.cpp
+++ cplus/leetcode/202509/algo0030.cpp
@@ -7,24 +7,26 @@ public:
     vector<int> findSubstring(string s, vector<string>& words) {
         // 参考题解 - 看懂了，明天再做
         vector<int> ans;
-        int p = words[0].length(), q = words.size();
-        if (s.length() < p * q) return ans;
+        int m = words[0].length(), n = words.size(), ln = s.length();
+        if (ln < m * n) return ans;
         unordered_map<string, int> cnt;
         for (auto w: words) cnt[w] ++;
 
-        for (int i=0; i<s.length() - p * q; ++i) {
-            auto check = [&]() {
-                unordered_map<string, int> cur;
-                for (int j=0; j<q; ++j) {
-                    auto word = s.substr(i+j*p, p);
-                    if (cnt.count(word)) {
-                        cur[word] ++;
-                        if (cur[word] > cnt[word]) return false;
-                    } else return false;
+        for (int i=0; i<m; ++i) {
+            unordered_map<string, int> cur;
+            auto check = [&] () -> bool {
+                for (auto [k, v]:cnt) {
+                    if (cur[k] != v) return false;
                 }
                 return true;
             };
-            if (check()) ans.push_back(i);
+
+            for (int j=i; j+m<=ln && j<(n-1)*m; j+=m) cur[s.substr(j, m)]++;
+            for (int j=i+(n-1)*m; j+m<=ln; j+=m) {
+                cur[s.substr(j, m)] ++;
+                if (check()) ans.push_back(j-(n-1)*m);
+                cur[s.substr(j-(n-1)*m, m)] --;
+            }
         }
         return ans;
     }

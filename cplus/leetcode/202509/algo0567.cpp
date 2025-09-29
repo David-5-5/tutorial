975592644eaf39165ce5d4adcf1d508c5f595f9e
David-5-5 <13910767907@139.com>
2025-09-30 00:29:57 +0800

    Add algo using sliding window

---
+++ cplus/leetcode/202509/algo0567.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 滑动窗口与双指针 - 定长滑动窗口 1.2 进阶
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        // 自行解答
+        if (s1.length() > s2.length()) return false;
+        vector<int> target(26), cur(26);
+        for (auto ch: s1) target[ch-'a']++;
+
+        for (int i=0; i<s1.length()-1; i++) cur[s2[i]-'a'] ++;
+        for (int i=s1.length()-1; i<s2.length(); ++i) {
+            cur[s2[i]-'a'] ++;
+            auto check = [&]() {
+                for (int j=0; j<26; ++j) {
+                    if (target[j] != cur[j]) return false;
+                }
+                return true;
+            };
+            if (check()) return true;
+            cur[s2[i-s1.length()+1]-'a'] --;
+        }
+        return false;
+    }
+}; 

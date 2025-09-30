440bd4ef07f874aa7efe1e078b262d76bd2fbc3c
David-5-5 <13910767907@139.com>
2025-09-30 17:57:58 +0800

    Append implementation using lagrange's four square theorem

--- cplus/leetcode/202502/algo0279.cpp
+++ cplus/leetcode/202502/algo0279.cpp
@@ -24,4 +24,31 @@ public:
 
         return dp[n];
     }
+
+
+    int numSquares2(int n) {
+        // 数学 拉格朗日四平方和定理：任何一个整数都可以被表示为至多四个正整数的平方和
+        // 当 n != 4^k(8m+7) 时， n 可以被表示为至多三个整数的平方和
+        // 当 n = 4^k(8m+7) 时， n 可以被表示为4个整数的平方和
+        auto a = static_cast<int>(sqrt(n));
+        if (a * a == n) return 1;
+
+        auto val = n;
+        while (val % 4 == 0) {
+            val /= 4;
+        }
+        if ((val - 7)%8 == 0) return 4;
+
+        // 两数的平方 参见 633
+        a = 0;
+        while (a * a * 2 <= n) {
+            auto b = static_cast<int>(sqrt(n-a*a));
+            if (a * a + b * b == n) return 2;
+            a ++;
+        }
+
+        return 3;
+    }
+
+
 };

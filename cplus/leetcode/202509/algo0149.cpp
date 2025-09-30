316dc060579daa255d49b0c27831fa839db46ef5
David-5-5 <13910767907@139.com>
2025-09-30 11:11:27 +0800

    Append implementation using pair of slope

--- cplus/leetcode/202509/algo0149.cpp
+++ cplus/leetcode/202509/algo0149.cpp
@@ -24,4 +24,44 @@ public:
         return ans+1;
     }
 
+    int maxPoints2(vector<vector<int>>& points) {
+
+        // 模板 计算斜率，按照斜率计数
+        struct PairHash {
+            size_t operator()(const pair<int, int>& p) const {
+                // 分别计算两个指针的哈希值
+                size_t hash1 = std::hash<int>()(p.first);
+                size_t hash2 = std::hash<int>()(p.second);
+                
+                // 组合两个哈希值（避免碰撞的简单方式）
+                return hash1 ^ (hash2 << 1);
+            }
+        }; 
+        int n = points.size(), ans = 0;
+        
+        // 归一化斜率
+        auto slope = [](int dx, int dy) -> pair<int, int> {
+            if (dx == 0) return {1, 0};
+            if (dy == 0) return {0, 1};
+            int g = gcd(dy, dx);
+            auto a = dy/g, b = dx/g;
+            if (b < 0) {a = -a; b= - b;}
+            return {a, b};
+        };
+
+        for (int i=0; i<n-1; ++i) {
+            // key is slope , value is count of the slope
+            // i is the origin
+            unordered_map<pair<int, int>, int, PairHash> k_cnt;
+            for (int j=i+1; j<n; ++j) {
+                pair<int,int> sl = slope(points[j][0]-points[i][0],  points[j][1] -points[i][1]);
+                k_cnt[sl] ++;
+                ans = max(ans, k_cnt[sl]);
+            }
+
+        }
+        return ans+1;        
+
+
+    }  
 };

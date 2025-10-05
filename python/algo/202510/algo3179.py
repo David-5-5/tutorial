1041b340b2303306cd99bf4e7134c7440a728e74
David-5-5 <13910767907@139.com>
2025-10-05 14:55:42 +0800

    Add algo using pascal's triangle

--- python/algo/202510/algo3179.py
+++ python/algo/202510/algo3179.py
@@ -1,4 +1,7 @@
 # 数学算法 - 组合数学 - 2.2 组合计数
+from math import comb
+
+
 class Solution:
     def valueAfterKSeconds(self, n: int, k: int) -> int:
         # 暴力 O(km)
@@ -8,3 +11,7 @@ class Solution:
             for i in range(1, n):
                 nums[i] = (nums[i] + nums[i-1]) % mod
         return nums[-1]
+
+    def valueAfterKSeconds(self, n: int, k: int) -> int:
+        # 帕斯卡三角，计算第 n+k-1 的第 k 个数 c(n+k-1, k)
+        return comb(n+k-1, k) % (10 ** 9 + 7)

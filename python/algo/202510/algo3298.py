5558e639e9a0af6e98a5169c32d94953a59b3611
David-5-5 <13910767907@139.com>
2025-10-17 04:10:42 +0800

    Add algo using sliding windon

---
+++ python/algo/202510/algo3298.py
@@ -0,0 +1,27 @@
+from collections import Counter, defaultdict
+
+# 滑动窗口与双指针 - 不定长滑动窗口 2.3.2 越长越合法
+class Solution:
+    def validSubstringCount(self, word1: str, word2: str) -> int:
+        # 自行解答
+        cnt2 = Counter(word2)
+        cnt1 = defaultdict(int)
+
+        # 判断是否包含，性能较差
+        def contain() -> bool:
+            for k, v in cnt2.items():
+                if cnt1[k] < v: return False
+            return True
+
+        l = ans = 0
+        for ch in word1:
+            cnt1[ch] += 1
+            cur = True
+            while cur and contain():
+                cnt1[word1[l]] -= 1
+                cur = cnt1[word1[l]] >= cnt2[word1[l]]
+                l += 1
+            ans += l
+        return ans
+    
+    

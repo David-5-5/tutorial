43d1974ad43cca3a767ea23ba1b11b6c3da97e6a
David-5-5 <13910767907@139.com>
2025-10-21 10:08:52 +0800

    Append implementation using dp but overtime

--- python/algo/202510/algo1621.py
+++ python/algo/202510/algo1621.py
@@ -12,3 +12,19 @@ class Solution:
         mod = 10 **9 + 7
         return comb(n+k-1, k*2) % mod
 
+    def numberOfSets2(self, n: int, k: int) -> int:
+        mod = 10 **9 + 7
+
+        # 记忆化搜索 超时
+        @cache
+        def dfs(i:int, j:int) -> int:
+            if i < j or i < 0: return 0
+            # if i == j: return 1
+            if i == 0: return 1 if j == 0 else 0
+            res = dfs(i-1, j) # 不选
+            for k_len in range(1, i-j+2):
+                res = (res + dfs(i-k_len, j-1)) % mod
+
+            return res
+        return dfs(n-1, k)
+

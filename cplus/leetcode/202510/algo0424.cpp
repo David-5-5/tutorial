f4bfd5f2b6622fee4b528b2c9be74c0fa14d97f4
David-5-5 <13910767907@139.com>
2025-10-24 00:28:15 +0800

    Append implementation using vector replace of unordered_map

--- cplus/leetcode/202510/algo0424.cpp
+++ cplus/leetcode/202510/algo0424.cpp
@@ -21,5 +21,20 @@ public:
         return ans;     
     }
 
-
+    int characterReplacement2(string s, int k) {
+        // 自行解答，差一点
+        int ans = 0, l = 0, n = s.length(), maxn = 0;
+        vector<int> freqs;
+        for (int r = 0; r<n; ++r) {
+            freqs[s[r]-'A'] ++;
+            maxn = max(maxn, freqs[s[r]-'A']); // 始终找窗口中出现频率最大的值
+            while (r-l+1 - maxn > k) {
+                freqs[s[l]-'A'] --;
+                l ++;
+            }
+            if (r-l+1-maxn == k) ans = max(ans, r-l+1);
+            else ans = max(ans, r-l+1 + min(k+maxn+l-r-1, n-r-1)); // 修改后面
+        }
+        return ans;     
+    }    
 };

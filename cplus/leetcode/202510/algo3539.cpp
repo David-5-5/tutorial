2a9fa1bbbcc94b68b474ce937fc7978afa6b304e
David-5-5 <13910767907@139.com>
2025-10-25 21:17:09 +0800

    Add algo using combinational counting

---
+++ cplus/leetcode/202510/algo3539.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// 数学算法 - 组合数学 - 2.2 组合计数
+class Solution {
+public:
+    int magicalSum(int m, int k, vector<int>& nums) {
+        int n = nums.size();
+        
+    }
+};

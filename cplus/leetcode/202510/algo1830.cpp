ba3f78c26b663405e89326ce52ad24776d7864c1
David-5-5 <13910767907@139.com>
2025-10-26 21:59:15 +0800

    Rename method avoid conflict with builit-in pow function

--- cplus/leetcode/202510/algo1830.cpp
+++ cplus/leetcode/202510/algo1830.cpp
@@ -4,7 +4,7 @@ using namespace std;
 const int MOD = 1e9 + 7;
 const int MX = 3001;   // 根据取值范围进行变更
 
-long long pow(long long base, int exp)
+long long qpow(long long base, int exp)
 {
     base %= MOD;
     long long res = 1;
@@ -25,7 +25,7 @@ auto init = [] {
     for (int i=1; i<MX+1; i++) {
         fac[i] = fac[i-1] * i % MOD;
     }
-    inv_fac[MX] = pow(fac[MX], MOD-2);
+    inv_fac[MX] = qpow(fac[MX], MOD-2);
     for (int i=MX; i; i--) {
         inv_fac[i-1] = inv_fac[i] * i % MOD;
     }

48d28e48a2699309e40d2c950c29aa4bffdd6303
David-5-5 <13910767907@139.com>
2025-11-02 14:39:38 +0800

    Append comments

--- python/algo/leecode/algo_lcp37.py
+++ python/algo/leecode/algo_lcp37.py
@@ -1,50 +1,8 @@
 class Solution:
     def minRecSize(self, lines) -> float:
-        n = len(lines)
-        if n < 3: return 0
-
-        points = []
-        
-        for i in range(n-1):
-            for j in range(i+1, n):
-                if (lines[i][0] - lines[j][0]) == 0 : continue
-                x = (lines[j][1] - lines[i][1]) / (lines[i][0] - lines[j][0])
-                y = x * lines[i][0] + lines[i][1]
-                points.append((x,y))
-
-        if len(points) < 3: return 0
-        orgX, orgY = points.pop(0)
-        maxX, minX = orgX, orgX 
-        maxY, minY = orgY, orgY
-
-        notInLine = False
-        horizontal = False
-        vertical = False
-        slope = 0
-
-        for x, y in points:
-            maxX = max(maxX, x)
-            minX = min(minX, x)
-            maxY = max(maxY, y)
-            minY = min(minY, y)
-            if not notInLine:
-                if abs(x - orgX) < 0.1 ** -5:
-                    if horizontal or slope != 0:
-                        notInLine = True
-                    else:
-                        vertical = True
-                elif abs(y - orgY) < 0.1 ** -5:
-                    if vertical or slope != 0:
-                        notInLine = True
-                    else:
-                        horizontal = True
-                elif slope == 0 and not horizontal and not vertical:
-                    slope = (y - orgY)/(x - orgX)
-                elif abs(slope - (y - orgY)/(x - orgX)) > 0.1 ** 2:
-                    notInLine = True
-        return (maxX - minX) * (maxY - minY)
-
-    def minRecSize2(self, lines) -> float:
+        # 几何题目
+        # 对直线i+1, 只需考虑斜率恰好小于直线i+1的直线组中，最上方和最下方的直线
+        # 因此可以考虑排序后按照斜率分组，每组保留最下方和最上方的直线，然后遍历更新
         n = len(lines)
         if n < 3: return 0
 

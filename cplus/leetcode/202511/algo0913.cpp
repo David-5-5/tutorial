f097fda70f093a240bbef16758583c720b39fac4
David-5-5 <13910767907@139.com>
2025-11-06 17:01:54 +0800

    Add algo using try the maximum steps = 5 * n

--- cplus/leetcode/202511/algo0913.cpp
+++ cplus/leetcode/202511/algo0913.cpp
@@ -43,5 +43,42 @@ public:
         return dfs(1, 2, 0);
     }
 
+    int catMouseGame2(vector<vector<int>>& graph) {
+        // 探测最大步数 - 机会主义
+        int n = graph.size();
+        int memo[50][50][5 * 50]; 
+        memset(memo, -1, sizeof(memo));
+        
+        auto dfs = [&](this auto&& dfs, int mouse, int cat, int step) -> int {
 
+            if (mouse == 0) return 1;
+            if (mouse == cat) return 2;
+            if (step >= 5 * n ) return 0;
+            int& res = memo[mouse][cat][step];
+            if (res != -1) return res;
+
+            
+            bool has_draw = false;
+            if (step % 2 == 0) { // mouse
+                
+                for (auto v: graph[mouse]) {
+                    int cur = dfs(v, cat, step+1);
+                    if (cur == 1) return res = 1;
+                    else if (cur == 0) has_draw = true;
+                }
+                return res = (has_draw)?0:2;
+            } else {
+                for (auto v: graph[cat]) {
+                    if (v == 0) continue;
+                    int cur = dfs(mouse, v, step+1);
+                    if (cur == 2) return res = 2;
+                    else if (cur == 0) has_draw = true;
+                }
+                return res = (has_draw)?0:1;
+            }
+
+        };
+
+        return dfs(1, 2, 0);
+    }    
 };

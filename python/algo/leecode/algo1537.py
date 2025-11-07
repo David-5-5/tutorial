45b6db354844b3874888c1fb19a1242903b65957
David-5-5 <13910767907@139.com>
2025-11-08 03:03:10 +0800

    Append optimizing code implementation

--- python/algo/leecode/algo1537.py
+++ python/algo/leecode/algo1537.py
@@ -1,3 +1,6 @@
+from typing import List
+
+# 滑动窗口与双指针 - 双序列双指针 - 4.1 双指针
 class Solution:
     def maxSum(self, nums1, nums2) -> int:
         sum1, sum2, inx1, inx2 = 0, 0, 0, 0
@@ -28,6 +31,27 @@ class Solution:
                     sum2 += nums2[inx2]
                     inx2 += 1
         return max(sum1, sum2) % (10 ** 9 + 7)
+
+    def maxSum2(self, nums1: List[int], nums2: List[int]) -> int:
+        # 复习，简化上述代码
+        sum1 = sum2 = inx1 = inx2 = 0
+        n1, n2 = len(nums1), len(nums2)
+        
+        while inx1 < n1 and inx2 < n2:
+            if nums1[inx1] < nums2[inx2]:
+                sum1 += nums1[inx1]
+                inx1 += 1
+            elif nums1[inx1] > nums2[inx2]:
+                sum2 += nums2[inx2]
+                inx2 += 1
+            else:      # equals
+                sum2 = sum1 = max(sum1, sum2) + nums1[inx1]
+                inx1 += 1
+                inx2 += 1
+        for i in range(inx1, n1): sum1 += nums1[i]
+        for i in range(inx2, n2): sum2 += nums2[i]
+
+        return max(sum1, sum2) % (10 ** 9 + 7)    
 
 if __name__ == "__main__":
     sol = Solution()

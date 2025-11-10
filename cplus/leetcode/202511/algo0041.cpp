94272ac78cc15a76dc7c4eb8658fc5fb972790a1
David-5-5 <13910767907@139.com>
2025-11-11 00:22:41 +0800

    Add algo using in-place update

---
+++ cplus/leetcode/202511/algo0041.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 滑动窗口与双指针 - 单序列双指针 3.4 原地修改
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        // 自行解答
+        nums.push_back(0);
+        int n = nums.size();
+        for (int i=0; i<n; i++) {
+            while (nums[i] > 0 && nums[i]<n && nums[i]!=nums[nums[i]])
+                swap(nums[i], nums[nums[i]]);
+        }
+        for (int i=1; i<n; i++) if (nums[i]!=i) return i;
+
+        return n;
+    }
+};

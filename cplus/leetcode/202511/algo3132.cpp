b1f75d1153051465b1bba81ac725b06c5e04db7d
David-5-5 <13910767907@139.com>
2025-11-17 23:40:51 +0800

    Append optimizied implementation

--- cplus/leetcode/202511/algo3132.cpp
+++ cplus/leetcode/202511/algo3132.cpp
@@ -22,6 +22,23 @@ public:
         return -1;
     }
 
+    int minimumAddedInteger2(vector<int>& nums1, vector<int>& nums2) {
+        // 参考题解，前三个数至少保留一个
+        int n = nums1.size(), m = nums2.size();
+        sort(nums1.begin(), nums1.end()); sort(nums2.begin(), nums2.end()); 
+        
+        for (int i=2; i>=0; i--) {
+            int x = nums2[0] - nums1[i];
+            int inx1 = 0, inx2 = 0;
+            while (inx1 <n && inx2 < m) {
+                if (nums1[inx1] + x == nums2[inx2]) {
+                    inx1 ++; inx2 ++;
+                } else inx1 ++;
+            }
+            if (inx2 == m) return x;
 
+        }
+        return -1;
+    }
 };
 

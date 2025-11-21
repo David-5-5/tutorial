2b4eed0fbdd4b6390be6eb8744c43079b8cd3701
David-5-5 <13910767907@139.com>
2025-11-22 00:08:25 +0800

    Add algo using grouped loop

---
+++ cplus/leetcode/202511/algo0467.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 滑动窗口与双指针 - 六、分组循环
+class Solution {
+public:
+    int findSubstringInWraproundString(string s) {
+        // 自行解答
+        vector<int> cnt(26);
+
+        int ans = 1, prev = 1, n = s.length();
+        cnt[s[0]-'a'] = 1;
+        for (int i=1; i<n; ++i) {
+            if ((s[i-1]-'a' + 1)%26 == s[i]-'a')
+                prev += 1;
+            else prev = 1;
+
+            if (cnt[s[i]-'a'] < prev) {
+                ans += prev - cnt[s[i]-'a'];
+                cnt[s[i]-'a'] = prev;
+            }
+        }
+        return ans;        
+    }
+};

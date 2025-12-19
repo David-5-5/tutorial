6e8ec93fe8e016d5a60f3012e01625d191fb072c
David-5-5 <13910767907@139.com>
2025-12-19 23:53:13 +0800

    Append implementation using arithmetic progression

--- cplus/leetcode/202512/algo2829.cpp
+++ cplus/leetcode/202512/algo2829.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Solution {
 public:
     int minimumSum(int n, int k) {
-        // 自行解答 hash表
+        // 自行解答 hash表 O(NlogN)
         unordered_set<int> val; int start = 1, ans = 0;
         while (val.size() < n) {
             if (!val.count(k - start)) {
@@ -17,4 +17,13 @@ public:
 
         return ans;
     }
+
+    int minimumSum2(int n, int k) {
+        // 参考题解 等差数列 O(1)
+        int h = min(n,k/2);
+        int ans = (1 + h) * h/ 2;
+        if (n > h) ans += (k + k+n-h-1) * (n-h) /2 ;
+
+        return ans;
+    }    
 };

7166a9d9a4464326b1ca5aef3a1fdc117ea1ffca
David-5-5 <13910767907@139.com>
2025-12-21 23:16:43 +0800

    Add algo using greedy

---
+++ cplus/leetcode/202512/algo3627.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 一、贪心策略 - 1.1 从最小/最大开始贪心
+class Solution {
+public:
+    long long maximumMedianSum(vector<int>& nums) {
+        int i = 0, j = nums.size() - 1; long long ans = 0;
+        sort(nums.begin(), nums.end());
+
+        while (i < j) {
+            ans += nums[j-1];
+            i ++, j -= 2;
+        }
+        return ans;
+    }
+};

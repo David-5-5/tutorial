5aa8bccf73b968349c26113b3dfbfc6f7d044d1e
David-5-5 <13910767907@139.com>
2025-12-30 01:45:28 +0800

    Add algo using full knapsack

---
+++ cplus/leetcode/202512/algo3592.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 动态规划 - 三、背包 3.2 完全背包
+class Solution {
+public:
+    vector<int> findCoins(vector<int>& numWays) {
+        // 自行解答 
+        int n = numWays.size(); vector<int> ans;
+
+        for (int i=1; i<=n; i++) {
+            vector<int> dp(i+1); dp[0] = 1; // 每次循环重新计算 dp
+            for (int j=0; j<ans.size(); j++) {
+                for (int k=ans[j]; k<=i; k++) {
+                    dp[k] += dp[k-ans[j]];
+                }
+            }
+
+            if (dp[i] < numWays[i-1]) {
+                ans.push_back(i); dp[i] += 1;
+            }
+
+            if (dp[i]!=numWays[i-1]) return {};
+        }
+
+        return ans;
+    }
+
+};
+
+int main() {
+    vector<int> numWay = {0,1,0,2,0,3,0,4,0,5};
+    Solution().findCoins(numWay);
+}

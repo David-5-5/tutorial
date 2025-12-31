ce29065824c98096a793bc2c22fe645040e525d0
David-5-5 <13910767907@139.com>
2026-01-01 04:20:07 +0800

    Add algo using dp of state machine

---
+++ python/algo/202601/algo3573.py
@@ -0,0 +1,27 @@
+
+from cmath import inf
+from functools import cache
+from typing import List
+
+# 动态规划 - 六、状态机 DP 6.1 买卖股票
+class Solution:
+    def maximumProfit(self, prices: List[int], k: int) -> int:
+        # 参考题解
+        n = len(prices)
+
+        @cache
+        def dfs(i: int, c:int, l:int) -> int:
+            if l < 0 : return -inf
+            if i < 0: return -inf if c else 0
+
+            p = prices[i]
+            if c == 0:
+                return max(dfs(i-1, 0, l), dfs(i-1, 1, l) + p, dfs(i-1, 2, l) - p)  # 普通交易 买入
+            if c == 1:
+                return max(dfs(i-1, 1, l), dfs(i-1, 0, l-1) - p) # 普通交易 买出
+            return max(dfs(i-1, 2, l), dfs(i-1, 0, l-1) + p) # 做空交易 买入
+
+            return res
+        ans = dfs(n-1, 0, k)
+        dfs.cache_clear()
+        return ans

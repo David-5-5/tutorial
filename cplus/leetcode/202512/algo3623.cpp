37e6cc5d4222edeec3e7df2758814819b40fa641
David-5-5 <13910767907@139.com>
2025-12-31 17:51:17 +0800

    Append new implementation using two level loop

--- cplus/leetcode/202512/algo3623.cpp
+++ cplus/leetcode/202512/algo3623.cpp
@@ -21,6 +21,27 @@ public:
 
     }
 
+    int countTrapezoids2(vector<vector<int>>& points) {
+        // 自行解答
+        const int mod = 1e9 + 7;
+        // 排序一定要要传入 vector<int>& ，否则需要创建数组，超时！！！
+        sort(points.begin(), points.end(), [](vector<int>& a, vector<int>& b){
+            return a[1] < b[1];
+        });
 
+        int ans = 0, i=1, n = points.size(); long long pre = 0;
+        while (i < n) {
+            int cnt = 1;
+            while (i < n && points[i-1][1] == points[i][1]) {
+                cnt ++; i++;
+            }
+
+            int comb =  1LL * cnt * (cnt-1) / 2 % mod;
+            ans = (pre * comb + ans)  %  mod;
+            pre = (pre + comb) % mod;
+            i++;
+        }
+        return ans;   
+    }    
 };
 

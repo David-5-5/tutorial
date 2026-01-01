472416816fe9c228e2c9b33b891a00d1370e1f1c
David-5-5 <13910767907@139.com>
2026-01-01 22:46:48 +0800

    Add algo using enumerate method and math

---
+++ cplus/leetcode/202512/algo0479.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 杂项 - 7.1 回文数
+class Solution {
+public:
+    int largestPalindrome(int n) {
+        // 参考题解，枚举回文数的右半部分从 10^n - 1 开始，回文数为 p
+        // 枚举 n 的整数 x 从 10 ^n -1 开始到 sqrt(P)
+        // 时间复杂度为 O(10^(2*N)) 看着很大实际很小
+        
+        if (n == 1) return 9;
+
+        int upper = pow(10, n) - 1;
+        for (int left=upper; left > 0; left --) {
+            long long p = left, x = left;
+            while (x) {
+                p = p * 10 + x % 10; x /= 10;
+            }
+
+            for (int x=upper; 1LL * x * x >= p; x--) {
+                if (p % x == 0) {
+                    return p % 1337;
+                }
+            }
+        }
+        return -1;
+    }
+};

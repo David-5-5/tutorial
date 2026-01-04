65a1bdda413bde0c196ef120037b1ce9c3518840
David-5-5 <13910767907@139.com>
2026-01-04 18:14:23 +0800

    Append optimization implementation

--- cplus/leetcode/202512/algo3102.cpp
+++ cplus/leetcode/202512/algo3102.cpp
@@ -25,4 +25,37 @@ public:
 
     }
 
+    int minimumDistance2(vector<vector<int>>& points) {
+        // 优化，仅删除 xplusy.begin() xplusy.begin() xminusy.begin() xminusy.rbgein
+        // first is x + y, x - y, second is index of (x, y) in points
+        vector<pair<int, int>> xplusy, xminusy;  // 预处理 
+        int n = points.size();
+        auto remove = [&](vector<pair<int,int>>& arr, int i) -> int {
+            if (arr[0].second == i) 
+                return arr[n-1].first - arr[1].first;
+            else if (arr[n-1].second == i) 
+                return arr[n-2].first - arr[0].first;
+            else return arr[n-1].first - arr[0].first;
+        };
+
+        for (int i=0; i<n; ++i) {
+            auto & x = points[i][0], & y = points[i][1];
+            xplusy.emplace_back(x + y, i), xminusy.emplace_back(x - y, i);
+        }
+        
+        sort(xplusy.begin(), xplusy.end()); sort(xminusy.begin(), xminusy.end());
+        int ans = INT_MAX;
+        int maxValue1 = xplusy[n-1].first - xplusy[0].first;
+        int maxValue2 = xminusy[n-1].first - xminusy[0].first;
+        if (maxValue1 > maxValue2) {
+            int i = xplusy[0].second, j = xplusy.back().second;
+            ans = min(ans, max(remove(xplusy, i), remove(xminusy, i)));
+            ans = min(ans, max(remove(xplusy, j), remove(xminusy, j)));
+        } else {
+            int i = xminusy[0].second, j = xminusy.back().second;
+            ans = min(ans, max(remove(xplusy, i), remove(xminusy, i)));
+            ans = min(ans, max(remove(xplusy, j), remove(xminusy, j)));
+        }
+        return ans;
+    }    
 };

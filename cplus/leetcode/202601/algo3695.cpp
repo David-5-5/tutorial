851cd28ec41886bc872757e6ce01be34c46ff916
David-5-5 <13910767907@139.com>
2026-01-09 00:34:23 +0800

    Append implementation by comparation of diffirent vis object

--- cplus/leetcode/202601/algo3695.cpp
+++ cplus/leetcode/202601/algo3695.cpp
@@ -40,5 +40,40 @@ public:
         return ans;
     }
 
+    long long maxAlternatingSum2(vector<int>& nums, vector<vector<int>>& swaps) {
+        // 自行解答 - 两种 vis 遍历方法 设置 vis[u] vs vis[v]
+        int n = nums.size();
+        vector<vector<int>> g(n); vector<bool> vis(n);
+        for (auto& e : swaps)  {
+            g[e[0]].emplace_back(e[1]); g[e[1]].emplace_back(e[0]);
+        }
 
+        long long ans = 0;
+
+        for (int i=0; i<n; i++) {
+            vector<int> s_set; int odd = 0;
+            auto dfs = [&] (this auto&& dfs, int u) -> void {
+         
+                for (auto & v: g[u]) {
+                    if (vis[v]) continue;
+                    vis[v] = true;
+                    s_set.push_back(nums[v]);
+                    nums[v] = 0; if (v % 2) odd ++;       
+                    dfs(v);
+                }
+            };
+            dfs(i);
+            sort(s_set.begin(), s_set.end());
+            for (int j=0; j<s_set.size(); ++j) {
+                if (j < odd) ans -= s_set[j];
+                else ans += s_set[j];
+            }
+        }
+
+        for (int i=0; i<n; ++i) {
+            if (i % 2) ans -= nums[i];
+            else ans += nums[i];
+        }
+        return ans;        
+    }    
 };

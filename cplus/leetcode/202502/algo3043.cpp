38a867f741bb6f14c7a17d08578ef05ceda07435
David-5-5 <13910767907@139.com>
2026-01-12 10:10:12 +0800

    Append new implementation using set and math

--- cplus/leetcode/202502/algo3043.cpp
+++ cplus/leetcode/202502/algo3043.cpp
@@ -42,4 +42,27 @@ public:
         return ans;
 
     }
+
+
+    int longestCommonPrefix2(vector<int>& arr1, vector<int>& arr2) {
+        // 使用整数的set
+        set<int> prefixs;
+        for (auto v: arr1) {
+            while (v) {
+                prefixs.insert(v); v /= 10;
+            }
+        }
+
+        int ans = 0;
+        for (auto v: arr2) {
+            while (v) {
+                if (prefixs.count(v)) {
+                    ans = max(ans, (int)log10(v) + 1);
+                    break;
+                }
+                v /= 10;
+            }
+        }
+        return ans;
+    }    
 };

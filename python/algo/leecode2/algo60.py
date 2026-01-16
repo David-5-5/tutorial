f0944a675c46ed712511a85d9b923728e44c3623
David-5-5 <13910767907@139.com>
2026-01-16 15:02:11 +0800

    Append simpfied implementation

--- python/algo/leecode2/algo60.py
+++ python/algo/leecode2/algo60.py
@@ -7,7 +7,8 @@ class Solution:
             if k == 1: return ''.join([str(i) for i in ds])
             if k == factorial(len(ds)) : return ''.join([str(i) for i in ds[::-1]])
 
-            m, rem = k // factorial(len(ds)-1), k % factorial(len(ds)-1)
+            # m, rem = k // factorial(len(ds)-1), k % factorial(len(ds)-1)
+            m, rem = divmod(k, factorial(len(ds)-1))
             if rem == 0:
                 head = str(ds.pop(m-1))
                 return head + ''.join([str(i) for i in ds[::-1]])
@@ -16,7 +17,20 @@ class Solution:
 
         digits = [i for i in range(1, n+1)]
         return permutation(digits, k)
+    
+    def getPermutation2(self, n: int, k: int) -> str:
+        # 优化实现，去除冗余判断
+        def permutation(ds:List[int], k):
+            if k == 0: return ''.join([str(i) for i in ds])
+            if k == factorial(len(ds))-1: return ''.join([str(i) for i in ds[::-1]])
 
+            m, rem = divmod(k, factorial(len(ds)-1))
+            head = ds.pop(m)
+            return str(head) + permutation(ds, rem)
+
+        digits = [i for i in range(1, n+1)]
+        return permutation(digits, k-1)
+    
 if __name__ == "__main__":
     sol = Solution()
     print(sol.getPermutation(3, 2))

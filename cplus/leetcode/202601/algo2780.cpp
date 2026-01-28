ca8dade4955a354a37585699dc0a7b29b7406fb9
David-5-5 <13910767907@139.com>
2026-01-28 18:08:57 +0800

    Append implementation using Boyer–Moore majority vote algorithm

--- cplus/leetcode/202601/algo2780.cpp
+++ cplus/leetcode/202601/algo2780.cpp
@@ -22,4 +22,28 @@ public:
         }
         return -1;        
     }
+
+    int minimumIndex2(vector<int>& nums) {
+        // 摩尔投票法
+        int candidate = 0, cnt = 0, n = 0;
+        for (auto& v: nums) {
+            if (cnt == 0) {
+                candidate = v; cnt ++;
+            } else if (v == candidate) cnt ++;
+            else cnt --;
+       }
+       cnt = 0;
+       for (auto& v: nums) {
+            n ++; if (v == candidate) cnt ++;
+       }
+
+       if (cnt * 2 <= n) return -1;
+        int prec = 0;
+       for (int i=0; i<n; i++) {
+            if (nums[i] == candidate) prec ++;
+            if (prec * 2 > i+1 && (cnt-prec) * 2 > n-i-1) return i;
+       }
+       return -1;
+    }
+    
 };

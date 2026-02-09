f3efcc236bee51de193568e5095bde7f618031af
David-5-5 <13910767907@139.com>
2026-02-09 13:05:42 +0800

    Add algo using prefix sum

---
+++ cplus/leetcode/202602/algo3755.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 一、前缀和 1.2 前缀和与哈希表
+class Solution {
+public:
+    int maxBalancedSubarray(vector<int>& nums) {
+        struct PairHash {
+            size_t operator()(const pair<int, int>& p) const {
+                // 分别计算两个指针的哈希值
+                size_t hash1 = std::hash<int>()(p.first);
+                size_t hash2 = std::hash<int>()(p.second);
+                
+                // 组合两个哈希值（避免碰撞的简单方式）
+                return hash1 ^ (hash2 << 1);
+            }
+        };         
+        unordered_map<pair<int, int>, int, PairHash> pres; int n = nums.size();
+
+        int prex = 0, pre01 = 0, ans = 0; pres[{0,0}] = -1;
+
+        for (int i=0; i<n; i++) {
+            prex ^= nums[i];
+            if ( nums[i]%2 ) pre01++;
+            else pre01--;
+            
+            if (pres.count({prex, pre01})) ans = max(ans, i - pres[{prex, pre01}]);
+            else pres[{prex, pre01}] = i;
+        }
+        return ans;
+
+    }
+
+
+};

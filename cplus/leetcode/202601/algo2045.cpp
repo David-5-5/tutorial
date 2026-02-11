527a18a51ca0220f3f815b4ab70a98a33ab380a3
David-5-5 <13910767907@139.com>
2026-02-12 00:42:38 +0800

    Append optimized implementation

--- cplus/leetcode/202601/algo2045.cpp
+++ cplus/leetcode/202601/algo2045.cpp
@@ -49,5 +49,34 @@ public:
         return ans;  
     }
 
-  
+    int secondMinimum2(int n, vector<vector<int>>& edges, int time, int change) {
+        // 参考题解，题解的思路是建立二维 dis 数组 vector = dis(n+1, vector<int>(2, INT_MAX));
+        // 这样每个节点仅经过两次
+        vector<vector<int>> g(n+1);
+        for (auto& e: edges) {
+            g[e[0]].emplace_back(e[1]); g[e[1]].emplace_back(e[0]);
+        }
+        deque<pair<int, int>> q = {{1,0}};
+        vector dis(n+1, vector<int>(2, INT_MAX)); dis[1][0] = 0;
+        while (dis[n][1] == INT_MAX) {
+            auto [u, d] = q.front(); q.pop_front();
+            for (auto& v: g[u]) {
+                if (dis[v][0] > d+1) {
+                    dis[v][0] = d+1; q.emplace_back(v, dis[v][0]);
+                } else if (dis[v][0] < d+1 && dis[v][1] > d+1) {
+                    // 最初 dis[v][0] < d+1 &&  这个条件遗漏了
+                    // 按照题意次小的路径，必须是严格大于最小的路径
+                    // 因此当第二次到达节点 v 时，必须设置一个大于 dis[v][0] 的值
+                    dis[v][1] = d+1; q.emplace_back(v, dis[v][1]);
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i=0; i<dis[n][1]; i++) {
+            if (ans / change % 2) ans += change-(ans%change);
+            ans += time;
+        }
+        return ans;
+    }    
 };

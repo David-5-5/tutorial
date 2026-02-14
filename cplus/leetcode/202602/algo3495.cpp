594e4d8f8c1c0890959bc3754605723ed17d6f7d
David-5-5 <13910767907@139.com>
2026-02-15 00:07:08 +0800

    Append implementation without inner function

--- cplus/leetcode/202602/algo3495.cpp
+++ cplus/leetcode/202602/algo3495.cpp
@@ -21,5 +21,24 @@ public:
         return ans;    
     }
 
+    long long minOperations(vector<vector<int>>& queries) {
+        // 自行解答 - 不用函数
+        long long ans = 0;
+        for (auto & q: queries) {
+            auto l = q[0], r = q[1];
+            long long mn = (int)log2(l)/2 + 1, mx = (int)log2(r)/2 + 1;
 
+            // long long mn = (int)(log(l)/log(4)) + 1, mx = (int)(log(r)/log(4)) + 1;
+            // 以下错误写法，写将 log(l) -> int 然后再除以 log(4)
+            // long long mn_wrong = (int)(log(l)/log(4)) + 1, mx_wrong = (int)(log(r)/log(4)) + 1;
+            long long n = 0;
+
+            for (int i=mn; i<=mx; i++) {
+                n += 1LL * (min((int)pow(4, i)-1, r) - max((int)pow(4,i-1), l) + 1) * i;
+            }  
+            ans += max((n + 1)/2, mx);
+        }
+
+        return ans;    
+    }    
 };

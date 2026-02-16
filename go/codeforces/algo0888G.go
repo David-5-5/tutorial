ea961892b48f00d84f5dfc2c357c0aa3b5084925
David-5-5 <13910767907@139.com>
2026-02-16 13:25:34 +0800

    Add algo using go accept by codeforces

---
+++ go/codeforces/algo0888G.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+)
+
+type node88 struct{ son [2]*node88 }
+type trie88 struct{ root *node88 }
+
+const trieBitLen88 = 30
+
+func (t *trie88) put(v int32) *node88 {
+	o := t.root
+	for i := trieBitLen88 - 1; i >= 0; i-- {
+		b := v >> i & 1
+		if o.son[b] == nil {
+			o.son[b] = &node88{}
+		}
+		o = o.son[b]
+	}
+	return o
+}
+
+func (t *trie88) minXor(v int32) (ans int) {
+	o := t.root
+	for i := trieBitLen88 - 1; i >= 0; i-- {
+		b := v >> i & 1
+		if o.son[b] == nil {
+			ans |= 1 << i
+			b ^= 1
+		}
+		o = o.son[b]
+	}
+	return
+}
+
+// 补充缺失的min函数
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func CF888G(_r io.Reader, out io.Writer) {
+	in := bufio.NewReader(_r)
+	var n int
+	var ans int64 // 改用int64避免溢出（CF888G的答案可能超过int范围）
+	fmt.Fscan(in, &n)
+	a := make([]int32, n)
+	for i := range a {
+		fmt.Fscan(in, &a[i])
+	}
+
+	var f func([]int32, int)
+	f = func(a []int32, p int) {
+		if a == nil || p < 0 {
+			return
+		}
+		b := [2][]int32{}
+		for _, v := range a {
+			k := v >> p & 1
+			b[k] = append(b[k], v)
+		}
+		if b[0] != nil && b[1] != nil {
+			t := &trie88{&node88{}}
+			for _, v := range b[0] {
+				t.put(v)
+			}
+			res := math.MaxInt
+			for _, v := range b[1] {
+				res = min(res, t.minXor(v))
+			}
+			ans += int64(res) // 类型匹配
+		}
+		f(b[0], p-1)
+		f(b[1], p-1)
+	}
+	f(a, 29)
+	fmt.Fprint(out, ans)
+}
+
+func main() {
+	CF888G(os.Stdin, os.Stdout)
+}

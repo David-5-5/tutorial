de79520814749c07ddb375a119c02c57fa8234c0
David-5-5 <13910767907@139.com>
2026-02-18 15:40:12 +0800

    Add algo using greedy

---
+++ cplus/leetcode/202601/algo3228.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 一、贪心策略 - 1.4 从最左/最右开始贪心
+class Solution {
+public:
+    int maxOperations(string s) {
+        // 自行解答，从左开始贪心
+        int ones = 0, ans = 0, n = s.length();
+        bool shift = false;
+        for (int i = 0; i<n; ++i) {
+            if (s[i] == '1') {
+                if (shift) {ans += ones; shift = false;}
+                ones ++;
+            } else if (ones) shift = true;
+        }
+        if (shift) ans += ones;
+        return ans;  
+    }
+
+ 
+};

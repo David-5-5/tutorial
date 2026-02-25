3fcc51fe5aa76ef79750f7d8f481af494f9d992d
David-5-5 <13910767907@139.com>
2026-02-26 01:11:48 +0800

    Add LCR series

---
+++ cplus/leetcode/202602/algolcr045.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// 同 513
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        // 自行解答
+        vector<TreeNode*> cur;
+        cur.push_back(root); int ans;
+        while (!cur.empty()) {
+            ans = cur[0]->val;
+            vector<TreeNode*> nxt;
+            for (auto node:cur) {
+                if (node->left) nxt.push_back(node->left);
+                if (node->right) nxt.push_back(node->right);
+            }
+            cur = move(nxt);
+        }
+        return ans;
+    }
+};

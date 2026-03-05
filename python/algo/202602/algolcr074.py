525669c757ef861751d1095f5e99d47b045fb2a3
David-5-5 <13910767907@139.com>
2026-03-06 01:25:58 +0800

    Add LCR series

---
+++ python/algo/202602/algolcr074.py
@@ -0,0 +1,18 @@
+from typing import List
+
+# 同 56
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key= lambda p:p[0])
+
+        ans = []
+        left, right = intervals[0]
+        for l, r in intervals[1:]:
+            if l > right:
+                ans.append([left, right])
+                left, right = l, r
+            else:
+                if r > right : right = r
+        ans.append([left, right])
+
+        return ans

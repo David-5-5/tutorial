d817eedc287bea5b775765ee7b9fdb8a890d68bf
David-5-5 <13910767907@139.com>
2026-03-11 00:58:30 +0800

    Add LCR series

---
+++ cplus/leetcode/202603/algo0128.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// LCR series
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        // 参考题解 O(N) 从最小的 x 开始遍历
+        unordered_set<int> n_set(nums.begin(), nums.end());
+
+        int ans = 0; 
+        for (auto x: n_set) {
+            if (!n_set.count(x-1)) {
+                int res = 1;
+                while (n_set.count(++x)) res ++;
+                ans = max(ans, res);
+            }
+        }
+
+        return ans;    
+    }
+};

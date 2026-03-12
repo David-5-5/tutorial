86249d0b17a4c2f8a918404b95a5b2db040d409f
David-5-5 <13910767907@139.com>
2026-03-13 00:09:13 +0800

    Add LCR series

---
+++ cplus/leetcode/202603/algolcr093.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 同 873
+class Solution {
+public:
+    int lenLongestFibSubseq(vector<int>& arr) {
+        // 参考题解
+        int n = arr.size();
+        unordered_map<int, int> indices;
+        vector<vector<int>> dp(n, vector<int>(n));
+        int ans = 0;
+     
+        for (int i=0; i<n; i++) {
+            // 下面的边界条件很关键 arr[j] * 2 > arr[i] 保证 k <
+            for (int j = i - 1; j >= 0 && arr[j] * 2 > arr[i]; j--) {
+                if (indices.contains(arr[i]-arr[j])){
+                    int k = indices[arr[i]-arr[j]];
+                    dp[j][i] = max(dp[k][j] + 1, 3);                    
+                }
+                ans = max(ans, dp[j][i]);
+            }
+            indices[arr[i]] = i;
+        }
+        
+        return ans > 2?ans:0;
+        
+    }
+};

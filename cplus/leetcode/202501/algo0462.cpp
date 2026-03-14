09c9834ccc6c71537b6112c91ab3f05425a98302
David-5-5 <13910767907@139.com>
2026-03-15 02:34:01 +0800

    Append implementation using quick select

--- cplus/leetcode/202501/algo0462.cpp
+++ cplus/leetcode/202501/algo0462.cpp
@@ -16,4 +16,27 @@ public:
         
         return ans;
     }
+
+    int minMoves2_qs(vector<int>& nums) {
+        // 自行解答
+        auto qs = [&] (this auto&& qs, int l, int r, int k) -> int {
+            if (l == r) return nums[k];
+            auto pivot = nums[l]; auto i = l-1, j = r+1; 
+            while (i < j) {
+                do i++; while (nums[i] < pivot);
+                do j--; while (nums[j] > pivot);
+                if (i < j) swap(nums[i], nums[j]);
+            }
+
+            if (k<=j) return qs(l, j, k);
+            else return qs(j+1, r, k);
+        };
+
+        int n = nums.size(); 
+        int median = qs(0, n-1, n/2);
+        int ans = 0; for(auto v:nums) ans += abs(v-median);
+        return ans;
+    }  
+
+
 };

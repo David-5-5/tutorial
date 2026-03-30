220c59e7c2757598d2ebf869512ad027d086d97f
David-5-5 <13910767907@139.com>
2026-03-31 00:27:23 +0800

    Append implementation using 2bit imply a char

--- cplus/leetcode/202603/algo0187.cpp
+++ cplus/leetcode/202603/algo0187.cpp
@@ -17,5 +17,24 @@ public:
         return ans;
     }
 
+    vector<string> findRepeatedDnaSequences(string s) {
+        // 'A' : 0, 'C': 1, 'G': 2 和 'T : 3
+        // each char 2bit 20 bit
+        unordered_map<char, int> bin = {{'A', 0}, {'C', 1}, {'G', 2}, {'T', 3}};
+        const int mask = (1 << 20) - 1;
+        unordered_map<int, int> cnt;
+        vector<string> ans;
+        int cur = 0, L = 10, n = s.length();
+
+        for (int i=0; i<L-1; i++) {
+            cur = (cur << 2) | bin[s[i]];
+        }
+        for (int i=L-1; i<n; ++i) {
+            cur = ((cur << 2) | bin[s[i]]) & mask;
+            cnt[cur] += 1;
+            if (cnt[cur] == 2) ans.emplace_back(s.substr(i-L+1, L));
+        }
+        return ans;
+    }
 
 };

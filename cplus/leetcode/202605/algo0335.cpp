58909da2d287c8fe14fed28652ea8167f3c7a27e
David-5-5 <13910767907@139.com>
2026-05-12 14:30:03 +0800

    Add algo using case analysis

---
+++ cplus/leetcode/202605/algo0335.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 五、思维题 - 5.7 分类讨论
+class Solution {
+public:
+    bool isSelfCrossing(vector<int>& distance) {
+        // 参考题解，分析能力
+        int n = distance.size();
+        
+        for (int i=3; i<n; i++) {
+            if (distance[i-1] <= distance[i-3] && distance[i] >= distance[i-2] )
+                return true;
+            
+            if (i==4 && distance[i-1] == distance[i-3] && distance[i] + distance[i-4] >= distance[i-2])
+                return true;
+            
+            if (i>=5 && distance[i-1] <= distance[i-3] && 
+                distance[i-1] + distance[i-5] >= distance[i-3] && 
+                distance[i] + distance[i-4] >= distance[i-2] && 
+                distance[i-2] >= distance[i-4])
+                return true;
+        }
+
+        return false;    
+    }
+};

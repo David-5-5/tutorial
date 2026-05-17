d30267d438edb8bab46095a95f14dd3ec8e739fd
David-5-5 <13910767907@139.com>
2026-05-18 01:08:52 +0800

    Append implementation using sliding windows

--- cplus/leetcode/202605/algo0239.cpp
+++ cplus/leetcode/202605/algo0239.cpp
@@ -17,5 +17,18 @@ public:
         }
         return ans;    
     }
-  
+
+    vector<int> maxSlidingWindow2(vector<int>& nums, int k) {
+        // 复习 方案二 滑动窗口 
+        deque<int> que;    // 单调递减队列
+        int n = nums.size();  vector<int> ans;
+
+        for (int i=0; i<n; i++) {
+            while (!que.empty() && nums[que.back()]<nums[i]) que.pop_back();
+            que.emplace_back(i);
+            while (que.front() + k <= i) que.pop_front();
+            if (i>=k-1) ans.push_back(nums[que.front()]);
+        }
+        return ans;      
+    }    
 };

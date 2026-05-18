304504f553fca76dba96ac54672558ddd7ffb99e
David-5-5 <13910767907@139.com>
2026-05-18 19:55:07 +0800

    Add algo using enumerate

---
+++ cplus/leetcode/202605/algo1980.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 六、构造题
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        // 暴力 
+        unordered_set<int> num_hash; int n = nums.size();
+        for (auto & s: nums) num_hash.insert(stoi(s, nullptr, 2));
+
+        for (int i=0; i<=n; i++) {
+            // bitset 整数转固定长度二进制字符串
+            if (!num_hash.count(i)) return bitset<16>(i).to_string().substr(16-n);
+        }
+        return "";
+    }
+
+
+};

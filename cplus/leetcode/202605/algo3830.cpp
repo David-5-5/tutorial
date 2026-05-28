b35cbd116708cbd9043185bb4b4b12f89a75cd19
David-5-5 <13910767907@139.com>
2026-05-29 00:27:28 +0800

    Append implementation using state dp

--- cplus/leetcode/202605/algo3830.cpp
+++ cplus/leetcode/202605/algo3830.cpp
@@ -44,7 +44,44 @@ public:
         return ans;     
     }
 
-  
+    int longestAlternating2(vector<int>& nums) {
+        // 状态机DP 记忆化搜索 translate from python
+        int n = nums.size();
+        if (n == 0) return 0;
+
+        // 记忆化数组：i, can_del(0/1), inc(0/1)
+        int memo[100005][2][2];
+        memset(memo, -1, sizeof(memo));
+
+        auto dfs = [&](this auto&& dfs, int i, bool can_del, bool inc) -> int {
+            if (i == 0) return 1;
+
+            int c_del = can_del ? 1 : 0;
+            int c_inc = inc ? 1 : 0;
+            if (memo[i][c_del][c_inc] != -1) {
+                return memo[i][c_del][c_inc];
+            }
+
+            int res = 1;
+            // 不删除前一个
+            if (nums[i-1] != nums[i] && (nums[i-1] < nums[i]) == inc) {
+                res = dfs(i-1, can_del, !inc) + 1;
+            }
+            // 可以删除，且跳过前一个
+            if (can_del && i > 1 && nums[i-2] != nums[i] && (nums[i-2] < nums[i]) == inc) {
+                res = max(res, dfs(i-2, false, !inc) + 1);
+            }
+
+            return memo[i][c_del][c_inc] = res;
+        };
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, max(dfs(i, true, true), dfs(i, true, false)));
+        }
+
+        return ans;
+    }    
 
 };
 

0ac275bc7048512e01af7359c05591aa627d525d
David-5-5 <13910767907@139.com>
2026-05-30 03:17:01 +0800

    Append implementation with overtime

--- cplus/leetcode/202605/algo3130.cpp
+++ cplus/leetcode/202605/algo3130.cpp
@@ -26,6 +26,27 @@ public:
         return (dfs(zero, one, 0) + dfs(zero, one, 1)) % mod;
     }   
 
+    int numberOfStableArrays2(int zero, int one, int limit) {
+        const int mod = 1e9 + 7;
+        // 超时 200万记录不适合 unordered_map 作为记忆化搜索的数据结构
+        unordered_map<int, long long> memo;     
+
+        auto dfs = [&](this auto&& dfs, int i, int j, int k) -> long long {
+
+            if (i < 0 || j < 0) return 0;
+            // if (i == 0 && j == 0) return 1;
+            if (i == 0) return k == 1 && j <= limit;
+            if (j == 0) return k == 0 && i <= limit;
+
+            int mask = (k << 21) | (i << 11) | j;
+            if (memo.count(mask)) return memo[mask];
+
+            if (k == 0) return memo[mask] = (mod + dfs(i-1, j, 0) + dfs(i-1, j, 1) - dfs(i-limit-1, j, 1)) % mod;
+            else return memo[mask] = (mod + dfs(i, j-1, 0) + dfs(i, j-1, 1) - dfs(i, j-limit-1, 0)) % mod;
+        };
+
+        return (dfs(zero, one, 0) + dfs(zero, one, 1)) % mod;
+    }
 
  
 };

092f9627f019e83c6e7c7047b74137d93f3e0622
David-5-5 <13910767907@139.com>
2026-06-05 19:21:16 +0800

    greedy: min swaps with const reference minimax

---
+++ cplus/leetcode/202606/algo3587.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 贪心 八、其他
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        vector<int> odd, even;
+        int n = nums.size();
+
+        for (int i=0; i<n; i++) {
+            if (nums[i] % 2) odd.push_back(i);
+            else even.push_back(i);
+        }
+
+        if (abs((int)(odd.size() - even.size())) > 1) return -1;
+
+        auto arrange = [&](const vector<vector<int>>& arrs) -> int {
+            long long  res = 0;
+            for (int i=0; i<n; i++) {
+                int j = i % 2, k = i / 2;
+                res += abs(arrs[j][k] - i);
+            }
+            return res / 2;
+        };
+
+        if (odd.size() == even.size()) return min(arrange({odd, even}), arrange({even, odd}));
+        else if (odd.size() > even.size()) return arrange({odd, even});
+        else return arrange({even, odd});        
+    } 
+
+
+};

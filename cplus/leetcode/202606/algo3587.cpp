7ecea7a0ab2226fde6b51a722248e432f73528f6
David-5-5 <13910767907@139.com>
2026-06-05 19:22:02 +0800

    greedy: add optimized 0-copy version minSwaps2 for parity swap

--- cplus/leetcode/202606/algo3587.cpp
+++ cplus/leetcode/202606/algo3587.cpp
@@ -29,5 +29,33 @@ public:
         else return arrange({even, odd});        
     } 
 
+    int minSwaps2(vector<int>& nums) {
+        vector<int> odd, even;
+        int n = nums.size();
 
+        for (int i=0; i<n; i++) {
+            if (nums[i] % 2) odd.push_back(i);
+            else even.push_back(i);
+        }
+
+        if (abs((int)(odd.size() - even.size())) > 1) return -1;
+
+        auto arrange = [&](vector<int>& arr0, vector<int>& arr1) -> int {
+            long long  res = 0;
+            for (int i=0; i<n; i++) {
+                int j = i % 2, k = i / 2;
+                if (j == 0)
+                    res += abs(arr0[k] - i);
+                else res += abs(arr1[k] - i);
+            }
+            return res / 2;
+        };
+
+        if (odd.size() == even.size()) return min(arrange(odd, even), arrange(even, odd));
+        else if (odd.size() > even.size()) return arrange(odd, even);
+        else return arrange(even, odd);   
+
+    }
+
+  
 };

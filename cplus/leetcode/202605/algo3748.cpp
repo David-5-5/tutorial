a01fba14057e42560a7a846f0c41b0fb1e09f015
David-5-5 <13910767907@139.com>
2026-06-05 10:58:20 +0800

    fix: use non-decreasing segment + prefix sum to solve stable subarrays

--- cplus/leetcode/202605/algo3748.cpp
+++ cplus/leetcode/202605/algo3748.cpp
@@ -1,23 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// 随机
 class Solution {
 public:
     vector<long long> countStableSubarrays(vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size(), l = 0;
-        vector<long long> pres(n+1);
-        
-        for (int r=0; r<n; r++) {
-            while (l<r && nums[l]>nums[l+1]) l++;
-            pres[r+1] = pres[r] + r - l + 1;
+        int n = nums.size(), l=0, r=0; nums.emplace_back(0); // sentinel value
+        vector<int> l_idx; vector<long long> pres(1, 0);
+        while (r<n) {
+            if (nums[r] <= nums[r+1]) r ++;
+            else {
+                l_idx.push_back(l);
+                pres.emplace_back(pres.back() + 1LL * (r - l + 1) * (r-l+2) / 2);
+                l = ++r;
+            }
         }
 
         vector<long long> ans;
         for (auto q:queries) {
             auto l = q[0], r = q[1];
-            ans.push_back(pres[r+1] - pres[l]);
+            auto it_l = ranges::upper_bound(l_idx, l);
+            auto it_r = ranges::upper_bound(l_idx, r);
+            if (it_l == it_r) ans.emplace_back(1LL * (r - l + 1) * (r-l+2) / 2);
+            else {
+                long long res = 0; auto l_r = *it_l; auto r_l = *prev(it_r);
+                res += 1LL * (l_r - l + 1) * (l_r - l) / 2;
+                res += 1LL * (r - r_l + 1) * (r - r_l + 2) / 2;
+                res += pres[prev(it_r)-l_idx.begin()] - pres[it_l - l_idx.begin()];
+                ans.emplace_back(res);
+            }
         }
-
         return ans;
-    }
+    }    
 };

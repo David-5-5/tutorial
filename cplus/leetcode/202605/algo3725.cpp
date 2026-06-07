b801710ca50cf26beec69e80e8f395eab6159c54
David-5-5 <13910767907@139.com>
2026-06-08 01:11:08 +0800

    matrix coprime count dp array optimization

--- cplus/leetcode/202605/algo3725.cpp
+++ cplus/leetcode/202605/algo3725.cpp
@@ -23,5 +23,24 @@ public:
         return cnt[1];
     }
 
- 
+    int countCoprime2(vector<vector<int>>& mat) {
+        // 自行解答 暴力 gcd 的数量指数下降
+        const int mod = 1e9 + 7;
+        vector<int> cnt(151, 0);
+        int n = mat.size(), m = mat[0].size();
+        for (int j=0; j<m; j++) cnt[mat[0][j]] ++;
+        
+        for (int i=1; i<n; i++) {
+            vector<int> nxt(151, 0);
+
+            for (int g=1; g<=150; g++) {
+                if (cnt[g] == 0) continue;
+                for (int j=0; j<m; j++) 
+                        nxt[gcd(g, mat[i][j])] = (nxt[gcd(g, mat[i][j])] + cnt[g]) % mod;
+            }
+
+            cnt = move(nxt);
+        }
+        return cnt[1];
+    }    
 };

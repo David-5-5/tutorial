f6f26ea48c81335b6dab3669ff37543c6079d567
David-5-5 <13910767907@139.com>
2026-06-13 13:39:44 +0800

    solve: kth magic number with dynamic programming three pointers

--- python/algo/202606/algoiv17.09.py
+++ python/algo/202606/algoiv17.09.py
@@ -19,3 +19,15 @@ class Solution:
 
         return heapq.heappop(h)
     
+    def getKthMagicNumber(self, k: int) -> int:
+        # 动态规划
+        dp = [1] * (k+1)
+        p3 = p5 = p7 = 1
+        for i in range(2, k+1):
+            dp[i] = min(dp[p3]*3, dp[p5]*5, dp[p7]*7)
+            
+            if dp[i] == dp[p3] * 3: p3 += 1
+            if dp[i] == dp[p5] * 5: p5 += 1
+            if dp[i] == dp[p7] * 7: p7 += 1
+        return dp[k]
+

02c63f40814dbb327a1373fe0c2f0737bcb3519a
David-5-5 <13910767907@139.com>
2026-06-17 01:43:01 +0800

    refactor: optimize minOperations with precomputed char codes and optimized cost calculate

--- python/algo/202606/algo3579.py
+++ python/algo/202606/algo3579.py
@@ -32,3 +32,35 @@ class Solution:
 
         return dfs(0, 1)
 
+    def minOperations2(self, word1: str, word2: str) -> int:
+        n = len(word1)
+        # 预处理字符编码，避免循环重复计算
+        c1 = [ord(c) - ord('a') for c in word1]
+        c2 = [ord(c) - ord('a') for c in word2]
+
+        def cal(s,e,rev):
+            arr, res, L = [0]*676, 0, e-s
+            for i in range(L):
+                b=c2[s+i]
+                a=c1[s+i] if not rev else c1[e-1-i]
+                if a==b: continue
+                res += 1
+                kr, ka = b * 26 + a, a * 26 + b
+                if arr[kr]: arr[kr] -= 1; res -= 1
+                else: arr[ka] += 1
+            return res
+                
+        @cache
+        def count(start: int, end: int) -> int:
+            normal = cal(start, end, rev=False)
+            reverse_cost = 1 + cal(start, end, rev=True)
+            return min(normal, reverse_cost)
+
+        @cache
+        def dfs(i: int, j: int):
+            if j == n:
+                return count(i, j)
+            # 两种选择：不分割，继续右扩；或者在j分割
+            return min(dfs(i, j+1), count(i, j) + dfs(j, j+1))
+
+        return dfs(0, 1)

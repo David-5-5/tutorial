8b4494f821d5f87672d5411978ae2b8c77aa6c25
David-5-5 <13910767907@139.com>
2026-06-18 16:43:27 +0800

    Append comments

--- cplus/leetcode/202509/algo1888.cpp
+++ cplus/leetcode/202509/algo1888.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class Solution {
 public:
     int minFlips(string s) {
-        // 参考题解， 并未实现，打心里特别厌恶这样的题目，没有意义，为啥操作1不计数，操作2要计数
+        // 参考题解
         int n = s.size(), ans = n, cnt = 0;
         for (int i = 0; i < n * 2 - 1; i++) {
             cnt += (s[i % n] ^ i) & 1;

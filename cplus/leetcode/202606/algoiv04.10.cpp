4801081bf90749517e4f34b9d10562dc5e6ea50c
David-5-5 <13910767907@139.com>
2026-06-24 16:01:30 +0800

    recursively traverse t1, check if any subtree fully matches t2

---
+++ cplus/leetcode/202606/algoiv04.10.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// 同 572
+class Solution {
+public:
+    bool checkSubTree(TreeNode* t1, TreeNode* t2) {
+
+        auto isSub = [&](this auto&& isSub, TreeNode* node, TreeNode* subNode) -> bool {
+            if (node->val != subNode->val) return false;
+            if ((node->left && !subNode->left) || (!node->left && subNode->left) ||
+                (node->right && !subNode->right) || (!node->right && subNode->right))
+                return false; 
+            if (node->left && subNode->left && !isSub(node->left, subNode->left)) return false;
+            if (node->right && subNode->right && !isSub(node->right, subNode->right)) return false;
+            
+            return true;
+        };
+
+        auto dfs = [&](this auto&& dfs, TreeNode* node) -> bool {
+            if (isSub(node, t2)) return true;
+            if (node->left && dfs(node->left)) return true;
+            if (node->right && dfs(node->right)) return true;
+
+            return false;
+        };
+        return dfs(t1);
+    }
+
+
+};

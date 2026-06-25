a14effb9bb70ef0fd887508db28fbadaa649ef86
David-5-5 <13910767907@139.com>
2026-06-25 20:04:21 +0800

    optimize same-bit-count neighbor numbers calculation with bit mask formula simplification

--- python/algo/202606/algoiv05.04.py
+++ python/algo/202606/algoiv05.04.py
@@ -25,3 +25,30 @@ class Solution:
             elif num >> i & 1 == 1: low += 1 << i
         return [nxt, prev]
 
+
+    def findClosedNumbers2(self, num: int) -> List[int]:
+        # 优化
+        nxt = prev = -1
+
+        if num == (1 << 31) - 1: return [-1, -1] # 上限
+
+        for i in range(32):
+            if (num >> i) & 1 == 1 and (num >> (i + 1)) & 1 == 0:
+                # 将 i 位右边的连续 1 右移到末尾
+                # 形如：11.0.. 01 1..100000 -> 11.0.. 10 000001..1
+                #              i                      i
+                suf = num & ((1 << i) - 1)
+                nxt = num + (1 << i) + (suf >> (i-suf.bit_count())) - suf
+                break
+
+        for i in range(32):
+            if num >> i+1 & 1 == 1 and num >> i & 1 == 0:
+                # 将 i 位右边的连续 1 左移移到i
+                # 形如：11.0.. 10 0..01..1 -> 11.0.. 01 1..10..0
+                #              i                     i
+                suf = num & ((1 << i) - 1)
+                prev = num - (1 << i)  + (suf << (i-suf.bit_count())) - suf
+                break
+
+        return [nxt, prev]
+

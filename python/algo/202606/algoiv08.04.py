931175637007a264ecc8a4f703f7677fbd666e52
David-5-5 <13910767907@139.com>
2026-06-26 17:42:26 +0800

    bitmask enumeration to generate all subsets of integer array

---
+++ python/algo/202606/algoiv08.04.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+
+        ans = []
+        for i in range(1<<n):
+            sub = []
+            for j in range(n):
+                if i >> j & 1: sub.append(nums[j])
+            ans.append(sub)
+
+        return ans

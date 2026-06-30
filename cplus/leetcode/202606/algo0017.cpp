2ebeda6c08de0d179a820bdb85ac19478a675c83
David-5-5 <13910767907@139.com>
2026-06-30 11:23:03 +0800

    backtracking letter combinations with push/pop path rollback

--- cplus/leetcode/202606/algo0017.cpp
+++ cplus/leetcode/202606/algo0017.cpp
@@ -22,4 +22,23 @@ public:
         return ans;
     }
 
+    vector<string> letterCombinations2(string digits) {
+        unordered_map<char, string> d2l{ {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, 
+             {'5', "jkl"}, {'6', "mno"},  {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
+
+        vector<string> ans;
+        auto dfs = [&](this auto&& dfs, int i, string& path) -> void {
+            if (i == digits.length()) ans.emplace_back(path);
+            
+            for (auto ch : d2l[digits[i]]) {
+                path.push_back(ch);
+                dfs(i+1, path);
+                path.pop_back(); // 恢复现场
+            }
+        };
+
+        string path = "";
+        dfs(0, path);
+        return ans;
+    }    
 };

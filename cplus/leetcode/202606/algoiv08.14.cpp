54f96bfcb6608f5b0fcb42c615121fb91c45b2a3
David-5-5 <13910767907@139.com>
2026-07-15 00:15:39 +0800

    optimized memo dfs store zero/one count pair for boolean expression evaluation

--- cplus/leetcode/202606/algoiv08.14.cpp
+++ cplus/leetcode/202606/algoiv08.14.cpp
@@ -37,5 +37,37 @@ public:
         return dfs(s, result);
     }
 
+    int countEval(string s, int result) {
+        // 根据大模型提示进行优化，性能显著提升
+        unordered_map<string, pair<int, int>> memo;
 
+        auto dfs = [&](this auto&& dfs, string expr) -> pair<int, int>{
+            int n = expr.length();
+
+            if (n == 1) return {stoi(expr) == 0, stoi(expr) == 1};
+
+            if (memo.count(expr)) return memo[expr];
+
+            int zero = 0, one = 0;
+            for (int i=1; i<n; i+=2) {
+                auto [left_0, left_1]  = dfs(expr.substr(0, i));
+                auto [right_0, right_1] = dfs(expr.substr(i+1));
+
+                if (expr[i] == '^') {
+                    one += left_0 * right_1 + left_1 * right_0;
+                    zero += left_0 * right_0 + left_1 * right_1;
+                } else if (expr[i] == '&') {
+                    one += left_1 * right_1;
+                    zero += left_0 * right_0 + left_0 * right_1 + left_1 * right_0;
+                } else {
+                    one += left_0 * right_1 + left_1 * right_0 + left_1 * right_1 ;
+                    zero += left_0 * right_0;
+                }
+            }
+
+            return memo[expr] = {zero, one};
+        };
+        auto [zero, one] = dfs(s);
+        return result ? one : zero;
+    }    
 };

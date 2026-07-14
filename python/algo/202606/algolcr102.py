19f814040e2dea4bec8f60400775a330137bc75e
David-5-5 <13910767907@139.com>
2026-07-14 09:58:40 +0800

    convert target sum to 01 knapsack subset sum dp

---
+++ python/algo/202606/algolcr102.py
@@ -0,0 +1,17 @@
+from typing import List
+
+# 同 494
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        s = sum(nums)
+        t = (s + target) // 2
+        if (s+target) % 2 == 1:return 0
+        if t < 0 or target > s: return 0
+
+        dp = [0] * (t+1)
+        dp[0] = 1
+        for num in nums:
+            for cap in range(t, num-1,-1):
+                dp[cap] += dp[cap-num]
+        
+        return dp[-1]

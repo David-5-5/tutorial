e8237b0a3e6ad00e4104a72a950a6aeac92c4947
David-5-5 <13910767907@139.com>
2026-07-20 14:09:33 +0800

    optimize zero square search with left/up boundary DP

--- python/algo/202607/algoiv17.23.py
+++ python/algo/202607/algoiv17.23.py
@@ -29,6 +29,27 @@ class Solution:
         return [] if ans[2] == 0 else ans
 
 
+    def findSquare2(self, matrix: List[List[int]]) -> List[int]:
+        # 参考官方题解 left, up，节省大量的无效计算，性能
+        n = len(matrix)
+
+        left = [[0] * (n+1) for _ in range(n+1)]
+        up = [[0] * (n+1) for _ in range(n+1)]
+
+        ans = [-1, -1, 0]
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                if matrix[i-1][j-1] == 0:
+                    left[i][j] = left[i][j-1] + 1
+                    up[i][j] = up[i-1][j] + 1
+                    size = min(left[i][j], up[i][j])
+                    while up[i][j-size+1] < size or left[i-size+1][j] < size:
+                        size -= 1
+                    if size > ans[2]:
+                        ans = [i-size, j-size, size]
+
+        return [] if ans[2] == 0 else ans
+
 if __name__ == "__main__":
     matrix = [[1,0,1],[0,0,1],[0,0,1]]
     print(Solution().findSquare2(matrix))

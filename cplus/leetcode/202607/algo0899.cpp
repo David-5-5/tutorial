03ae3dec7ec759b20b77ff223ec66a5b3f328c84
David-5-5 <13910767907@139.com>
2026-07-22 20:29:50 +0800

    implement O(n) minimal string representation algorithm

--- cplus/leetcode/202607/algo0899.cpp
+++ cplus/leetcode/202607/algo0899.cpp
@@ -44,4 +44,23 @@ public:
         return s.substr(i) + s.substr(0, i);
     }
 
+    string orderlyQueue3(string s, int k) {
+        // 进阶
+        if (k > 1) {
+            sort(s.begin(), s.end()); return s;
+        }
+
+        // oi-wiki 字符串 循环同构最小字典序  最小表示法 O(N)
+        int l = 0, i = 0, j = 1, n = s.length();
+        while (l < n && i < n && j < n) {
+            if (s[(i+l) % n] == s[(j+l) % n]) l++;
+            else {
+                s[(i+l) % n] > s[(j+l) % n]?i=i+l+1:j=j+l+1;
+                if (i == j) i++;
+                l = 0;
+            }
+        }
+        i = min(i, j);
+        return s.substr(i) + s.substr(0, i);
+    }    
 };

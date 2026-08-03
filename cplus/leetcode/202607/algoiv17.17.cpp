664ab4630fe45007c6e44f3968eeff2fc385a9ad
David-5-5 <13910767907@139.com>
2026-08-03 11:05:09 +0800

    append implementation using AC automaton version

--- cplus/leetcode/202607/algoiv17.17.cpp
+++ cplus/leetcode/202607/algoiv17.17.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// AC 自动机
+struct Node {
+    Node* son[26]{};
+    Node* fail;
+    bool end = false;
+    int len = 0, idx = -1;
+};
+
 class Solution {
+private:
+    Node * root;
 public:
     vector<vector<int>> multiSearch(string big, vector<string>& smalls) {
         auto kmp = [](string str, string pat) -> vector<int> {
@@ -36,4 +46,53 @@ public:
         }
         return ans;
     }
+
+    vector<vector<int>> multiSearch2(string big, vector<string>& smalls) {
+        root = new Node(); int n = smalls.size();
+        for (int i=0; i<n; i++) {
+            auto & word = smalls[i];
+            auto node = root;
+            for (auto c : word) {
+                if (node->son[c-'a'] == nullptr) node->son[c-'a'] = new Node();
+                node = node->son[c-'a'];
+            }
+            node->end = true, node->len = word.length(), node->idx = i;
+        } 
+        queue<Node*> q;
+        for (int i=0; i<26; i++) {
+            if (root->son[i] != nullptr) {
+                root->son[i]->fail = root; q.emplace(root->son[i]);
+            }  else root->son[i] = root;
+        }
+
+        while (!q.empty()) {
+            Node* node = q.front(); q.pop();
+            // node->end = node->end || node->fail->end;    // 和 1032 差异，一点点体会
+            for (int i=0; i<26; i++) {
+                if (node->son[i] != nullptr) {
+                    node->son[i]->fail = node->fail->son[i];
+                    q.emplace(node->son[i]);
+                } else node->son[i] = node->fail->son[i];
+            }
+        }
+        auto cur = root; vector<vector<int>> ans(n);
+
+        for (int i=0; i<big.length(); i++) {
+            auto & c = big[i];
+            cur = cur->son[c - 'a'];
+            if (cur == nullptr) break;
+            auto fail = cur;
+            while (fail != nullptr && fail != root) {
+                if (fail->end) ans[fail->idx].emplace_back(i+1-fail->len);
+                fail = fail->fail;
+            }
+        }
+
+        return ans;
+    }    
 };
+
+int main() {
+    string big = "mississippi"; vector<string> smalls = {"is","ppi","hi","sis","i","ssippi"};
+    Solution().multiSearch2(big, smalls);
+}

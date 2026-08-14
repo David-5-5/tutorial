6626594595b56ce0cc3176312bd4c504ee6922f3
David-5-5 <13910767907@139.com>
2026-08-14 16:13:30 +0800

    perf: optimize champagne tower dp to O(n) rolling‑array space

--- python/algo/202608/algo0799.py
+++ python/algo/202608/algo0799.py
@@ -9,3 +9,16 @@ class Solution:
 
         return min(flow[query_row][query_glass], 1)
 
+    def champagneTower2(self, poured: int, query_row: int, query_glass: int) -> float:
+        # 空间优化
+        flow = [0] * (query_row + 1)
+        flow[0] = poured
+        for i in range(query_row):
+            for j in range(i, -1, -1):
+                x = flow[j] - 1
+                if x > 0:
+                    flow[j+1] += x / 2
+                    flow[j] = x / 2
+                else: flow[j] = 0.0
+
+        return min(flow[query_glass], 1)    

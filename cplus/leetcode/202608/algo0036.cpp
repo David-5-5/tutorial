e17da177f539021f9977c098da945080ff690465
David-5-5 <13910767907@139.com>
2026-08-20 00:22:23 +0800

    `feat: validate sudoku with bitmask and dual nested loop`

--- cplus/leetcode/202608/algo0036.cpp
+++ cplus/leetcode/202608/algo0036.cpp
@@ -41,4 +41,27 @@ public:
         return true;     
     }
 
+    bool isValidSudoku2(vector<vector<char>>& board) {
+        // 精简代码
+        for (int i=0; i<9; i++) {   // 按行
+            int rows = 0, cols = 0, block = 0;
+            for (int j=0; j<9; j++) {
+                auto & x = board[i][j], & y = board[j][i], & z = board[3*(i/3)+j/3][3*(i%3)+j%3];
+                if (x != '.') {
+                    if ((rows & 1<< (x - '0')) == 0) rows |= 1<< (x - '0');
+                    else return false;          
+                }
+                if (y != '.') {
+                    if ((cols & 1<< (y - '0')) == 0) cols |=  1<< (y - '0');
+                    else return false;
+                }   
+                if (z != '.') {
+                    if ((block & 1<< (z - '0')) == 0) block |= 1<< (z - '0');
+                    else return false;
+                }
+            }
+        }
+
+        return true;     
+    }    
 };

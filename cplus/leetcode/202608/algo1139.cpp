287fc537f794b0648c266bd784541e403a29cf64
David-5-5 <13910767907@139.com>
2026-09-05 00:45:15 +0800

    refactor: use  prefix arrays for  boundary check

--- cplus/leetcode/202608/algo1139.cpp
+++ cplus/leetcode/202608/algo1139.cpp
@@ -30,4 +30,27 @@ public:
         return ans * ans;
     }
 
+    int largest1BorderedSquare(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+
+        vector pre_r(m+1, vector<int>(n+1, 0)), pre_c(m+1, vector<int>(n+1, 0));
+        for (int i=0; i<m; i++) for (int j=0; j<n; j++) {
+            if (grid[i][j]) {
+                pre_r[i+1][j+1] = pre_r[i+1][j] + 1;
+                pre_c[i+1][j+1] = pre_c[i][j+1] + 1;
+            }
+        }
+
+        int ans = 0;
+        for (int i=0; i<m; i++) for (int j=0; j<n; j++) {
+            int side = min(pre_r[i+1][j+1], pre_c[i+1][j+1]);
+            while (side > ans) {
+                if (pre_r[i-side+2][j+1] >= side && pre_c[i+1][j-side+2]>=side) {
+                    ans = side; break;
+                }
+                side --;
+            }
+        }            
+        return ans * ans;
+    }    
 };

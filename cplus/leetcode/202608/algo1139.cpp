dfd7e62a94954e1deccd4feae141b9097c23778b
David-5-5 <13910767907@139.com>
2026-09-05 00:44:29 +0800

    row and column prefix consecutive count preprocessing

---
+++ cplus/leetcode/202608/algo1139.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int largest1BorderedSquare(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+
+        vector pre_r(m, vector<int>(n, 0)), pre_c(m, vector<int>(n, 0));
+        for (int i=0; i<m; i++) pre_r[i][0] = grid[i][0];
+        for (int i=0; i<n; i++) pre_c[0][i] = grid[0][i];
+
+        for (int i=0; i<m; i++) for (int j=0; j<n; j++) {
+            if (grid[i][j]) {
+                if (j) pre_r[i][j] = pre_r[i][j-1] + 1;
+                if (i) pre_c[i][j] = pre_c[i-1][j] + 1;
+            }
+        }
+
+        int ans = 0;
+        for (int i=0; i<m; i++) for (int j=0; j<n; j++) {
+            int side = min(pre_r[i][j], pre_c[i][j]);
+            while (side > ans) {
+                if (pre_r[i-side+1][j] >= side && pre_c[i][j-side+1]>=side) {
+                    ans = side; break;
+                }
+                side --;
+            }
+        }            
+        return ans * ans;
+    }
+
+};

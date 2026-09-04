e918cb68ac9ce7c06eb54e430e25173a0c562e5d
David-5-5 <13910767907@139.com>
2026-09-04 15:48:07 +0800

    implementation for longest balanced subarray with distinct elements

--- python/algo/202608/algo3719.py
+++ python/algo/202608/algo3719.py
@@ -18,23 +18,3 @@ class Solution:
 
         return ans
 
-    def longestBalanced(self, nums: List[int]) -> int:
-        # 
-        idxs = [{}, {}]
-        vis, ans = set(), 0
-        idxs[0][0], idxs[1][0] = -1, -1
-
-        for i, v in enumerate(nums):
-            is_odd = v % 2
-            if v not in vis:
-                vis.add(v)
-                idxs[is_odd][len(idxs[is_odd])] = i
-            
-            cur_l, opp_l = len(idxs[is_odd]), len(idxs[1^is_odd])
-
-            if cur_l >= opp_l:
-                ans = max(ans, i - idxs[is_odd][cur_l-opp_l])
-            else:
-                ans = max(ans, i-idxs[1^is_odd][opp_l-cur_l])
-        
-        return ans

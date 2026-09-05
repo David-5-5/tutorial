9a02d610eda55e00726c19fd97221674262246c1
David-5-5 <13910767907@139.com>
2026-09-05 13:14:20 +0800

    refactor: replace multiset with ordered map for sliding‑window min‑max subarray counting

--- cplus/leetcode/202609/algo3835.cpp
+++ cplus/leetcode/202609/algo3835.cpp
@@ -18,4 +18,22 @@ public:
         return ans;     
     }
 
+    long long countSubarrays(vector<int>& nums, long long k) {
+        // 性能比较 map 653
+        long long ans = 0; int l = 0, n = nums.size();
+        map<int, int> sorted;
+        for (int r=0; r<n; r++) {
+            sorted[nums[r]] ++;
+            while (!sorted.empty() && 1LL * (r-l+1) * (sorted.rbegin()->first-sorted.begin()->first) > k) {
+                sorted[nums[l]] --; 
+                if (sorted[nums[l]] == 0) sorted.erase(nums[l]);
+                l++;
+            }
+            ans += r - l + 1;
+        }
+
+        return ans;   
+    }
+
+
 };

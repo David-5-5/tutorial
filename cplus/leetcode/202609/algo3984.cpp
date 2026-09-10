59e75aa29e422713f7364e57b0e1192305147aa3
David-5-5 <13910767907@139.com>
2026-09-11 00:59:10 +0800

    feat: enumerate divisors and scan subarray for divisible game

---
+++ cplus/leetcode/202609/algo3984.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int divisibleGame(vector<int>& nums) {
+        // using set
+        const int mod = 1e9 + 7; set<int> divs;
+        for (auto & v: nums) {
+            if (v == 1) continue;
+            divs.insert(v); int mx = sqrt(v);
+            for (int i=2; i<=mx; i++) {
+                if (v % i == 0) {
+                    divs.insert(i); divs.insert(v / i);
+                }
+            }
+        }
+
+        if (divs.empty()) return mod - 2;
+
+        int ans = 0; long long global_mx = 0;
+        for (auto it=divs.begin(); it!=divs.end(); it++) {
+            auto k = *it; long long local_mx = 0, cur = 0;
+            for (auto & v: nums) {
+                if (cur < 0) cur = 0;
+                if (v % k) cur -= v;
+                else cur += v;
+                local_mx = max(local_mx, cur);
+            }
+            if (local_mx > global_mx) {
+                global_mx = local_mx; ans = local_mx * k % mod;
+            }
+        }
+
+        return ans;        
+    }
+
+
+};

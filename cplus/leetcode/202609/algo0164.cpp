15565b1311db3b3bfec3586dda408f8e8cf08152
David-5-5 <13910767907@139.com>
2026-09-19 12:43:56 +0800

    feat: radix‑sort followed by linear scan for maximum gap

--- cplus/leetcode/202609/algo0164.cpp
+++ cplus/leetcode/202609/algo0164.cpp
@@ -14,5 +14,35 @@ public:
         return ans;        
     }
 
+    int maximumGap2(vector<int>& nums) {
+        // 基数排序
+        int n = nums.size(); long long exp = 1; if (n < 2) return 0;
+        vector<int> buf(n) ;
+
+        // auto mx = ranges::max(nums);
+        auto mx = *max_element(nums.begin(), nums.end());
+        
+        while (mx >= exp) {
+            int cnt[10] = {0};
+            for (auto &v : nums) {
+                cnt[v / exp % 10] ++;
+            }
+            for (int i=1; i<10; i++) cnt[i] += cnt[i-1];
+            
+            for (int i=n-1; i>=0; i--) {
+                auto & v = nums[i];
+                buf[--cnt[v/exp % 10]] = v;
+            }
+            copy(buf.begin(), buf.end(), nums.begin());
+            exp *= 10;
+        }
+        
+        int ans = 0;
+        for (int i=1; i<n; i++) {
+            ans = max(ans, nums[i] - nums[i-1]);
+        }
+        return ans;
+    }
+
 
 };
